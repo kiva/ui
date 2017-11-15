cfc8d13ab9bf876930fc322595eb2ebb13d173ce
Matt Stover <mcs@mcstover.com>
2017-11-15 15:24:03 -0800

    Updated serverRoutes middleware to write routes to file system as json on server start and then serve that file instead of generating the json every time.

--- server/serverRoutes.js
+++ server/serverRoutes.js
@@ -5,9 +5,15 @@
 const fs = require('fs');
 const path = require('path');
 
+const vuejsRouteFile = '../src/router/routes.js';
+const extractedRouteFileName = 'available-routes.json';
+
+/*
+ * Extract Routes as formatted for Vue Router, removing async function calls and converting them into a valid json object.
+ */
 function buildRouteJSON () {
 	// Pull in Route Declarations as a string so import statements aren't attempted
-	const routesAsString = fs.readFileSync(path.resolve(__dirname, '../src/router/routes.js'), 'utf-8');
+	const routesAsString = fs.readFileSync(path.resolve(__dirname, vuejsRouteFile), 'utf-8');
 	// Clean out the arrow functions and imports used for code-splitting routes in vue
 	const cleanedRoutes = routesAsString.replace('module.exports =', '').replace(/[()@;=>]/g, '').replace(/import/g, '');
 	// re-establish a js array from our cleaned string
@@ -25,7 +31,37 @@ function buildRouteJSON () {
 	return routes;
 };
 
+// Attempt to write json file to filesystem
+try {
+	let availableRoutesJSON = buildRouteJSON();
+
+	fs.writeFileSync(path.resolve(__dirname, extractedRouteFileName), JSON.stringify(availableRoutesJSON), (err) => {
+		if (err) {
+			console.log(err);
+		}
+		// TODO: Convert to Log Statement
+		console.log('Routes written to ' + extractedRouteFileName);
+	});
+
+} catch (err) {
+	// TODO: Convert to Log Statement
+	console.log('Failed to create and write routes json file.');
+	console.log(err);
+}
+
 module.exports = function(req, res, next) {
-	console.log('server routes middle ware');
-	res.json(buildRouteJSON());
+	if (fs.existsSync(path.resolve(__dirname, extractedRouteFileName))) {
+		res.sendFile(path.resolve(__dirname, extractedRouteFileName), function (err) {
+			if (err) {
+				console.log(err);
+			} else {
+				// TODO: Convert to Log Statement
+				console.log('Sent generated file (' + extractedRouteFileName + ') from the file system.');
+			}
+		});
+	} else {
+		res.json(buildRouteJSON());
+		// TODO: Convert to Log Statement
+		console.log('Available routes json served directly from buildRouteJSON method.')
+	}
 }
