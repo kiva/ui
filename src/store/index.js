9c204ec57d141b42ae0d31e25c84ab9e9c3dbe93
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2017-11-16 18:34:38 -0800

    small improvements

--- src/store/index.js
+++ src/store/index.js
@@ -10,9 +10,6 @@ export default function createStore() {
 	const apolloClient = createApolloClient();
 
 	return new Vuex.Store({
-		state: {
-			env: process && process.browser ? process.browser : 'node',
-		},
 		modules: {
 			my: createMyModule(apolloClient),
 		},
