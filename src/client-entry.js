21936422cdaf3ad85cfca5340a7fefcae784f85d
Joshua Kiwiet-Pantaleoni <emuvente@gmail.com>
2017-11-29 11:06:50 -0800

    Revert "pass csrf cookie to apollo config & fix client async-data promise chain"
    This reverts commit 6c460257f47995bf84eac92cb6de41e5327d1b64.

--- src/client-entry.js
+++ src/client-entry.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import _dropWhile from 'lodash/dropWhile';
 import _values from 'lodash/values';
-import cookie from 'js-cookie';
 import createApp from '@/main';
 
 const config = window.__KV_CONFIG__ || {};
@@ -13,8 +12,7 @@ const {
 	apolloClient,
 } = createApp({
 	apollo: {
-		uri: config.graphqlUri,
-		csrfToken: cookie.get('kvis') && cookie.get('kvis').substr(6),
+		uri: config.graphqlUri
 	}
 });
 
@@ -42,7 +40,7 @@ router.onReady(() => {
 		function callAsyncData({ asyncData, components }) {
 			return Promise.all([
 				asyncData && asyncData({ store, route: to }),
-				components && Promise.all(_values(components).map(callAsyncData))
+				components && _values(components).map(callAsyncData)
 			]);
 		}
 		Promise.all(activated.map(callAsyncData)).then(next).catch(next);
