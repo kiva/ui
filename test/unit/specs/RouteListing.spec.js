64a7ad3f0ef5868bcaa848e66142e81097e0c8dc
Matt Stover <mcs@mcstover.com>
2017-12-04 15:07:58 -0800

    Comment out full file coverage to prevent global compile. TODO: Return to this later. Updated RouteListing.spec test to instantiate router and pass into the constructor.

--- test/unit/specs/RouteListing.spec.js
+++ test/unit/specs/RouteListing.spec.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import RouteListing from '@/components/RouteListing';
+import createRouter from '@/router';
+
 
 describe('RouteListing.vue', () => {
 	it('should render 2 labels', () => {
+		const router = createRouter();
 		const Constructor = Vue.extend(RouteListing);
-		const vm = new Constructor().$mount();
+		const vm = new Constructor({ router }).$mount();
 		expect(vm.$el.querySelectorAll('label').length)
 			.to.equal(2);
 	});
