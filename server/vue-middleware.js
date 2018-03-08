b2cc1904242aff8c54b4a5f9c1dea182ffd8aee5
Matt Stover <matthews@kiva.org>
2018-03-08 15:24:06 -0800

    Redirect undefined routes for links wrapped in router-link. Add a TODO + some scratch code for redirecting to the main kiva 404 page.

--- server/vue-middleware.js
+++ server/vue-middleware.js
@@ -12,6 +12,8 @@ function handleError(err, req, res) {
 		res.redirect(err.url);
 	} else if (err.code === 404) {
 		res.status(404).send('404 | Page Not Found');
+		// TOOO: consider sending to Kiva 404
+		// res.redirect('/error.html?url='+ req.url.replace('/', '') +&status=404');
 	} else {
 		// Render Error Page or Redirect
 		res.status(500).send('500 | Internal Server Error');
