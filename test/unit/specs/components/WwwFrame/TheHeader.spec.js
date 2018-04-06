82556b703431ae0bd4698a748b8e731d90c1a758
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-04-05 17:40:07 -0700

    ENG-1660 add simple test for TheHeader

---
+++ test/unit/specs/components/WwwFrame/TheHeader.spec.js
@@ -0,0 +1,39 @@
+import { shallow, createLocalVue, RouterLinkStub } from '@vue/test-utils';
+import Vuex from 'vuex';
+import TheHeader from '@/components/WwwFrame/TheHeader';
+import createMyModule from '@/store/modules/my';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('TheHeader', () => {
+	it('should hide/show the search area when the search toggle button is clicked', () => {
+		const wrapper = shallow(TheHeader, {
+			localVue,
+			store: new Vuex.Store({ modules: { my: createMyModule() } }),
+			stubs: {
+				RouterLink: RouterLinkStub,
+				SearchBar: {
+					template: '<div></div>',
+					methods: { focus() {} }
+				},
+			},
+		});
+		const toggle = wrapper.find('.search-toggler');
+		const area = wrapper.find('#top-nav-search-area');
+
+		expect(area.attributes()['aria-hidden']).toBe('true');
+		toggle.trigger('click');
+		expect(area.attributes()['aria-hidden']).not.toBeDefined();
+		toggle.trigger('click');
+		expect(area.attributes()['aria-hidden']).toBe('true');
+	});
+
+	it('asyncData() should dispatch the \'getMyKivaInfo\' action', () => {
+		const store = {
+			dispatch: jest.fn()
+		};
+		TheHeader.asyncData({ store });
+		expect(store.dispatch.mock.calls[0][0]).toBe('getMyKivaInfo');
+	});
+});
