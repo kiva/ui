326ef6c665f49018f983a9070bf3c478887b77c2
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-04-11 15:15:30 -0700

    Filter for kebab-case formatting

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,11 @@
 import asyncDataMixin from './async-data-mixin';
+import kebabCaseFilter from './kebab-case-filter';
 import numeralFilter from './numeral-filter';
 
 export default {
 	install: Vue => {
 		Vue.mixin(asyncDataMixin);
+		Vue.filter('kebabCase', kebabCaseFilter);
 		Vue.filter('numeral', numeralFilter);
 	}
 };
