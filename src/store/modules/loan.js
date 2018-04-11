152a0cd1395b90750d16e0109d9c666df96120b7
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-04-11 15:16:25 -0700

    ENG-1659 initial data fetching for lend menu

--- src/store/modules/loan.js
+++ src/store/modules/loan.js
@@ -1,3 +1,5 @@
+import _sortBy from 'lodash/sortBy';
+import lendMenuDataQuery from '@/graphql/query/lendMenuData.graphql';
 import loanSearchSuggestionsQuery from '@/graphql/query/loanSearchSuggestions.graphql';
 import * as types from '@/store/mutation-types';
 
@@ -5,6 +7,18 @@ export default apollo => {
 	return {
 		state: {
 			searchSuggestions: [],
+			headerCategories: [],
+			countryFacets: [],
+			regionDisplayOrder: [
+				'North America',
+				'Central America',
+				'South America',
+				'Africa',
+				'Eastern Europe',
+				'Middle East',
+				'Asia',
+				'Oceania'
+			],
 		},
 		getters: {},
 		actions: {
@@ -17,10 +31,25 @@ export default apollo => {
 						return suggestions;
 					});
 			},
+
+			getLendMenuInfo({ commit }) {
+				return apollo.query({ query: lendMenuDataQuery })
+					.then(result => result.data)
+					.then(data => {
+						commit(types.SET_PUBLIC_LEND_MENU_DATA, {
+							categories: data.loanChannels.values,
+							countryFacets: data.countryFacets
+						});
+					});
+			},
 		},
 		mutations: {
 			[types.RECEIVE_LOAN_SEARCH_SUGGESTIONS](state, { suggestions }) {
 				state.searchSuggestions = suggestions;
+			},
+			[types.SET_PUBLIC_LEND_MENU_DATA](state, { categories, countryFacets }) {
+				state.countryFacets = countryFacets;
+				state.headerCategories = _sortBy(categories, 'index');
 			},
 		},
 	};
