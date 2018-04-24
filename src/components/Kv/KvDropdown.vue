663a48d4d2b3f2080e03b4e0f32106a6feee1b34
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-04-23 17:26:21 -0700

    ENG-2039 fix mobile dropdown double-tap

--- src/components/Kv/KvDropdown.vue
+++ src/components/Kv/KvDropdown.vue
@@ -27,6 +27,16 @@ export default {
 		toggle() {
 			this.dropdown.toggle();
 		},
+		fixZfDropdown() {
+			const self = this;
+
+			// Check if the user is using touch, and if so, prevent dropdown closing on mouseleave
+			// so that only 1 tap (instead of 2) is required to open a link in the dropdown.
+			window.addEventListener('touchstart', function onFirstTouch() {
+				self.dropdown.$anchors.off('mouseleave.zf.dropdown');
+				window.removeEventListener(onFirstTouch);
+			});
+		}
 	},
 	mounted() {
 		const jqElem = $(this.$el);
@@ -36,15 +46,19 @@ export default {
 			hoverDelay: 100,
 		});
 
+		// proxy foundation jquery events to vue component events
 		const events = ['closeme', 'show', 'hide'];
 		for (let i = 0; i < events.length; i += 1) {
 			const zfEvent = `${events[i]}.zf.dropdown`;
 			jqElem.off(zfEvent).on(zfEvent, () => this.$emit(events[i]));
 		}
+
+		this.fixZfDropdown();
 	},
 	updated() {
 		// eslint-disable-next-line no-underscore-dangle
 		this.dropdown._init();
+		this.fixZfDropdown();
 	},
 	beforeDestroy() {
 		// eslint-disable-next-line no-underscore-dangle
