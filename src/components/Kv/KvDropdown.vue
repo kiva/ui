c14e09f6921739ad57e8c8f95035f799b7c8a678
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-04-27 14:13:04 -0700

    ENG-2038 implement dropdowns using popper

--- src/components/Kv/KvDropdown.vue
+++ src/components/Kv/KvDropdown.vue
@@ -1,69 +1,159 @@
 <template>
-	<div :id="name" class="dropdown-pane" data-dropdown>
+	<div class="dropdown-pane"
+		:class="{'is-open': show}"
+		:style="styles"
+		:aria-hidden="show ? 'false' : 'true'"
+	>
 		<slot></slot>
 	</div>
 </template>
 
 <script>
-import $ from 'jquery';
-import { Dropdown } from 'foundation/foundation.dropdown';
+import Popper from 'popper.js';
+import { mapState } from 'vuex';
+import _map from 'lodash/map';
 
 export default {
+	props: {
+		controller: { type: String, required: true },
+		openDelay: { type: Number, default: 0 },
+		closeDelay: { type: Number, default: 200 },
+	},
 	data() {
 		return {
-			dropdown: null
+			popper: null,
+			styles: {},
+			show: false,
+			timeout: null,
 		};
 	},
-	props: {
-		name: { type: String, required: true }
+	computed: {
+		reference() {
+			return document.getElementById(this.controller);
+		},
+		...mapState({
+			usingTouch: state => state.browser.usingTouch
+		}),
+	},
+	watch: {
+		usingTouch() {
+			if (this.usingTouch && this.popper) {
+				this.removeMouseEvents();
+			}
+		},
+		show(showing) {
+			if (this.reference) {
+				this.reference.setAttribute('aria-expanded', showing ? 'true' : 'false');
+			}
+			this.$emit(showing ? 'show' : 'hide');
+		},
+	},
+	mounted() {
+		this.initPopper();
+		this.attachEvents();
+	},
+	updated() {
+		if (this.popper) {
+			this.popper.scheduleUpdate();
+		}
+	},
+	beforeDestroy() {
+		this.removeEvents();
+		if (this.popper) {
+			this.popper.destroy();
+		}
 	},
 	methods: {
 		open() {
-			this.dropdown.open();
+			this.setTimeout(() => {
+				this.show = true;
+				if (this.usingTouch) {
+					this.attachBodyEvents();
+				}
+			}, this.openDelay);
 		},
 		close() {
-			this.dropdown.close();
+			this.setTimeout(() => {
+				this.show = false;
+				if (this.usingTouch) {
+					this.removeBodyEvents();
+				}
+			}, this.closeDelay);
 		},
 		toggle() {
-			this.dropdown.toggle();
+			if (this.show) {
+				this.close();
+			} else {
+				this.open();
+			}
 		},
-		fixZfDropdown() {
-			const self = this;
-
-			// Check if the user is using touch, and if so, prevent dropdown closing on mouseleave
-			// so that only 1 tap (instead of 2) is required to open a link in the dropdown.
-			window.addEventListener('touchstart', function onFirstTouch() {
-				self.dropdown.$anchors.off('mouseleave.zf.dropdown');
-				window.removeEventListener(onFirstTouch);
+		initPopper() {
+			this.popper = new Popper(this.reference, this.$el, {
+				placement: 'bottom',
+				modifiers: {
+					applyStyle: data => {
+						this.styles = data.styles;
+						this.setAttributes(data.attributes);
+					}
+				}
 			});
+		},
+		bodyTapHandler(e) {
+			if (e.target === this.reference ||
+				e.target === this.$el ||
+				this.reference.contains(e.target) ||
+				this.$el.contains(e.target)
+			) {
+				return;
+			}
+			this.show = false;
+			this.removeBodyEvents();
+		},
+		referenceTapHandler(e) {
+			e.preventDefault();
+			this.toggle();
+		},
+		attachEvents() {
+			this.reference.addEventListener('mouseover', this.open);
+			this.reference.addEventListener('mouseout', this.close);
+			this.$el.addEventListener('mouseover', this.open);
+			this.$el.addEventListener('mouseout', this.close);
+			this.reference.addEventListener('touchstart', this.referenceTapHandler);
+		},
+		attachBodyEvents() {
+			[...document.body.children].forEach(child => child.addEventListener('touchstart', this.bodyTapHandler));
+		},
+		removeEvents() {
+			this.removeMouseEvents();
+			this.removeBodyEvents();
+			this.reference.removeEventListener('touchstart', this.referenceTapHandler);
+		},
+		removeMouseEvents() {
+			this.reference.removeEventListener('mouseover', this.open);
+			this.reference.removeEventListener('mouseover', this.close);
+			this.$el.removeEventListener('mouseover', this.open);
+			this.$el.removeEventListener('mouseout', this.close);
+		},
+		removeBodyEvents() {
+			[...document.body.children].forEach(child => child.removeEventListener('touchstart', this.bodyTapHandler));
+		},
+		setAttributes(attrs) {
+			_map(attrs, (value, attr) => {
+				if (value === false) {
+					this.$el.removeAttribute(attr);
+				} else {
+					this.$el.setAttribute(attr, value);
+				}
+			});
+		},
+		setTimeout(fn, delay) {
+			window.clearTimeout(this.timeout);
+			this.timeout = window.setTimeout(() => {
+				fn();
+				window.clearTimeout(this.timeout);
+			}, delay);
 		}
 	},
-	mounted() {
-		const jqElem = $(this.$el);
-		this.dropdown = new Dropdown(jqElem, {
-			hover: true,
-			hoverPane: true,
-			hoverDelay: 100,
-		});
-
-		// proxy foundation jquery events to vue component events
-		const events = ['closeme', 'show', 'hide'];
-		for (let i = 0; i < events.length; i += 1) {
-			const zfEvent = `${events[i]}.zf.dropdown`;
-			jqElem.off(zfEvent).on(zfEvent, () => this.$emit(events[i]));
-		}
-
-		this.fixZfDropdown();
-	},
-	updated() {
-		// eslint-disable-next-line no-underscore-dangle
-		this.dropdown._init();
-		this.fixZfDropdown();
-	},
-	beforeDestroy() {
-		// eslint-disable-next-line no-underscore-dangle
-		this.dropdown._destroy();
-	}
 };
 </script>
 
