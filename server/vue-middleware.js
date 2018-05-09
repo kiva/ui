3c0171f848eb64b1e04e5c3f6d97db8ca80f0004
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-05-09 14:06:06 -0700

    ENG-2069 fetch session cookies

--- server/vue-middleware.js
+++ server/vue-middleware.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cookie = require('cookie');
 const { createBundleRenderer } = require('vue-server-renderer');
 const getGqlFragmentTypes = require('./util/getGqlFragmentTypes');
+const getSessionCookies = require('./util/getSessionCookies');
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -53,20 +54,32 @@ module.exports = function createMiddleware({ serverBundle, clientManifest, confi
 
 		res.setHeader('Content-Type', 'text/html');
 
-		getGqlFragmentTypes(config.server.graphqlUri)
-			.then(types => {
-				if (!isProd) {
-					console.log(`fragment fetch: ${Date.now() - s}ms`);
-				}
+		// get graphql api fragment types for the graphql client
+		const typesPromise = getGqlFragmentTypes(config.server.graphqlUri);
+
+		// fetch initial session cookies in case starting session with this request
+		const cookiePromise = getSessionCookies(config.server.sessionUri, cookies);
+
+		if (!isProd) {
+			typesPromise.then(() => console.log(`fragment fetch: ${Date.now() - s}ms`));
+			cookiePromise.then(() => console.log(`session fetch: ${Date.now() - s}ms`));
+		}
+
+		Promise.all([typesPromise, cookiePromise])
+			.then(([types, cookieInfo]) => {
+				// add fetched types to rendering context
 				context.config.graphqlFragmentTypes = types;
+				// update cookies in the rendering context with any newly fetched session cookies
+				context.cookies = Object.assign(context.cookies, cookieInfo.cookies);
+				// forward any newly fetched 'Set-Cookie' headers
+				cookieInfo.setCookies.forEach(setCookie => res.append('Set-Cookie', setCookie));
+				// render the app
 				return renderer.renderToString(context);
-			})
-			.then(html => {
+			}).then(html => {
 				res.send(html);
 				if (!isProd) {
 					console.log(`whole request: ${Date.now() - s}ms`);
 				}
-			})
-			.catch(err => handleError(err, req, res));
+			}).catch(err => handleError(err, req, res));
 	};
 };
