d3626a35a71c84493dc76bdf36ad4b01075f3d80
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-05-21 19:06:16 -0700

    Make fewer promises

--- server/util/getGqlFragmentTypes.js
+++ server/util/getGqlFragmentTypes.js
@@ -1,61 +1,39 @@
 const fetch = require('./fetch');
 
-let cacheInstance;
+function fetchGqlFragments(url, cache) {
+	return fetch(url, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			query: '{ __schema { types { kind name possibleTypes { name } } } }'
+		}),
+	})
+		.then(result => result.json())
+		.then(result => {
+			// eslint-disable-next-line no-underscore-dangle
+			const fragmentTypes = result.data.__schema.types.filter(t => t.possibleTypes !== null);
 
-function fetchGqlFragments(url) {
-	return new Promise((resolve, reject) => {
-		fetch(url, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				query: '{ __schema { types { kind name possibleTypes { name } } } }'
-			}),
-		})
-			.then(result => result.json())
-			.then(result => {
-				// eslint-disable-next-line no-underscore-dangle
-				const fragmentTypes = result.data.__schema.types.filter(t => t.possibleTypes !== null);
+			cache.set('ui-gql-fragment-types', fragmentTypes, 24 * 60 * 60, err => {
+				if (err) console.error(`gql fragment cache error : ${err}`);
+			});
 
-				cacheInstance.set('ui-gql-fragment-types', fragmentTypes, 5000, err => {
-					if (err) console.error(`gql fragment cache error : ${err}`);
-				});
-				resolve(fragmentTypes);
-			})
-			.catch(err => reject(err));
-	});
+			return fragmentTypes;
+		});
 }
 
-function getGqlFragmentsFromCache(url) {
-	return new Promise((resolve, reject) => {
-		cacheInstance.get('ui-gql-fragment-types', (err, data) => {
-			if (data) {
-				resolve(data);
-			} else {
-				fetchGqlFragments(url)
-					.then(result => {
-						resolve(result);
-					})
-					.catch(error => {
-						console.error(error);
-						reject(error);
-					});
-			}
+function getGqlFragmentsFromCache(cache) {
+	return new Promise(resolve => {
+		cache.get('ui-gql-fragment-types', data => {
+			resolve(data || []);
 		});
 	});
 }
 
 module.exports = function getGqlFragmentTypes(url, cache) {
-	// share cache instance
-	cacheInstance = cache;
-
-	return new Promise((resolve, reject) => {
-		getGqlFragmentsFromCache(url)
-			.then(data => {
-				resolve(data);
-			})
-			.catch(error => {
-				console.error(error);
-				reject(error);
-			});
+	return getGqlFragmentsFromCache(cache).then(data => {
+		if (data.length) {
+			return data;
+		}
+		return fetchGqlFragments(url, cache);
 	});
 };
