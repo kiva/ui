4b3a03dc54c80aa9a3292a3e2edac6ce17d12985
joshua starkey <joshua@kiva.org>
2018-06-11 17:26:15 -0700

    vue-32 lend-vue page with frame and heading text

---
+++ src/pages/Lend/LendPage.vue
@@ -0,0 +1,32 @@
+<template>
+	<www-page>
+		<div class="row">
+			<div class="small-12 columns heading-region">
+				<h1>Make a loan, change a life</h1>
+				<p class="small-12 xlarge-9">Each Kiva loan helps people build a better
+				future for themselves and their families.</p>
+			</div>
+		</div>
+	</www-page>
+</template>
+
+<script>
+import WwwPage from '@/components/WwwFrame/WwwPage';
+
+export default {
+	components: {
+		WwwPage
+	},
+	metaInfo: {
+		title: 'Lend page'
+	}
+};
+</script>
+
+<style lang="scss">
+@import 'settings';
+
+.heading-region {
+	margin-top: 1.25rem;
+}
+</style>
