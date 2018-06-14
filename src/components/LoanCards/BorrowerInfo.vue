1a59869485385e3d26650147cea0cf0a307de640
joshua starkey <joshua@kiva.org>
2018-06-14 12:36:42 -0700

    vue-67 less specific styles are better at this point until all the pieces come together.

--- src/components/LoanCards/BorrowerInfo.vue
+++ src/components/LoanCards/BorrowerInfo.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="borrower-info-wrapper small-12">
+	<div class="borrower-info-wrapper">
 		<a class="name">Bobert { name }</a>
 		<div class="country">U.S.A. { country }</div>
 		<div class="loan-use">A loan of { loanAmount } helps this dude to buy hotdogs
