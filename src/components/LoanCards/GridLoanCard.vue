21c7e606e2e45b91860e0360e83625418301f2d9
joshua starkey <joshua@kiva.org>
2018-06-18 10:05:16 -0700

    vue-70 putting matching text pieces here while we try and fix UI pages.

--- src/components/LoanCards/GridLoanCard.vue
+++ src/components/LoanCards/GridLoanCard.vue
@@ -6,15 +6,19 @@
 			:amount="loan.loanAmount"
 			:use="loan.use"
 			:country="loan.geocode.country.name" />
+
+		<MatchingText />
 	</div>
 </template>
 
 <script>
 import BorrowerInfo from '@/components/LoanCards/BorrowerInfo';
+import MatchingText from '@/components/LoanCards/MatchingText';
 
 export default {
 	components: {
 		BorrowerInfo,
+		MatchingText,
 	},
 	props: {
 		loan: {
---
+++ src/components/LoanCards/MatchingText.vue
@@ -0,0 +1,13 @@
+<template functional>
+	<div class="matching-text-wrapper">
+		<span>2x matching by {{ MATCHERNAMEHERE }}</span>
+	</div>
+</template>
+
+<style lang="scss">
+@import 'settings';
+
+.matching-text-wrapper {
+	text-align: center;
+}
+</style>
