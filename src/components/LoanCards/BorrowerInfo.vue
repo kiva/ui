08a20dd6d339e8d2a72fde0396fa32831b32f942
joshua starkey <joshua@kiva.org>
2018-06-20 15:16:52 -0700

    functional componenet to non-functional component that's more functional :)

--- src/components/LoanCards/BorrowerInfo.vue
+++ src/components/LoanCards/BorrowerInfo.vue
@@ -1,18 +1,50 @@
-<template functional>
+<template>
 	<div class="borrower-info-wrapper">
-		<a class="name">{{ props.name }}</a>
-		<div class="country">{{ props.country }}</div>
+		<a class="name">{{ name }}</a>
+		<div class="country">{{ country }}</div>
 		<div class="loan-use">
-			<span v-if="props.use.length">
-				A loan of {{ props.amount | numeral('$0,0') }} helps {{ props.name }} {{ props.use }}
-			</span>
-			<span v-else>
-				For the borrower's privacy, this loan has been made anonymous.
+			<span>
+				A loan of {{ amount | numeral('$0,0') }} helps {{ name }} {{ shortenedLoanUse }}
 			</span>
 			<a class="borrower-page-link" href="">Read more</a>
 		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	props: {
+		use: {
+			type: String,
+			default: ''
+		},
+		name: {
+			type: String,
+			default: ''
+		},
+		country: {
+			type: String,
+			default: ''
+		},
+		amount: {
+			type: String,
+			default: ''
+		}
+	},
+	computed: {
+		shortenedLoanUse() {
+			const maxLength = 100;
+			if (this.use.length === 0) {
+				return 'For the borrower\'s privacy, this loan has been made anonymous.';
+			} else if (this.use.length > maxLength) {
+				return `${this.use.substring(0, maxLength)}...`;
+			} else if (this.use.length < maxLength) {
+				return this.use;
+			}
+		},
+	}
+};
+</script>
 
 <style lang="scss">
 @import 'settings';
