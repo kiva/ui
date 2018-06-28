f7bea56ea181dfc8b83a8a42f46e40fab14305d7
joshua starkey <joshua@kiva.org>
2018-06-28 10:45:03 -0700

    vue-82 small fix to make travis green.

--- src/components/LoanCards/Buttons/CheckoutNow.vue
+++ src/components/LoanCards/Buttons/CheckoutNow.vue
@@ -36,6 +36,15 @@ export default {
 	box-shadow: 0 rem-calc(2) $charcoal;
 	padding: rem-calc(13) 0;
 
+	.icon-check-in-circle {
+		width: rem-calc(30);
+		height: rem-calc(30);
+		vertical-align: middle;
+		position: relative;
+		margin-top: rem-calc(-5);
+		margin-right: rem-calc(5);
+	}
+
 	&:hover {
 		color: $blue;
 		border: 1px solid $blue;
@@ -44,15 +53,6 @@ export default {
 		.icon-check-in-circle {
 			color: $blue;
 		}
-	}
-
-	.icon-check-in-circle {
-		width: rem-calc(30);
-		height: rem-calc(30);
-		vertical-align: middle;
-		position: relative;
-		margin-top: rem-calc(-5);
-		margin-right: rem-calc(5);
 	}
 }
 </style>
