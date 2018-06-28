8dc63ebcf98c7af427cf0a7f6cf554bc68b00d4c
joshua starkey <joshua@kiva.org>
2018-06-28 10:47:57 -0700

    vue-82 fixing v-on:listeners -> v-on:

--- src/components/LoanCards/Buttons/LendButton.vue
+++ src/components/LoanCards/Buttons/LendButton.vue
@@ -2,7 +2,7 @@
 	<kv-button
 		class="button"
 		v-kv-track-event="['Lending', 'click-Read more', 'lend-button-click', id, 'true']"
-		v-on="listeners">
+		v-on="$listeners">
 		Lend $25
 	</kv-button>
 </template>
@@ -16,10 +16,6 @@ export default {
 	},
 	props: {
 		id: {
-			type: Number,
-			default: null
-		},
-		listeners: {
 			type: Number,
 			default: null
 		},
