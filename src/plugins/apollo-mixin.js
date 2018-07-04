66bbcd19f9ecfde7fb14a09f6fe78e27cd7e5cd0
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-07-03 17:24:46 -0700

    VUE-34 WIP handle variables for apollo

--- src/plugins/apollo-mixin.js
+++ src/plugins/apollo-mixin.js
@@ -5,17 +5,33 @@ export default {
 				throw new Error('No apollo client provided! Add "inject: [\'apollo\']" to this component definition.');
 			}
 
-			const { query, preFetch, result = () => {} } = this.$options.apollo;
+			const {
+				query,
+				preFetch,
+				preFetchVariables = () => {},
+				variables = () => {},
+				result = () => {},
+			} = this.$options.apollo;
 
 			if (query) {
 				// if the query was prefetched, read the data from the cache
 				if (preFetch) {
-					const data = this.apollo.readQuery({ query });
+					const data = this.apollo.readQuery({
+						query,
+						variables: preFetchVariables({ route: this.$route }),
+					});
 					result.call(this, { data });
 				}
 
+				const observer = this.apollo.watchQuery({
+					query,
+					variables: variables.call(this)
+				});
+
+				this.$watch(variables, vars => observer.setVariables(vars), { deep: true });
+
 				// Watch for changes in the query results
-				this.apollo.watchQuery({ query }).subscribe({
+				observer.subscribe({
 					next: apolloResult => result.call(this, apolloResult)
 				});
 			}
