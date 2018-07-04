119798068a3996cf6b95a29e2597eb37d08fe8af
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-07-03 17:25:17 -0700

    VUE-34 Pagination component

---
+++ src/components/Kv/KvPagination.vue
@@ -0,0 +1,182 @@
+<template>
+	<nav aria-label="Pagination">
+		<ul class="pagination">
+			<li v-if="current !== 1" class="pagination-previous">
+				<router-link :to="routeForPage(current - 1)" aria-label="Previous page">
+					<kv-icon name="triangle" />
+					<span class="show-for-sr">Previous page</span>
+				</router-link>
+			</li>
+			<li v-else class="pagination-previous disabled">
+				<kv-icon name="triangle" />
+				<span class="show-for-sr">Previous page</span>
+			</li>
+			<li v-for="(number, index) in numbers"
+				:key="number || -index"
+				:class="{ ellipsis: number === 0, current: number === current }"
+				:aria-hidden="number === 0"
+			>
+				<router-link v-if="number > 0 && number !== current"
+					:to="routeForPage(number)"
+					:aria-label="`Page ${number}`"
+				>
+					{{ number }}
+				</router-link>
+				<span v-if="number === current">
+					<span class="show-for-sr">You're on page </span>
+					{{ number }}
+				</span>
+			</li>
+			<li v-if="current !== totalPages" class="pagination-next">
+				<router-link :to="routeForPage(current + 1)" aria-label="Next page">
+					<span class="show-for-sr">Next page</span>
+					<kv-icon name="triangle" />
+				</router-link>
+			</li>
+			<li v-else class="pagination-next disabled">
+				<span class="show-for-sr">Next page</span>
+				<kv-icon name="triangle" />
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+import _clone from 'lodash/clone';
+import _range from 'lodash/range';
+import _uniq from 'lodash/uniq';
+import KvIcon from './KvIcon';
+
+export default {
+	components: {
+		KvIcon,
+	},
+	props: {
+		limit: {
+			type: Number,
+			default: 20,
+		},
+		total: {
+			type: Number,
+			default: 1000,
+		},
+	},
+	data() {
+		return {
+			maxExtraPages: 3,
+		};
+	},
+	computed: {
+		current() {
+			return Number(this.$route.query.page) || 1;
+		},
+		totalPages() {
+			return Math.ceil(this.total / this.limit);
+		},
+		numbers() {
+			// if less than the max, there will be no ellipsis, so just return the numbers
+			if (this.totalPages < (this.maxExtraPages + 2)) {
+				return _range(1, this.totalPages + 1);
+			}
+
+			let numbers = [];
+
+			// add the 'middle' block of numbers based upon the current page
+			if (this.current === 1 || this.current === 2) {
+				numbers = _range(1, this.maxExtraPages + 1);
+			} else if (this.current === this.totalPages - 1 || this.current === this.totalPages) {
+				numbers = _range(this.totalPages - (this.maxExtraPages - 1), this.totalPages + 1);
+			} else {
+				const delta = Math.floor(this.maxExtraPages / 2);
+				numbers = _range(this.current - delta, this.current + delta + 1);
+			}
+
+			// add the first and last page numbers
+			numbers = numbers.concat([1, this.totalPages]);
+
+			// sort by number & remove duplicates
+			numbers.sort((a, b) => a - b);
+			numbers = _uniq(numbers, true);
+
+			// add a placeholder for first ellipsis, if needed
+			if (numbers[1] !== 2) {
+				numbers.splice(1, 0, 0);
+			}
+
+			// add a placeholder for last ellipsis, if needed
+			if (numbers[numbers.length - 2] !== this.totalPages - 1) {
+				numbers.splice(numbers.length - 1, 0, 0);
+			}
+
+			return numbers;
+		},
+	},
+	methods: {
+		routeForPage(number) {
+			const query = _clone(this.$route.query);
+			if (number > 1) {
+				query.page = number;
+			} else {
+				delete query.page;
+			}
+			return { query };
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+@import "settings";
+
+$spacing: 0.75rem;
+
+.pagination {
+	text-align: center;
+	margin: $spacing 0;
+
+	li {
+		text-decoration: none;
+		display: inline-block;
+		margin: 0 $spacing;
+
+		&.pagination-previous,
+		&.pagination-next {
+			margin: 0;
+		}
+	}
+}
+
+.icon {
+	width: 1rem;
+	height: 1rem;
+	vertical-align: text-top;
+
+	a & {
+		fill: $kiva-textlink;
+	}
+
+	a:hover & {
+		fill: $kiva-textlink-hover;
+	}
+}
+
+.pagination-previous .icon {
+	transform: rotate(-90deg);
+}
+
+.pagination-next .icon {
+	transform: rotate(90deg);
+}
+
+.disabled, .current {
+	color: $kiva-text-light;
+
+	.icon {
+		fill: $kiva-text-light;
+	}
+}
+
+.ellipsis::after {
+	content: '\2026';
+}
+</style>
