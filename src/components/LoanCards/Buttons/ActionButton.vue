e51c3ee4312b5d3cfa0061cd736750e6fef630a8
joshua starkey <joshua@kiva.org>
2018-07-12 17:08:21 -0700

    vue-90 adding to basket state to lend button

--- src/components/LoanCards/Buttons/ActionButton.vue
+++ src/components/LoanCards/Buttons/ActionButton.vue
@@ -1,7 +1,7 @@
 <template>
 	<component
 		:is="currentButtonState"
-		class="action-button"
+		class="action-button smaller"
 		:loan-id="loanId" />
 </template>
 
@@ -55,13 +55,6 @@ export default {
 .action-button {
 	margin-top: rem-calc(30);
 	margin-bottom: rem-calc(10);
-	height: rem-calc(46);
-	line-height: 1;
-	font-size: rem-calc(20);
 	width: 100%;
-
-	@include breakpoint(medium) {
-		line-height: rem-calc(15);
-	}
 }
 </style>
--- src/components/LoanCards/Buttons/LendButton.vue
+++ src/components/LoanCards/Buttons/LendButton.vue
@@ -1,8 +1,13 @@
 <template>
 	<kv-button @click.native="addToBasket"
 		:v-kv-track-event="`['Lending', 'Add to basket', 'lend-button-click', ${loanId}, 'true']`"
+		v-if="!loading"
 	>
 		<slot>Lend now</slot>
+	</kv-button>
+	<kv-button v-else>
+		<kv-loading-spinner />
+		Adding to basket
 	</kv-button>
 </template>
 
@@ -12,10 +17,12 @@ import addToBasketMutation from '@/graphql/mutation/addToBasket.graphql';
 import basketCountQuery from '@/graphql/query/basketCount.graphql';
 import basketItemsQuery from '@/graphql/query/basketItems.graphql';
 import KvButton from '@/components/Kv/KvButton';
+import KvLoadingSpinner from '@/components/Kv/KvLoadingSpinner';
 
 export default {
 	components: {
 		KvButton,
+		KvLoadingSpinner,
 	},
 	inject: ['apollo'],
 	props: {
@@ -28,8 +35,14 @@ export default {
 			default: 25,
 		},
 	},
+	data() {
+		return {
+			loading: false,
+		};
+	},
 	methods: {
 		addToBasket() {
+			this.loading = true;
 			this.apollo.mutate({
 				mutation: addToBasketMutation,
 				variables: {
@@ -40,9 +53,27 @@ export default {
 					{ query: basketCountQuery },
 					{ query: basketItemsQuery },
 				]
+			}).finally(() => {
+				this.loading = false;
 			});
 		}
 	},
 };
 
 </script>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+.loading-spinner {
+	width: 1.5rem;
+	height: 1.5rem;
+	vertical-align: middle;
+	margin-right: 3px;
+
+	.line {
+		background: $white;
+	}
+}
+
+</style>
