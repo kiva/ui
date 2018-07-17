776edc547386a00e029bc1cd70799449793dd39a
joshua starkey <joshua@kiva.org>
2018-07-16 17:24:04 -0700

    feck-126 start of the vue login form.

--- src/pages/LoginAndRegister/LoginAndRegister.vue
+++ src/pages/LoginAndRegister/LoginAndRegister.vue
@@ -1,15 +1,46 @@
 <template>
 	<www-page>
-		<div> Filler for now </div>
+		<div id="loginForm" class="small-2 loginForm">
+			<KvButton>FACEBOOK BUTTON HERE</KvButton>
+			<hr>
+			<div class="featured-text">Or use your email</div>
+
+			<label for="loginForm_email">Email</label>
+			<input type="email" name="email">
+
+			<label for="loginForm_pass">Password</label>
+			<input id="loginForm_pass" type="password" name="password">
+
+			<KvButton class="login" type="submit">Sign in</KvButton>
+		</div>
 	</www-page>
 </template>
 
 <script>
 import WwwPage from '@/components/WwwFrame/WwwPage';
+import KvButton from '@/components/Kv/KvButton';
 
 export default {
 	components: {
 		WwwPage,
+		KvButton,
 	}
 };
 </script>
+<style lang="scss" scoped>
+@import 'settings';
+
+.loginForm {
+	margin: 0 auto;
+
+	.featured-text {
+		text-align: center;
+		color: $charcoal;
+	}
+
+	.login {
+		width: 100%;
+	}
+}
+
+</style>
