c99c54610fba4c959cb211f5a609aa4d113e2b9b
joshua starkey <joshua@kiva.org>
2018-07-18 10:54:52 -0700

    feck-127 more progress toward the registration form

--- src/pages/LoginAndRegister/LoginAndRegister.vue
+++ src/pages/LoginAndRegister/LoginAndRegister.vue
@@ -1,20 +1,22 @@
 <template>
 	<www-page>
-		<div id="loginForm" class="small-3 login-form">
+		<div id="loginForm" class="small-12 large-3 login-form">
 			<KvButton>FACEBOOK BUTTON HERE</KvButton>
 			<hr>
 			<div class="featured-text">Or use your email</div>
 
 			<h3 class="input-label">Email</h3>
-			<input type="email" name="email" class="bottom-spacing">
+			<input type="email" name="email" class="input-spacing bottom-spacing">
 
 			<h3 class="input-label">Password</h3>
-			<input type="password" name="password" class="bottom-spacing">
+			<input type="password" name="password" class="input-spacing bottom-spacing">
 
 			<div class="bottom-spacing">
 				<input type="checkbox" name="persist_login" id="loginForm_persist_login">
 				<span id="keep_me_signed_id" style="cursor: pointer;">Keep me signed in.</span>
 				<a class="helpTip sfHelpTip_old "
+					data-tooltip="http://na3.salesforce.com/_ui/selfservice/pkb/
+					PublicKnowledgeSolution/d?orgId=00D500000006svl&amp;id=50150000000TCyi"
 					href="http://na3.salesforce.com/_ui/selfservice/pkb/
 					PublicKnowledgeSolution/d?orgId=00D500000006svl&amp;id=50150000000TCyi"
 					id="persist_login_details">Details
@@ -26,25 +28,76 @@
 			<a href="/help/forgot-password" class="forgot-password-link">Forgot your password?</a>
 			<a href="/register" class="register-link">Sign up for Kiva</a>
 		</div>
+
+		<hr class="spacer">
+
+		<div id="registerForm" class="small-12 large-3 register-form">
+			<KvButton>FACEBOOK BUTTON HERE</KvButton>
+			<hr>
+			<div class="featured-text">Or</div>
+			<h3 class="input-label">First name</h3>
+			<input type="text" name="firstname" maxlength="40" class="input-spacing bottom-spacing">
+
+			<h3 class="input-label">Last name</h3>
+			<input type="text" name="lastname" class="input-spacing bottom-spacing">
+
+			<h3 class="input-label">Email</h3>
+			<input type="email" name="email" class="input-spacing bottom-spacing">
+
+			<h3 class="input-label">Password</h3>
+			<input type="password" name="password" class="input-spacing bottom-spacing">
+
+			<div class="bottom-spacing terms-and-policy">
+				<input type="checkbox" name="persist_login" id="registerForm_terms_of_use_privacy_poicy">
+				I have read and agree to the
+				<a href="legal/terms" target="_blank"
+					title="Open Terms of Use in a new window"
+					class="">Terms of Use
+				</a> and
+				<a href="legal/privacy" target="_blank"
+					title="Open Privacy Policy in a new window"
+					class="">Privacy Policy
+				</a>.
+			</div>
+			<KvButton class="register-button bottom-spacing" type="submit">Continue</KvButton>
+		</div>
 	</www-page>
 </template>
 
 <script>
 import WwwPage from '@/components/WwwFrame/WwwPage';
 import KvButton from '@/components/Kv/KvButton';
+// import Popper from 'popper.js';
 
 export default {
 	components: {
 		WwwPage,
 		KvButton,
-	}
+	},
+	data() {
+		return {
+			popper: null,
+		};
+	},
+	mounted: {
+		// https://matwrites.com/create-beautiful-tooltips-with-popper-js-tooltip-js/
+	},
 };
 </script>
 <style lang="scss" scoped>
 @import 'settings';
 
-.login-form {
-	margin: 0 auto;
+.spacer {
+	margin: 150px 0;
+}
+
+.login-form,
+.register-form {
+	margin: 0 rem-calc(10);
+
+	@include breakpoint(large) {
+		margin: 0 auto;
+	}
 
 	.featured-text {
 		text-align: center;
@@ -55,11 +108,16 @@ export default {
 		font-weight: 500;
 	}
 
+	.input-spacing {
+		padding: rem-calc(5) rem-calc(7);
+	}
+
 	.bottom-spacing {
 		margin-bottom: rem-calc(20);
 	}
 
-	.sign-in-button {
+	.sign-in-button,
+	.register-button {
 		width: 100%;
 	}
 
@@ -68,6 +126,14 @@ export default {
 		display: block;
 		text-align: center;
 		margin-bottom: rem-calc(15);
+	}
+
+	.terms-and-policy {
+		line-height: rem-calc(20);
+
+		input {
+			margin: 0;
+		}
 	}
 }
 
