cc02ba6bc66cf0ef0e2adb63a88c80a134af46d1
Matt Stover <matthews@kiva.org>
2018-08-02 15:54:39 -0700

    InitializeBasket query and inclusion in beta checkout frame.

---
+++ src/pages/Checkout/CheckoutBetaPage.vue
@@ -0,0 +1,68 @@
+<template>
+	<www-page>
+		<div id="checkout-slim" class="row page-content">
+			<div class="columns">
+				<ul>
+					<li v-for="loan in loans" :key="loan.id">
+						<img :src="loan.loan.image.url" :alt="loan.loan.name">
+						{{ loan.loan.name }} {{ loan.price }}
+					</li>
+					<li v-for="donation in donations" :key="donation.id">
+						{{ donation.price }}
+					</li>
+				</ul>
+				<hr>
+				<router-link to="/ui-site-map">Site Map</router-link>
+			</div>
+		</div>
+	</www-page>
+</template>
+
+<script>
+import _get from 'lodash/get';
+import _filter from 'lodash/filter';
+// import _map from 'lodash/map';
+import WwwPage from '@/components/WwwFrame/WwwPage';
+import initializeCheckout from '@/graphql/query/initializeCheckout.graphql';
+
+export default {
+	components: {
+		WwwPage,
+	},
+	inject: ['apollo'],
+	metaInfo: {
+		title: 'Checkout'
+	},
+	data() {
+		return {
+			currentStep: 'basket',
+			loans: () => {},
+			totals: () => {},
+			donations: () => {}
+		};
+	},
+	apollo: {
+		query: initializeCheckout,
+		prefetch: true,
+		result({ data }) {
+			console.log(data);
+			this.totals = _get(data, 'shop.basket.totals');
+			this.loans = _filter(_get(data, 'shop.basket.items.values'), { __typename: 'LoanReservation' });
+			this.donations = _filter(_get(data, 'shop.basket.items.values'), { __typename: 'Donation' });
+		}
+	},
+	methods: {
+
+	},
+	computed: {
+	}
+};
+</script>
+
+<style lang="scss">
+@import 'settings';
+
+.page-content {
+	padding: 1.625rem 0;
+}
+</style>
