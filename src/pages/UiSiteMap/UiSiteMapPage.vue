2227610f97a8a1276bb3364e77a0eb79fade648b
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-08-14 20:59:08 -0700

    LC-41 add experiment example to sitemap

--- src/pages/UiSiteMap/UiSiteMapPage.vue
+++ src/pages/UiSiteMap/UiSiteMapPage.vue
@@ -6,6 +6,11 @@
 				<route-listing/>
 
 				<br>
+				<!-- TODO: Remove after testing VUE-97/LC-41 -->
+				<hr>
+				<p>Assigned: {{ assignedVersion }}</p>
+
+				<br>
 				<!-- TODO: Remove after testing VUE-37 -->
 				<hr>
 				<h4>Sample Tip Messages</h4>
@@ -67,6 +72,9 @@
 </template>
 
 <script>
+import _get from 'lodash/get';
+import expQuery from '@/graphql/query/experimentSetting.graphql';
+import verQuery from '@/graphql/query/experimentAssignment.graphql';
 import WwwPage from '@/components/WwwFrame/WwwPage';
 import KvLightbox from '@/components/Kv/KvLightbox';
 import RouteListing from './RouteListing';
@@ -84,8 +92,49 @@ export default {
 	data() {
 		return {
 			defaultLbVisible: false,
-			invertedLbVisible: false
+			invertedLbVisible: false,
+			assignedVersion: null,
 		};
+	},
+	apollo: {
+		// example prefetch function to get an experiment setting and then an assigned version
+		preFetch(config, client) {
+			return new Promise((resolve, reject) => {
+				// first query the setting
+				client.query({
+					query: expQuery,
+					variables: { key: 'uiexp.test' },
+				}).then(() => {
+					// with the cache now primed, query for a version assignment
+					client.query({
+						query: verQuery,
+						variables: { id: 'test' },
+					}).then(resolve).catch(reject);
+				}).catch(reject);
+			});
+		},
+	},
+	created() {
+		// read the experiment version assignment from the cache for ssr
+		const data = this.apollo.readQuery({
+			query: verQuery,
+			variables: { id: 'test' },
+		});
+		this.assignedVersion = _get(data, 'experiment.version');
+
+		// alternatively, comment out the above and the preFetch function and uncomment this
+		// section to only do the experiment assignment from the client.
+		// this.apollo.query({
+		// 	query: expQuery,
+		// 	variables: { key: 'uiexp.test' },
+		// }).then(() => {
+		// 	this.apollo.query({
+		// 		query: verQuery,
+		// 		variables: { id: 'test' },
+		// 	}).then(({ data }) => {
+		// 		this.assignedVersion = _get(data, 'experiment.version');
+		// 	});
+		// });
 	},
 	methods: {
 		triggerDefaultLightbox() {
