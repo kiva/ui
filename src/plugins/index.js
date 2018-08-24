b3ca42c585feea24ed9c33d67c0b0b5ebcf47a6c
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2018-08-24 16:55:28 -0700

    Move kebabCase to util

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,5 +1,5 @@
+import kebabCase from '@/util/kebabCase';
 import apolloMixin from './apollo-mixin';
-import kebabCaseFilter from './kebab-case-filter';
 import numeralFilter from './numeral-filter';
 import kvAnayltics from './kv-analytics-plugin';
 import tipMessage from './tip-message-mixin';
@@ -7,7 +7,7 @@ import tipMessage from './tip-message-mixin';
 export default {
 	install: Vue => {
 		Vue.mixin(apolloMixin);
-		Vue.filter('kebabCase', kebabCaseFilter);
+		Vue.filter('kebabCase', kebabCase);
 		Vue.filter('numeral', numeralFilter);
 		kvAnayltics(Vue);
 		Vue.mixin(tipMessage);
