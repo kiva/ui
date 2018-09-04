59716e5afda65f0427e60c96db99985db11e2baa
joshua starkey <joshua@kiva.org>
2018-09-04 15:17:46 -0700

    feck-192 Attempt to get this in a new format that handles JS in a valid way.

--- src/components/Checkout/LoanReservation.vue
+++ src/components/Checkout/LoanReservation.vue
@@ -1,19 +1,38 @@
 <template>
 	<div class="reservation-info small-text">
 		<span v-if="expiryTime"
-			class="loan-message"
-			v-html="reservedMessage"></span>
-		<!-- This lightbox will be replaced with a Popper tip message. -->
-		<kv-lightbox
-			:visible="defaultLbVisible"
-			@lightbox-closed="lightboxClosed">
-			<h2 slot="title">What does it mean that my loan is not reserved?</h2>
-			<div>
-				Loans will not be reserved if they've been in your basket for more than 45 minutes or have less
-				than 6 hours left to fundraise. This means there's a chance this loan may be funded by other lenders
-				even though it's in your basket. To make this loan, please proceed through the checkout process.
+			class="loan-message">
+			<!-- v-html="reservedMessage -->
+			<div
+				v-if="loanReservationMsg1"
+				class="red">Loan not reserved. <a @click.prevent="triggerDefaultLightbox">Why?</a>
 			</div>
-		</kv-lightbox>
+			<!-- This lightbox will be replaced with a Popper tip message. -->
+			<kv-lightbox
+				:visible="defaultLbVisible"
+				@lightbox-closed="lightboxClosed">
+				<h2 slot="title">What does it mean that my loan is not reserved?</h2>
+				<div>
+					Loans will not be reserved if they've been in your basket for more than 45 minutes or have less
+					than 6 hours left to fundraise. This means there's a chance this loan may be funded by other lenders
+					even though it's in your basket. To make this loan, please proceed through the checkout process.
+				</div>
+			</kv-lightbox>
+
+			<div
+				v-if="loanReservationMsg2">Reserved for ${this.mins} more minutes
+			</div>
+
+			<div
+				v-if="loanReservationMsg3"
+				class="red">Reserved for ${this.mins} more minutes
+			</div>
+
+			<div
+				v-if="loanReservationMsg4"
+				class="red">Reserved for ${this.mins} more minute
+			</div>
+		</span>
 	</div>
 </template>
 
@@ -28,6 +47,10 @@ export default {
 	data() {
 		return {
 			defaultLbVisible: false,
+			// ReservationMsg1: false,
+			// ReservationMsg2: false,
+			// ReservationMsg3: false,
+			// ReservationMsg4: false
 		};
 	},
 	props: {
@@ -38,7 +61,7 @@ export default {
 		isExpiringSoon: {
 			type: Boolean,
 			default: false
-		}
+		},
 	},
 	methods: {
 		triggerDefaultLightbox() {
@@ -49,26 +72,39 @@ export default {
 		},
 	},
 	computed: {
-		reservedMessage() {
+		loanReservationMsg1() {
+			const reservedDate = new Date(this.expiryTime);
+			const timeLeft = reservedDate.getTime() - Date.now();
+
+			if (timeLeft <= 0 || this.isExpiringSoon) {
+				return this.ReservationMsg1 === true;
+			}
+		},
+		loanReservationMsg2() {
 			const reservedDate = new Date(this.expiryTime);
 			const mins = differenceInMinutes(reservedDate.getTime(), Date.now());
 
-			if (this.expiryTime !== null) {
-				const timeLeft = reservedDate.getTime() - Date.now();
-
-				// TODO: update text of Loan not reserved message. Needs to read "Loan not reserved. Why?"
-				if (timeLeft <= 0 || this.isExpiringSoon) {
-					// eslint-disable-next-line max-len
-					return '<div class="red">Loan not reserved. <a @click.prevent="triggerDefaultLightbox">Why?</a></div>';
-				} else if (mins > 6) {
-					return `<div>Reserved for ${mins} more minutes</div>`;
-				} else if (mins <= 6) {
-					return `<div class="red">Reserved for ${mins} more minutes</div>`;
-				} else if (mins <= 1) {
-					return `<div class="red">Reserved for ${mins} more minute</div>`;
-				}
+			if (mins > 6) {
+				return this.ReservationMsg2 === true;
 			}
-		}
+		},
+		loanReservationMsg3() {
+			const reservedDate = new Date(this.expiryTime);
+			const mins = differenceInMinutes(reservedDate.getTime(), Date.now());
+
+			if (mins <= 6) {
+				return this.ReservationMsg2 === true;
+			}
+		},
+		loanReservationMsg4() {
+			const reservedDate = new Date(this.expiryTime);
+			const mins = differenceInMinutes(reservedDate.getTime(), Date.now());
+
+			if (mins <= 1) {
+				return this.ReservationMsg4 === true;
+			}
+		},
+
 	},
 };
 
