22c6abb531d3756bd90043a89f939a9ec9e2878e
joshua starkey <joshua@kiva.org>
2018-09-04 10:47:31 -0700

    feck-192 pieces in place, yet to work.

--- src/components/Checkout/LoanReservation.vue
+++ src/components/Checkout/LoanReservation.vue
@@ -3,13 +3,33 @@
 		<span v-if="expiryTime"
 			class="loan-message"
 			v-html="reservedMessage"></span>
+		<!-- This lightbox will be replaced with a Popper tip message. -->
+		<kv-lightbox
+			:visible="defaultLbVisible"
+			@lightbox-closed="lightboxClosed">
+			<h2 slot="title">What does it mean that my loan is not reserved?</h2>
+			<div>
+				Loans will not be reserved if they've been in your basket for more than 45 minutes or have less
+				than 6 hours left to fundraise. This means there's a chance this loan may be funded by other lenders
+				even though it's in your basket. To make this loan, please proceed through the checkout process.
+			</div>
+		</kv-lightbox>
 	</div>
 </template>
 
 <script>
 import { differenceInMinutes } from 'date-fns';
+import KvLightbox from '@/components/Kv/KvLightbox';
 
 export default {
+	components: {
+		KvLightbox
+	},
+	data() {
+		return {
+			defaultLbVisible: false,
+		};
+	},
 	props: {
 		expiryTime: {
 			type: String,
@@ -20,6 +40,14 @@ export default {
 			default: false
 		}
 	},
+	methods: {
+		triggerDefaultLightbox() {
+			this.defaultLbVisible = !this.defaultLbVisible;
+		},
+		lightboxClosed() {
+			this.defaultLbVisible = false;
+		},
+	},
 	computed: {
 		reservedMessage() {
 			const reservedDate = new Date(this.expiryTime);
@@ -29,9 +57,9 @@ export default {
 				const timeLeft = reservedDate.getTime() - Date.now();
 
 				// TODO: update text of Loan not reserved message. Needs to read "Loan not reserved. Why?"
-				// Why? needs to be a link to helptext handled using popper's lightbox or helptext
 				if (timeLeft <= 0 || this.isExpiringSoon) {
-					return '<div class="red">Loan not reserved</div>';
+					// eslint-disable-next-line max-len
+					return '<div class="red">Loan not reserved. <a @click.prevent="triggerDefaultLightbox">Why?</a></div>';
 				} else if (mins > 6) {
 					return `<div>Reserved for ${mins} more minutes</div>`;
 				} else if (mins <= 6) {
@@ -41,7 +69,7 @@ export default {
 				}
 			}
 		}
-	}
+	},
 };
 
 </script>
