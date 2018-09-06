06c4fe93ce56ed13cd4b86cdb55266803b33c3e4
joshua starkey <joshua@kiva.org>
2018-09-06 09:25:24 -0700

    feck-191 Attempting to use foundation accordion, having issues getting the JS I need

--- src/components/Checkout/KivaCardRedemption.vue
+++ src/components/Checkout/KivaCardRedemption.vue
@@ -1,24 +1,29 @@
 <template>
 	<div>
 		<hr>
-		<div class="row">
-			<span class="small-3 medium-1">
-				<kv-accordion
-					class="accordion-arrow"
-					@click.prevent="accordionToggle"/>
-			</span>
-			<span class="featured-text small-9 medium-8">Have a Kiva Card?</span>
-		</div>
+		<ul class="row accordion" data-accordion>
+			<li class="accordion-item" data-accordion-item>
+				<span class="small-3 medium-1">
+					<kv-accordion
+						class="accordion-arrow" />
+				</span>
+				<a class="featured-text small-9 medium-8 accordion-title">Have a Kiva Card?</a>
+				<div class="accordion-content" data-tab-content>Check this shit out.</div>
+			</li>
+		</ul>
 		<hr>
 	</div>
 </template>
 
 <script>
 import KvAccordion from '@/components/Kv/KvAccordion';
+import { Accordion } from 'foundation-sites';
+
 
 export default {
 	components: {
 		KvAccordion,
+		Accordion
 	},
 	data() {
 		return {
@@ -31,6 +36,10 @@ export default {
 			const arrow = document.getElementByClassName('.accordion-arrow');
 			arrow.setAttribute('style', 'transform: rotate(180deg)');
 		},
+		initAccordion() {
+			const $accordion = new Accordion('.accordion');
+			console.log($accordion);
+		}
 	}
 };
 </script>
--- src/components/Kv/KvAccordion.vue
+++ src/components/Kv/KvAccordion.vue
@@ -10,16 +10,23 @@ import KvIcon from '@/components/Kv/KvIcon';
 export default {
 	components: {
 		KvIcon
-	}
+	},
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import 'settings';
+@import 'foundation';
+@include foundation-accordion;
 
 .medium-chevron {
 	height: rem-calc(18);
 	width: rem-calc(40);
+}
+
+.accordion-title {
+	line-height: rem-calc(36);
+	border: none;
 }
 
 </style>
