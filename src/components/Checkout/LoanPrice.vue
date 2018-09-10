40f7b643c7d2b88ca9fe35a439157bd25df2c70e
Matt Stover <matthews@kiva.org>
2018-09-10 12:13:03 -0700

    remove unused var

--- src/components/Checkout/LoanPrice.vue
+++ src/components/Checkout/LoanPrice.vue
@@ -48,8 +48,7 @@ export default {
 	data() {
 		return {
 			selectedOption: numeral(this.price).format('0,0'),
-			selectLimit: 150,
-			additionalSharesLimit: 6
+			selectLimit: 150
 		};
 	},
 	computed: {
