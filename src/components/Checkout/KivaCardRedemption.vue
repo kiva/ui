a73abe842f255cec30dd491f010ba6d3847fa960
joshua starkey <joshua@kiva.org>
2018-09-10 10:03:50 -0700

    refactored a little bit.

--- src/components/Checkout/KivaCardRedemption.vue
+++ src/components/Checkout/KivaCardRedemption.vue
@@ -5,7 +5,12 @@
 			<div class="accordion-item">
 				<a >
 					<span class="small-3 medium-1">
-						<kv-accordion />
+						<a @click="toggleAccordion">
+							<kv-icon
+								:class="{ flipped: isFlipped }"
+								class="medium-chevron"
+								name="medium-chevron" />
+						</a>
 					</span>
 					<span class="featured-text small-9 medium-8 accordion-title">Have a Kiva Card?</span>
 				</a>
@@ -19,16 +24,40 @@
 </template>
 
 <script>
-import KvAccordion from '@/components/Kv/KvAccordion';
+import KvIcon from '@/components/Kv/KvIcon';
 
 export default {
 	components: {
-		KvAccordion,
+		KvIcon,
 	},
+	data() {
+		return {
+			isFlipped: false
+		};
+	},
+	methods: {
+		toggleAccordion() {
+			console.log('toggle accordion triggered');
+			this.isFlipped = !this.isFlipped;
+		},
+	}
 };
 </script>
 
 <style lang="scss" scoped>
 @import 'settings';
 
+.medium-chevron {
+	height: rem-calc(18);
+	width: rem-calc(40);
+}
+
+.accordion-title {
+	line-height: rem-calc(36);
+	border: none;
+}
+
+.flipped {
+	transform: rotate(180deg);
+}
 </style>
