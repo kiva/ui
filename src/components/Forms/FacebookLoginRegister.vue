e1b9e3ad175a2bc66fc53455d72fdcc032b9205f
joshua starkey <joshua@kiva.org>
2018-09-18 14:04:45 -0700

    feck-194 making these paths relative.

--- src/components/Forms/FacebookLoginRegister.vue
+++ src/components/Forms/FacebookLoginRegister.vue
@@ -57,9 +57,9 @@
 							v-model="newAcctTerms"
 							@click="showNewAcctTermsError = validateTerms()">
 						I have read and agree to the Kiva
-						<a href="https://www.kiva.org/legal/terms" target="_blank">Terms of Use</a>
+						<a href="/legal/terms" target="_blank">Terms of Use</a>
 						and
-						<a href="https://www.kiva.org/legal/privacy" target="_blank">Privacy Policy</a>
+						<a href="/legal/privacy" target="_blank">Privacy Policy</a>
 					</label>
 					<ul v-show="showNewAcctTermsError" class="validation-errors">
 						<li>
