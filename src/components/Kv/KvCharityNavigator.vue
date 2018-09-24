467b30ccd971125d2f57405db174f82bc13c088b
Karan Sekhri <ksekhri@gmail.com>
2018-09-24 16:25:51 -0700

    Add Charity Navigator Component

---
+++ src/components/Kv/KvCharityNavigator.vue
@@ -0,0 +1,11 @@
+<template functional>
+	<div class="container">
+		<div>Awarded for effectiveness and efficiency</div>
+		<div><img src="~@/assets/images/charity-navigator-retina.png"></div>
+		<div>Charity Navigator's highest rating</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	@import 'settings';
+</style>
--- src/pages/Protocol.vue
+++ src/pages/Protocol.vue
@@ -15,21 +15,31 @@
 				<p v-for="(paragraph, index) in infoText" :key="index">{{ paragraph }}</p>
 				<p><a href="#">How the Protocol works</a></p>
 			</div>
+			<div class="small-12 medium-8 columns">
+				<h1>About Kiva</h1>
+				<p>{{ aboutText }}</p>
+			</div>
+			<div id="charity-navigator-container" class="small-12 medium-4 columms">
+				<kv-charity-navigator />
+			</div>
 		</div>
 	</www-page>
 </template>
 
 <script>
 import WwwPage from '@/components/WwwFrame/WwwPage';
+import KvCharityNavigator from '@/components/Kv/KvCharityNavigator';
 
 export default {
 	components: {
 		WwwPage,
+		KvCharityNavigator,
 	},
 	data() {
 		return {
+			/* eslint-disable max-len */
+			aboutText: 'Kiva is a nonprofit focused on expanding financial access for underserved people around the world. Since 2005, Kiva’s global community of 1.7 million lenders has crowdfunded more than $1.2 billion in microloans to 3 million borrowers in more than 80 countries. On Kiva’s lending platform, anyone can help a borrower start or grow a business, go to school and realize their potential. Kiva also works to address the systemic issues that keep communities from accessing the financial services they need, through projects like Kiva Protocol.',
 			infoText: [
-				/* eslint-disable max-len */
 				'Kiva, Sierra Leone and U.N. agencies announced the first implementation of the Kiva Protocol on Sept. 27, 2018, at the U.N. General Assembly. The Kiva Protocol will be used to create a nationwide digital identification system designed to help the country’s 7 million citizens access the financial services they need to improve their lives.',
 				'Globally, 1.7 billion adults are unbanked, including 80% of the citizens of Sierra Leone. Two of the major barriers to accessing financial services are a lack of formal identification and a lack of verifiable credit history.',
 				'The new Kiva Protocol is designed to address these barriers by using distributed ledger technology to issue digital identification to all citizens. Through the Kiva Protocol, both formal and informal financial institutions (from banks to shopkeepers giving credit) can help contribute to a person’s credit history.',
@@ -47,9 +57,23 @@ export default {
 <style lang="scss" scoped>
 	@import 'settings';
 
+	#banner-container {
+		background: darkblue;
+	}
+
+	#banner-content {
+		background: #ffffff;
+	}
+
 	#banner-text {
 		@include impact-text();
 
 		color: #3361A0;
+	}
+
+	#charity-navigator-container {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
 	}
 </style>
