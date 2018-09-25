e25cbc112409be5c3a2959370603e54064d00309
Karan Sekhri <ksekhri@gmail.com>
2018-09-25 16:48:31 -0700

    Add partners and press, cleanup styles

--- src/pages/Protocol.vue
+++ src/pages/Protocol.vue
@@ -6,21 +6,84 @@
 					Building the credit bureau of the future
 				</div>
 				<div id="banner-logo">
-					<img src="~@/assets/images/protocol/kiva-protocol.svg">
+					<img id="banner-logo-image" src="~@/assets/images/protocol/kiva-protocol.svg">
 				</div>
 			</div>
 		</div>
 		<div class="row" id="intro">
 			<div class="small-12 columns">
-				<p v-for="(paragraph, index) in infoText" :key="index">{{ paragraph }}</p>
-				<p><a href="#">How the Protocol works</a></p>
+				<h3 id="intro-title">{{ introTitle }}</h3>
+				<p v-for="(paragraph, index) in introText" :key="index">{{ paragraph }}</p>
+				<p><a href="#">Learn more about how the Protocol works</a></p>
+			</div>
+		</div>
+		<div class="row" id="partners">
+			<div class="small-12 columns">
+				<div id="partners-title">In partnership with</div>
+				<div id="partners-row">
+					<div class="partners-block">
+						<img
+							class="partners-image"
+							src="~@/assets/images/protocol/partner-sierra-leone.svg"
+							alt="Sierra Leone"
+						>
+					</div>
+					<div class="partners-block">
+						<img
+							class="partners-image"
+							src="~@/assets/images/protocol/partner-undp.svg"
+							alt="UNDP"
+						>
+					</div>
+					<div class="partners-block">
+						<img
+							class="partners-image"
+							src="~@/assets/images/protocol/partner-UNCDF.svg"
+							alt="UNCDF"
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="press-container">
+			<div class="row" id="press">
+				<div class="press-block">
+					<div>
+						<img class="press-image" src="~@/assets/images/protocol/time.png" >
+					</div>
+					<div
+						class="press-text"
+					>
+						"The 0% small business loan that's for real."
+					</div>
+				</div>
+				<div class="press-block">
+					<div>
+						<img class="press-image" src="~@/assets/images/protocol/fast-company.png" >
+					</div>
+					<div
+						class="press-text"
+					>
+						"less impersonal than the data-driven approach taken by mainstream lenders..."
+					</div>
+				</div>
+				<div class="press-block">
+					<div>
+						<img class="press-image" src="~@/assets/images/protocol/wired.png" >
+					</div>
+					<div
+						class="press-text"
+					>
+						"Best perk: no interest"
+					</div>
+				</div>
 			</div>
 		</div>
 		<div id="support-container">
 			<div class="row">
 				<div class="small-12 large-6 columns">
 					<h2 id="donate-title">Support Kiva innovation</h2>
-					{{ supportText }}
+					<div id="support-text">{{ supportText }}</div>
 					<kv-custom-donation :default-value="100" />
 				</div>
 				<div class="small-10 small-offset-1 medium-8 medium-offset-2 large-5 large-offset-1 columns">
@@ -60,11 +123,12 @@ export default {
 		return {
 			/* eslint-disable max-len */
 			aboutText: 'Kiva is a nonprofit focused on expanding financial access for underserved people around the world. Since 2005, Kiva’s global community of 1.7 million lenders has crowdfunded more than $1.2 billion in microloans to 3 million borrowers in more than 80 countries. On Kiva’s lending platform, anyone can help a borrower start or grow a business, go to school and realize their potential. Kiva also works to address the systemic issues that keep communities from accessing the financial services they need, through projects like Kiva Protocol.',
-			infoText: [
-				'Kiva, Sierra Leone and U.N. agencies announced the first implementation of the Kiva Protocol on Sept. 27, 2018, at the U.N. General Assembly. The Kiva Protocol will be used to create a nationwide digital identification system designed to help the country’s 7 million citizens access the financial services they need to improve their lives.',
-				'Globally, 1.7 billion adults are unbanked, including 80% of the citizens of Sierra Leone. Two of the major barriers to accessing financial services are a lack of formal identification and a lack of verifiable credit history.',
-				'The new Kiva Protocol is designed to address these barriers by using distributed ledger technology to issue digital identification to all citizens. Through the Kiva Protocol, both formal and informal financial institutions (from banks to shopkeepers giving credit) can help contribute to a person’s credit history.',
+			introText: [
+				'Kiva, Sierra Leone and U.N. agencies announced the first implementation of Kiva Protocol on Sept. 27 at the U.N. General Assembly.  The Protocol will be used to create a nationwide digital identification system designed to help the country’s 7 million citizens access the services they need to improve their lives.',
+				'Globally, 1.7 billion adults are unbanked and can’t access critical services like loans and savings accounts. In Sierra Leone, only 20% of the population is financially included. Two of the major barriers to accessing financial services are a lack of formal identification and a lack of verifiable credit history.',
+				'The new Kiva Protocol™ is designed to address these barriers by issuing digital identification to all citizens and enabling formal and informal financial institutions to contribute to a person’s verifiable credit history.',
 			],
+			introTitle: 'Introducing Kiva Protocol™, an exciting new initiative to give unbanked people formal identity and secure control over their own credit information.',
 			supportText: 'Donate to Kiva to help us create innovative solutions to the global challenge of financial inclusion.',
 			/* eslint-enable max-len */
 		};
@@ -103,9 +167,15 @@ export default {
 		}
 	}
 
+	#banner-logo-image {
+		display: block;
+		max-width: 200px;
+		margin: 0 auto;
+	}
+
 	#banner-content {
 		background: #fff;
-		padding: 40px 80px;
+		padding: 60px 80px 40px 80px;
 		max-height: 500px;
 		max-width: 650px;
 	}
@@ -113,8 +183,8 @@ export default {
 	#banner-text {
 		@include impact-text();
 
-		color: #3361A0;
-
+		margin-bottom: 20px;
+		color: #43657D;
 		text-align: center;
 	}
 
@@ -124,9 +194,69 @@ export default {
 		margin-bottom: 80px;
 	}
 
+	#intro-title {
+		margin-bottom: 1rem;
+	}
+
 	/* Partners */
+	#partners {
+		margin-bottom: 80px;
+	}
+
+	#partners-title {
+		padding-bottom: 40px;
+		text-align: center;
+		font-size: 0.9rem;
+	}
+
+	#partners-row {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+
+	.partners-image {
+		height: 120px;
+	}
+
+	/* Press */
+	#press-container {
+		padding: 80px 0;
+		background: #EFEFEF;
+	}
+
+	#press {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+
+	.press-block {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-start;
+		width: 30%;
+		height: 110px;
+	}
+
+	.press-image {
+		height: 34px;
+	}
+
+	.press-text {
+		margin-top: 20px;
+		text-align: center;
+	}
 
 	/* Support */
+	#support-container {
+		padding: 80px 0 40px 0;
+	}
+
+	#support-text {
+		margin: 40px 0 30px 0;
+	}
 
 	/* About */
 
