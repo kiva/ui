495738caeb08a61bd092f9ec84c105263ea7f6b9
Karan Sekhri <ksekhri@gmail.com>
2018-09-25 13:55:54 -0700

    Update about and support sections

--- src/pages/Protocol.vue
+++ src/pages/Protocol.vue
@@ -2,28 +2,43 @@
 	<www-page>
 		<div id="banner-container">
 			<div id="banner-content">
-				<div id="banner-logo">
-					<img>
-				</div>
 				<div id="banner-text">
-					an exciting new initiative that could radically expand financial exclusion
+					Building the credit bureau of the future
+				</div>
+				<div id="banner-logo">
+					<img src="~@/assets/images/protocol/kiva-protocol.svg">
 				</div>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row" id="intro">
 			<div class="small-12 columns">
 				<p v-for="(paragraph, index) in infoText" :key="index">{{ paragraph }}</p>
 				<p><a href="#">How the Protocol works</a></p>
 			</div>
-			<div class="small-12 large-7 columns">
-				<h1>About Kiva</h1>
-				<p>{{ aboutText }}</p>
+		</div>
+		<div id="about-container">
+			<div class="row" id="about">
+				<div class="small-12 large-7 columns">
+					<h1>About Kiva</h1>
+					<p>{{ aboutText }}</p>
+				</div>
+				<div
+					id="charity-navigator-container"
+					class="small-12 medium-8 medium-offset-2 large-4 large-offset-1 columns"
+				>
+					<kv-charity-navigator />
+				</div>
 			</div>
-			<div
-				id="charity-navigator-container"
-				class="small-12 medium-8 medium-offset-2 large-4 large-offset-1 columns"
-			>
-				<kv-charity-navigator />
+		</div>
+		<div id="support-container">
+			<div class="row">
+				<div class="small-12 large-6 columns">
+					<h2 id="donate-title">Support Kiva innovation</h2>
+					{{ supportText }}
+				</div>
+				<div class="small-10 small-offset-1 medium-8 medium-offset-2 large-5 large-offset-1 columns">
+					<img src="~@/assets/images/10-years-billion-impact.jpg">
+				</div>
 			</div>
 		</div>
 	</www-page>
@@ -46,8 +61,9 @@ export default {
 				'Kiva, Sierra Leone and U.N. agencies announced the first implementation of the Kiva Protocol on Sept. 27, 2018, at the U.N. General Assembly. The Kiva Protocol will be used to create a nationwide digital identification system designed to help the country’s 7 million citizens access the financial services they need to improve their lives.',
 				'Globally, 1.7 billion adults are unbanked, including 80% of the citizens of Sierra Leone. Two of the major barriers to accessing financial services are a lack of formal identification and a lack of verifiable credit history.',
 				'The new Kiva Protocol is designed to address these barriers by using distributed ledger technology to issue digital identification to all citizens. Through the Kiva Protocol, both formal and informal financial institutions (from banks to shopkeepers giving credit) can help contribute to a person’s credit history.',
-				/* eslint-enable max-len */
 			],
+			supportText: 'Donate to Kiva to help us create innovative solutions to the global challenge of financial inclusion.',
+			/* eslint-enable max-len */
 		};
 	},
 	metaInfo: {
@@ -65,16 +81,23 @@ export default {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		background: darkblue;
+		background: url('~@/assets/images/protocol/protocol-hero.jpg') no-repeat;
+		background-size: cover;
 		padding: 0.625rem;
 		box-sizing: border-box;
 
 		* {
 			box-sizing: border-box;
 		}
-	}
 
-	@media print, screen and (min-width: 30em) {
+		@media print, screen and (min-width: 30em) {
+			padding: 2rem;
+		}
+
+		@media print, screen and (min-width: 42.5em) {
+			padding-top: 3.8rem;
+			padding-bottom: 3.8rem;
+		}
 	}
 
 	#banner-content {
@@ -88,13 +111,25 @@ export default {
 		@include impact-text();
 
 		color: #3361A0;
+
+		text-align: center;
 	}
 
 	/* Intro */
+	#intro {
+		margin-top: 80px;
+		margin-bottom: 80px;
+	}
 
 	/* Partners */
 
-	/* About */
+	/* About, Support */
+
+	#about-container,
+	#support-container {
+		background: #FAFAFA;
+	}
+
 	h1,
 	h2 {
 		color: $kiva-green;
@@ -107,4 +142,7 @@ export default {
 	}
 
 	/* Donate */
+	#donate-title {
+		font-weight: 400;
+	}
 </style>
