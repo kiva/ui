6e3b916026b97a73d34cc34d90b4290edc5c417d
Karan Sekhri <ksekhri@gmail.com>
2018-09-25 10:32:59 -0700

    Add details to Charity Navigator

--- src/components/Kv/KvCharityNavigator.vue
+++ src/components/Kv/KvCharityNavigator.vue
@@ -1,11 +1,34 @@
 <template functional>
-	<div class="container">
-		<div>Awarded for effectiveness and efficiency</div>
-		<div><img src="~@/assets/images/charity-navigator.png"></div>
-		<div>Charity Navigator's highest rating</div>
+	<div id="container">
+		<div id="title">Awarded for effectiveness and efficiency</div>
+		<div id="image"><img src="~@/assets/images/charity-navigator.png"></div>
+		<div id="subtitle">Charity Navigator's highest rating</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 	@import 'settings';
+
+	#container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		* {
+			text-align: center;
+		}
+	}
+
+	#title {
+		font-weight: 500;
+	}
+
+	#image {
+		max-width: 75%;
+	}
+
+	#subtitle {
+		font-size: 0.875rem;
+	}
 </style>
