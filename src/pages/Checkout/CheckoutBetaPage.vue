3d542d5954ff0725d71d612be349f19f0c66c0dd
Matt Stover <matthews@kiva.org>
2018-10-29 11:31:29 -0700

    Tweak some logic so async application of kiva codes is handled properly and timing of lightbox is long enough to read.

--- src/pages/Checkout/CheckoutBetaPage.vue
+++ src/pages/Checkout/CheckoutBetaPage.vue
@@ -203,6 +203,7 @@ export default {
 			loans: [],
 			donations: [],
 			redemption_credits: [],
+			hasFreeCredits: false,
 			totals: {},
 			updatingTotals: false,
 			showReg: true,
@@ -227,7 +228,7 @@ export default {
 				// check for free credit, bonus credit or lending rewards and redirect if present
 				// IMPORTANT: THIS IS DEPENDENT ON THE CheckoutBeta Experiment
 				// TODO: remove once bonus credit functionality is added
-				if (hasFreeCredits) {
+				if (hasFreeCredits && typeof window === 'undefined') {
 					// cancel the promise, returning a route for redirect
 					return Promise.reject({
 						path: '/basket',
@@ -250,6 +251,7 @@ export default {
 				_get(data, 'shop.basket.credits.values'),
 				{ __typename: 'Credit', creditType: 'redemption_code' }
 			);
+			this.hasFreeCredits = _get(data, 'shop.basket.hasFreeCredits');
 			this.activeLoginDuration = parseInt(_get(data, 'general.activeLoginDuration.value'), 10) || 3600;
 			this.lastActiveLogin = parseInt(_get(data, 'my.lastActiveLogin.data'), 10) || 0;
 		}
@@ -272,6 +274,11 @@ export default {
 		// Run our validate items method once in the client on page load
 		if (this.isLoggedIn) {
 			this.validatePreCheckout();
+		}
+
+		// check for free credits
+		if (this.hasFreeCredits) {
+			this.refreshTotals('kiva-card-applied');
 		}
 	},
 	computed: {
@@ -370,7 +377,7 @@ export default {
 					}
 					this.redirectLbVisible = true;
 					// automatically redirect to legacy after 7 seconds
-					window.setTimeout(this.redirectToLegacy(), 7000);
+					window.setTimeout(this.redirectToLegacy, 7000);
 				} else {
 					this.setUpdatingTotals(false);
 				}
