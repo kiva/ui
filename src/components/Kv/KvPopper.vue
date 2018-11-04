f3390234a28dad25bcd197f5230086fa6492454c
Matt Stover <matthews@kiva.org>
2018-11-04 12:48:52 -0800

    make fastfade fast and remove commented out styles

--- src/components/Kv/KvPopper.vue
+++ src/components/Kv/KvPopper.vue
@@ -26,6 +26,7 @@ export default {
 		controller: { type: String, required: true },
 		openDelay: { type: Number, default: 0 },
 		closeDelay: { type: Number, default: 200 },
+		// must be defined in our globa/transitions.scss
 		transitionType: { type: String, default: '' },
 		popperPlacement: { type: String, default: 'bottom-start' },
 		popperModifiers: { type: Object, default: () => {} }
@@ -168,106 +169,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// @import 'settings';
 @import 'global/transitions';
 
-// $arrow-border-width: rem-calc(14);
-
-// .fade-enter-active,
-// .fade-leave-active {
-// 	transition: opacity 0.3s;
-// }
-
-// .fade-enter,
-// .fade-leave-to {
-// 	opacity: 0;
-// }
-
-.tooltip-pane {
+.popper-pane {
 	position: absolute;
-	// background: $aqua-light-green;
-	// border-radius: rem-calc(3);
-	// box-shadow: 0 2px 0 rgba(196, 231, 219, 0.8);
-	// border: 1px solid $aqua-medium-green;
 }
-
-// .tooltip-content {
-// 	padding: 1rem;
-// 	max-width: rem-calc(250);
-// 	line-height: 1.4;
-// }
-
-// .tooltip-content .title-slot {
-// 	font-weight: 800;
-// 	margin-bottom: 0.75rem;
-// }
-
-// .tooltip-arrow {
-// 	width: 0;
-// 	height: 0;
-// 	border-style: solid;
-// 	position: absolute;
-// 	margin: $arrow-border-width;
-// 	border-color: $aqua-medium-green;
-// }
-
-// .tooltip-pane[x-placement^="top"] {
-// 	margin-bottom: $arrow-border-width;
-// }
-
-// .tooltip-pane[x-placement^="top"] .tooltip-arrow {
-// 	border-width: $arrow-border-width $arrow-border-width 0 $arrow-border-width;
-// 	border-left-color: transparent;
-// 	border-right-color: transparent;
-// 	border-bottom-color: transparent;
-// 	bottom: -$arrow-border-width;
-// 	left: calc(50% - #{$arrow-border-width});
-// 	margin-top: 0;
-// 	margin-bottom: 0;
-// }
-
-// .tooltip-pane[x-placement^="bottom"] {
-// 	margin-top: $arrow-border-width;
-// }
-
-// .tooltip-pane[x-placement^="bottom"] .tooltip-arrow {
-// 	border-width: 0 $arrow-border-width $arrow-border-width $arrow-border-width;
-// 	border-left-color: transparent;
-// 	border-right-color: transparent;
-// 	border-top-color: transparent;
-// 	top: -$arrow-border-width;
-// 	left: calc(50% - #{$arrow-border-width});
-// 	margin-top: 0;
-// 	margin-bottom: 0;
-// }
-
-// .tooltip-pane[x-placement^="right"] {
-// 	margin-left: $arrow-border-width;
-// }
-
-// .tooltip-pane[x-placement^="right"] .tooltip-arrow {
-// 	border-width: $arrow-border-width $arrow-border-width $arrow-border-width 0;
-// 	border-left-color: transparent;
-// 	border-top-color: transparent;
-// 	border-bottom-color: transparent;
-// 	left: -$arrow-border-width;
-// 	top: calc(50% - #{$arrow-border-width});
-// 	margin-left: 0;
-// 	margin-right: 0;
-// }
-
-// .tooltip-pane[x-placement^="left"] {
-// 	margin-right: $arrow-border-width;
-// }
-
-// .tooltip-pane[x-placement^="left"] .tooltip-arrow {
-// 	border-width: $arrow-border-width 0 $arrow-border-width $arrow-border-width;
-// 	border-top-color: transparent;
-// 	border-right-color: transparent;
-// 	border-bottom-color: transparent;
-// 	right: -$arrow-border-width;
-// 	top: calc(50% - #{$arrow-border-width});
-// 	margin-left: 0;
-// 	margin-right: 0;
-// }
 </style>
