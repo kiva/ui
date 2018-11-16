2b6347995bd32edd0beeff5f4e7003122208e822
Matt Stover <matthews@kiva.org>
2018-11-15 22:35:10 -0800

    fix cherry-pickin merge conflicts

--- src/components/WwwFrame/PromotionalBanner/GlobalPromotionalBanner.vue
+++ src/components/WwwFrame/PromotionalBanner/GlobalPromotionalBanner.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import _get from 'lodash/get';
 import { settingEnabled } from '@/util/settingsUtils';
 import promoQuery from '@/graphql/query/promotionalBanner.graphql';
 import GiftBanner from './Banners/GiftBanner';
