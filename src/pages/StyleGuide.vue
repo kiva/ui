89d11aebf5d4096e6bb8744553bf37800af05914
joshua starkey <joshua@kiva.org>
2018-11-26 16:53:17 -0800

    rest of the color, just need to organize them

--- src/pages/StyleGuide.vue
+++ src/pages/StyleGuide.vue
@@ -181,40 +181,143 @@
 
 		<section>
 			<h2>Colors</h2>
-
-			<h4>Primary</h4>
 			<hr>
 			<div class="row">
 				<div class="columns small-2 color-card">
-					<div class="color-box green"></div>
-					<div class="color-hex small-text">#4fafe</div>
-					<div class="color-name">$green</div>
+					<div class="color-box kiva-green"></div>
+					<div class="color-hex small-text">#4faf4e</div>
+					<div class="color-name">$kiva-green</div>
 				</div>
 				<div class="columns small-2 color-card">
-					<div class="color-box dark-green"></div>
+					<div class="color-box kiva-dark-green"></div>
 					<div class="color-hex small-text">#247037</div>
-					<div class="color-name">$dark-green</div>
+					<div class="color-name">$kiva-dark-green</div>
 				</div>
-
 				<div class="columns small-2 color-card">
-					<div class="color-box graphic-green"></div>
+					<div class="color-box kiva-accent-green"></div>
+					<div class="color-hex small-text">#aee15c</div>
+					<div class="color-name">$kiva-accent-green</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-icon-green"></div>
 					<div class="color-hex small-text">#49953f</div>
-					<div class="color-name">$graphic-green</div>
+					<div class="color-name">$kiva-icon-green</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-accent-blue"></div>
+					<div class="color-hex small-text">#118aec</div>
+					<div class="color-name">$kiva-accent-blue</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-accent-darkblue"></div>
+					<div class="color-hex small-text">#006ed3</div>
+					<div class="color-name">$kiva-accent-darkblue</div>
 				</div>
 			</div>
 
-			<h4>Secondary</h4>
-			<hr>
+			<div class="row">
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-accent-red"></div>
+					<div class="color-hex small-text">#d74937</div>
+					<div class="color-name">$kiva-accent-red</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-accent-purple"></div>
+					<div class="color-hex small-text">#6859c0</div>
+					<div class="color-name">$kiva-accent-purple</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-alert-yellow"></div>
+					<div class="color-hex small-text">#fee259</div>
+					<div class="color-name">$kiva-alert-yellow</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-bg-lightgray"></div>
+					<div class="color-hex small-text">#fafafa</div>
+					<div class="color-name">$kiva-bg-lightgray</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-bg-darkgray"></div>
+					<div class="color-hex small-text">#efefef</div>
+					<div class="color-name">$kiva-bg-darkgray</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-stroke-gray"></div>
+					<div class="color-hex small-text">#d8d8d8</div>
+					<div class="color-name">$kiva-stroke-gray</div>
+				</div>
+			</div>
 
-			<h4>Background</h4>
-			<hr>
+			<div class="row">
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-text-dark"></div>
+					<div class="color-hex small-text">#484848</div>
+					<div class="color-name">$kiva-text-dark</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-text-medium"></div>
+					<div class="color-hex small-text">#666</div>
+					<div class="color-name">$kiva-text-medium</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-text-light"></div>
+					<div class="color-hex small-text">#999</div>
+					<div class="color-name">$kiva-text-light</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-textlink-visited"></div>
+					<div class="color-hex small-text">#0066a9</div>
+					<div class="color-name">$kiva-textlink-visited</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box kiva-filter-indicator-bg"></div>
+					<div class="color-hex small-text">#d2f0f8</div>
+					<div class="color-name">$kiva-filter-indicator-bg</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box shadow-blue"></div>
+					<div class="color-hex small-text">#1f92ba</div>
+					<div class="color-name">$shadow-blue</div>
+				</div>
+			</div>
 
-			<h4>Text and links</h4>
-			<hr>
-
-			<h4>Graph & Chart elements</h4>
-			<hr>
-
+			<div class="row">
+				<div class="columns small-2 color-card">
+					<div class="color-box shadow-lightblue"></div>
+					<div class="color-hex small-text">#6badc2</div>
+					<div class="color-name">$shadow-lightblue</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box aqua-light-green"></div>
+					<div class="color-hex small-text">#DDFFF4</div>
+					<div class="color-name">$aqua-light-green</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box aqua-medium-green"></div>
+					<div class="color-hex small-text">#C4E7DB</div>
+					<div class="color-name">$aqua-medium-green</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box white"></div>
+					<div class="color-hex small-text">#FFF</div>
+					<div class="color-name">$white</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box black"></div>
+					<div class="color-hex small-text">#000</div>
+					<div class="color-name">$black</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box subtle-gray"></div>
+					<div class="color-hex small-text">#CCC</div>
+					<div class="color-name">$subtle-gray</div>
+				</div>
+				<div class="columns small-2 color-card">
+					<div class="color-box dark-charcoal"></div>
+					<div class="color-hex small-text">#333</div>
+					<div class="color-name">$dark-charcoal</div>
+				</div>
+			</div>
 		</section>
 	</div>
 </template>
@@ -268,10 +371,10 @@ export default {
 	font-size: 80%;
 }
 
+// Colors section
 .color-card {
 	text-align: center;
 	padding: 5px;
-	border: 1px solid black;
 }
 
 .color-box {
@@ -288,16 +391,104 @@ export default {
 	font-weight: bold;
 }
 
-.green {
+.kiva-green {
 	background: $green;
 }
 
-.dark-green {
+.kiva-dark-green {
 	background: $dark-green;
 }
 
-.graphic-green {
+.kiva-icon-green {
 	background: $graphic-green;
+}
+
+.kiva-accent-green {
+	background: $light-green;
+}
+
+.kiva-accent-blue {
+	background: $blue;
+}
+
+.kiva-accent-darkblue {
+	background: $dark-blue;
+}
+
+.kiva-accent-red {
+	background: $red;
+}
+
+.kiva-accent-purple {
+	background: $purple;
+}
+
+.kiva-alert-yellow {
+	background: $vivid-yellow;
+}
+
+.kiva-bg-lightgray {
+	background: $ghost;
+}
+
+.kiva-bg-darkgray {
+	background: $ghost;
+}
+
+.kiva-stroke-gray {
+	background: $platinum;
+}
+
+.kiva-text-dark {
+	background: $charcoal;
+}
+
+.kiva-text-medium {
+	background: $dark-gray;
+}
+
+.kiva-text-light {
+	background: $gray;
+}
+
+.kiva-textlink-visited {
+	background: $faded-blue;
+}
+
+.kiva-filter-indicator-bg {
+	background: $pale-blue;
+}
+
+.shadow-blue {
+	background: $shadow-blue;
+}
+
+.shadow-lightblue {
+	background: $shadow-lightblue;
+}
+
+.aqua-light-green {
+	background: $aqua-light-green;
+}
+
+.aqua-medium-green {
+	background: $aqua-medium-green;
+}
+
+.white {
+	background: $white;
+}
+
+.black {
+	background: $black;
+}
+
+.subtle-gray {
+	background: $subtle-gray;
+}
+
+.dark-charcoal {
+	background: $dark-charcoal;
 }
 
 #section-form {
