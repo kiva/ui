e2112bc20f6610c6e26e4db0ef9ca20432599ece
Karan Sekhri <ksekhri@gmail.com>
2018-12-18 14:57:19 -0800

    Add multi code block

--- src/components/Kv/KvMultiCodeBlock.vue
+++ src/components/Kv/KvMultiCodeBlock.vue
@@ -26,12 +26,67 @@ export default {
 		return {
 			code: [
 				{
-					title: 'JS',
-					snippet: 'dfghjkl;rgthjklghjkl',
+					// eslint-disable-next-line no-script-url
+					title: 'Javascript: POST Method',
+					snippet:
+`fetch('https://api.kivaws.org/graphql', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query: "{lend {loan (id: 1568001){id name}}}" }),
+})
+  .then(res => res.json())
+  .then(res => console.log(res.data));
+`,
 				},
 				{
-					title: 'ES',
-					snippet: '123',
+					title: 'Python 3: GET request',
+					snippet:
+`# this snippet requires the requests library which can be installed
+# via pip with the command: pip install requests
+import requests
+
+base_url = 'https://api.kivaws.org/graphql?query='
+
+graphql_query = "{lend {loan (id: 1568001){id name}}}"
+
+r = requests.get(base_url+ graphql_query )
+r.json()
+`,
+				},
+				{
+					title: 'Python 3: POST request',
+					snippet:
+`# this snippet requires the requests library which can be installed
+# via pip with the command: pip install requests
+import requests
+
+base_url = 'https://api.kivaws.org/graphql?query='
+
+graphql_query = "{lend {loan (id: 1568001){id name}}}"
+
+r = requests.post(base_url+ graphql_query )
+r.json()
+`,
+				},
+				{
+					title: 'CURL: GET method',
+					snippet:
+`curl \\
+  -X GET \\
+  -H "Content-Type: application/json" \\
+  --data '{ "query": "{lend {loan (id: 1568001){id name}}}"}' \\
+  'https://api.kivaws.org/graphql'
+`,
+				},
+				{
+					title: 'CURL: POST method',
+					snippet:
+`curl \\
+  -X POST \\
+  -H "Content-Type: application/json" \\
+  --data '{ "query": "{lend {loan (id: 1568001){id name}}}"}' \\
+  'https://api.kivaws.org/graphql'
+`,
 				},
 			].map((codeObject, index) => ({ ...codeObject, index })),
 			selectedIndex: 0,
@@ -75,17 +130,20 @@ export default {
 
 	.tab {
 		border-bottom: 1px solid rgba(17, 138, 238, 0);
+		text-align: center;
 		padding: 0.5rem 0.75rem;
 		cursor: pointer;
 		user-select: none;
-		transition: color 0.18s linear, border-bottom 0.18s linear;
+		transition: color 0.16s linear, border-bottom 0.16s linear, text-shadow 0.16s linear;
 
-		&:hover, &.active {
+		&:hover,
+		&.active {
 			color: $blue;
 		}
 
 		&.active {
 			border-bottom: 1px solid rgba(17, 138, 238, 1);
+			text-shadow: 0.5px 0 $blue;
 		}
 	}
 }
