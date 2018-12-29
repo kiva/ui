d87840fb4605f7dd0447234b46b89a687dc6448e
Matt Stover <matthews@kiva.org>
2018-12-29 11:03:35 -0800

    Update global mixins to use plugin wrapper

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,15 +1,15 @@
 import changeCaseFilter from './change-case-filter';
-import apolloMixin from './apollo-mixin';
+import apolloMixin from './apollo-plugin';
 import numeralFilter from './numeral-filter';
 import kvAnayltics from './kv-analytics-plugin';
-import tipMessage from './tip-message-mixin';
+import tipMessage from './tip-message-plugin';
 
 export default {
 	install: Vue => {
-		Vue.mixin(apolloMixin);
+		apolloMixin(Vue);
 		Vue.filter('changeCase', changeCaseFilter);
 		Vue.filter('numeral', numeralFilter);
 		kvAnayltics(Vue);
-		Vue.mixin(tipMessage);
+		tipMessage(Vue);
 	}
 };
