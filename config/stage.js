8228946ef88704fb9bb376cbddff3fba4b0475d6
Matt Stover <matthews@kiva.org>
2019-01-04 12:23:38 -0800

    new config for stage server

---
+++ config/stage.js
@@ -0,0 +1,33 @@
+var merge = require('webpack-merge')
+var base = require('./index.js')
+
+module.exports = merge(base, {
+	app: {
+		publicPath: 'https://www-stage-kiva-org.global.ssl.fastly.net/ui/',
+		graphqlUri: 'https://www.stage.kiva.org/ajax/graphql',
+		enablePerimeterx: true,
+		perimeterxAppId: 'PX5u4Lz98O',
+		enableAnalytics: true,
+		enableGTM: true,
+		googleTagmanagerId: 'GTM-K6HR28',
+		enableGA: true,
+		gaId: 'UA-11686022-5',
+		enableSnowplow: true,
+		snowplowUri: 'events.fivetran.com/snowplow/v5qt54ocr2nm',
+		enableFB: true,
+		fbApplicationId: '2260431077572912',
+		fbPixelId: '',
+		fbOgNameSpace: 'stage-kiva',
+		enableSentry: true,
+		sentryURI: 'https://7ce141b23c4a4e6091c206d08442f0e9@sentry.io/1201287',
+		algoliaAppId: 'H4ONVZQ2C6',
+		algoliaApiKey: 'a373a52c000e929706c9e02a5862a327',
+		algoliaDefaultIndex: 'dev_all_loans'
+	},
+	server: {
+		graphqlUri: 'https://api.stage.kivaws.org/graphql',
+		sessionUri: 'https://www.stage.kiva.org/start-ui-session',
+		memcachedEnabled: true,
+		memcachedServers: 'stage-web-01:11211',
+	}
+})
