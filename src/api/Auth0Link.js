3af96b89fd5e59e67bdabc4db27b22f3ddb3c0d2
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2019-01-10 19:46:57 -0800

    AUTH-58 refactor to handle multiple async requests, allow retry during prefetch

--- src/api/Auth0Link.js
+++ src/api/Auth0Link.js
@@ -1,6 +1,7 @@
 import { setContext } from 'apollo-link-context';
 import _set from 'lodash/set';
 
+// Add the user info to the context and add the access token to the authorization header
 function getAuthContext(context, user, token) {
 	_set(context, 'user', user);
 	if (token) {
@@ -10,24 +11,20 @@ function getAuthContext(context, user, token) {
 }
 
 export default kvAuth0 => {
-	let fetching = 0;
-
 	return setContext((operation, previousContext) => {
+		// If auth0 is not enabled, don't add anything to the context
 		if (!kvAuth0.enabled) return getAuthContext(previousContext);
 
-		if (kvAuth0.user || fetching > 0) {
+		// If we already have user info, just add that to the context
+		if (kvAuth0.user) {
 			return getAuthContext(previousContext, kvAuth0.user, kvAuth0.accessToken);
 		}
 
+		// Check to see if the user is authenticated and then add that info to the context
 		return new Promise((resolve, reject) => {
-			fetching += 1;
 			kvAuth0.checkSession().then(() => {
-				fetching -= 1;
 				resolve(getAuthContext(previousContext, kvAuth0.user, kvAuth0.accessToken));
-			}).catch(e => {
-				fetching -= 1;
-				reject(e);
-			});
+			}).catch(reject);
 		});
 	});
 };
--- src/util/KvAuth0.js
+++ src/util/KvAuth0.js
@@ -1,3 +1,11 @@
+// These symbols are unique, and therefore are private to this scope.
+// For more details, see https://medium.com/@davidrhyswhite/private-members-in-es6-db1ccd6128a5
+const assertBrowser = Symbol('assertBrowser');
+const authCallback = Symbol('authCallback');
+const loginPromise = Symbol('loginPromise');
+const sessionPromise = Symbol('sessionPromise');
+
+// Class to handle interacting with auth0 in the browser
 export default class KvAuth0 {
 	constructor({
 		accessToken = '',
@@ -14,6 +22,8 @@ export default class KvAuth0 {
 		this.isServer = typeof window === 'undefined';
 
 		if (!this.isServer) {
+			// delay loading of webauth until we've confirmed we're in the browser,
+			// as it complains about not having window
 			this.webAuth = import('auth0-js').then(({ WebAuth }) => {
 				return new WebAuth({
 					audience,
@@ -27,23 +37,28 @@ export default class KvAuth0 {
 		}
 	}
 
-	authCallback(err, result) {
+	// Default handler for the authentication result
+	[authCallback](err, result) {
 		return new Promise((resolve, reject) => {
 			if (err) {
 				switch (err.error) {
 					case 'login_required':
 					case 'consent_required':
 					case 'interaction_required':
+						// These errors require interaction beyond what can be provided by webauth,
+						// so resolve quietly without authentication
 						this.user = null;
 						this.accessToken = '';
 						console.log(`Auth session not started (${err.error_description})`);
 						resolve();
 						break;
 					default:
+						// Everything else, actually throw an error
 						console.error(err);
 						reject(err);
 				}
 			} else {
+				// Successful authentication
 				this.user = result.idTokenPayload;
 				this.accessToken = result.accessToken;
 				// handle expiration?
@@ -52,35 +67,64 @@ export default class KvAuth0 {
 		});
 	}
 
-	checkSession() {
+	// Throw an error if this gets called while not in a browser
+	[assertBrowser]() {
 		if (this.isServer) return Promise.reject(new Error('Client auth called in server mode'));
-		return this.webAuth.then(webAuth => new Promise((resolve, reject) => {
-			webAuth.checkSession({}, (err, result) => {
-				this.authCallback(err, result).then(resolve).catch(reject);
-			});
-		}));
 	}
 
+	// Silently check for a logged in session with auth0 using hidden iframes
+	checkSession() {
+		this[assertBrowser]();
+
+		// Ensure that we only check the session once at a time
+		if (this[sessionPromise]) return this[sessionPromise];
+
+		this[sessionPromise] = this.webAuth.then(webAuth => new Promise((resolve, reject) => {
+			webAuth.checkSession({}, (err, result) => {
+				this[authCallback](err, result).then(resolve).catch(reject);
+			});
+		}));
+
+		this[sessionPromise].finally(() => {
+			this[sessionPromise] = null;
+		});
+
+		return this[sessionPromise];
+	}
+
+	// Open a popup window to the login page
 	popupLogin() {
-		if (this.isServer) return Promise.reject(new Error('Client auth called in server mode'));
-		return this.webAuth.then(webAuth => new Promise((resolve, reject) => {
+		this[assertBrowser]();
+
+		// Ensure we only ask to login once at a time
+		if (this[loginPromise]) return this[loginPromise];
+
+		this[loginPromise] = this.webAuth.then(webAuth => new Promise((resolve, reject) => {
 			webAuth.popup.authorize({
 				popupOptions: {
 					width: 480,
 					height: 740,
 				},
 			}, (err, result) => {
-				this.authCallback(err, result).then(resolve).catch(reject);
+				this[authCallback](err, result).then(resolve).catch(reject);
 			});
 		}));
+
+		this[loginPromise].finally(() => {
+			this[loginPromise] = null;
+		});
+
+		return this[loginPromise];
 	}
 
+	// Handle the auth0 callback in the popup frame
 	popupCallback() {
-		if (this.isServer) return Promise.reject(new Error('Client auth called in server mode'));
+		this[assertBrowser]();
 		return this.webAuth.then(webAuth => webAuth.popup.callback());
 	}
 }
 
+// Provide a mock class for testing and disabling auth0 usage
 export const MockKvAuth0 = {
 	enabled: false,
 	user: {},
