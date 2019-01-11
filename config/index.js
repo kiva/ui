569501fe01f999ae8d1ac6cd57992eb4fb7427f1
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2019-01-10 19:39:09 -0800

    AUTH-58 update configs

--- config/index.js
+++ config/index.js
@@ -4,6 +4,7 @@ module.exports = {
 	app: {
 		publicPath: 'https://www-kiva-org.global.ssl.fastly.net/ui/',
 		graphqlUri: 'https://www.kiva.org/ajax/graphql',
+		// graphqlUri: 'https://api.kivaws.org/graphql', // when using auth0
 		enablePerimeterx: true,
 		perimeterxAppId: 'PXr3pNVz1F',
 		enableAnalytics: true,
@@ -20,12 +21,12 @@ module.exports = {
 		enableSentry: true,
 		sentryURI: 'https://3ab8031cd8bf45d48f79e2b77657e16e@sentry.io/1201288',
 		enableAuth0: false,
-		auth0ApiAudience: 'https://api.dev.kivaws.org/graphql',
-		auth0BrowserClientID: 'ouGKxT4mE4wQEKqpfsHSE96c9rHXQqZF',
-		auth0ServerClientID: 'KIzjUBQjKZwMRgYSn6NvMxsUwNppwnLH',
+		auth0ApiAudience: 'https://api.kivaws.org/graphql',
+		auth0BrowserClientID: 'ouGKxT4mE4wQEKqpfsHSE96c9rHXQqZF', // @todo update with prod app id
+		auth0ServerClientID: 'KIzjUBQjKZwMRgYSn6NvMxsUwNppwnLH', // @todo update with prod app id
 		auth0BrowserCallbackUri: 'https://www.kiva.org/process-browser-auth',
 		auth0ServerCallbackUri: 'https://www.kiva.org/process-ssr-auth',
-		auth0Domain: 'login.dev.kiva.org',
+		auth0Domain: 'login.kiva.org',
 		auth0Scope: 'https://www.kiva.org/last_login ' +
 			'https://www.kiva.org/kiva_id ' +
 			'https://www.kiva.org/context.connectionStrategy ' +
