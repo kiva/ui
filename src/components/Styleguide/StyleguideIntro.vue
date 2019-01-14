ba54658dfb9b415d56e67821138655e2adbc28e3
Matt Stover <matthews@kiva.org>
2019-01-13 17:44:23 -0800

    Foundation note and callout on intro page.

--- src/components/Styleguide/StyleguideIntro.vue
+++ src/components/Styleguide/StyleguideIntro.vue
@@ -4,7 +4,7 @@
 
 		<p>The basis for Kiva's web styleguide is Zurb's
 			<a href="https://foundation.zurb.com/sites/docs/" target="_blank">Foundation for Sites</a>
-			responsive front-end framework</p>
+			responsive front-end framework.</p>
 
 		<h2>Sensible HTML Element Defaults</h2>
 		<p>Foundation provides a set of sensible defaults for all HTML elements by way of the
@@ -15,7 +15,23 @@
 				_settings.scss</a>
 			file.</p>
 
-		<p><i>* We only use the scss portion of the Foundation toolset, opting not to include any of their
-		javascript or any components that depend on that inclusion.</i></p>
+		<div class="callout warning">
+			<h3><strong>No Foundation Javascript</strong></h3>
+			<p class="small-text">
+				We only use the scss portion of the Foundation toolset, opting NOT to include any of their
+				javascript or any components that depend on that inclusion.</p>
+			<p class="small-text">
+				This decision was made primarily due to the additional filesize that including
+				jQuery and other associated scripts would add.</p>
+		</div>
+
+		<p><strong>Look for Foundation Callouts in other sections for tips on how the framework is used.</strong></p>
 	</section>
 </template>
+
+<style lang="scss" scoped>
+@import 'settings';
+@import 'foundation';
+@include foundation-callout;
+</style>
+
