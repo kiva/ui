cbeadd1dc42c801108f3215c7d5bc0449a6942ec
Elli <ebalcomb@gmail.com>
2019-01-16 18:00:11 -0800

    AUTH-72: add new team joining page and component

---
+++ src/components/Forms/JoinTeamForm.vue
@@ -0,0 +1,98 @@
+<template>
+	<div>
+		<h2 v-if="inviterDisplayName" slot="title">{{ inviterDisplayName }} invited you to the {{ teamName }} team!</h2>
+		<h2 v-else slot="title">You're invited to the {{ teamName }} team!</h2>
+		<p>
+			<!-- eslint-disable-next-line max-len -->
+			By joining the team, you can see your impact, interact with teammates, and get more out of Kiva.
+		</p>
+		<div class="join-team-button-container">
+			<kv-button class="smaller secondary" @click.native.prevent="handleJoinTeamButton(false)">
+				No Thanks
+			</kv-button>
+			<kv-button class="smaller" @click.native.prevent="handleJoinTeamButton(true)">
+				Join Team
+			</kv-button>
+		</div>
+	</div>
+</template>
+
+<script>
+
+import KvButton from '@/components/Kv/KvButton';
+import TeamInfoFromId from '@/graphql/query/teamInfoFromId.graphql';
+import joinTeam from '@/graphql/mutation/joinTeam.graphql';
+import _get from 'lodash/get';
+
+export default {
+	components: {
+		KvButton,
+		TeamInfoFromId,
+	},
+	inject: ['apollo'],
+	props: {
+		teamId: {
+			type: Number,
+			required: true,
+		},
+	},
+	apollo: {
+		query: TeamInfoFromId,
+		prefetch: true,
+		variables() {
+			return {
+				team_id: this.teamId
+			};
+		},
+		result({ data }) {
+			this.teamName = _get(data, 'community.team.name');
+		},
+	},
+	data() {
+		return {
+			teamName: ''
+		};
+	},
+	methods: {
+		handleJoinTeamButton(join) {
+			if (join) {
+				console.log('joining');
+				console.log(this.teamId);
+				this.apollo.mutate({
+					mutation: joinTeam,
+					variables: {
+						team_id: this.teamId
+					}
+				}).catch(error => {
+					console.error(error);
+				});
+			} else {
+				// don't join team
+				console.log('not joining');
+			}
+		}
+	},
+	created() {
+		this.invitationId = this.$route.query.invitation_id;
+		this.teamId = Number(this.$route.query.team_id);
+		this.inviterId = this.$route.query.inviter_id;
+		this.inviterDisplayName = this.$route.query.inviter_display_name;
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+
+.join-team-button-container {
+	margin: 0 auto;
+	display: flex;
+	justify-content: space-between;
+	flex-direction: column;
+
+	@include breakpoint(medium) {
+		flex-direction: row;
+	}
+}
+</style>
---
+++ src/pages/LoginAndRegister/JoinTeam.vue
@@ -0,0 +1,51 @@
+<template>
+	<www-page>
+		<div class="row page-content">
+			<join-team-form :team-id="teamId"/>
+		</div>
+	</www-page>
+</template>
+
+<script>
+import JoinTeamForm from '@/components/Forms/JoinTeamForm';
+import WwwPage from '@/components/WwwFrame/WwwPage';
+
+export default {
+	components: {
+		WwwPage,
+		JoinTeamForm
+	},
+	data() {
+		return {
+		};
+	},
+	methods: {
+	},
+	created() {
+		this.teamId = Number(this.$route.query.team_id);
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'settings';
+.page-content {
+	padding: 1.625rem rem-calc(10);
+	max-width: 340px;
+
+	@include breakpoint(medium) {
+		padding: 1.625rem 0;
+	}
+}
+
+// Control form layouts from parent to allow independence in other layouts
+.join-team-form {
+	max-width: rem-calc(280);
+	margin: 0 auto;
+}
+
+// This spacer will be deleted
+.spacer {
+	margin: rem-calc(75) 0;
+}
+</style>
