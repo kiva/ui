9d899b544a3baacc6ed93a87391db65436d4cc78
joshua starkey <joshua@kiva.org>
2019-01-24 16:55:53 -0800

    toggle functioning

--- src/components/LoansByCategory/ViewToggle.vue
+++ src/components/LoansByCategory/ViewToggle.vue
@@ -1,13 +1,25 @@
 <template>
 	<div class="view-toggle">
-		<span class="browse-box">
-			<kv-icon name="browse-toggle" class="browse-icon"/>
-			<span class="small-text hide-for-small-only">Explore</span>
-		</span>
-		<span class="filter-box">
-			<kv-icon name="filters-toggle" class="filter-toggle-icon"/>
-			<span class="small-text hide-for-small-only">Filter</span>
-		</span>
+		<router-link
+			to="/lend-by-category"
+			class="browse-box">
+			<span>
+				<kv-icon
+					name="browse-toggle"
+					class="browse-toggle-icon"/>
+				<span class="small-text hide-for-small-only view-text">Explore</span>
+			</span>
+		</router-link>
+		<router-link
+			to="/lend"
+			class="filter-box">
+			<span>
+				<kv-icon
+					name="filters-toggle"
+					class="filter-toggle-icon"/>
+				<span class="small-text hide-for-small-only view-text">Filter</span>
+			</span>
+		</router-link>
 	</div>
 </template>
 
@@ -18,10 +30,15 @@ export default {
 	components: {
 		KvIcon,
 	},
+	data() {
+		return {
+			isSelected: false,
+		};
+	},
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang='scss' scoped>
 @import 'settings';
 
 .view-toggle {
@@ -34,16 +51,20 @@ export default {
 	}
 }
 
+.view-text {
+	color: $kiva-text-light;
+}
+
 .browse-box {
 	margin-right: 0.75rem;
 }
 
 .filter-box {
-	border-left: 1px solid black;
+	border-left: 1px solid $kiva-stroke-gray;
 	padding-left: 0.75rem;
 }
 
-.browse-icon,
+.browse-toggle-icon,
 .filter-toggle-icon {
 	display: block;
 	height: 30px;
@@ -52,8 +73,10 @@ export default {
 	fill: $kiva-text-light;
 }
 
-// Selected toggled color will be:
-// stroke: $kiva-green;
-// opacity: 0.85;
+// Changing the color of the selected toggle
+.router-link-active .browse-toggle-icon, {
+	fill: $kiva-green;
+	opacity: 0.85;
+}
 
 </style>
