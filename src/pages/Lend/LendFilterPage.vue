b0e1beae1651e7eb64218df984ed4b893ded40e7
Karan Sekhri <ksekhri@gmail.com>
2019-02-11 14:08:19 -0800

    CASH-452: Instantiate Algolia List View

--- src/pages/Lend/LendFilterPage.vue
+++ src/pages/Lend/LendFilterPage.vue
@@ -1,81 +1,132 @@
 <template>
 	<www-page class="lend-filter-page">
-		<div class="row heading-region-row">
-			<div class="heading-region column small-12">
-				<view-toggle />
-
-				<h1>Make a loan, <br class="hide-for-medium">change a life</h1>
-				<p class="page-subhead show-for-large">Each Kiva loan helps people build a better future for
-				themselves and their families.
-				</p>
+		<lend-header />
+		<div class="row page-content">
+			<div class="small-12 columns">
+				<ais-instant-search
+					v-if="searchClient"
+					:search-client="searchClient"
+					:index-name="algoliaIndex">
+					<!-- eslint-disable vue/attribute-hyphenation -->
+					<ais-configure
+						:hitsPerPage="12"
+						:filters="defaultFilter" />
+					<ais-current-refinements />
+					<ais-refinement-list :attribute="'sector.name'" />
+					<ais-hits
+						class="loan-card-group row small-up-1 large-up-2 xxlarge-up-3"
+						:results-per-page="12">
+						<template slot="default" slot-scope="{ items }">
+							<algolia-adapter
+								v-for="(item, itemIndex) in items" :key="itemIndex"
+								:loan="item"
+								:items-in-basket="itemsInBasket" />
+						</template>
+					</ais-hits>
+					<algolia-pagination :padding="2" />
+					<ais-stats />
+				</ais-instant-search>
 			</div>
 		</div>
 	</www-page>
 </template>
 
 <script>
+import _get from 'lodash/get';
+import _map from 'lodash/map';
 import LoadingOverlay from '@/pages/Lend/LoadingOverlay';
-import ViewToggle from '@/components/LoansByCategory/ViewToggle';
 import WwwPage from '@/components/WwwFrame/WwwPage';
+import LendHeader from '@/pages/Lend/LendHeader';
+
+import itemsInBasketQuery from '@/graphql/query/basketItems.graphql';
+import experimentSetting from '@/graphql/query/experimentSetting.graphql';
+import experimentQuery from '@/graphql/query/lendByCategory/experimentAssignment.graphql';
+
+// Algolia Imports
+import algoliasearch from 'algoliasearch/lite';
+import {
+	AisConfigure,
+	AisInstantSearch,
+	AisHits,
+	AisCurrentRefinements,
+	AisRefinementList,
+	AisStats,
+} from 'vue-instantsearch';
+import AlgoliaAdapter from '@/components/LoanCards/AlgoliaLoanCardAdapter';
+import AlgoliaPagination from '@/pages/Lend/AlgoliaPagination';
+
 
 export default {
 	components: {
 		LoadingOverlay,
-		ViewToggle,
 		WwwPage,
+		LendHeader,
+		AisConfigure,
+		AisInstantSearch,
+		AisHits,
+		AisCurrentRefinements,
+		AisRefinementList,
+		AisStats,
+		AlgoliaAdapter,
+		AlgoliaPagination,
 	},
-
 	metaInfo: {
 		title: 'Lend Filter'
 	},
-
-	created() {
-	},
-
 	mounted() {
+		// initialize searchClient + components on mount
+		// TODO: update initialization once vue-instantsearch V2 supports SSR
+		this.searchClient = algoliasearch(
+			this.algoliaConfig.appId,
+			this.algoliaConfig.apiKey
+		);
+	},
+	data() {
+		return {
+			// These are required in each instance of the plugin
+			algoliaAppId: this.algoliaConfig.appId,
+			algoliaApiKey: this.algoliaConfig.apiKey,
+			searchClient: null,
+			// The index will likey be different based on context
+			algoliaIndex: this.algoliaConfig.fundraisingIndex, // defaultIndex
+			defaultSearch: 'Energy',
+			// Focus in on fundraising Loans
+			defaultFilter: '', // No Need with new fundraising index 'status:fundraising',
+			itemsInBasket: null
+		};
+	},
+	inject: [
+		'apollo',
+		'algoliaConfig'
+	],
+	apollo: {
+		preFetch(config, client) {
+			return client.query({
+				query: itemsInBasketQuery,
+			}).then(({ data }) => {
+				this.itemsInBasket = _map(_get(data, 'shop.basket.items.values'), 'id');
+			}).then(() => {
+				// TODO: REMOVE Once CASH-103: Lend Increment Button EXP ENDS
+				const cash103 = [
+					// Pre-fetch the setting for lend increment button
+					client.query({ query: experimentSetting, variables: { key: 'uiexp.lend_increment_button' } }),
+					// Pre-fetch the assigned version for lend increment button
+					client.query({ query: experimentQuery, variables: { id: 'lend_increment_button' } }),
+				];
+
+				return Promise.all(cash103);
+			});
+		}
 	},
 };
 </script>
 
 <style lang="scss" scoped>
 @import 'settings';
-@import 'global/transitions';
 
 .lend-filter-page {
 	main {
 		background-color: $kiva-bg-lightgray;
-	}
-
-	.heading-region-row {
-		max-width: 63.75rem;
-	}
-
-	.heading-region {
-		margin: 2rem 0;
-		padding: 0 2.5rem;
-
-		.view-toggle {
-			margin: 0.125rem 0 0 0.375rem;
-			float: right;
-			display: flex;
-
-			@include breakpoint(large) {
-				margin: 0.375rem 0 0.375rem 0.375rem;
-			}
-		}
-
-		h1 {
-			margin: 0;
-		}
-
-		p {
-			margin-top: 0.75rem;
-		}
-
-		@media (hover: none) {
-			margin: 1rem 0;
-			padding: 0 1rem;
-		}
 	}
 }
 </style>
