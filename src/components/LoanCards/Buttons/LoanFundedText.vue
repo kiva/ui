656a733e3efebf365db34bca31a323ab1436871c
Chris Foo <foochris@gmail.com>
2019-02-11 13:26:40 -0800

    CASH-400 Update loan statua funded action button on loan card view

---
+++ src/components/LoanCards/Buttons/LoanFundedText.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="loan-funded-text small-text">
+		This loan has already funded!<br>
+		Please choose another one.
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+.loan-funded-text {
+	background-color: $very-light-green;
+	padding: 1rem;
+	text-align: center;
+}
+</style>
