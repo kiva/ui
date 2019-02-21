83ca4660fb23bfe4d65e6a92f8a6157716ea07cb
joshua starkey <joshua@kiva.org>
2019-02-20 16:21:03 -0800

    new reuseable kv-toggle component

---
+++ src/components/Kv/KvPillToggle.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<div
+			v-for="option in options" :key="option.key"
+			@click="pillToggled(option.key)"
+			:class="{ active: selected === option.key }"
+		>
+			<h3 class="">{{ option.title }}</h3>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		options: {
+			type: Array,
+			required: true
+		},
+		selected: {
+			type: String,
+			required: true
+		}
+	},
+	methods: {
+		pillToggled(key) {
+			this.$emit('pillToggled', key);
+		}
+	}
+};
+
+</script>
+
+<style lang="scss">
+@import "settings";
+
+.active {
+	background-color: red;
+}
+
+</style>
