6b4466a7e74a5a4106cf1cf0c3bd8667cce1e1f6
Karan Sekhri <ksekhri@gmail.com>
2019-02-22 14:07:52 -0800

    Wrap filters in their own section to allow DOM changes without resetting filters

---
+++ src/pages/Lend/Filter/FilterSectionCategories.vue
@@ -0,0 +1,33 @@
+<template>
+	<filter-menu-section :title="title" :open="open" :result-count="10">
+		<ais-refinement-list :attribute="'sector.name'" :sort-by="['name:asc']"/>
+	</filter-menu-section>
+</template>
+
+<script>
+import FilterMenuSection from '@/pages/Lend/Filter/FilterMenuSection';
+import { AisRefinementList } from 'vue-instantsearch';
+
+export default {
+	components: {
+		FilterMenuSection,
+		AisRefinementList,
+	},
+	props: {
+		title: {
+			type: String,
+			default: 'Categories',
+		},
+		open: {
+			type: Boolean,
+			default: true,
+		},
+		resultCount: {
+			type: Number,
+			required: true,
+		},
+	},
+};
+</script>
+<style>
+</style>
