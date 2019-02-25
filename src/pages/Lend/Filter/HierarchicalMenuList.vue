7dc41c4f32204794b5016b6144c759c542a71897
Karan Sekhri <ksekhri@gmail.com>
2019-02-25 13:36:45 -0800

    Add stub from Algolia

---
+++ src/pages/Lend/Filter/HierarchicalMenuList.vue
@@ -0,0 +1,27 @@
+<template functional>
+	<ul>
+		<li v-for="item in props.items" :key="item.value">
+			<a
+				:href="props.createURL(item.value)"
+				:style="{ fontWeight: item.isRefined ? 'bold' : '' }"
+				@click.prevent="props.refine(item.value)"
+			>
+				{{ item.label }} ({{ item.count }})
+			</a>
+			<!-- eslint-disable vue/attribute-hyphenation -->
+			<hierarchical-menu-list
+				v-if="item.data"
+				:items="item.data"
+				:refine="props.refine"
+				:createURL="props.createURL"
+			/>
+			<!-- eslint-enable vue/attribute-hyphenation -->
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	name: 'HierarchicalMenuList',
+};
+</script>
