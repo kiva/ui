59c309efa1f56fea5801e59c9d2eda2f32083cb4
Karan Sekhri <ksekhri@gmail.com>
2019-03-12 11:04:33 -0700

    CASH-402: Prep for ListLoanCard

---
+++ src/components/LoanCards/ListLoanCard.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="list-loan-card">
+		ListLoanCard Placeholder
+	</div>
+</template>
+
+<script>
+export default {
+
+	props: {
+		loan: {
+			type: Object,
+			default: () => {
+				return {
+					userProperties: {},
+					loanFundraisingInfo: {},
+					geocode: {
+						country: {},
+					},
+					image: {},
+				};
+			}
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+.list-loan-card {
+	background-color: $white;
+}
+</style>
