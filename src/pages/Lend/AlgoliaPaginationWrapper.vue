65d0cc6a91fd5e8d3281ca61200b32435f68a1bf
Chris Foo <foochris@gmail.com>
2019-03-14 17:41:08 -0400

    CASH-635 Update Algolia pagination component to align with current UI

---
+++ src/pages/Lend/AlgoliaPaginationWrapper.vue
@@ -0,0 +1,43 @@
+<template>
+	<ais-pagination :padding="padding">
+		<div slot-scope="{
+				currentRefinement,
+				nbPages,
+				pages,
+				isFirstPage,
+				isLastPage,
+				refine,
+				createURL
+			}"
+		>
+			<algolia-pagination
+				:current-refinement="currentRefinement"
+				:nb-pages="nbPages"
+				:pages="pages"
+				:is-first-page="isFirstPage"
+				:is-last-page="isLastPage"
+				:refine="refine"
+				:create-url="createURL"
+			/>
+		</div>
+	</ais-pagination>
+</template>
+
+<script>
+import { AisPagination } from 'vue-instantsearch';
+import AlgoliaPagination from '@/pages/Lend/AlgoliaPagination';
+
+export default {
+	components: {
+		AisPagination,
+		AlgoliaPagination,
+	},
+	props: {
+		padding: {
+			type: Number,
+			default: 0,
+		},
+	},
+};
+</script>
+
