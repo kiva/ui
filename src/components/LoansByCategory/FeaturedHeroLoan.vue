649b40b6181cd7b1779be24c4c6ae498449624c0
Chris Foo <foochris@gmail.com>
2019-03-21 08:48:51 -0700

    CASH-737 Add featured hero wrapper to use loan card controller

--- src/components/LoansByCategory/FeaturedHeroLoan.vue
+++ src/components/LoansByCategory/FeaturedHeroLoan.vue
@@ -1,106 +1,96 @@
 <template>
-	<div class="featured-section-wrapper" v-if="loan">
-		<div class="row">
-			<div class="column small-12">
-				<h2 class="section-name">Featured: Research-backed impact</h2>
-				<p v-if="showCategoryDescription" class="section-description show-for-large">
-					{{ loanChannel.description }}
-				</p>
-			</div>
-		</div>
-		<div class="featured-row-wrapper">
-			<div class="featured-cards-display-window">
-				<div class="grid-loan-card">
-					<div class="row featured-hero-loan">
-						<div class="column small-12 large-6">
-							<loan-card-image
-								:loan-id="loan.id"
-								:name="loan.name"
-								:retina-image-url="loan.image.retina"
-								:standard-image-url="loan.image.default"
-								:is-visitor="!isLoggedIn"
-								:is-favorite="loan.userProperties.favorited"
-								:image-enhancement-experiment-version="imageEnhancementExperimentVersion"
-								:loan-image-hash="loan.image.hash"
+	<div class="featured-row-wrapper">
+		<div class="featured-cards-display-window">
+			<div class="grid-loan-card">
+				<div class="row featured-hero-loan">
+					<div class="column small-12 large-6">
+						<loan-card-image
+							:loan-id="loan.id"
+							:name="loan.name"
+							:retina-image-url="loan.image.retina"
+							:standard-image-url="loan.image.default"
+							:is-visitor="isVisitor"
+							:is-favorite="loan.userProperties.favorited"
+							:image-enhancement-experiment-version="imageEnhancementExperimentVersion"
+							:loan-image-hash="loan.image.hash"
 
-								@track-loan-card-interaction="trackInteraction"
-								@favorite-toggled="toggleFavorite"
-							/>
-						</div>
-						<div class="column small-12 large-6">
-							<div class="borrower-info-wrapper">
-								<div class="name">
-									<router-link
-										:to="`/lend/${loan.id}`"
-										v-kv-track-event="['Lending', 'click-Read more', 'Name', loan.id, 'true']">
-										<span
-											@click="$emit('track-loan-card-interaction', {
+							@track-loan-card-interaction="trackInteraction"
+							@favorite-toggled="toggleFavorite"
+						/>
+					</div>
+					<div class="column small-12 large-6">
+						<div class="borrower-info-wrapper">
+							<div class="name">
+								<router-link
+									:to="`/lend/${loan.id}`"
+									v-kv-track-event="['Lending', 'click-Read more', 'Name', loan.id, 'true']">
+									<span
+										@click="$emit('track-interaction', {
+											interactionType: 'viewBorrowerPage',
+											interactionElement: 'borrowerName'
+										})"
+									>{{ loan.name }}</span>
+								</router-link>
+							</div>
+
+							<div class="country">
+								<!-- eslint-disable-next-line max-len -->
+								{{ loan.geocode.country.name }} <span v-if="loan.activity.name">/ {{ loan.activity.name }}</span>
+							</div>
+
+							<div class="use">
+								{{ loanUse }}
+
+								<div v-if="showReadMore">
+									<!-- eslint-disable-next-line max-len -->
+									<router-link :to="`/lend/${loan.id}`" v-kv-track-event="['Lending', 'click-Read more', 'Read more', loan.id, 'true']">
+										<div
+											@click="$emit('track-interaction', {
 												interactionType: 'viewBorrowerPage',
-												interactionElement: 'borrowerName'
+												interactionElement: 'readMore'
 											})"
-										>{{ loan.name }}</span>
+										>Read more</div>
 									</router-link>
 								</div>
+							</div>
 
-								<div class="country">
-									<!-- eslint-disable-next-line max-len -->
-									{{ loan.geocode.country.name }} <span v-if="loan.activity.name">/ {{ loan.activity.name }}</span>
-								</div>
-
-								<div class="use">
-									{{ loanUse }}
-
-									<div v-if="showReadMore">
-										<!-- eslint-disable-next-line max-len -->
-										<router-link :to="`/lend/${loan.id}`" v-kv-track-event="['Lending', 'click-Read more', 'Read more', loan.id, 'true']">
-											<div
-												@click="$emit('track-loan-card-interaction', {
-													interactionType: 'viewBorrowerPage',
-													interactionElement: 'readMore'
-												})"
-											>Read more</div>
-										</router-link>
-									</div>
-								</div>
-
-								<div class="why-special">
-									<div v-if="showFundraisingThermometer" class="fundraising-thermometer">
-										<fundraising-status
-											:amount-left="amountLeft"
-											:is-expiring-soon="false"
-											:is-funded="false"
-											:percent-raised="percentRaised"
-											:single-line="true"
-										/>
-									</div>
-									<div v-else>
-										<div class="bold">This loan is special because:</div>
-										{{ loan.whySpecial }}
-									</div>
-								</div>
-
-								<div class="action">
-									<action-button
-										:loan-id="loan.id"
-										:loan="loan"
-										:items-in-basket="itemsInBasket"
-										:is-lent-to="loan.userProperties.lentTo"
-										:is-funded="isFunded"
-										:is-selected-by-another="isSelectedByAnother"
-										:lend-increment-button-version="lendIncrementButtonVersion"
-
-										@click.native="trackInteraction({
-											interactionType: 'addToBasket',
-											interactionElement: 'Lend25'
-										})"
-									/>
-
-									<matching-text
-										:matching-text="loan.matchingText"
-										:is-funded="isFunded"
-										:is-selected-by-another="isSelectedByAnother"
+							<div class="why-special">
+								<div v-if="showFundraisingThermometer" class="fundraising-thermometer">
+									<fundraising-status
+										:amount-left="amountLeft"
+										:is-expiring-soon="false"
+										:is-funded="false"
+										:percent-raised="percentRaised"
+										:single-line="true"
 									/>
 								</div>
+								<div v-else>
+									<div class="bold">This loan is special because:</div>
+									{{ loan.whySpecial }}
+								</div>
+							</div>
+
+							<div class="action">
+								<action-button
+									:loan-id="loan.id"
+									:loan="loan"
+									:items-in-basket="itemsInBasket"
+									:is-lent-to="loan.userProperties.lentTo"
+									:is-funded="isFunded"
+									:is-selected-by-another="isSelectedByAnother"
+									:lend-increment-button-version="lendIncrementButtonVersion"
+
+									@click.native="trackInteraction({
+										interactionType: 'addToBasket',
+										interactionElement: 'Lend25'
+									})"
+								/>
+
+								<matching-text
+									:matching-text="loan.matchingText"
+									:is-funded="isFunded"
+									:is-selected-by-another="isSelectedByAnother"
+								/>
 							</div>
 						</div>
 					</div>
@@ -111,28 +101,13 @@
 </template>
 
 <script>
-import _filter from 'lodash/filter';
-import _get from 'lodash/get';
 import numeral from 'numeral';
-
-import {
-	differenceInMinutes,
-	differenceInHours,
-	differenceInDays
-} from 'date-fns';
-
-import experimentAssignmentQuery from '@/graphql/query/lendByCategory/experimentAssignment.graphql';
-import featuredLoansQuery from '@/graphql/query/featuredLoansData.graphql';
-import loanFavoriteMutation from '@/graphql/mutation/updateLoanFavorite.graphql';
 
 import ActionButton from '@/components/LoanCards/Buttons/ActionButton';
 import BorrowerInfo from '@/components/LoanCards/BorrowerInfo/BorrowerInfo';
 import FundraisingStatus from '@/components/LoanCards/FundraisingStatus';
 import LoanCardImage from '@/components/LoanCards/LoanCardImage';
 import MatchingText from '@/components/LoanCards/MatchingText';
-
-// research-backed impact category
-const featuredCategoryIds = [56];
 
 export default {
 	components: {
@@ -144,17 +119,29 @@ export default {
 	},
 	inject: ['apollo'],
 	props: {
-		enableTracking: {
-			type: Boolean,
-			default: false,
+		amountLeft: {
+			type: Number,
+			default: 0,
+		},
+		experimentData: {
+			type: Object,
+			default: () => {},
 		},
 		imageEnhancementExperimentVersion: {
 			type: String,
 			default: ''
 		},
-		isLoggedIn: {
+		isFunded: {
 			type: Boolean,
 			default: false
+		},
+		isSelectedByAnother: {
+			type: Boolean,
+			default: false
+		},
+		isVisitor: {
+			type: Boolean,
+			required: true,
 		},
 		itemsInBasket: {
 			type: Array,
@@ -164,53 +151,37 @@ export default {
 			type: String,
 			default: ''
 		},
-		showCategoryDescription: {
-			type: Boolean,
-			default: false,
+		loan: {
+			type: Object,
+			default: () => {
+				return {
+					userProperties: {},
+					loanFundraisingInfo: {},
+					geocode: {
+						country: {},
+					},
+					image: {},
+				};
+			}
+		},
+		percentRaised: {
+			type: Number,
+			default: 0,
 		},
 	},
 	data() {
 		return {
-			featuredCategoryIds,
-			isFavorite: null,
-			loading: false,
-			loan: null,
+			featuredCategoryIds: null,
 			loanUseMaxLength: 100,
-			loanChannel: null,
 			showFundraisingThermometer: false,
 		};
 	},
 	computed: {
-		amountLeft() {
-			const {
-				fundedAmount,
-				reservedAmount
-			} = this.loan.loanFundraisingInfo;
-			return this.loan.loanAmount - fundedAmount - reservedAmount;
-		},
 		borrowerCountLanguage() {
 			if (this.loan.borrowerCount > 1) {
 				return ' a member ';
 			}
 			return ' ';
-		},
-		expiringSoonMessage() {
-			if (!this.loan.loanFundraisingInfo.isExpiringSoon) {
-				return '';
-			}
-			const days = differenceInDays(this.loan.plannedExpirationDate, Date.now());
-			if (days >= 2) {
-				return `Only ${days} days left! `;
-			}
-			const hours = differenceInHours(this.loan.plannedExpirationDate, Date.now());
-			if (hours >= 2) {
-				return `Only ${hours} hours left! `;
-			}
-			const mins = differenceInMinutes(this.loan.plannedExpirationDate, Date.now());
-			if (mins >= 2) {
-				return `Only ${mins} minutes left! `;
-			}
-			return 'Expiring now!';
 		},
 		helpedLanguage() {
 			if (this.loan.status === 'fundraising'
@@ -220,25 +191,13 @@ export default {
 			}
 			return 'helped';
 		},
-		isFunded() {
-			return this.loan.status === 'funded';
-		},
-		isSelectedByAnother() {
-			return this.amountLeft <= 0 && !this.isFunded;
-		},
 		loanUse() {
 			// eslint-disable-next-line max-len
 			return `A loan of ${numeral(this.loan.loanAmount).format('$0,0')} ${this.helpedLanguage} ${this.borrowerCountLanguage} ${this.loanUseTruncated()}`;
 		},
-		percentRaised() {
-			return (this.loan.loanAmount - this.amountLeft) / this.loan.loanAmount;
-		},
 		showReadMore() {
 			return !!(this.loanUse.length > this.loanUseMaxLength);
 		},
-	},
-	mounted() {
-		this.isFavorite = this.loan.userProperties.favorited;
 	},
 	methods: {
 		loanUseTruncated() {
@@ -256,92 +215,15 @@ export default {
 			return convertedUse;
 		},
 		toggleFavorite() {
-			// optimistically toggle it locally first
-			this.isFavorite = !this.isFavorite;
-
-			this.apollo.mutate({
-				mutation: loanFavoriteMutation,
-				variables: {
-					loan_id: this.loan.id,
-					is_favorite: this.loan.userProperties.favorited
-				}
-			}).then(({ data }) => {
-				if (data) {
-					const favorite = _get(data, 'loan.favorite');
-
-					if (favorite === null) {
-						this.loan.userProperties.favorited = !this.loan.userProperties.favorited;
-					}
-				}
-			});
+			this.$emit('toggle-favorite');
 		},
 		trackInteraction(args) {
-			if (!this.enableTracking) {
-				return;
-			}
-
-			// eslint-disable-next-line max-len
-			const schema = 'https://raw.githubusercontent.com/kiva/snowplow/master/conf/snowplow_category_row_loan_interaction_event_schema_1_0_0.json#';
-			const interactionType = args.interactionType || 'unspecified';
-			const interactionElement = args.interactionElement || 'unspecified';
-			const loanInteractionTrackData = { schema, data: {} };
-			const featureHeroCategory = this.featuredCategoryIds[0];
-
-			loanInteractionTrackData.data.interactionType = interactionType;
-			loanInteractionTrackData.data.interactionElement = interactionElement;
-			loanInteractionTrackData.data.loanId = this.loan.id;
-			loanInteractionTrackData.data.categorySetIdentifier = 'featured-hero-loan';
-			loanInteractionTrackData.data.categoryId = featureHeroCategory;
-			loanInteractionTrackData.data.row = 0;
-			loanInteractionTrackData.data.position = 1;
-
-			this.$kvTrackSelfDescribingEvent(loanInteractionTrackData);
+			this.$emit('track-interaction', args);
 		},
-	},
-	apollo: {
-		query: featuredLoansQuery,
-		preFetchVariables() {
-			return {
-				ids: featuredCategoryIds
-			};
-		},
-		variables() {
-			return {
-				ids: this.featuredCategoryIds,
-			};
-		},
-		preFetch(config, client) {
-			return client.query({
-				query: featuredLoansQuery,
-				variables: {
-					ids: featuredCategoryIds
-				},
-			}).then(() => {
-				// eslint-disable-next-line max-len
-				return client.query({ query: experimentAssignmentQuery, variables: { id: 'featured_hero_loan_fundraising_thermometer' } });
-			});
-		},
-		result({ data }) {
-			this.loan = _get(
-				_filter(data.lend.loanChannelsById, ['id', this.featuredCategoryIds[0]]),
-				'[0].loans.values[0]'
-			);
-			this.loanChannel = _get(
-				_filter(data.lend.loanChannelsById, ['id', this.featuredCategoryIds[0]]),
-				'[0]'
-			);
-		}
 	},
 	created() {
 		// CASH-684 : Experiment : Featured hero loan fundraising thermometer
-		// eslint-disable-next-line max-len
-		const experimentQuery = this.apollo.readQuery({
-			query: experimentAssignmentQuery,
-			variables: { id: 'featured_hero_loan_fundraising_thermometer' },
-		});
-
-		// eslint-disable-next-line max-len
-		this.experimentVersion = _get(experimentQuery, 'experiment.version') || null;
+		this.experimentVersion = this.experimentData.featured_hero_loan_fundraising_thermometer;
 
 		if (this.experimentVersion === 'variant-a') {
 			this.$kvTrackEvent('Lending', 'EXP-CASH-684-Mar2019', 'a');
@@ -359,126 +241,94 @@ export default {
 
 $row-max-width: 58.75rem;
 
-.featured-section-wrapper {
-	background-color: $kiva-bg-darkgray;
-	margin: 0 auto 2rem;
-	padding: 2rem 0;
+.featured-row-wrapper {
+	align-items: center;
+	display: flex;
+	justify-content: center;
+	margin: 0 auto;
+	max-width: $row-max-width;
+	position: relative;
 
-	@include breakpoint(xxlarge down) {
-		padding-left: rem-calc(40);
-		padding-right: rem-calc(40);
-	}
-
-	.row {
-		max-width: $row-max-width;
-
-		.column {
-			padding-left: 0;
+	.featured-cards-display-window {
+		&:hover {
+			box-shadow: rem-calc(2) rem-calc(2) rem-calc(4) rgba(0, 0, 0, 0.1);
 		}
-	}
 
-	.section-name {
-		font-weight: $global-weight-highlight;
-		margin-bottom: rem-calc(8);
-	}
+		overflow: hidden;
+		width: 100%;
+		z-index: 10;
 
-	.section-description {
-		@extend .section-name;
+		.grid-loan-card {
+			background-color: $white;
+			border: 1px solid $kiva-stroke-gray;
 
-		font-weight: $global-weight-normal;
-		margin-top: rem-calc(12);
-		margin-bottom: rem-calc(20);
-	}
+			.featured-hero-loan {
+				height: 100%;
+				margin: rem-calc(20);
+				width: inherit;
 
-	.featured-row-wrapper {
-		align-items: center;
-		display: flex;
-		justify-content: center;
-		margin: 0 auto;
-		max-width: $row-max-width;
-		position: relative;
+				.column {
+					padding-left: 0;
 
-		.featured-cards-display-window {
-			&:hover {
-				box-shadow: rem-calc(2) rem-calc(2) rem-calc(4) rgba(0, 0, 0, 0.1);
-			}
+					@include breakpoint(small only) {
+						padding-right: 0;
+					}
+				}
 
-			overflow: hidden;
-			width: 100%;
-			z-index: 10;
+				.borrower-info-wrapper {
+					flex-grow: 1;
+					line-height: rem-calc(22);
+					padding: 0 rem-calc(20);
+					text-align: left;
+					margin-top: 0;
 
-			.grid-loan-card {
-				background-color: $white;
-				border: 1px solid $kiva-stroke-gray;
+					@include breakpoint(medium down) {
+						padding: 0;
+					}
 
-				.featured-hero-loan {
-					height: 100%;
-					margin: rem-calc(20);
-					width: inherit;
+					@include breakpoint(small only) {
+						margin-top: rem-calc(20);
+					}
 
-					.column {
-						padding-left: 0;
+					.name {
+						font-size: rem-calc(22);
+						font-weight: $global-weight-highlight;
+						line-height: rem-calc(27);
 
-						@include breakpoint(small only) {
-							padding-right: 0;
+						@include breakpoint(medium down) {
+							margin-top: rem-calc(10);
+							text-align: center;
 						}
 					}
 
-					.borrower-info-wrapper {
-						flex-grow: 1;
-						line-height: rem-calc(22);
-						padding: 0 rem-calc(20);
-						text-align: left;
-						margin-top: 0;
+					.country {
+						color: $kiva-text-light;
+						font-weight: $global-weight-highlight;
 
 						@include breakpoint(medium down) {
-							padding: 0;
-						}
-
-						@include breakpoint(small only) {
-							margin-top: rem-calc(20);
-						}
-
-						.name {
-							font-size: rem-calc(22);
-							font-weight: $global-weight-highlight;
-							line-height: rem-calc(27);
-
-							@include breakpoint(medium down) {
-								margin-top: rem-calc(10);
-								text-align: center;
-							}
-						}
-
-						.country {
-							color: $kiva-text-light;
-							font-weight: $global-weight-highlight;
-
-							@include breakpoint(medium down) {
-								text-align: center;
-							}
-						}
-
-						.use,
-						.why-special {
-							margin-top: rem-calc(20);
-							margin-bottom: rem-calc(20);
-						}
-
-						.why-special .bold {
-							font-weight: $global-weight-bold;
-						}
-
-						.action {
 							text-align: center;
+						}
+					}
 
-							@include breakpoint(medium up) {
-								max-width: rem-calc(330);
-							}
+					.use,
+					.why-special {
+						margin-top: rem-calc(20);
+						margin-bottom: rem-calc(20);
+					}
 
-							@include breakpoint(large) {
-								text-align: left;
-							}
+					.why-special .bold {
+						font-weight: $global-weight-bold;
+					}
+
+					.action {
+						text-align: center;
+
+						@include breakpoint(medium up) {
+							max-width: rem-calc(330);
+						}
+
+						@include breakpoint(large) {
+							text-align: left;
 						}
 					}
 				}
