09b702aa2c49c02306efa71ab99c8588b11d299c
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2019-04-09 13:40:35 -0700

    Only load dart-sass if running in node

--- build/style-loaders.js
+++ build/style-loaders.js
@@ -1,3 +1,10 @@
+// If document is defined, dart-sass will attempt to run as if it were in the browser. However, some
+// code editors expose document, so dart-sass will sometimes throw errors when this config is loaded
+// by editor plugins. We won't use webpack in a browser, so to resolve the issue we can just check
+// for document before attempting to load dart-sass.
+var isNode = typeof document === 'undefined';
+var sass = isNode ? require('dart-sass') : function() {};
+
 module.exports = [
 	'css-loader',
 	{
@@ -13,7 +20,7 @@ module.exports = [
 	{
 		loader: 'sass-loader',
 		options: {
-			implementation: require('dart-sass'),
+			implementation: sass,
 			includePaths: [
 				'src/assets/fonts',
 				'src/assets/scss',
