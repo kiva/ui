24c6895544eddbb93f185985ba3e0a4e97145d68
Matt Stover <matthews@kiva.org>
2019-04-12 15:33:20 -0700

    Fix featured loan v3 read from query

--- src/pages/Lend/LendByCategoryPage.vue
+++ src/pages/Lend/LendByCategoryPage.vue
@@ -322,7 +322,7 @@ export default {
 					// experiment: image enhancement
 					client.query({ query: experimentQuery, variables: { id: 'image_enhancement' } }),
 					// experiment: featured hero loan
-					client.query({ query: experimentQuery, variables: { id: 'featured_hero_loan_v2' } }),
+					client.query({ query: experimentQuery, variables: { id: 'featured_hero_loan_v3' } }),
 					// experiment: category description
 					client.query({ query: experimentQuery, variables: { id: 'category_description' } }),
 					// experiment: add to basket interstitial
