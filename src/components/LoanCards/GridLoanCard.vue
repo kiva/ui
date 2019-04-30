719bc938cb667f7c0c3027ecd8b975b8152f3458
Matt Stover <matthews@kiva.org>
2019-04-30 12:01:48 -0700

    Remove isExpiringSoon as condition for loan card display of Expiring Soon message. We now just use calculations against plannedExpirationDate to show a message from 5 days to go or less. The isExpiringSoon field is still valid in the context of loans in the basket.

--- src/components/LoanCards/GridLoanCard.vue
+++ src/components/LoanCards/GridLoanCard.vue
@@ -34,7 +34,6 @@
 				<fundraising-status
 					:amount-left="amountLeft"
 					:percent-raised="percentRaised"
-					:is-expiring-soon="loan.loanFundraisingInfo.isExpiringSoon"
 					:expiring-soon-message="expiringSoonMessage"
 					:is-funded="loan.status==='funded'"
 				/>
