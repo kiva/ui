5a1d2f07e1d0ffff287b14b3fc06aa1cbcde33e3
joshua starkey <joshua@kiva.org>
2019-05-09 14:31:19 -0700

    cash-891 formatting borrower information into borrower page like formatting

--- src/pages/BorrowerProfile/fundedBorrowerProfile.vue
+++ src/pages/BorrowerProfile/fundedBorrowerProfile.vue
@@ -1,37 +1,63 @@
 <template>
 	<www-page>
-		<!-- Borrower photo -->
-		<div class="row">
-			<div class="small-6 columns">
+		<div class="row borrower-profile-wrapper">
+			<div class="small-12 medium-4 columns">
 				<!-- TODO:
-				- tracking info needs to be updated
-				- need to fold back in logged in state :is-visitor="isVisitor" -->
+				- tracking info needs to be updated -->
+				<!-- Borrower photo -->
 				<loan-card-image
 					:loan-id="loan.id"
 					:name="loan.name"
 					:retina-image-url="loan.image.retina"
 					:standard-image-url="loan.image.default"
+					is-visitor="false"
 					v-kv-track-event="['basket', 'basket-loan-profile', 'basket-loan-profile']"
 					:open-in-new-tab="true"
 					:use-default-styles="false"
 				/>
 			</div>
+			<div class="small-12 medium-8 columns">
+				<!-- Funded State/ FUNDED! -->
+				<h2 class="strong funding-status">100% {{ loan.status }}</h2>
+				<!-- Total funded/loan amount -->
+				<div class="loan-total-text">
+					Loan total: ${{ loan.loanFundraisingInfo.fundedAmount | numeral('0,0') }}
+				</div>
+				<!-- Borrower Name -->
+				<h1> {{ loan.name }} </h1>
+				<!-- Borrower location -->
+				<div>
+					<span class="country-image">
+						<kv-flag :country="`${ loan.geocode.country.isoCode }`" />
+					</span>
+					<span class="loan-location-text">
+						{{ loan.geocode.city }}, {{ loan.geocode.state }}, {{ loan.geocode.country.name }}
+						/ {{ loan.sector.name }}
+					</span>
+
+					<div>
+						<!-- Link to see full borrower profile in old stack -->
+						<router-link :to="`/lend/${loan.id}`">See full borrower profile</router-link>
+					</div>
+				</div>
+			</div>
 		</div>
-		<!-- Borrower Name -->
-		<div class='row'> {{ loan.name }} </div>
-		<!-- Total funded/loan amount -->
-		<div class='row'> {{ loan.loanFundraisingInfo.fundedAmount }}</div>
-		<!-- Funded State/ FUNDED! -->
-		<div class='row'> {{ loan.status }}</div>
-		<!-- Borrower location -->
-		<div class='row'> {{ loan.geocode.city }}</div>
-		<!-- Loan use -->
-		<div class='row'> {{ loan.use }} </div>
-		<!-- Loan description -->
-		<div class='row' v-html="loan.description"></div>
-		<!-- Link to see full borrower profile in old stack. Need borrower ID here -->
+		<hr>
 		<div class='row'>
-			<router-link :to="`/lend/${loan.id}`">Full Borrower Profile</router-link>
+			<div class="small-12 columns text-center">
+				<!-- Loan use -->
+				<h2 class="loan-use-text">
+					A loan of {{ loan.loanFundraisingInfo.fundedAmount | numeral('0,0') }} helps {{ loan.use }}
+				</h2>
+			</div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="small-12 columns loan-description-wrapper">
+				<h2 class="loan-description-heading-text"> {{ loan.name }}'s story </h2>
+				<!-- Loan description -->
+				<div v-html="loan.description"></div>
+			</div>
 		</div>
 	</www-page>
 </template>
@@ -39,6 +65,7 @@
 <script>
 import WwwPage from '@/components/WwwFrame/WwwPage';
 import _get from 'lodash/get';
+import KvFlag from '@/components/Kv/KvFlag';
 import fundedBorrowerProfile from '@/graphql/query/fundedBorrowerProfile.graphql';
 import LoanCardImage from '@/components/LoanCards/LoanCardImage';
 
@@ -46,6 +73,7 @@ export default {
 	components: {
 		WwwPage,
 		LoanCardImage,
+		KvFlag
 	},
 	inject: ['apollo'],
 	data() {
@@ -74,12 +102,32 @@ export default {
 		});
 
 		this.loan = _get(loanData, 'lend.loan');
-		console.log('loanData', this.loan);
+		// console.log('loanData', this.loan);
 	}
 };
 </script>
 
 <style lang="scss">
 @import 'settings';
+
+.borrower-profile-wrapper {
+	padding-top: 1rem;
+}
+
+.funding-status {
+	color: $kiva-green;
+	font-size: 2.5rem;
+}
+
+.loan-use-text,
+.loan-total-text,
+.loan-description-heading-text,
+.loan-location-text {
+	font-weight: $button-font-weight;
+}
+
+.loan-description-wrapper {
+	padding-bottom: 1.25rem;
+}
 
 </style>
