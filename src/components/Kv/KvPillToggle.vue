825c0858436511b072a10445091354e595d3d298
Matt Stover <matthews@kiva.org>
2019-05-16 15:53:32 -0700

    no dangling quotations, that is a step too far...

--- src/components/Kv/KvPillToggle.vue
+++ src/components/Kv/KvPillToggle.vue
@@ -3,8 +3,7 @@
 		<span
 			v-for="option in options" :key="option.key"
 			@click="pillToggled(option.key)"
-			:class="{ active: selected === option.key }
-			"
+			:class="{ active: selected === option.key }"
 		>
 			<div class="title">
 				{{ option.title }}
