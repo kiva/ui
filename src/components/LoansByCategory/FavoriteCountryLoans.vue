4c521edd9545e58396fd148e7490d54c92643433
joshua starkey <joshua@kiva.org>
2019-05-20 11:49:19 -0700

    cash-794 first commit with new graphql query and .vue file

---
+++ src/components/LoansByCategory/FavoriteCountryLoans.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="most-lent-country-holder">
+
+	</div>
+</template>
+
+<script>
+import favoriteCountry from '@/graphql/query/lendByCategory/favoriteCountry.graphql';
+
+</script>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+</style>
