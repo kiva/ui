df29225b344cb871319ee6358d58d2f36fb52e0b
Karan Sekhri <ksekhri@gmail.com>
2019-05-21 11:27:06 -0700

    Add KvControlledCheckbox

---
+++ src/components/Kv/KvControlledCheckbox.vue
@@ -0,0 +1,62 @@
+<template>
+	<div
+		class="kv-checkbox"
+		@click="handleInput"
+		@keypress="handleKeyPressContainer"
+		:tabindex="tabindex"
+	>
+		<label class="kv-checkbox-label">
+			<input
+				type="checkbox"
+				:checked="isChecked"
+				aria-hidden="true"
+				role="presentation"
+				tabindex="-1"
+			>
+			<slot v-if="hasDefaultSlot"></slot>
+			<span v-else-if="label">{{ label }}</span>
+		</label>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		label: {
+			type: String,
+			default: null
+		},
+		isChecked: {
+			type: Boolean,
+			default: null,
+		},
+		tabindex: {
+			type: Number,
+			default: 0,
+		},
+	},
+	computed: {
+		hasDefaultSlot() {
+			return !!this.$slots.default;
+		},
+	},
+	methods: {
+		handleInput() {
+			this.$emit('checkbox-input');
+		},
+		handleKeyPressContainer(event) {
+			if (event.keyCode === 32) {
+				this.handleInput();
+			}
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.kv-checkbox {
+	.kv-checkbox-label {
+		pointer-events: none;
+	}
+}
+</style>
