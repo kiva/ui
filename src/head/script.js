4c4f342a695f6a0a00590368cb45478b5afdadf6
Matt Stover <matthews@kiva.org>
2019-06-12 12:38:51 -0700

    add polyfill if startsWith is absent

--- src/head/script.js
+++ src/head/script.js
@@ -47,6 +47,14 @@ export default config => {
 
 		if (config.algoliaConfig.enableAA && !optout) {
 			/* eslint-disable */
+			if (!String.prototype.startsWith) {
+				Object.defineProperty(String.prototype, 'startsWith', {
+					value: function(search, pos) {
+						pos = !pos || pos < 0 ? 0 : +pos;
+						return this.substring(pos, pos + search.length) === search;
+					}
+				});
+			}
 			(function(e,a,t,n,s,i,c){e.AlgoliaAnalyticsObject=s,e.aa=e.aa||function(){
 				(e.aa.queue=e.aa.queue||[]).push(arguments)},i=a.createElement(t),c=a.getElementsByTagName(t)
 				[0],i.async=1,i.src="https://cdn.jsdelivr.net/npm/search-insights@1.2.0",
