4001f5fe62514e6a63bec45ad743009482b1879f
Matt Stover <matthews@kiva.org>
2019-06-27 13:49:09 -0700

    Only load search insights if startsWith is already present. Otherwise skip. Loading the polyfill here breaks other polyfills later in the code.

--- src/head/script.js
+++ src/head/script.js
@@ -55,18 +55,12 @@ export default config => {
 
 		if (config.algoliaConfig.enableAA && !optout) {
 			/* eslint-disable */
-			if (!String.prototype.startsWith) {
-				Object.defineProperty(String.prototype, 'startsWith', {
-					value: function(search, pos) {
-						pos = !pos || pos < 0 ? 0 : +pos;
-						return this.substring(pos, pos + search.length) === search;
-					}
-				});
+			if (typeof String.prototype.startsWith === 'function') {
+				(function(e,a,t,n,s,i,c){e.AlgoliaAnalyticsObject=s,e.aa=e.aa||function(){
+					(e.aa.queue=e.aa.queue||[]).push(arguments)},i=a.createElement(t),c=a.getElementsByTagName(t)
+					[0],i.async=1,i.src="https://cdn.jsdelivr.net/npm/search-insights@1.2.0",
+					c.parentNode.insertBefore(i,c)})(window,document,"script",0,"aa");
 			}
-			(function(e,a,t,n,s,i,c){e.AlgoliaAnalyticsObject=s,e.aa=e.aa||function(){
-				(e.aa.queue=e.aa.queue||[]).push(arguments)},i=a.createElement(t),c=a.getElementsByTagName(t)
-				[0],i.async=1,i.src="https://cdn.jsdelivr.net/npm/search-insights@1.2.0",
-				c.parentNode.insertBefore(i,c)})(window,document,"script",0,"aa");
 			/* eslint-enable */
 		}
 	}
