79b9e7c728b722c530e4fdcc1b54a6be8945d5ae
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2019-07-24 15:59:42 -0700

    AUTH-226 have KvAuth0 fetch kiva_id and last_login

--- src/api/localResolvers/my.js
+++ src/api/localResolvers/my.js
@@ -1,14 +1,9 @@
-import _get from 'lodash/get';
-
 export default ({ kvAuth0 }) => {
 	return {
 		resolvers: {
 			My: {
 				lastLoginTimestamp() {
-					const lastLoginKey = 'https://www.kiva.org/last_login';
-					return _get(kvAuth0, `user["${lastLoginKey}"]`)
-						|| _get(kvAuth0, `user._json["${lastLoginKey}"]`)
-						|| 0;
+					return kvAuth0.getLastLogin();
 				}
 			},
 		},
