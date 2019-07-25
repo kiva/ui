e6dc505c12d6d1ea1f2f0c6399b971408fbaa5d5
joshua starkey <joshua@kiva.org>
2019-07-24 17:01:28 -0700

    cash-965 new graphql file for details tab, basic structure for content

--- src/components/LoanCards/HoverLoanCard/InfoPanels/LoanDetailsPanel.vue
+++ src/components/LoanCards/HoverLoanCard/InfoPanels/LoanDetailsPanel.vue
@@ -3,16 +3,67 @@
 		<template #title>
 			Loan Details
 		</template>
-		<p>Details about the loan.</p>
+		<ul>
+			<li>
+				<label>Loan length:</label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Repayment schedule:</label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Disbursal date:</label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Currency exchange loss:</label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Facilitated by Field Partner:</label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Is borrower paying interest? </label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Field Partner risk rating:</label>
+				<p class="data">{{  }}</p>
+			</li>
+		</ul>
+		<ul>
+			<!-- The size of the following tag will need to be adjusted -->
+			<h3>{{ COUNTRY }} country facts</h3>
+			<li>
+				<label>Average annual income (USD):</label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Funds lent in {{ COUNTRY }}:</label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Loans currently fundraising:</label>
+				<p class="data">{{  }}</p>
+			</li>
+			<li>
+				<label>Loans transacted in:</label>
+				<p class="data">{{  }}</p>
+			</li>
+		</ul>
 	</info-panel>
 </template>
 
 <script>
 import InfoPanel from './InfoPanel';
+import KvLoadingSpinner from '@/components/Kv/KvLoadingSpinner';
 
 export default {
 	components: {
 		InfoPanel,
+		KvLoadingSpinner,
 	},
 	props: {
 		expandable: {
@@ -31,3 +82,31 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss">
+@import 'settings';
+
+#loading-overlay {
+	position: absolute;
+	width: auto;
+	height: auto;
+	left: 1rem;
+	right: 1rem;
+	bottom: 0;
+	top: 0;
+	background-color: rgba($platinum, 0.7);
+
+	.spinner-wrapper {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		position: relative;
+		height: 100%;
+		top: auto;
+		left: auto;
+		transform: none;
+		transition: top 100ms linear;
+	}
+}
+
+</style>
