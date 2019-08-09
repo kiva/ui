4e8a4b1d20593e62a5b5d221901db3252914c4ed
Matt Stover <matthews@kiva.org>
2019-08-08 18:03:59 -0700

    Update k8sdev.js

--- config/k8sdev.js
+++ config/k8sdev.js
@@ -4,7 +4,7 @@ var dev  = require('./dev.js')
 module.exports = merge(dev, {
 	app: {
 		publicPath: 'https://www-dev-kiva-org.global.ssl.fastly.net/',
-	}
+	},
 	server: {
 		memcachedServers: 'k8sdev-elasticache.bu9ifv.0001.usw1.cache.amazonaws.com:11211',
 	}
