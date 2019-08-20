6aa65914534544418d2d2c3f75604e8b953a193f
joshua starkey <joshua@kiva.org>
2019-08-20 14:44:29 -0700

    cash-1219 new ui homepage skeleton

---
+++ src/pages/Homepage/Homepage.vue
@@ -0,0 +1,30 @@
+<template>
+	<www-page>
+		<div class="row page-content">
+			<div class="small-12 columns">
+				<h1>NEW UI HOMEPAGE</h1>
+			</div>
+		</div>
+	</www-page>
+</template>
+
+<script>
+import WwwPage from '@/components/WwwFrame/WwwPage';
+
+export default {
+	components: {
+		WwwPage
+	},
+	metaInfo: {
+		title: 'Sitemap'
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+.page-content {
+	padding: 1.625rem 0;
+}
+</style>
