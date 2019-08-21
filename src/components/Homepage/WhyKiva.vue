eef77a1249135f2aa599d7e8fc6c26650aba634b
joshua starkey <joshua@kiva.org>
2019-08-21 16:32:11 -0700

    cash-1216 little formatting work before another meeting

--- src/components/Homepage/WhyKiva.vue
+++ src/components/Homepage/WhyKiva.vue
@@ -1,10 +1,10 @@
 <template>
-	<div>
-		<h1 class="text-center kiva-green">
+	<div class="row">
+		<h1 class="text-center kiva-green show-for-large large-12 column">
 			Why Kiva?
 		</h1>
 		<img class="map-background" src="https://www.kiva.org/cms/page/images/world-map-simple.min_.svg">
-		<div>
+		<div class="small-12 large-6 column text-center">
 			<h2 class="kiva-green">
 				<span class="large-number">{{ repaymentRateFormatted }}% </span>
 				repayment rate
@@ -13,7 +13,7 @@
 				It's a loan not a donation; so when you're repaid you can use the money again.
 			</p>
 		</div>
-		<div>
+		<div class="small-12 large-6 column text-center">
 			<h2 class="kiva-green">
 				<span class="large-number">100%</span>
 				goes to the field
@@ -22,7 +22,7 @@
 				Your voluntary donations and our incredible partners make this possible.
 			</p>
 		</div>
-		<div>
+		<div class="small-12 large-6 column text-center">
 			<span>
 				<kv-icon name="star" class="kiva-green star" />
 				<kv-icon name="star" class="kiva-green star" />
@@ -33,19 +33,19 @@
 				Charity Navigator's highest award rating
 			</p>
 		</div>
-		<div>
+		<div class="small-12 large-6 column text-center">
 			<h2 class="kiva-green">
 				<span class="large-number">{{ totalLoansInDollarsFormatted }}</span>
 				billion in loans
 			</h2>
 		</div>
-		<div>
+		<div class="small-12 large-6 column text-center">
 			<h2 class="kiva-green">
 				<span class="large-number">{{ numCountries }}</span>
 				countries
 			</h2>
 		</div>
-		<p>
+		<p class="show-for-large column large-12 text-center">
 			<router-link
 				:to="`/about`"
 				class="kiva-text-grey"
