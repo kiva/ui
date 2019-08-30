b6bd5b6d80881a126178610b3b6b47392e005692
joshua starkey <joshua@kiva.org>
2019-08-30 15:09:56 -0700

    cash-1217 final touches

--- src/components/Homepage/CategoryGrid.vue
+++ src/components/Homepage/CategoryGrid.vue
@@ -4,42 +4,52 @@
 			Browse loans by category
 		</h1>
 		<div class="row card-row">
-			<a
-				:href="`/lend-by-category/${loanChannel.name.toLowerCase()}`"
+			<router-link
+				:to="`/lend-by-category/${loanChannel.name.toLowerCase().split(' ').join('-')}`"
 				v-for="loanChannel in loanChannels" :key="loanChannel.id"
-				class="columns small-6 medium-4 category-card"
+				class="small-6 medium-4"
 				v-kv-track-event="[
 					'homepage',
 					'click-Category-card',
 					`${loanChannel.name}-category-card`,
 					'true']"
 			>
-				<kv-responsive-image
-					:images="[
-						['small', loanChannel.image.url],
-						['small-retina', loanChannel.retinaImage.url]
-					]"
-					class="loan-channel-image"
-				/>
-				<div class="category-card-title-wrap">
-					<p class="category-card-title featured-text">
-						{{ loanChannel.name }}
-					</p>
+				<div class="category-card-overlay">
+					<div>
+						<kv-responsive-image
+							:images="[
+								['small', loanChannel.image.url],
+								['small-retina', loanChannel.retinaImage.url]
+							]"
+							class="loan-channel-image"
+						/>
+						<div class="category-card-title-wrap">
+							<p class="category-card-title featured-text">
+								{{ loanChannel.name }}
+							</p>
+						</div>
+					</div>
 				</div>
-			</a>
-			<div class="columns small-12 medium-4 view-all-card">
-				<router-link
-					:to="`/lend-by-category`"
-					class="view-all-card-text featured-text"
-					v-kv-track-event="[
-						'homepage',
-						'click-View all categories',
-						'view-all-categories-homepage-card',
-						'true']"
-				>
-					View all
-				</router-link>
-			</div>
+			</router-link>
+			<router-link
+				:to="`/lend-by-category`"
+				class="small-12 medium-4"
+				v-kv-track-event="[
+					'homepage',
+					'click-View all categories',
+					'view-all-categories-homepage-card',
+					'true']"
+			>
+				<div class="solid-card-overlay">
+					<div class="view-all-card">
+						<div class="solid-category-card-title-wrap">
+							<p class="category-card-title featured-text view-all-card-title">
+								View all
+							</p>
+						</div>
+					</div>
+				</div>
+			</router-link>
 		</div>
 		<div class="featured-text row small-10 category-sub-text">
 			By lending as little as $25, you can help people around the world
@@ -70,10 +80,6 @@ export default {
 		KvResponsiveImage,
 	},
 	inject: ['apollo'],
-	data() {
-		return {
-		};
-	},
 	apollo: {
 		query: categoryRowsQuery,
 		preFetch: true,
@@ -96,17 +102,11 @@ export default {
 	display: contents;
 }
 
-.view-all-card-text {
-	color: $white;
-	line-height: 2.5;
-}
-
 .category-sub-text {
 	padding-top: 6.25rem;
 	padding-bottom: 6.25rem;
 	color: $kiva-text-medium;
 	margin: 0 auto;
-
 }
 
 .frame {
@@ -119,13 +119,31 @@ export default {
 	}
 }
 
-.category-card {
-	position: relative;
-	padding: 2px;
-}
-
 .loan-channel-image {
 	width: 100%;
+}
+
+.category-card-overlay {
+	margin: 2px;
+	position: relative;
+    background-color: $black;
+}
+
+.solid-card-overlay {
+	margin: 2px;
+	position: relative;
+    background-color: $white;
+	height: 98%;
+}
+
+.view-all-card {
+	height: 4rem;
+	background-color: $kiva-accent-blue;
+	opacity: 0.7;
+
+	@include breakpoint(medium) {
+		height: 100%;
+	}
 }
 
 .category-card-title-wrap {
@@ -135,20 +153,39 @@ export default {
 	transform: translateY(-50%);
 }
 
+.solid-category-card-title-wrap {
+	top: 50%;
+	position: absolute;
+	width: 100%;
+	transform: translateY(-50%);
+}
+
 .category-card-title {
 	padding: 0 0.375rem;
 	margin: 0;
 	text-align: center;
-	text-shadow: 1px 1px 3px #333;
+	text-shadow: 1px 1px 3px $black;
+	font-weight: 400;
 	color: $white;
 }
 
-.view-all-card {
-	height: 4rem;
-	background-color: $kiva-accent-blue;
+// card hover styles
+.category-card-overlay:hover {
+	.loan-channel-image {
+		opacity: 0.5;
+	}
 
-	@include breakpoint(medium) {
-		height: inherit;
+	.category-card-title {
+		text-decoration: underline;
+	}
+}
+
+.solid-card-overlay:hover {
+	.view-all-card {
+		opacity: 1;
+	}
+	.view-all-card-title {
+		text-decoration: underline;
 	}
 }
 
