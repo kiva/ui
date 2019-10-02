b32976075f4c1feeff1781dceeb4c4cd92e857f3
joshua starkey <joshua@kiva.org>
2019-10-02 13:48:13 -0700

    cash-1369 exp setup about 50% of the way there

---
+++ src/components/WwwFrame/PromotionalBanner/Banners/BillionToWomenBanner.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="row align-center gift-banner">
+		<router-link
+			to="/monthlygood"
+			class="banner-link"
+			v-kv-track-event="['TopNav','click-Promo','Monthly Good Sept 2019']"
+		>
+			<kv-icon name="monthly-good" class="monthly-good-icon" />
+			<div class="content">
+				Make a bigger impact with a<br class="smo">
+				Monthly Good subscription. <span class="call-to-action-text">Sign up.</span>
+			</div>
+		</router-link>
+	</div>
+</template>
+
+<script>
+import KvIcon from '@/components/Kv/KvIcon';
+
+export default {
+	components: {
+		KvIcon
+	},
+};
+</script>
