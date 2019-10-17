abc090f7eeffd0abcbfb8432827d8a5d00edd86c
joshua starkey <joshua@kiva.org>
2019-10-17 14:02:43 -0700

    kv-dropdown-rounded rem-calcs-> rem

--- src/components/Kv/KvDropdownRounded.vue
+++ src/components/Kv/KvDropdownRounded.vue
@@ -24,12 +24,12 @@
 		border-radius: $button-radius;
 		line-height: $medium-text-line-height;
 		background-image: url('~@/assets/images/customDropdown.png');
-		background-size: rem-calc(23) rem-calc(20);
+		background-size: 1.438rem 1.25rem;
 		color: $charcoal;
 		font-size: $medium-text-font-size;
 		font-weight: $global-weight-highlight;
 		cursor: pointer;
-		padding-right: rem-calc(40);
+		padding-right: 2.5rem;
 		background-position: right -1.9rem center;
 	}
 
