809e172fa80775d319e97c10ea895aae34d70b7d
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2019-10-21 12:10:13 -0700

    CASH-1253 component just to display the loan count

---
+++ src/pages/Autolending/LoanCountSpan.vue
@@ -0,0 +1,28 @@
+<template>
+	<span v-if="counting">
+		<kv-loading-spinner />
+	</span>
+	<span v-else>
+		{{ count | numeral('0,0') }}
+	</span>
+</template>
+
+<script>
+import KvLoadingSpinner from '@/components/Kv/KvLoadingSpinner';
+
+export default {
+	props: {
+		count: {
+			type: Number,
+			default: 0,
+		},
+		counting: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	components: {
+		KvLoadingSpinner,
+	},
+};
+</script>
