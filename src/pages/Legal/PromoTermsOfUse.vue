af12974edcbcf402810850909554fd48da55410c
Eddie Ferrer <eddief@kiva.org>
2019-10-30 11:56:35 -0700

    Scoped styles for TOU and Promo Terms

--- src/pages/Legal/PromoTermsOfUse.vue
+++ src/pages/Legal/PromoTermsOfUse.vue
@@ -114,7 +114,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import 'settings';
 
 h1 { margin-top: 0.5rem; }
--- src/pages/Legal/TermsOfUse.vue
+++ src/pages/Legal/TermsOfUse.vue
@@ -889,7 +889,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import 'settings';
 
 .tertiary-menu-terms {
