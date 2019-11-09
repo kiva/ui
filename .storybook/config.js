1afb1989388e805870169c6ccf89f08be2b3d6df
Ryan Ludwig <ryanl@kiva.org>
2019-11-08 16:04:56 -0800

    Add a11y and viewport addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from '@storybook/vue';
+import { configure, addDecorator } from '@storybook/vue';
+import { withA11y } from '@storybook/addon-a11y';
+
+addDecorator(withA11y)
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
