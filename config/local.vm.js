52cedc2ac862235f3c2cdf5e3c21afa706e466d9
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2019-11-12 11:22:56 -0800

    These are not needed because they are inherited from the base config

--- config/local.vm.js
+++ config/local.vm.js
@@ -5,13 +5,11 @@ module.exports = merge(base, {
 	app: {
 		host: 'localhost',
 		publicPath: '/',
-		graphqlUri: 'https://api-vm.kiva.org/graphql',
 		auth0: {
 			enable: false,
 		},
 	},
 	server: {
-		graphqlUri: 'https://api-vm.kiva.org/graphql',
 		sessionUri: '',
 		memcachedEnabled: false,
 		memcachedServers: '',
