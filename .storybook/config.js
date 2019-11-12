b7d0b313f1f2b78a7adf92ded64fc4ea64bc355e
Ryan Ludwig <ryanl@kiva.org>
2019-11-11 16:39:59 -0800

    Stub out custom theming

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,14 @@
-import { configure, addDecorator } from '@storybook/vue';
+import { configure, addDecorator, addParameters } from '@storybook/vue';
 import { withA11y } from '@storybook/addon-a11y';
+
+
+import kvTheme from './kvtheme';
+
+addParameters({
+  options: {
+    theme: kvTheme,
+  },
+});
 
 addDecorator(withA11y)
 
