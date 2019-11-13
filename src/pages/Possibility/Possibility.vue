852b8c4cefeef36287a657aee208de5e95537d04
Ryan Ludwig <ryanl@kiva.org>
2019-11-13 13:10:20 -0800

    Create Possibility page frame. Redirect to certain campaign pages depending on date.
    CASH-1540

---
+++ src/pages/Possibility/Possibility.vue
@@ -0,0 +1,33 @@
+<template>
+	<www-page>
+		<router-view />
+	</www-page>
+</template>
+
+<script>
+import WwwPage from "@/components/WwwFrame/WwwPage";
+
+export default {
+	components: {
+		WwwPage
+	},
+	beforeCreate() {
+		const todaysDate = new Date();
+		const day = todaysDate.getDate();
+		const month = todaysDate.getMonth() + 1; // getMonth is 0 based
+
+		let url = '/';
+		if (month === 12 && (day >= 3 && day <= 13)) {
+			url = '/possibility/giving-tuesday';
+		} else if (month === 12 && (day >= 14 && day <= 25)) {
+			url = '/possibility/12-days-of-lending';
+		} else if (month === 12 && (day >= 26 && day <= 31)) {
+			url = '/possibility/year-end';
+		}
+
+		if (this.$route.path === '/possibility') {
+			this.$router.push({ path: url, query: this.$route.query });
+		}
+	}
+};
+</script>
