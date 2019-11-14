334b716cc821f4164bd97559bd14b84f3cd8fe0f
Ryan Ludwig <ryanl@kiva.org>
2019-11-14 10:24:28 -0800

    Stub out 12 days of lending and year end pages. Uncomment routes.

---
+++ src/pages/Possibility/12DaysOfLending.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		12 days of lending
+	</div>
+</template>
+
+<script>
+// import KvButton from '@/components/Kv/KvButton';
+// import KvHero from '@/components/Kv/KvHero';
+// import KvResponsiveImage from '@/components/Kv/KvResponsiveImage';
+
+export default {
+	components: {
+		// KvButton,
+		// KvHero,
+		// KvResponsiveImage,
+	},
+	metaInfo: {
+		title: '12 Days of Lending'
+	},
+	data() {
+		return {
+		};
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+@import "settings";
+
+</style>
