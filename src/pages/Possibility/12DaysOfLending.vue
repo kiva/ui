0f54338985ac99f0e83dbf07dce894654c4b4d55
Ryan Ludwig <ryanl@kiva.org>
2019-11-14 16:01:09 -0800

    Add 12 days of lending banner
    CASH-1525

--- src/pages/Possibility/12DaysOfLending.vue
+++ src/pages/Possibility/12DaysOfLending.vue
@@ -1,25 +1,43 @@
 <template>
 	<div>
-		12 days of lending
+		<kv-hero>
+			<template v-slot:images>
+				<kv-responsive-image :images="twelveDaysImages" alt="Be a part of Possibility: 12 Days of Lending" />
+			</template>
+		</kv-hero>
 	</div>
 </template>
 
 <script>
 // import KvButton from '@/components/Kv/KvButton';
-// import KvHero from '@/components/Kv/KvHero';
-// import KvResponsiveImage from '@/components/Kv/KvResponsiveImage';
+import KvHero from '@/components/Kv/KvHero';
+import KvResponsiveImage from '@/components/Kv/KvResponsiveImage';
+
+const possibilitiesImageRequire = require.context('@/assets/images/possibilities-banners/', true);
 
 export default {
 	components: {
 		// KvButton,
-		// KvHero,
-		// KvResponsiveImage,
+		KvHero,
+		KvResponsiveImage,
 	},
 	metaInfo: {
 		title: '12 Days of Lending'
 	},
 	data() {
 		return {
+			twelveDaysImages: [
+				['small', possibilitiesImageRequire('./Phase2-sm-std.jpg')],
+				['small retina', possibilitiesImageRequire('./Phase2-sm-retina.jpg')],
+				['medium', possibilitiesImageRequire('./Phase2-med-std.jpg')],
+				['medium retina', possibilitiesImageRequire('./Phase2-med-retina.jpg')],
+				['large', possibilitiesImageRequire('./Phase2-lg-std.jpg')],
+				['large retina', possibilitiesImageRequire('./Phase2-lg-retina.jpg')],
+				['xga', possibilitiesImageRequire('./Phase2-xl-std.jpg')],
+				['xga retina', possibilitiesImageRequire('./Phase2-xl-retina.jpg')],
+				['wxga', possibilitiesImageRequire('./Phase2-xxl-std.jpg')],
+				['wxga retina', possibilitiesImageRequire('./Phase2-xxl-retina.jpg')]
+			],
 		};
 	}
 };
