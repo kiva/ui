75af1df08c7b602b334ad95c25d06b57299a8c15
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2019-11-18 13:41:55 -0800

    Add gift/store suggestions to header search bar

--- src/components/WwwFrame/SearchBar.vue
+++ src/components/WwwFrame/SearchBar.vue
@@ -106,7 +106,8 @@ export default {
 				'Group or Individual',
 				'Attributes',
 				'User tags',
-				'Partners'
+				'Partners',
+				'Gifts'
 			];
 			return unsortedSections.sort(indexIn(sectionOrder, 'name'));
 		}
@@ -119,7 +120,20 @@ export default {
 			this.hasFocus = true;
 			this.apollo.query({ query: suggestionsQuery }).then(({ data }) => {
 				if (data && data.lend) {
-					engine.reset(data.lend.loanSearchSuggestions);
+					const suggestions = data.lend.loanSearchSuggestions;
+					suggestions.push({
+						group: 'Gifts',
+						label: 'Kiva Cards',
+						keywords: ['gift card', 'kiva card', 'gift', 'gift certificate'],
+						url: 'https://www.kiva.org/gifts/kiva-cards',
+					});
+					suggestions.push({
+						group: 'Gifts',
+						label: 'Kiva Store',
+						keywords: ['gift card', 'kiva card', 'gift', 'gift certificate'],
+						url: 'https://store.kiva.org',
+					});
+					engine.reset(suggestions);
 				}
 			});
 		},
@@ -152,14 +166,19 @@ export default {
 		},
 		runSearch(suggestion) {
 			let query;
-			if (suggestion.query) {
-				const [key, value] = suggestion.query.split('=');
-				query = { [key]: value };
-			} else {
-				query = { queryString: suggestion };
-			}
 			this.searching = true;
-			this.$router.push({ path: '/lend', query });
+
+			if (suggestion.url) {
+				window.location = suggestion.url;
+			} else {
+				if (suggestion.query) {
+					const [key, value] = suggestion.query.split('=');
+					query = { [key]: value };
+				} else {
+					query = { queryString: suggestion };
+				}
+				this.$router.push({ path: '/lend', query });
+			}
 		},
 		formatResult({ label, matches }) {
 			// If no match is found, just return the label, unmarked
