6286e5f12b44b969f59a22eeffad6085a0eeb62e
Ryan Ludwig <ryanl@kiva.org>
2019-11-25 16:22:45 -0800

    Start reworking styleguide colors component, add copy-to-clipboard

--- src/components/Styleguide/StyleguideColors.vue
+++ src/components/Styleguide/StyleguideColors.vue
@@ -1,260 +1,115 @@
 <template>
 	<section>
 		<h1>Colors</h1>
-		<hr>
-		<!-- Greens -->
-		<div class="row">
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-accent-green"></div>
-				<div class="color-hex small-text">
-					#aee15c
-				</div>
-				<div class="color-name">
-					$kiva-accent-green
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-light-green"></div>
-				<div class="color-hex small-text">
-					#8ccb8c
-				</div>
-				<div class="color-name">
-					$kiva-light-green
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-green"></div>
-				<div class="color-hex small-text">
-					#4faf4e
-				</div>
-				<div class="color-name">
-					$kiva-green
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-icon-green"></div>
-				<div class="color-hex small-text">
-					#49953f
-				</div>
-				<div class="color-name">
-					$kiva-icon-green
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-dark-green"></div>
-				<div class="color-hex small-text">
-					#247037
-				</div>
-				<div class="color-name">
-					$kiva-darkgreen
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box aqua-light-green"></div>
-				<div class="color-hex small-text">
-					#DDFFF4
-				</div>
-				<div class="color-name">
-					$aqua-light-green
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box aqua-medium-green"></div>
-				<div class="color-hex small-text">
-					#C4E7DB
-				</div>
-				<div class="color-name">
-					$aqua-medium-green
-				</div>
-			</div>
-		</div>
-		<!-- Blues -->
-		<div class="row">
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-filter-indicator-bg"></div>
-				<div class="color-hex small-text">
-					#d2f0f8
-				</div>
-				<div class="color-name">
-					$kiva-filter-indicator-bg
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box shadow-lightblue"></div>
-				<div class="color-hex small-text">
-					#6badc2
-				</div>
-				<div class="color-name">
-					$shadow-lightblue
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box shadow-blue"></div>
-				<div class="color-hex small-text">
-					#1f92ba
-				</div>
-				<div class="color-name">
-					$shadow-blue
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-accent-blue"></div>
-				<div class="color-hex small-text">
-					#118aec
-				</div>
-				<div class="color-name">
-					$kiva-accent-blue
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-accent-darkblue"></div>
-				<div class="color-hex small-text">
-					#006ed3
-				</div>
-				<div class="color-name">
-					$kiva-accent-darkblue
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-textlink-visited"></div>
-				<div class="color-hex small-text">
-					#0066a9
-				</div>
-				<div class="color-name">
-					$kiva-textlink-visited
-				</div>
-			</div>
-		</div>
-		<!-- Greyscale -->
-		<div class="row">
-			<div class="columns small-2 color-card">
-				<div class="color-box white"></div>
-				<div class="color-hex small-text">
-					#FFF
-				</div>
-				<div class="color-name">
-					$white
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-bg-lightgray"></div>
-				<div class="color-hex small-text">
-					#fafafa
-				</div>
-				<div class="color-name">
-					$kiva-bg-lightgray
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-bg-darkgray"></div>
-				<div class="color-hex small-text">
-					#efefef
-				</div>
-				<div class="color-name">
-					$kiva-bg-darkgray
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-stroke-gray"></div>
-				<div class="color-hex small-text">
-					#d8d8d8
-				</div>
-				<div class="color-name">
-					$kiva-stroke-gray
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box subtle-gray"></div>
-				<div class="color-hex small-text">
-					#CCC
-				</div>
-				<div class="color-name">
-					$subtle-gray
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-text-light"></div>
-				<div class="color-hex small-text">
-					#999
-				</div>
-				<div class="color-name">
-					$kiva-text-light
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-text-medium"></div>
-				<div class="color-hex small-text">
-					#666
-				</div>
-				<div class="color-name">
-					$kiva-text-medium
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-text-dark"></div>
-				<div class="color-hex small-text">
-					#484848
-				</div>
-				<div class="color-name">
-					$kiva-text-dark
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box dark-charcoal"></div>
-				<div class="color-hex small-text">
-					#333
-				</div>
-				<div class="color-name">
-					$dark-charcoal
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box black"></div>
-				<div class="color-hex small-text">
-					#000
-				</div>
-				<div class="color-name">
-					$black
-				</div>
-			</div>
-		</div>
-		<!-- Highlight colors -->
-		<div class="row">
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-accent-red"></div>
-				<div class="color-hex small-text">
-					#d74937
-				</div>
-				<div class="color-name">
-					$kiva-accent-red
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-accent-purple"></div>
-				<div class="color-hex small-text">
-					#6859c0
-				</div>
-				<div class="color-name">
-					$kiva-accent-purple
-				</div>
-			</div>
-			<div class="columns small-2 color-card">
-				<div class="color-box kiva-alert-yellow"></div>
-				<div class="color-hex small-text">
-					#fee259
-				</div>
-				<div class="color-name">
-					$kiva-alert-yellow
-				</div>
-			</div>
-		</div>
+		<ul class="color-groups">
+			<li class="color-group" v-for="group in colorGroups" :key="group.categoy">
+				<h2>{{ group.category }}</h2>
+				<ul class="color-blocks">
+					<li class="color-block" v-for="color in group.colors" :key="color[0]">
+						<div class="color-box" :style="{ backgroundColor: color[0] }"></div>
+						<div>
+							<kv-button
+								class="smallest secondary"
+								@click.native="copyToClipboard(color[0])">
+								{{ color[0] }}
+							</kv-button>
+							<kv-button
+								class="smallest secondary"
+								@click.native="copyToClipboard(color[1])">
+								{{ color[1] }}
+							</kv-button>
+						</div>
+					</li>
+				</ul>
+			</li>
+		</ul>
 	</section>
 </template>
 
+<script>
+import copy from 'copy-to-clipboard';
+import KvButton from '@/components/Kv/KvButton';
+
+export default {
+	components: {
+		KvButton
+	},
+	data() {
+		return {
+			colorGroups: [
+				{
+					category: 'Greens',
+					colors: [
+						['#8ccb8c', '$kiva-accent-green'],
+						['#aee15c', '$kiva-light-green'],
+						['#4faf4e', '$kiva-green'],
+						['#49953f', '$kiva-icon-green'],
+						['#247037', '$kiva-darkgreen'],
+						['#DDFFF4', '$aqua-light-green'],
+						['#C4E7DB', '$aqua-medium-green'],
+					]
+				},
+				{
+					category: 'Blues',
+					colors: [
+						['#d2f0f8', '$kiva-filter-indicator-bg'],
+						['#6badc2', '$shadow-lightblue'],
+						['#1f92ba', '$shadow-blue'],
+						['#118aec', '$kiva-accent-blue'],
+						['#006ed3', '$kiva-accent-darkblue'],
+						['#0066a9', '$kiva-textlink-visited'],
+					]
+				},
+				{
+					category: 'Greys',
+					colors: [
+						['#ffffff', '$white'],
+						['#fafafa', '$kiva-bg-lightgray'],
+						['#efefef', '$kiva-bg-darkgray'],
+						['#d8d8d8', '$kiva-stroke-gray'],
+						['#cccccc', '$subtle-gray'],
+						['#999999', '$kiva-text-light'],
+						['#666666', '$kiva-text-medium'],
+						['#484848', '$kiva-text-dark'],
+						['#333333', '$dark-charcoal'],
+						['#000000', '$black'],
+
+					]
+				},
+				{
+					category: 'Highlights',
+					colors: [
+						['#d74937', '$kiva-accent-red'],
+						['#6859c0', '$kiva-accent-purple'],
+						['#fee259', '$kiva-alert-yellow'],
+					]
+				},
+			]
+		};
+	},
+	methods: {
+		copyToClipboard(string) {
+			return copy(string);
+		}
+	}
+};
+</script>
+
 <style lang="scss" scoped>
 @import 'settings';
+
+.color-groups,
+.color-blocks {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+}
+
+.color-blocks {
+	display: grid;
+	grid-template-columns: 1fr 1fr 1fr;
+	grid-gap: 1rem;
+}
 
 .color-card {
 	text-align: center;
@@ -263,7 +118,6 @@
 
 .color-box {
 	height: rem-calc(75);
-	width: rem-calc(75);
 	margin: 0 auto;
 }
 
@@ -275,103 +129,6 @@
 	font-weight: bold;
 }
 
-.kiva-light-green {
-	background: $kiva-light-green;
-}
 
-.kiva-green {
-	background: $kiva-green;
-}
 
-.kiva-dark-green {
-	background: $kiva-darkgreen;
-}
-
-.kiva-icon-green {
-	background: $kiva-icon-green;
-}
-
-.kiva-accent-green {
-	background: $kiva-accent-green;
-}
-
-.kiva-accent-blue {
-	background: $kiva-accent-blue;
-}
-
-.kiva-accent-darkblue {
-	background: $dark-blue;
-}
-
-.kiva-accent-red {
-	background: $kiva-accent-red;
-}
-
-.kiva-accent-purple {
-	background: $kiva-accent-purple;
-}
-
-.kiva-alert-yellow {
-	background: $kiva-alert-yellow;
-}
-
-.kiva-bg-lightgray {
-	background: $kiva-bg-lightgray;
-}
-
-.kiva-stroke-gray {
-	background: $kiva-bg-darkgray;
-}
-
-.kiva-text-dark {
-	background: $kiva-text-dark;
-}
-
-.kiva-text-medium {
-	background: $kiva-text-medium;
-}
-
-.kiva-text-light {
-	background: $kiva-text-light;
-}
-
-.kiva-textlink-visited {
-	background: $kiva-textlink-visited;
-}
-
-.kiva-filter-indicator-bg {
-	background: $kiva-filter-indicator-bg;
-}
-
-.shadow-blue {
-	background: $shadow-blue;
-}
-
-.shadow-lightblue {
-	background: $shadow-lightblue;
-}
-
-.aqua-light-green {
-	background: $aqua-light-green;
-}
-
-.aqua-medium-green {
-	background: $aqua-medium-green;
-}
-
-.white {
-	background: $white;
-}
-
-.black {
-	background: $black;
-}
-
-.subtle-gray {
-	background: $subtle-gray;
-}
-
-.dark-charcoal {
-	background: $dark-charcoal;
-}
 </style>
