755f0ced4a1f12a9e218e85076a8ebeb932cffc1
Eddie Ferrer <eddief@kiva.org>
2019-12-06 13:57:16 -0800

    removed logReadQueryError Sentry exception

--- src/util/logReadQueryError.js
+++ src/util/logReadQueryError.js
@@ -1,9 +1,3 @@
-import * as Sentry from '@sentry/browser';
-
 export default (error, queryType = 'undefined_query_error_type') => {
 	console.error(queryType, error);
-	Sentry.withScope(scope => {
-		scope.setTag('query_error_type', queryType);
-		Sentry.captureException(error);
-	});
 };
