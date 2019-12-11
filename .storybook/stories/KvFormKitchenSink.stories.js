46b1ef7420b0746d41e9461b63ece8e36a661ffa
Ryan Ludwig <ryanl@kiva.org>
2019-12-11 15:07:40 -0800

    Start filling up the kitchen sink
    VUE-191

---
+++ .storybook/stories/KvFormKitchenSink.stories.js
@@ -0,0 +1,143 @@
+import KvDropdownRounded from '@/components/Kv/KvDropdownRounded';
+import KvPillToggle from '@/components/Kv/KvPillToggle';
+import KvRadio from '@/components/Kv/KvRadio';
+
+export default { title: 'Kv | Form Elements' };
+
+export const KitchenSink = () => ({
+	components: {
+		KvDropdownRounded,
+		KvPillToggle,
+		KvRadio,
+	},
+	data() {
+		return {
+			kvRadioSelected: 'female',
+			kvPillOptions: [
+				{
+					title: 'Option 1',
+					key: 'o1',
+					disabled: true
+				},
+				{
+					title: 'Option 2',
+					key: 'o2',
+				},
+				{
+					title: 'Option 3',
+					key: 'o3',
+				},
+			],
+			kvPillSelected: 'o2',
+			kvDropdownRoundedModel: 'test2'
+		}
+	},
+	template: `
+		<section id="section-form">
+			<form>
+				<fieldset>
+					<legend>Example Legend</legend>
+
+					<fieldset style="margin-bottom: 2rem;">
+						<kv-pill-toggle
+							:options="kvPillOptions"
+							:selected="kvPillSelected"
+						/>
+					</fieldset>
+
+					<fieldset style="margin-bottom: 2rem;">
+						<div>
+							<kv-radio
+								name="gender-radio"
+								radioValue="both"
+								v-model="kvRadioSelected"
+								disabled
+							>
+								Everyone
+							</kv-radio>
+						</div>
+						<div>
+							<kv-radio
+								name="gender-radio"
+								radioValue="female"
+								v-model="kvRadioSelected"
+							>
+								Women only
+							</kv-radio>
+						</div>
+						<div>
+							<kv-radio
+								name="gender-radio"
+								radioValue="male"
+								v-model="kvRadioSelected"
+							>
+								Men only
+							</kv-radio>
+						</div>
+					</fieldset>
+
+					<fieldset style="margin-bottom: 2rem;">
+						<kv-dropdown-rounded v-model="kvDropdownRoundedModel">
+							<option value="test">Test</option>
+							<option value="test2">Test2</option>
+							<option value="test3">Test3</option>
+						</kv-dropdown-rounded>
+					</fieldset>
+
+				</fieldset>
+
+				<fieldset>
+					<label for="example-select">
+						Select
+						<select
+							name="example-select"
+							id="example-select"
+						>
+							<option
+								v-for="singleOption in options"
+								:key="singleOption"
+								:value="singleOption"
+							>
+								{{ singleOption }}
+							</option>
+						</select>
+					</label>
+					<label for="example-multiple-select">
+						Multiple Select
+						<select
+							name="example-multiple-select"
+							id="example-multiple-select"
+							multiple
+						>
+							<option
+								v-for="singleOption in options"
+								:key="singleOption"
+								:value="singleOption"
+							>
+								{{ singleOption }}
+							</option>
+						</select>
+					</label>
+				</fieldset>
+
+				<fieldset>
+					<label>
+						Input Text
+						<input
+							type="text"
+							name="inputText"
+							maxlength="40"
+						>
+					</label>
+					<label>
+						Input Text Area
+						<textarea
+							name="inputTextarea"
+							rows="4"
+						></textarea>
+					</label>
+				</fieldset>
+			</form>
+		</section>
+	`
+});
