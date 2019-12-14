4eac97b0651557f67691506b85e08e03d13ef7ec
Ryan Ludwig <ryanl@kiva.org>
2019-12-13 16:19:59 -0800

    Add focus + disabled styles, use inputWrapperMixin, allow v-model and programatic setting of checked

--- src/components/Kv/KvCheckbox.vue
+++ src/components/Kv/KvCheckbox.vue
@@ -1,56 +1,118 @@
 <template>
-	<label class="kv-checkbox">
+	<div class="kv-checkbox">
 		<input
-			v-if="isControlledComponent"
+			class="input"
 			type="checkbox"
-			:checked="isChecked"
-			@change="handleChange"
-			@input="handleInput"
+			:id="id"
+			v-model="inputValue"
+			v-on="inputListeners"
+			v-bind="$attrs"
 		>
-		<input
-			v-else
-			type="checkbox"
-			v-model="checkboxStatus"
-			@change="handleChange"
-			@input="handleInput"
+		<label
+			class="label"
+			:for="id"
 		>
-		<slot v-if="hasDefaultSlot"></slot>
-		<span v-else-if="label">{{ label }}</span>
-	</label>
+			<div class="square"></div>
+			<div>
+				<slot></slot>
+			</div>
+		</label>
+	</div>
 </template>
 
 <script>
+import inputWrapperMixin from '@/plugins/input-wrapper-mixin';
+
 export default {
 	props: {
-		label: {
+		id: {
 			type: String,
-			default: null
+			required: true
 		},
-		isChecked: {
+		isChecked: { // workaround to allow both v-model binding and setting checked programatically
 			type: Boolean,
-			default: null,
-		},
+			default: null
+		}
 	},
-	data() {
-		return {
-			checkboxStatus: false,
-		};
+	watch: {
+		isChecked(val) {
+			this.inputValue = val;
+		}
 	},
-	computed: {
-		hasDefaultSlot() {
-			return !!this.$slots.default;
-		},
-		isControlledComponent() {
-			return this.isChecked !== null;
-		},
+	mounted() {
+		if (this.isChecked !== null) {
+			this.inputValue = this.isChecked;
+		}
 	},
-	methods: {
-		handleChange() {
-			this.$emit('checkbox-toggled', this.checkboxStatus);
-		},
-		handleInput() {
-			this.$emit('checkbox-input', this.checkboxStatus);
-		},
-	},
+	mixins: [inputWrapperMixin],
 };
 </script>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+.kv-checkbox {
+	display: block;
+	position: relative;
+
+	.label {
+		display: flex;
+		align-items: center;
+		font-size: 1em;
+		margin: 0;
+	}
+
+	.square {
+		width: 1em;
+		height: 1em;
+		flex-shrink: 0;
+		border-radius: 0.125em;
+		background-color: #fff;
+		border: 0.125em solid $subtle-gray;
+		margin-right: 0.5em;
+		box-shadow: 0 0 0 0 rgba(79, 175, 78, 0.2);
+		position: relative;
+		transition: background-color 200ms ease-in-out, box-shadow 200ms ease-in-out;
+
+		&::after {
+			content: '';
+			position: absolute;
+			top: 0;
+			left: 0.225em;
+			width: 0.3125em;
+			height: 0.625em;
+			border: solid transparent;
+			border-width: 0 0.125em 0.125em 0;
+			transform: rotate(45deg);
+		}
+	}
+
+	.input {
+		@include visually-hidden();
+
+		&:checked + .label {
+			.square {
+				background-color: $kiva-light-green;
+				border-color: $kiva-green;
+				border-width: 0.0625em;
+
+				&::after {
+					top: 0.0625em;
+					left: 0.2875em;
+					border-color: white;
+				}
+			}
+		}
+
+		&:focus + .label {
+			.square {
+				box-shadow: 0 0 0 0.25em rgba(174, 225, 92, 0.4); // $kiva-accent-green TODO: break into a scss mixin
+			}
+		}
+
+		&[disabled] + .label {
+			@include disabled();
+		}
+	}
+}
+</style>
