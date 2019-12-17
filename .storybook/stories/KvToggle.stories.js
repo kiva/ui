b498a7ac7c29d91e53ac0e24baaaecc708024e2f
Ryan Ludwig <ryanl@kiva.org>
2019-12-17 13:20:50 -0800

    Add stories

--- .storybook/stories/KvToggle.stories.js
+++ .storybook/stories/KvToggle.stories.js
@@ -1,4 +1,5 @@
 import KvToggle from '@/components/Kv/KvToggle';
+import { boolean } from '@storybook/addon-knobs';
 
 export default { title: 'Kv | Form Elements/KvToggle' };
 
@@ -6,23 +7,151 @@ export const Default = () => ({
 	components: {
 		KvToggle
 	},
-	data() {
-		return {
-			checked: true,
-		}
+	props: {
+		option1: {
+			type: Boolean,
+			default: boolean('option1', false)
+		},
+		option2: {
+			type: Boolean,
+			default: boolean('option2', true)
+		},
+		option3: {
+			type: Boolean,
+			default: boolean('option3', false)
+		 },
 	},
-	template: `<kv-toggle v-model="checked">Label here</kv-toggle>`
+	template: `
+		<fieldset>
+			<legend>Using v-model</legend>
+			<kv-toggle
+				id="toggle-1"
+				v-model="option1"
+				@change="onChange"
+				disabled
+			>
+				Option 1
+			</kv-toggle>
+			<kv-toggle
+				id="toggle-2"
+				v-model="option2"
+				@change="onChange"
+			>
+				Option 2
+			</kv-toggle>
+			<kv-toggle
+				id="toggle-3"
+				v-model="option3"
+				@change="onChange"
+			>
+				Option 3
+			</kv-toggle>
+		</fieldset>
+	`,
+	methods: {
+		onChange(val) {
+			console.log(val);
+		}
+	}
 });
 
-export const Disabled = () => ({
+export const IsChecked = () => ({
 	components: {
 		KvToggle
 	},
-	data() {
-		return {
-			checked: true,
+	template: `
+		<fieldset>
+			<legend>Using :checked</legend>
+			<kv-toggle
+				id="is-checked1"
+				:checked="someMethod(true)"
+				@change="onChange"
+			>
+				True by default
+			</kv-toggle>
+			<kv-toggle
+				id="is-checked2"
+				:checked="someMethod(false)"
+				@change="onChange"
+			>
+				False by default
+			</kv-toggle>
+		</fieldset>
+	`,
+	methods: {
+		onChange(val) {
+			console.log(val);
+		},
+		someMethod(param) {
+			return param;
 		}
-	},
-	template: `<kv-toggle disabled v-model="checked">Label here</kv-toggle>`
+	}
 });
 
+export const FontSized = () => ({
+	components: {
+		KvToggle
+	},
+	template: `
+		<fieldset>
+			<legend>Font Sized</legend>
+			<kv-toggle
+				id="big"
+				:checked="true"
+				style="font-size: 2rem;"
+			>
+				2rem
+			</kv-toggle>
+			<kv-toggle
+				id="normal"
+				:checked="true"
+			>
+			normal
+			</kv-toggle>
+			<kv-toggle
+				id="small"
+				:checked="true"
+				style="font-size: .875rem;"
+			>
+				.875rem
+			</kv-toggle>
+			<kv-toggle
+				id="smaller"
+				:checked="true"
+				style="font-size: .75rem;"
+			>
+				.75rem
+			</kv-toggle>
+		</fieldset>
+	`,
+});
+
+export const WithContents = () => ({
+	components: {
+		KvToggle
+	},
+	template: `
+		<fieldset>
+			<legend>Font Sized</legend>
+			<kv-toggle
+				id="option-1"
+				:checked="true"
+			>
+				<h2>Big Header</h2>
+				<p>Some explanatory text</p>
+			</kv-toggle>
+			<kv-toggle
+				id="option-2"
+				:checked="false"
+			>
+				Option 2
+			</kv-toggle>
+			<kv-toggle
+				id="option-3"
+				:checked="false"
+			>
+				Option 3
+			</kv-toggle>
+		</fieldset>
+	`,
+});
