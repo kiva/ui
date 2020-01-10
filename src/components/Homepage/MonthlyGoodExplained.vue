e3b2a0b753d90d332b7a32eb690ef59acb4fc38b
Joshua <joshuadstarkey@gmail.com>
2020-01-10 15:47:17 -0800

    cash-1714 styles in place, cleanup complete

--- src/components/Homepage/MonthlyGoodExplained.vue
+++ src/components/Homepage/MonthlyGoodExplained.vue
@@ -4,39 +4,57 @@
 			<h1 class="column small-12 text-center">
 				How does Monthly Good work?
 			</h1>
-			<div class="column small-12 medium-6 large-3 number">
-				1
-				<kv-icon name="hp-mg-1-signup mg-image" />
-				<h2>
-					Sign up
-				</h2>
-				to automatically help a borrower every month
+			<div class="steps-container">
+				<div class="column small-12 medium-6 large-3 step-wrapper">
+					<span class="number">
+						1
+					</span>
+					<kv-icon name="hp-mg-1-signup" class="mg-image" />
+					<h2>
+						Sign up
+					</h2>
+					<div class="description-text">
+						to automatically help a borrower every month
+					</div>
+				</div>
+				<div class="column small-12 medium-6 large-3 step-wrapper">
+					<span class="number">
+						2
+					</span>
+					<kv-icon name="hp-mg-2-choose" class="mg-image" />
+					<h2>
+						Choose
+					</h2>
+					<div class="description-text">
+						how much and who you want to lend to - Kiva's got it from here
+					</div>
+				</div>
+				<div class="column small-12 medium-6 large-3 step-wrapper">
+					<span class="number">
+						3
+					</span>
+					<kv-icon name="hp-mg-3-receive" class="mg-image" />
+					<h2>
+						Receive
+					</h2>
+					<div class="description-text">
+						a personalized report on your impact once a month
+					</div>
+				</div>
+				<div class="column small-12 medium-6 large-3 step-wrapper">
+					<span class="number">
+						4
+					</span>
+					<kv-icon name="hp-mg-4-multiply" class="mg-image" />
+					<h2>
+						Multiply
+					</h2>
+					<div class="description-text">
+						your impact as Kiva continues to re-lend your repayments
+					</div>
+				</div>
 			</div>
-			<div class="column small-12 medium-6 large-3 number">
-				2
-				<kv-icon name="hp-mg-2-choose mg-image" />
-				<h2>
-					Choose
-				</h2>
-				how much and who you want to lend to - Kiva's got it from here
-			</div>
-			<div class="column small-12 medium-6 large-3 number">
-				3
-				<kv-icon name="hp-mg-3-receive mg-image" />
-				<h2>
-					Receive
-				</h2>
-				a personalized report on your impact once a month
-			</div>
-			<div class="column small-12 medium-6 large-3 number">
-				4
-				<kv-icon name="hp-mg-4-multiply mg-image" />
-				<h2>
-					Multiply
-				</h2>
-				your impact as Kiva continues to re-lend your repayments
-			</div>
-			<p class="column large-12 text-center learn-more-wrap">
+			<div class="column large-12 text-center learn-more-wrapper">
 				<router-link
 					:to="`/monthly-good`"
 					class="learn-more-text"
@@ -49,7 +67,7 @@
 				>
 					Learn more
 				</router-link>
-			</p>
+			</div>
 		</div>
 	</div>
 </template>
@@ -71,20 +89,68 @@ export default {
 .monthly-good-explained {
 	background-color: $kiva-green;
 	margin-bottom: 6rem;
-}
 
-h1,
-.learn-more-text,
-.number {
-	color: $white;
-}
+	h1,
+	h2,
+	.learn-more-text,
+	.number,
+	.description-text {
+		color: $white;
+	}
 
-h1 {
-	padding: 1.6rem 0;
-}
+	h1 {
+		padding: 1.6rem 0;
+	}
 
-.learn-more-wrap {
-	margin: 2rem 0 2rem;
+	.steps-container {
+		display: contents;
+
+		.step-wrapper {
+			margin-bottom: 2.5rem;
+		}
+
+		.number {
+			padding-left: 5.3rem;
+
+			@include breakpoint(medium) {
+				padding-left: unset;
+			}
+		}
+
+		h2,
+		.description-text {
+			text-align: center;
+
+			@include breakpoint(medium) {
+				text-align: unset;
+			}
+		}
+
+		.description-text {
+			max-width: 12.5rem;
+			margin: 0 auto;
+
+			@include breakpoint(medium) {
+				margin: unset;
+			}
+		}
+
+		.mg-image {
+			width: 45%;
+			max-height: 6.25rem;
+			display: block;
+			margin: 0 auto 1rem auto;
+
+			@include breakpoint(medium) {
+				text-align: unset;
+				margin-left: 1.25rem;
+			}
+		}
+	}
+
+	.learn-more-wrapper {
+		margin-bottom: 2rem;
+	}
 }
 
 </style>
