3e7a20325ec4318bde2ac138e0d6a196482ad563
Ryan Ludwig <ryanl@kiva.org>
2020-01-15 15:32:51 -0800

    Add kiva cards and deposit information based on current thanks page

--- .storybook/stories/CheckoutReceipt.stories.js
+++ .storybook/stories/CheckoutReceipt.stories.js
@@ -3,169 +3,300 @@ import StoryRouter from 'storybook-vue-router';
 import CheckoutReceipt from '@/components/Checkout/CheckoutReceipt';
 import SocialShare from '@/components/Checkout/SocialShare';
 
-// http://api-vm.kiva.org/graphql?user_id=1003394&app_id=org.kiva.www&query=%7B%0A%20%20shop%20%7B%0A%20%20%20%20receipt(checkoutId%3A%2038646529)%20%7B%0A%20%20%20%20%20%20credits%20%7B%0A%20%20%20%20%20%20%20%20values%20%7B%0A%20%20%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20%20%20creditType%0A%20%20%20%20%20%20%20%20%20%20amount%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20totals%20%7B%0A%20%20%20%20%20%20%20%20itemTotal%0A%20%20%20%20%20%20%20%20donationTotal%0A%20%20%20%20%20%20%20%20kivaCardTotal%0A%20%20%20%20%20%20%20%20depositTotals%20%7B%0A%20%20%20%20%20%20%20%20%20%20depositTotal%0A%20%20%20%20%20%20%20%20%20%20kivaCreditAdded%0A%20%20%20%20%20%20%20%20%20%20kivaCreditUsed%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20kivaCreditAppliedTotal%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20hasFreeCredits%0A%20%20%20%20%20%20items%20%7B%0A%20%20%20%20%20%20%20%20totalCount%0A%20%20%20%20%20%20%20%20values%20%7B%0A%20%20%20%20%20%20%20%20%20%20price%0A%20%20%20%20%20%20%20%20%20%20basketItemType%0A%20%20%20%20%20%20%20%20%20%20creditsUsed%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20amount%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%20%20...%20on%20LoanReservation%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20loan%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20name%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20image%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20url%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20use%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20geocode%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20city%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20country%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20name%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%09%7D%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%7D%0A%20%20%20%20%7D%0A%20%20%7D%0A%20%20my%20%7B%0A%20%20%20%20userAccount%20%7B%0A%20%20%20%20%20%20firstName%0A%20%20%20%20%20%20lastName%0A%20%20%20%20%20%20email%0A%20%20%20%20%7D%0A%20%20%7D%20%20%0A%7D%0A%0A
+// https://api-vm.kiva.org/graphqlgraphql?user_id=1003394&app_id=org.kiva.www&query=query%20checkoutReceipt%20%7B%20%0A%09shop%20%7B%0A%09%09receipt(checkoutId%3A%2038663432)%20%7B%0A%09%09%09credits%20%7B%0A%09%09%09%09values%20%7B%0A%09%09%09%09%09creditType%0A%09%09%09%09%09amount%0A%09%09%09%09%7D%0A%09%09%09%7D%0A%09%09%09totals%20%7B%0A%20%20%20%20%20%20%20%20bonusAppliedTotal%0A%09%09%09%09itemTotal%0A%09%09%09%09donationTotal%0A%09%09%09%09kivaCardTotal%0A%09%09%09%09depositTotals%20%7B%0A%09%09%09%09%09depositTotal%0A%09%09%09%09%09kivaCreditAdded%0A%09%09%09%09%09kivaCreditUsed%0A%09%09%09%09%7D%0A%20%20%20%20%20%20%20%20freeTrialAppliedTotal%0A%09%09%09%09kivaCreditAppliedTotal%0A%20%20%20%20%20%20%20%20redemptionCodeAppliedTotal%0A%20%20%20%20%20%20%20%20universalCodeAppliedTotal%0A%09%09%09%7D%0A%09%09%09hasFreeCredits%0A%09%09%09items%20%7B%0A%09%09%09%09totalCount%0A%09%09%09%09values%20%7B%0A%09%09%09%09%09id%0A%09%09%09%09%09price%0A%09%09%09%09%09basketItemType%0A%09%09%09%09%09...%20on%20KivaCard%20%7B%0A%09%09%09%09%09%09individualPrice%0A%09%09%09%09%09%09basketItemType%0A%09%09%09%09%09%09kivaCardObject%20%7B%0A%09%09%09%09%09%09%09deliveryType%0A%09%09%09%09%09%09%09mailingInfo%20%7B%0A%09%09%09%09%09%09%09%09firstName%0A%09%09%09%09%09%09%09%09lastName%0A%09%09%09%09%09%09%09%09address%0A%09%09%09%09%09%09%09%09address2%0A%09%09%09%09%09%09%09%09city%0A%09%09%09%09%09%09%09%09state%0A%09%09%09%09%09%09%09%09zip%0A%09%09%09%09%09%09%09%7D%0A%09%09%09%09%09%09%09recipient%20%7B%0A%09%09%09%09%09%09%09%09name%0A%09%09%09%09%09%09%09%09email%0A%09%09%09%09%09%09%09%7D%0A%09%09%09%09%09%09%7D%0A%09%09%09%09%09%7D%0A%09%09%09%09%09...%20on%20LoanReservation%20%7B%0A%09%09%09%09%09%09loan%20%7B%0A%09%09%09%09%09%09%09name%0A%09%09%09%09%09%09%09id%0A%09%09%09%09%09%09%09image%20%7B%0A%09%09%09%09%09%09%09%09url%0A%09%09%09%09%09%09%09%7D%0A%09%09%09%09%09%09%09use%0A%09%09%09%09%09%09%09geocode%20%7B%0A%09%09%09%09%09%09%09%09city%0A%09%09%09%09%09%09%09%09country%20%7B%0A%09%09%09%09%09%09%09%09%09name%0A%09%09%09%09%09%09%09%09%7D%0A%09%09%09%09%09%09%09%7D%0A%09%09%09%09%09%09%7D%0A%09%09%09%09%09%7D%0A%09%09%09%09%7D%0A%09%09%09%7D%0A%09%09%7D%0A%09%7D%0A%09my%20%7B%0A%09%09userAccount%20%7B%0A%09%09%09firstName%0A%09%09%09lastName%0A%09%09%09email%0A%09%09%7D%0A%09%7D%0A%7D%0A&operationName=checkoutReceipt
 const mockedReceiptData = {
 	"data": {
 		"shop": {
-		"receipt": {
-			"credits": {
-			"values": [
-				{
-					"id": 1003394,
-					"creditType": "kiva_credit",
-					"amount": null
-					}
-				]
-			},
-			"totals": {
-				"itemTotal": "891.25",
-				"donationTotal": "116.25",
-				"kivaCardTotal": "150.00",
-				"depositTotals": {
-					"depositTotal": "891.25",
-					"kivaCreditAdded": "0.00",
-					"kivaCreditUsed": "0.00"
+			"receipt": {
+				"credits": {
+					"values": [
+						{
+							"creditType": "kiva_credit",
+							"amount": null
+						}
+					]
 				},
-				"kivaCreditAppliedTotal": "891.25"
-			},
+				"totals": {
+					"itemTotal": "960.25",
+					"donationTotal": "125.25",
+					"kivaCardTotal": "710.00",
+					"depositTotals": {
+						"depositTotal": "960.25",
+						"kivaCreditAdded": "0.00",
+						"kivaCreditUsed": "0.00"
+					},
+					"kivaCreditAppliedTotal": "960.25"
+				},
 				"hasFreeCredits": false,
 				"items": {
-				"totalCount": 7,
-				"values": [
-					{
-					"price": "25.00",
-					"basketItemType": "loan_reservation",
-					"creditsUsed": [
+					"totalCount": 11,
+					"values": [
 						{
-						"amount": "25.00"
-						}
-					],
-					"loan": {
-						"name": "Alan",
-						"id": 1890376,
-						"image": {
-						"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/35bbf2ba0ae472c4814b99f301adae10.jpg"
+							"id": 1911067,
+							"price": "25.00",
+							"basketItemType": "loan_reservation",
+							"creditsUsed": [
+								{
+									"amount": "25.00"
+								}
+							],
+							"loan": {
+								"name": "Alan",
+								"id": 1911067,
+								"image": {
+									"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/5e041e655faa305c49c13505d1c17cba.jpg"
+								},
+								"use": "to expand her business by purchasing more stock such as seed and manure for the expansion of her vegetable garden.",
+								"geocode": {
+									"city": "Chienge",
+									"country": {
+										"name": "Zambia"
+									}
+								}
+							}
 						},
-						"use": "to buy cattle feed , and to buy cattle to increase her income from raising cattle.",
-						"geocode": {
-						"city": "Kara-Suu village, Osh region",
-						"country": {
-							"name": "Kyrgyzstan"
-						}
-						}
-					}
-					},
-					{
-					"price": "25.00",
-					"basketItemType": "loan_reservation",
-					"creditsUsed": [
 						{
-						"amount": "25.00"
-						}
-					],
-					"loan": {
-						"name": "Alan's Group",
-						"id": 1908787,
-						"image": {
-						"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/afb2502655fd85ab49aad7c6e93c91f3.jpg"
+							"id": 1908530,
+							"price": "25.00",
+							"basketItemType": "loan_reservation",
+							"creditsUsed": [
+								{
+									"amount": "25.00"
+								}
+							],
+							"loan": {
+								"name": "Alan",
+								"id": 1908530,
+								"image": {
+									"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/da9ac74228e39e15cc198f8165e68c20.jpg"
+								},
+								"use": "to buy taro roots (seedlings), banana tubers (seedlings), chemicals, a bag back sprayer, and a chainsaw.",
+								"geocode": {
+									"city": "Vaiafai Savaii",
+									"country": {
+										"name": "Samoa"
+									}
+								}
+							}
 						},
-						"use": "to purchase hybrid seeds and fertilizer to improve harvests of maize.",
-						"geocode": {
-						"city": "Teso",
-						"country": {
-							"name": "Kenya"
-						}
-						}
-					}
-					},
-					{
-					"price": "25.00",
-					"basketItemType": "loan_reservation",
-					"creditsUsed": [
 						{
-						"amount": "25.00"
-						}
-					],
-					"loan": {
-						"name": "Dukoranumwete Tcb Group",
-						"id": 1907446,
-						"image": {
-						"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/4943ca4a6fefd22dafdb12d142826ff9.jpg"
+							"id": 1907517,
+							"price": "25.00",
+							"basketItemType": "loan_reservation",
+							"creditsUsed": [
+								{
+									"amount": "25.00"
+								}
+							],
+							"loan": {
+								"name": "Barrio Concepcion Group",
+								"id": 1907517,
+								"image": {
+									"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/213d4f59069e192a2a4b9ee1fb56b649.jpg"
+								},
+								"use": "to buy a stove with a grill, kitchen utensils, corn and firewood.",
+								"geocode": {
+									"city": "Retalhuleu",
+									"country": {
+										"name": "Guatemala"
+									}
+								}
+							}
 						},
-						"use": "to add more fish to her business in order to meet customer demand.",
-						"geocode": {
-						"city": null,
-						"country": {
-							"name": "Rwanda"
-						}
-						}
-					}
-					},
-					{
-					"price": "400.00",
-					"basketItemType": "loan_reservation",
-					"creditsUsed": [
 						{
-						"amount": "400.00"
-						}
-					],
-					"loan": {
-						"name": "Mujeres Del Milagro Group",
-						"id": 1908384,
-						"image": {
-						"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/ff35a3d4c8a68b1750108aece4ac1a4a.jpg"
+							"id": 1909791,
+							"price": "25.00",
+							"basketItemType": "loan_reservation",
+							"creditsUsed": [
+								{
+									"amount": "25.00"
+								}
+							],
+							"loan": {
+								"name": "Alan",
+								"id": 1909791,
+								"image": {
+									"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/26e15431f51b540f31cd9f011cc54f31.jpg"
+								},
+								"use": "to buy farming inputs and improve her production in 2020 for an increased income.",
+								"geocode": {
+									"city": "Kitale",
+									"country": {
+										"name": "Kenya"
+									}
+								}
+							}
 						},
-						"use": "to buy 1 industrial stove, pots, and ingredients to continue preparing her recipes.",
-						"geocode": {
-						"city": "Patzun, Chimaltenango",
-						"country": {
-							"name": "Guatemala"
-						}
-						}
-					}
-					},
-					{
-					"price": "150.00",
-					"basketItemType": "loan_reservation",
-					"creditsUsed": [
 						{
-						"amount": "150.00"
-						}
-					],
-					"loan": {
-						"name": "Alan",
-						"id": 1894131,
-						"image": {
-						"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/fa3f2827dde0a60b883411224c40e62e.jpg"
+							"id": 1906824,
+							"price": "25.00",
+							"basketItemType": "loan_reservation",
+							"creditsUsed": [
+								{
+									"amount": "25.00"
+								}
+							],
+							"loan": {
+								"name": "Las Victoriosas Group",
+								"id": 1906824,
+								"image": {
+									"url": "https://www-dev-kiva-org.global.ssl.fastly.net/img/s100/26342071330beb8309d17a5d262da2dd.jpg"
+								},
+								"use": "to buy used clothing for men, women and children in specific sizes.",
+								"geocode": {
+									"city": "Chimaltenango",
+									"country": {
+										"name": "Guatemala"
+									}
+								}
+							}
 						},
-						"use": "to purchase sandals, jeans, beauty products (shampoo, cologne, talcum powder) and kits for bonuses.",
-						"geocode": {
-						"city": "Soledad (Atlántico)",
-						"country": {
-							"name": "Colombia"
-						}
-						}
-					}
-					},
-					{
-					"price": "150.00",
-					"basketItemType": "kiva_card",
-					"creditsUsed": [
 						{
-						"amount": "150.00"
+							"id": 2118024,
+							"price": "30.00",
+							"basketItemType": "kiva_card",
+							"creditsUsed": [
+								{
+									"amount": "30.00"
+								}
+							],
+							"individualPrice": "30.00",
+							"kivaCardObject": {
+								"deliveryType": "email",
+								"mailingInfo": {
+									"firstName": null,
+									"lastName": null,
+									"address": null,
+									"address2": null,
+									"city": null,
+									"state": null,
+									"zip": null
+								},
+								"recipient": {
+									"name": "Ryan Ludwig eMail",
+									"email": "ryanl@kiva.org"
+								}
+							}
+						},
+						{
+							"id": 2118025,
+							"price": "30.00",
+							"basketItemType": "kiva_card",
+							"creditsUsed": [
+								{
+									"amount": "30.00"
+								}
+							],
+							"individualPrice": "30.00",
+							"kivaCardObject": {
+								"deliveryType": "email",
+								"mailingInfo": {
+									"firstName": null,
+									"lastName": null,
+									"address": null,
+									"address2": null,
+									"city": null,
+									"state": null,
+									"zip": null
+								},
+								"recipient": {
+									"name": "Ryan Ludwig eMail",
+									"email": "ryanl+123@kiva.org"
+								}
+							}
+						},
+						{
+							"id": 2118026,
+							"price": "150.00",
+							"basketItemType": "kiva_card",
+							"creditsUsed": [
+								{
+									"amount": "150.00"
+								}
+							],
+							"individualPrice": "150.00",
+							"kivaCardObject": {
+								"deliveryType": "print",
+								"mailingInfo": {
+									"firstName": null,
+									"lastName": null,
+									"address": null,
+									"address2": null,
+									"city": null,
+									"state": null,
+									"zip": null
+								},
+								"recipient": {
+									"name": "Ryan Ludwig Print",
+									"email": null
+								}
+							}
+						},
+						{
+							"id": 2118027,
+							"price": "250.00",
+							"basketItemType": "kiva_card",
+							"creditsUsed": [
+								{
+									"amount": "250.00"
+								}
+							],
+							"individualPrice": "250.00",
+							"kivaCardObject": {
+								"deliveryType": "postal",
+								"mailingInfo": {
+									"firstName": "RYAN",
+									"lastName": "LUDWIG",
+									"address": "11575 SW 13TH ST",
+									"address2": "",
+									"city": "BEAVERTON",
+									"state": "OR",
+									"zip": "97005"
+								},
+								"recipient": {
+									"name": "Ryan Ludwig Mail",
+									"email": null
+								}
+							}
+						},
+						{
+							"id": 2118028,
+							"price": "250.00",
+							"basketItemType": "kiva_card",
+							"creditsUsed": [
+								{
+									"amount": "250.00"
+								}
+							],
+							"individualPrice": "250.00",
+							"kivaCardObject": {
+								"deliveryType": "postal",
+								"mailingInfo": {
+									"firstName": "RYAN",
+									"lastName": "LUDWIG",
+									"address": "11575 SW 13TH ST",
+									"address2": "",
+									"city": "BEAVERTON",
+									"state": "OR",
+									"zip": "97005"
+								},
+								"recipient": {
+									"name": "Ryan Ludwig Mail",
+									"email": null
+								}
+							}
+						},
+						{
+							"id": 48417665,
+							"price": "125.25",
+							"basketItemType": "donation",
+							"creditsUsed": [
+								{
+									"amount": "125.25"
+								}
+							]
 						}
 					]
-					},
-					{
-					"price": "116.25",
-					"basketItemType": "donation",
-					"creditsUsed": [
-						{
-						"amount": "116.25"
-						}
-					]
-					}
-				]
 				}
 			}
 		},
@@ -178,7 +309,6 @@ const mockedReceiptData = {
 		}
 	}
 };
-
 
 export default {
 	title: 'Components|ThanksPage',
--- src/components/Checkout/CheckoutReceipt.vue
+++ src/components/Checkout/CheckoutReceipt.vue
@@ -9,69 +9,156 @@
 			{{ lender.email }}
 		</section>
 		<section>
-			<ul class="checkout-receipt__loan-list">
+			<div
+				class="section text-center"
+				v-if="printableKivaCards.length > 0"
+			>
+				<h2>Print your Kiva {{ printableKivaCards.length > 1 ? 'Cards' : 'Card' }}</h2>
+				<p>
+					The print-it-yourself Kiva {{ printableKivaCards.length > 1 ? 'Cards' : 'Card' }} you
+					ordered can be printed right now by clicking on the print
+					{{ printableKivaCards.length > 1 ? 'buttons' : 'button' }} in the receipt below.
+					Or, you can print a copy anytime directly from your email receipt.
+				</p>
+			</div>
+			<ul class="checkout-receipt__item-list">
+				<!-- Loans -->
 				<li
 					class="section"
-					v-for="item in receipt.items.values"
-					:key="item.id"
+					v-for="loan in loans"
+					:key="loan.id"
 				>
-					<div
-						class="loan"
-						v-if="item.basketItemType === 'loan_reservation'"
-					>
-						<h3>
+					<div class="loan">
+						<h3 class="loan__name">
 							<router-link
-								class="loan__name"
-								:to="`/lend/${item.loan.id}`"
+								:to="`/lend/${loan.id}`"
 							>
-								{{ item.loan.name }}
+								{{ loan.loan.name }}
 							</router-link>
 						</h3>
 						<div class="loan__meta">
 							<p class="loan__meta-city">
-								<span v-if="item.loan.geocode && item.loan.geocode.city">
-									{{ item.loan.geocode.city }},
-								</span>
-								<span v-if="item.loan.geocode && item.loan.geocode.country">
-									{{ item.loan.geocode.country.name }}
-								</span>
+								<template v-if="loan.loan.geocode && loan.loan.geocode.city">
+									{{ loan.loan.geocode.city }},
+								</template>
+								<template v-if="loan.loan.geocode && loan.loan.geocode.country">
+									{{ loan.loan.geocode.country.name }}
+								</template>
 							</p>
-							<p>{{ item.loan.use }}</p>
+							<p v-if="loan.loan.use">
+								A loan helps {{ loan.loan.use }}
+							</p>
 						</div>
 						<div class="loan__amount">
-							${{ item.price }}
-						</div>
-					</div>
-					<div
-						class="loan"
-						v-else-if="item.basketItemType === 'kiva_card'"
-					>
-						<h3 class="loan__name">
-							Kiva Card
-						</h3>
-						<div class="loan__amount">
-							${{ item.price }}
-						</div>
-					</div>
-					<div
-						class="loan"
-						v-else-if="item.basketItemType === 'donation'"
-					>
-						<h3 class="loan__name">
-							Donation
-						</h3>
-						<div class="loan__amount">
-							${{ item.price }}
+							${{ loan.price }}
 						</div>
 					</div>
 				</li>
+				<!-- Kiva Cards -->
+				<li
+					class="section"
+					v-for="card in kivaCards"
+					:key="card.id"
+				>
+					<div class="loan">
+						<template v-if="card.kivaCardObject.deliveryType === 'print'">
+							<h3 class="loan__name">
+								Print-it-yourself Kiva Card
+							</h3>
+							<kv-button
+								class="smallest"
+								@click="showPrintUrl(card.id)"
+							>
+								Print Kiva Card
+							</kv-button>
+							<!-- TODO: Should probably use a new icon here, maybe the (i) one. -->
+							<!-- <span :id="`print-card-${card.id}`">?</span> -->
+							<!-- TODO: This seems to blow up during SSR because of a window reference? -->
+							<!-- <kv-tooltip :controller="`print-card-${card.id}`">
+								You can print this card now. We'll also send it to
+								you in an email so you can print it later.
+							</kv-tooltip> -->
+						</template>
+						<template v-else-if="card.kivaCardObject.deliveryType === 'postal'">
+							<h3 class="loan__name">
+								Postal delivery Kiva Card
+							</h3>
+							<div>
+								For: {{ card.kivaCardObject.mailingInfo.firstName }}
+								{{ card.kivaCardObject.mailingInfo.lastName }}<br>
+								{{ card.kivaCardObject.mailingInfo.address }}<br>
+								<template v-if="card.kivaCardObject.mailingInfo.address2">
+									{{ card.kivaCardObject.mailingInfo.address2 }}<br>
+								</template>
+								{{ card.kivaCardObject.mailingInfo.city }},
+								{{ card.kivaCardObject.mailingInfo.state }}
+								{{ card.kivaCardObject.mailingInfo.zip }}
+							</div>
+						</template>
+						<template v-else-if="card.kivaCardObject.deliveryType === 'lender'">
+							<h3 class="loan__name">
+								Kiva Card
+							</h3>
+							<div v-if="card.kivaCardObject.recipient.name">
+								For: {{ card.kivaCardObject.recipient.name }}
+							</div>
+						</template>
+						<template v-else-if="card.kivaCardObject.deliveryType === 'email'">
+							<h3 class="loan__name">
+								Email delivery Kiva Card
+							</h3>
+							<div>
+								For:
+								<template v-if="card.kivaCardObject.recipient.name">
+									{{ card.kivaCardObject.recipient.name }} &ndash;
+								</template>
+								{{ card.kivaCardObject.recipient.email }}
+							</div>
+						</template>
+						<div class="loan__amount">
+							${{ card.price }}
+						</div>
+					</div>
+				</li>
+				<li
+					class="section text-center"
+					v-if="kivaCards.length > 0"
+				>
+					For more details about all your Kiva Card purchases, please visit your
+					<!-- TODO: is this the tracking we want? -->
+					<router-link to="/portfolio/kiva-cards"
+						v-kv-track-event="['Thanks','click', 'kiva-card-portfolio']"
+					>
+						Kiva portfolio
+					</router-link>.
+				</li>
 			</ul>
 		</section>
-		<section class="section split total">
-			<h3 class="total__header split__item">
-				Total:
-			</h3>
-			<span class="total__amount split__item split__item--end">${{ receipt.totals.itemTotal }}</span>
+		<section>
+			<ul class="checkout-receipt__item-list">
+				<li class="section">
+					<div class="loan">
+						<h3 class="loan__name">
+							Donation to Kiva
+						</h3>
+						<router-link
+							class="smallest"
+							to="/portfolio/donations"
+						>
+							Print Donation Information
+						</router-link>
+						<div class="loan__amount">
+							${{ receipt.totals.donationTotal }}
+						</div>
+					</div>
+				</li>
+				<li class="section total">
+					<h3 class="total__header">
+						Total:
+					</h3>
+					<span class="total__amount">${{ receipt.totals.itemTotal }}</span>
+				</li>
+			</ul>
 		</section>
 		<section class="section payments">
 			<h2 class="payments__header">
@@ -79,21 +166,40 @@
 			</h2>
 			<ul class="payments__list">
 				<li
-					class="total split"
-					v-if="receipt.totals.depositTotals.depositTotal > 0"
+					class="total"
+					v-if="receipt.totals.freeTrialAppliedTotal > 0"
 				>
-					<span class="total__header split__item">Kiva Credit:</span>
-					<span class="total__amount split__item split__item--end">
-						-${{ receipt.totals.depositTotals.kivaCreditUsed }}
-					</span>
+					<span class="total__header">Free Trial:</span>
+					<span class="total__amount">Free!</span>
 				</li>
 				<li
-					class="total split"
+					class="total"
+					v-if="receipt.totals.bonusAppliedTotal > 0"
+				>
+					<span class="total__header">Free credit:</span>
+					<span class="total__amount">{{ receipt.totals.bonusAppliedTotal }}</span>
+				</li>
+				<li
+					class="total"
+					v-if="receipt.totals.depositTotals.kivaCreditAdded > 0"
+				>
+					<span class="total__header">Kiva credit added:</span>
+					<span class="total__amount">${{ receipt.totals.depositTotals.kivaCreditAdded }}</span>
+				</li>
+				<li
+					class="total"
 					v-if="receipt.totals.depositTotals.kivaCreditUsed > 0"
 				>
-					<span class="total__header split__item">Kiva Credit:</span>
-					<span class="total__amount split__item split__item--end">
-						-${{ receipt.totals.depositTotals.kivaCreditUsed }}
+					<span class="total__header">Kiva credit:</span>
+					<span class="total__amount">${{ receipt.totals.depositTotals.kivaCreditUsed }}</span>
+				</li>
+				<li
+					class="total"
+					v-if="parseFloat(receipt.totals.depositTotals.depositTotal) > 0"
+				>
+					<span class="total__header">Amount charged:</span>
+					<span class="total__amount">
+						${{ receipt.totals.depositTotals.depositTotal }}
 					</span>
 				</li>
 			</ul>
@@ -108,11 +214,15 @@
 </template>
 
 <script>
+import KvButton from '@/components/Kv/KvButton';
+// import KvTooltip from '@/components/Kv/KvTooltip';
 import IconPrint from '@/assets/inline-svgs/icons/print.svg';
 
 export default {
 	components: {
 		IconPrint,
+		// KvTooltip,
+		KvButton,
 	},
 	props: {
 		lender: {
@@ -124,18 +234,48 @@ export default {
 			required: true
 		},
 	},
+	computed: {
+		loans() {
+			return this.receipt.items.values.filter(item => item.basketItemType === 'loan_reservation');
+		},
+		kivaCards() {
+			return this.receipt.items.values.filter(item => item.basketItemType === 'kiva_card');
+		},
+		printableKivaCards() {
+			return this.kivaCards.filter(card => card.kivaCardObject.deliveryType === 'print');
+		},
+		donations() {
+			return this.receipt.items.values.filter(item => item.basketItemType === 'donation');
+		}
+	},
 	methods: {
 		printReceipt() {
 			if (typeof window !== 'undefined') {
 				window.print();
 			}
-		}
+		},
+		showPrintUrl(/* cardId */) {
+			// TODO: figure out how to make the popup. What is this experience like on mobile?
+			// '/gifts/kiva-cards/print?giftCode=${cardId}'
+			// pop('{$item.display_details.print_url}', 'Kiva_Help', 740, 550, 'toolbar=1,menubar=1');
+		},
 	}
 };
 </script>
 
 <style lang="scss" scoped>
 @import 'settings';
+
+.section {
+	border-bottom: solid rem-calc(1) $light-gray;
+	padding: $global-margin 0;
+	display: block;
+	overflow: hidden;
+
+	&--print {
+		border-bottom: 0;
+	}
+}
 
 .checkout-receipt {
 	background: #fff;
@@ -148,9 +288,10 @@ export default {
 		text-align: center;
 	}
 
-	&__loan-list {
+	&__item-list {
 		list-style: none;
 		margin: 0;
+		width: 100%;
 	}
 }
 
@@ -177,10 +318,18 @@ export default {
 }
 
 .total {
+	display: flex;
+
 	&__header,
 	&__amount {
 		font-size: $medium-text-font-size;
 		font-weight: $global-weight-bold;
+		text-align: right;
+		flex: 1;
+	}
+
+	&__amount {
+		flex-grow: 0.33;
 	}
 }
 
@@ -232,27 +381,4 @@ export default {
 	}
 }
 
-.section {
-	border-bottom: solid rem-calc(1) $light-gray;
-	padding: $global-margin 0;
-	display: block;
-	overflow: hidden;
-
-	&--print {
-		border-bottom: 0;
-	}
-}
-
-.split {
-	display: flex;
-
-	&__item {
-		text-align: right;
-		flex: 1;
-
-		&--end {
-			flex-grow: 0.33;
-		}
-	}
-}
 </style>
