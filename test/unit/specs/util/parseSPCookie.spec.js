422c31b74ea144b67cb6c6248b0ac114fb75211b
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2020-01-15 16:58:09 -0800

    CASH-1649 add parser for snowplow cookie

---
+++ test/unit/specs/util/parseSPCookie.spec.js
@@ -0,0 +1,22 @@
+import cookieStore from '@/util/cookieStore';
+import parseSPCookie from '@/util/parseSPCookie';
+
+describe('parseSPCookie.js', () => {
+	afterEach(() => {
+		cookieStore.reset();
+	});
+
+	it('returns the user id and session id from the Snowplow cookie', () => {
+		cookieStore.reset({
+			'_sp_id.1234': 'snowplow-user-id-1234.1454737815.21.1459135379.1459124000.snowplow-session-id-5678'
+		});
+		expect(parseSPCookie()).toEqual({
+			snowplowUserId: 'snowplow-user-id-1234',
+			snowplowSessionId: 'snowplow-session-id-5678',
+		});
+	});
+
+	it('returns an empty object if the Snowplow cookie is not present', () => {
+		expect(parseSPCookie()).toEqual({});
+	});
+});
