031d3a141356b6bd38f1041c4fba57c427cc9ee6
Ryan Ludwig <ryanl@kiva.org>
2020-01-17 14:46:48 -0800

    Break SocialShare and CheckoutReceipt into separate .stories files

---
+++ .storybook/stories/SocialShare.stories.js
@@ -0,0 +1,30 @@
+import { mockedReceiptData } from './CheckoutReceipt.stories';
+import SocialShare from '@/components/Checkout/SocialShare';
+import StoryRouter from 'storybook-vue-router';
+
+export default {
+	title: 'Components/SocialShare',
+	decorators: [StoryRouter()],
+	component: SocialShare,
+};
+
+export const Default = () => ({
+	components: {
+		SocialShare,
+	},
+	template: `
+		<social-share
+			:loans="loans"
+		/>
+	`,
+	props: {
+		loans: {
+			type: Array,
+			default() {
+				return mockedReceiptData.data.shop.receipt.items.values
+					.filter(item => item.basketItemType === 'loan_reservation')
+					.map(item => item.loan);
+			}
+		}
+	},
+});
