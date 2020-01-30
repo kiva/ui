70514283fa278ce92e6d33318b7fe0f6f08da693
Ryan Ludwig <ryanl@kiva.org>
2020-01-29 16:34:35 -0800

    Add .stories for kv-tooltip

---
+++ .storybook/stories/KvTooltip.stories.js
@@ -0,0 +1,51 @@
+import KvTooltip from '@/components/Kv/KvTooltip';
+import IconQuestionMark from '@/assets/inline-svgs/icons/question-mark.svg';
+
+export default {
+	title: 'Kv/KvTooltip',
+	component: KvTooltip,
+};
+
+export const Default = () => ({
+	components: {
+		KvTooltip,
+	},
+	template: `
+		<div>
+			<button id="my-cool-btn">Hover of Focus Me!</button>
+			<kv-tooltip controller="my-cool-btn">
+				<template slot="title">
+					What is an Experimental Field Partner?
+				</template>
+				If a Field Partner is labeled as Experimental, this means that Kiva has
+				required only a comparatively light level of due diligence and
+				monitoring, in exchange for only allowing this Field Partner access to a
+				small amount of funding through Kiva at any given time.
+			</kv-tooltip>
+		</div>
+	`
+});
+
+export const QuestionMarkIcon = () => ({
+	components: {
+		KvTooltip,
+		IconQuestionMark,
+	},
+	template: `
+		<div>
+			<icon-question-mark
+				id="question"
+				style="fill: #d8d8d8; width: 1rem;"
+			/>
+			<kv-tooltip controller="question">
+				Staying signed in enables personalized browsing and seamless lending. You may be asked again
+				for your password to view account information or make withdrawals, but uncheck this option
+				if you’re using a public device.
+			</kv-tooltip>
+		</div>
+	`
+});
+
+
+
+
