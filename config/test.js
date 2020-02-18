4bfd00f7947c7fcb89fdbd466e20cadd5b93c470
Jay Mallatt <jmallatt@kiva.org>
2020-02-17 21:26:49 -0800

    audit and test envs

---
+++ config/test.js
@@ -0,0 +1,65 @@
+var merge = require('webpack-merge')
+var base = require('./index.js')
+
+module.exports = merge(base, {
+	app: {
+		host: 'www.test.kiva.org',
+		publicPath: 'https://www-test-kiva-org.freetls.fastly.net/ui/',
+		graphqlUri: 'https://marketplace-api.dk1.kiva.org/graphql',
+		enablePerimeterx: true,
+		perimeterxAppId: 'PX5u4Lz98O',
+		enableAnalytics: true,
+		enableGTM: true,
+		googleTagmanagerId: 'GTM-PXFRMT',
+		enableGA: true,
+		gaId: 'UA-11686022-3',
+		gaAlternateId: 'UA-11686022-6',
+		enableSnowplow: true,
+		snowplowUri: 'events.fivetran.com/snowplow/v5qt54ocr2nm',
+		enableFB: true,
+		fbApplicationId: '364044572460',
+		fbPixelId: '108245819986414',
+		fbOgNameSpace: 'test-kiva',
+		enableSentry: true,
+		sentryURI: 'https://7ce141b23c4a4e6091c206d08442f0e9@sentry.io/1201287',
+		algoliaConfig: {
+			enableAA: true,
+			group: 'test',
+			appId: 'testingZ9YK0WNQ85',
+			apiKey: '28aaa1d56e6a1688f13f6b41da0f27a5',
+			defaultIndex: 'test_fundraising_popularity',
+		},
+		auth0: {
+			loginRedirectUrls: {
+				cNTV7eN5sBKgv9nQOxDpAz1pPfJGlBI5: 'https://admin.test.kiva.org/login',
+				e6wSaTBDpKRkV5SV5cWw6zD6eJjd2DEk: 'https://partners.test.kiva.org/login',
+				xOXldYg02WsLnlnn0D5xoPWI2i3aNsFD: 'https://www.test.kiva.org/authenticate',
+				KIzjUBQjKZwMRgYSn6NvMxsUwNppwnLH: 'https://www.test.kiva.org/ui-login',
+				ouGKxT4mE4wQEKqpfsHSE96c9rHXQqZF: 'https://www.test.kiva.org/ui-login',
+			},
+			enable: true,
+			apiAudience: 'https://api.test.kivaws.org/graphql',
+			browserClientID: 'ouGKxT4mE4wQEKqpfsHSE96c9rHXQqZF',
+			serverClientID: 'KIzjUBQjKZwMRgYSn6NvMxsUwNppwnLH',
+			browserCallbackUri: 'https://www.test.kiva.org/process-browser-auth',
+			serverCallbackUri: 'https://www.test.kiva.org/process-ssr-auth',
+			domain: 'login.test.kiva.org',
+		},
+		contentful: {
+			environment: 'development'
+		},
+		intercom: {
+			enable: true,
+		},
+		paypal : {
+			url: 'www.sandbox.paypal.com',
+			environment: 'sandbox'
+		},
+	},
+	server: {
+		graphqlUri: 'https://marketplace-api.dk1.kiva.org/graphql',
+		sessionUri: 'https://www.test.kiva.org/start-ui-session',
+		memcachedEnabled: true,
+		memcachedServers: 'test-memcached-01:11211',
+	}
+})
