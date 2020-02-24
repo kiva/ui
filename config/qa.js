43b3d5300a957ba9f9df5e73e830af41575a90de
Matt Stover <mcs@mcstover.com>
2020-02-24 14:16:08 -0800

    update memcached urls for prod and qa

--- config/qa.js
+++ config/qa.js
@@ -56,6 +56,6 @@ module.exports = merge(base, {
 		graphqlUri: 'https://api.qa.kivaws.org/graphql',
 		sessionUri: 'https://www.qa.kiva.org/start-ui-session',
 		memcachedEnabled: true,
-		memcachedServers: 'qa-memcached-01:11211',
+		memcachedServers: 'qa-memcached-01:11211,qa-memcached-02:11211',
 	}
 })
