a2be0e440cce84dd6732ed2d7c93e7706941470e
Joshua <joshuadstarkey@gmail.com>
2020-03-02 16:10:22 -0800

    cash-1883 additional daysIdle = 0 text scenario

--- src/pages/Autolending/LendTimingDropdown.vue
+++ src/pages/Autolending/LendTimingDropdown.vue
@@ -104,15 +104,14 @@ export default {
 			const daysIdle = differenceInCalendarDays(new Date(), idleStartTime);
 			const daysUntilLend = this.lendAfterDaysIdle - daysIdle > 0 ? this.lendAfterDaysIdle - daysIdle : 0;
 			const daysUntilMay = differenceInCalendarDays(new Date('5/20/2020'), new Date());
-			const lendAfterDaysIdle = this.lendAfterDaysIdle;
-			
+
 			// TEMPORARY - Remove after 5/20/2020
 			// Special launch conditions for Autolenders with unchanged default of 90 days
 			if (this.lendAfterDaysIdle === 90 && daysUntilMay >= 0) {
 				// cash-1883
 				if (daysIdle === 0) {
 					// eslint-disable-next-line max-len
-					return `You're currently active! You'll be eligible for auto-lending if you don't make a loan yourself in the next 90 days.`;
+					return 'You\'re currently active! You\'ll be eligible for auto-lending if you don\'t make a loan yourself in the next 90 days.';
 				}
 				// lender has a qualifying credit change after 2/20/2020 thus resetting their 90 days beyond 5/20/2020
 				if (daysUntilLend > daysUntilMay) {
@@ -132,7 +131,7 @@ export default {
 			// cash-1883
 			if (daysIdle === 0) {
 				// eslint-disable-next-line max-len
-				return `You're currently active! You'll be eligible for auto-lending if you dot make a loan yourself in the next ${lendAfterDaysIdle} days.`;
+				return `You're currently active! You'll be eligible for auto-lending if you don't make a loan yourself in the next ${this.lendAfterDaysIdle} days.`;
 			}
 			// R1: User balance > $25 + the user's , # of days within dropdown - cIdleStartTime is greater than 0
 			if (daysUntilLend > 0) {
