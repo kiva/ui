69f19bb80e210150ca57bbcf555fb288a2eb8c57
Matt Stover <matthews@kiva.org>
2020-03-06 15:13:43 -0800

    Update k8sdev.js

--- config/k8sdev.js
+++ config/k8sdev.js
@@ -4,6 +4,6 @@ var dev  = require('./dev.js')
 module.exports = merge(dev, {
 	app: {
 		// removes 'ui' path segment
-		publicPath: 'http://ui-dev.dk1.kiva.org.global.prod.fastly.net/',
+		publicPath: 'https://ui-dev.dk1.kiva.org.global.prod.fastly.net/',
 	}
 })
