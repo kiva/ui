e77c4969e52e1e44686319403bc21418f9bc6c68
Eddie Ferrer <eddief@kiva.org>
2020-03-10 08:48:49 -0700

    Renamed tests, added a couple more

--- test/e2e/specs/AutolendingMessagingPage.spec.js
+++ test/e2e/specs/AutolendingMessagingPage.spec.js
@@ -1,6 +1,6 @@
 import wwwPageMock from '../fixtures/wwwPageMock';
 
-describe('Autolending Opt Out Spec', () => {
+describe('Autolending Messaging Spec', () => {
 	beforeEach(() => {
 		cy.mock(wwwPageMock());
 		// Hide tracking cookie notice banner
@@ -29,6 +29,36 @@ describe('Autolending Opt Out Spec', () => {
 		it('Opt-out page should display failure message when success param is false', () => {
 			// Visit autolending settings
 			cy.visit('/settings/autolending/opt-out?success=false');
+
+			// Assert that key elements of the page are visible
+			cy.get('[data-test=opt-out-failure]').should('exist');
+			cy.get('[data-test=opt-out-failure]').contains('Oops - this link no longer works');
+			cy.get('[data-test=opt-out-success]').should('not.exist');
+		});
+	});
+
+	describe('Visits autolending disable page', () => {
+		it('Disable page should display failure message with missing success param', () => {
+			// Visit autolending settings
+			cy.visit('/settings/autolending/disable');
+
+			// Assert that key elements of the page are visible
+			cy.get('[data-test=opt-out-failure]').should('exist');
+			cy.get('[data-test=opt-out-failure]').contains('Oops - this link no longer works');
+			cy.get('[data-test=opt-out-success]').should('not.exist');
+		});
+		it('Disable page should display success message when success param is true', () => {
+			// Visit autolending settings
+			cy.visit('/settings/autolending/disable?success=true');
+
+			// Assert that key elements of the page are visible
+			cy.get('[data-test=opt-out-success]').should('exist');
+			cy.get('[data-test=opt-out-success]').contains('You successfully turned off auto-lending');
+			cy.get('[data-test=opt-out-failure]').should('not.exist');
+		});
+		it('Disable page should display failure message when success param is false', () => {
+			// Visit autolending settings
+			cy.visit('/settings/autolending/disable?success=false');
 
 			// Assert that key elements of the page are visible
 			cy.get('[data-test=opt-out-failure]').should('exist');
