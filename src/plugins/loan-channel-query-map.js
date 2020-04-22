3d9e49fb9de862a5eaf24fad1a3b92830239041b
Matt Stover <mcs@mcstover.com>
2020-04-22 12:58:09 -0700

    set fallback for reserved tag

--- src/plugins/loan-channel-query-map.js
+++ src/plugins/loan-channel-query-map.js
@@ -344,8 +344,10 @@ export default {
 				{
 					id: 96,
 					url: 'covid-19',
-					queryParams: 'status=fundRaising&riskRating=0,5&distributionModel=field_partner&tag=41',
-					algoliaParams: 'tags=reserved_disaster_relief_covid&sortBy=popularity'
+					queryParams: 'status=fundRaising&tag=41',
+					// algoliaParams: 'tags=reserved_disaster_relief_covid&sortBy=popularity'
+					// eslint-disable-next-line
+					fallbackUrl: '/lend?status=fundRaising&tag=41&kexpn=lend_filter.lend_filter_versions&kexpv=c'
 				},
 				{
 					id: 65,
