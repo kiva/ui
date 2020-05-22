df05aea9320a0ee7683528bc2cb50a973741e8b8
Matt Stover <mcs@mcstover.com>
2020-05-21 17:50:51 -0700

    Update memcache config

--- config/k8stest.js
+++ config/k8stest.js
@@ -3,12 +3,9 @@ var test  = require('./test.js')
 
 module.exports = merge(test, {
 	app: {
-		// removes 'ui' path segment
-		// publicPath: 'https://ui-test-dk1-kiva-org.freetls.fastly.net/',
 		publicPath: 'https://www-test-kiva-org.freetls.fastly.net/',
 	},
 	server: {
-		// memcachedServers: 'k8sdev-elasticache.bu9ifv.0001.usw1.cache.amazonaws.com:11211',
-		memcachedServers: 'test-memcached-01:11211',
+		memcachedServers: 'kivadev-oregon-memcached.knmtma.0001.usw2.cache.amazonaws.com:11211',
 	}
 })
