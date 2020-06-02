8f1ec756a2f2780e24206fdfa095e0d70b14609f
Eddie Ferrer <eddief@kiva.org>
2020-06-02 14:22:08 -0700

    VUE-308 Subscription Settings Mutations
    * Adds mutations for modifying MG parameters in lightbox and updating status card on subscription settings page
    * Adds mutations for cancelling subscriptions
    * Adds lightbox to prompt for confirmation of cancelling subscription
    
    VUE-308

--- src/pages/Subscriptions/SubscriptionsMonthlyGood.vue
+++ src/pages/Subscriptions/SubscriptionsMonthlyGood.vue
@@ -27,7 +27,7 @@
 						>{{ dayOfMonth | numeral('Oo') }}</a> of each month <a
 							role="button"
 							@click.prevent="showLightbox = true;"
-						>${{ autoDepositAmount }}</a> will be
+						>{{ totalCombinedDeposit | numeral('$0,0.00') }}</a> will be
 						transferred from PayPal <a
 							role="button"
 							@click.prevent="showLightbox = true;"
@@ -254,13 +254,11 @@ export default {
 	data() {
 		return {
 			isSaving: false,
-			autoDepositAmount: 0,
 			category: null,
 			dayOfMonth: new Date().getDate(),
 			donation: 0,
 			mgAmount: 0,
 			isMonthlyGoodSubscriber: false,
-			selectedGroupDescriptor: '',
 			showLightbox: false,
 			isDayInputShown: false,
 		};
@@ -304,16 +302,11 @@ export default {
 		result({ data }) {
 			this.isMonthlyGoodSubscriber = _get(data, 'my.autoDeposit.isSubscriber', false);
 			if (this.isMonthlyGoodSubscriber) {
-				this.autoDepositAmount = parseFloat(_get(data, 'my.autoDeposit.amount', 0));
+				const autoDepositAmount = parseFloat(_get(data, 'my.autoDeposit.amount', 0));
 				this.donation = parseFloat(_get(data, 'my.autoDeposit.donateAmount', 0));
 				this.dayOfMonth = _get(data, 'my.autoDeposit.dayOfMonth');
 				this.category = _get(data, 'my.monthlyGoodCategory');
-
-				// eslint-disable-next-line max-len
-				const selectedCategory = this.lendingCategories.find(category => category.value === this.category) || {};
-				// Sanitize and set initial form values.
-				this.selectedGroupDescriptor = selectedCategory ? selectedCategory.shortName : 'all borrowers';
-				this.mgAmount = this.autoDepositAmount - this.donation;
+				this.mgAmount = autoDepositAmount - this.donation;
 			}
 		},
 	},
@@ -333,6 +326,12 @@ export default {
 		});
 	},
 	computed: {
+		selectedGroupDescriptor() {
+			// eslint-disable-next-line max-len
+			const selectedCategory = this.lendingCategories.find(category => category.value === this.category) || {};
+			// Sanitize and set initial form values.
+			return selectedCategory ? selectedCategory.shortName : 'all borrowers';
+		},
 		totalCombinedDeposit() {
 			return this.donation + this.mgAmount;
 		},
@@ -348,14 +347,33 @@ export default {
 		},
 		saveMonthlyGood() {
 			this.isSaving = true;
-			//! TODO add mutation to update MG settings
-			this.apollo.mutate({
-				// mutation: gql`mutation {
-				// 	autolending @client {
-				// 		saveProfile
-				// 	}
-				// }`
-			}).then(() => {
+			const updateMGCategory = this.apollo.mutate({
+				mutation: gql`mutation($category: MonthlyGoodCategoryEnum!) {
+					my {
+						updateMonthlyGoodCategory(category: $category)
+					}
+				}`,
+				variables: {
+					category: this.category,
+				}
+			});
+			const updateMGSettings = this.apollo.mutate({
+				mutation: gql`mutation($amount: Money, $donateAmount: Money, $dayOfMonth: Int) {
+					my {
+						updateAutoDeposit( autoDeposit: {
+							amount: $amount, donateAmount: $donateAmount, dayOfMonth: $dayOfMonth
+						}) {
+							amount donateAmount dayOfMonth
+						}
+					}
+				}`,
+				variables: {
+					amount: this.totalCombinedDeposit,
+					donateAmount: this.donation,
+					dayOfMonth: this.dayOfMonth,
+				}
+			});
+			Promise.all([updateMGCategory, updateMGSettings]).then(() => {
 				this.$showTipMsg('Settings saved!');
 			}).catch(e => {
 				console.error(e);
