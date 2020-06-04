1fd6a2a471583ec331489b2ef830008ed504108d
Eddie Ferrer <eddief@kiva.org>
2020-06-04 12:43:40 -0700

    VUE-308 Subscriptions Settings for legacy subscribers
    * Added messaging for legacy autodeposit subscribers
    VUE-308

---
+++ src/pages/Subscriptions/SubscriptionsLegacy.vue
@@ -0,0 +1,89 @@
+<template>
+	<div class="row autodeposits-area">
+		<div class="column large-8 settings-card">
+			<div class="icon-wrapper">
+				<kv-icon
+					class="icon"
+					title="Auto Deposits"
+					name="auto-icon-when"
+				/>
+			</div>
+			<div class="title-wrapper">
+				<h3>
+					Auto Deposits
+				</h3>
+			</div>
+			<div class="content-wrapper">
+				<div v-for="(sub, index) in legacySubscriptions" :key="index">
+					<p>
+						<!-- eslint-disable-next-line max-len -->
+						Each month <strong>{{ sub.amount | numeral('$0,0.00') }}</strong> will be transferred from PayPal. This includes a <strong>{{ sub.amount - sub.autoDonate | numeral('$0,0.00') }}</strong> deposit for lending and a <strong>{{ sub.autoDonate | numeral('$0,0.00') }}</strong> optional donation to support Kiva.
+					</p>
+					<p class="legacy_notice">
+						<!-- eslint-disable-next-line max-len -->
+						Because you have an existing auto deposit, to make changes you'll need to <a :href="subscriptionCancelLink(sub)">cancel your current auto deposit via PayPal</a> and then return to Kiva to set up a new one.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import _get from 'lodash/get';
+import gql from 'graphql-tag';
+import KvIcon from '@/components/Kv/KvIcon';
+
+const pageQuery = gql`{
+	my {
+		subscriptions {
+			values {
+				id
+				amount
+				autoDonate
+				subscrId
+				type
+			}
+		}
+	}
+}`;
+
+export default {
+	inject: ['apollo'],
+	components: {
+		KvIcon,
+	},
+	data() {
+		return {
+			legacySubscriptions: []
+		};
+	},
+	apollo: {
+		query: pageQuery,
+		preFetch(config, client) {
+			return client.query({
+				query: pageQuery
+			});
+		},
+		result({ data }) {
+			this.legacySubscriptions = _get(data, 'my.subscriptions.values', []);
+		},
+	},
+	methods: {
+		subscriptionCancelLink(legacySubObject) {
+			return `https://${this.$appConfig.paypal.url}/cgi-bin/webscr`
+				+ `?cmd=_profile-recurring-payments&encrypted_profile_id=${legacySubObject.subscrId}`;
+		}
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+.legacy_notice {
+	background-color: $vivid-yellow;
+	padding: 0.625rem;
+	margin-top: 1.25rem;
+}
+</style>
