4dabae35210d64e7e52d21a656c34b24990ce301
Eddie Ferrer <eddief@kiva.org>
2020-06-12 15:15:23 -0700

    World Refugee Day Homepage takeover
    * Added design assets
    * Extracted footer/header theme objects from homepage component to js file.
    * Added WRD homepage template based on IWD homepage template
    
    LW-1956

--- src/pages/Homepage/Homepage.vue
+++ src/pages/Homepage/Homepage.vue
@@ -11,20 +11,18 @@
 import _get from 'lodash/get';
 import gql from 'graphql-tag';
 import { settingEnabled } from '@/util/settingsUtils';
-import { lightHeader, lightFooter } from '@/util/siteThemes';
+import {
+	lightHeader, lightFooter, iwdHeaderTheme, iwdFooterTheme, wrdHeaderTheme, wrdFooterTheme
+} from '@/util/siteThemes';
 import { documentToHtmlString } from '~/@contentful/rich-text-html-renderer';
 import WwwPage from '@/components/WwwFrame/WwwPage';
 import DefaultHomePage from '@/pages/Homepage/DefaultHomepage';
 import IWDHomePage from '@/pages/Homepage/iwd/IWDHomepage';
+import WRDHomePage from '@/pages/Homepage/wrd/WRDHomepage';
+
 import TopMessageContentful from './TopMessageContentful';
 
 const activePageQuery = gql`query homepageFrame {
-	general {
-		iwd_homepage_active: uiConfigSetting(key: "iwd_homepage_active") {
-			key
-			value
-		}
-	}
 	contentful {
 		entries(contentType: "uiSetting", contentKey: "ui-homepage-top")
 	}
@@ -36,25 +34,13 @@ export default {
 		WwwPage,
 		DefaultHomePage,
 		IWDHomePage,
+		WRDHomePage,
 		TopMessageContentful,
 	},
 	data() {
 		return {
 			isIwdActive: false,
-			iwdHeaderTheme: {
-				themeKey: 'IWD',
-				backgroundColor: '#fff',
-				linkColor: '#060f9f',
-				linkHoverColor: '#a0e2ba',
-				separatorColor: 'transparent'
-			},
-			iwdFooterTheme: {
-				themeKey: 'IWD',
-				backgroundColor: '#fff',
-				textColor: '#484848',
-				linkColor: '#060f9f',
-				separatorColor: '#a0e2ba'
-			},
+			isWrdActive: true,
 			takeOverActive: false,
 			takeOverContent: '',
 		};
@@ -63,16 +49,22 @@ export default {
 		activeHomepage() {
 			if (this.takeOverActive) return TopMessageContentful;
 			if (this.isIwdActive) return IWDHomePage;
+			// ! TODO
+			// ! TODO make isWrdActive dynamic based on a contentful setting
+			// ! TODO
+			if (this.isWrdActive) return WRDHomePage;
 			return DefaultHomePage;
 		},
 		headerTheme() {
 			if (this.takeOverActive) return lightHeader;
-			if (this.isIwdActive) return this.iwdHeaderTheme;
+			if (this.isIwdActive) return iwdHeaderTheme;
+			if (this.isWrdActive) return wrdHeaderTheme;
 			return null;
 		},
 		footerTheme() {
 			if (this.takeOverActive) return lightFooter;
-			if (this.isIwdActive) return this.iwdFooterTheme;
+			if (this.isIwdActive) return iwdFooterTheme;
+			if (this.isWrdActive) return wrdFooterTheme;
 			return null;
 		}
 	},
@@ -80,8 +72,10 @@ export default {
 		query: activePageQuery,
 		preFetch: true,
 		result({ data }) {
-			// determine if IWD is active
-			this.isIwdActive = _get(data, 'general.iwd_homepage_active.value') === 'true' || false;
+			// isIwdActive used to be set based on an experiment query
+			// if something like this is to be implemented in the future it
+			// should be based on contentful, like isWrdActive.
+			this.isIwdActive = false;
 
 			// determine if take-over message content is active
 			const contentSetting = _get(data, 'contentful.entries.items', []).find(item => item.fields.key === 'ui-homepage-top'); // eslint-disable-line max-len
--- src/util/siteThemes.js
+++ src/util/siteThemes.js
@@ -15,3 +15,37 @@ export const lightFooter = {
 	linkColor: '#4faf4e',
 	separatorColor: '#8ccb8c'
 };
+
+export const iwdHeaderTheme = {
+	themeKey: 'IWD',
+	backgroundColor: '#fff',
+	linkColor: '#060f9f',
+	linkHoverColor: '#a0e2ba',
+	separatorColor: 'transparent'
+};
+
+export const iwdFooterTheme = {
+	themeKey: 'IWD',
+	backgroundColor: '#fff',
+	textColor: '#484848',
+	linkColor: '#060f9f',
+	separatorColor: '#a0e2ba'
+};
+
+export const wrdHeaderTheme = {
+	themeKey: 'WRD',
+	backgroundColor: '#fff',
+	logoColor: '#6b62ca',
+	linkColor: '#6b62ca',
+	linkHoverColor: '#f3f3f3',
+	separatorColor: 'transparent',
+	shadow: '0 0 0.5rem 0 rgba(0, 0, 0, 0.15)'
+};
+
+export const wrdFooterTheme = {
+	themeKey: 'WRD',
+	backgroundColor: '#6b62ca',
+	textColor: '#fff',
+	linkColor: '#b9ddb8',
+	separatorColor: '#fff'
+};
