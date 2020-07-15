53512ccab4e6b54abb88aa727cc56dea2651615c
Eddie Ferrer <eddieferrer@user.noreply.github.com>
2020-07-15 08:09:23 -0700

    Hotfix for GROW-127 Redirect to basket experiment

--- src/components/LoanCards/Buttons/ActionButton.vue
+++ src/components/LoanCards/Buttons/ActionButton.vue
@@ -11,7 +11,10 @@
 </template>
 
 <script>
+import gql from 'graphql-tag';
+import store2 from 'store2';
 import _includes from 'lodash/includes';
+import _get from 'lodash/get';
 import addToBasketInsterstitial from '@/plugins/add-to-basket-show-interstitial';
 import Lend25Button from './Lend25Button';
 import LendIncrementButton from './LendIncrementButton';
@@ -20,6 +23,16 @@ import LendAgainButton from './LendAgainButton';
 import LoanFundedText from './LoanFundedText';
 import LoanSelectedText from './LoanSelectedText';
 import LoanExpiredText from './LoanExpiredText';
+
+import experimentVersionFragment from '@/graphql/fragments/experimentVersion.graphql';
+
+const freeCreditQuery = gql`query hasFreeCredits($basketId: String) {
+	shop (basketId: $basketId) {
+		basket {
+			hasFreeCredits
+		}
+	}
+}`;
 
 export default {
 	mixins: [
@@ -68,6 +81,13 @@ export default {
 			default: false,
 		},
 	},
+	data() {
+		return {
+			addToBasketRedirectExperimentShown: false,
+			hasFreeCredits: false,
+			userPrefContinueBrowsing: false,
+		};
+	},
 	computed: {
 		currentButtonState() {
 			if (this.isFunded) {
@@ -88,13 +108,75 @@ export default {
 
 			return this.isSimpleLendButton ? Lend25Button : LendIncrementButton;
 		},
+		canParticipateInExperiment() {
+			// Determines if a user is eligible to be part of the experiment.
+			// - If they added first loan to basket
+			// - If they do not have promo credits (this is handled below in the add to basket event)
+			// - If they have not clicked on "continue browsing" on the checkout page
+			if (this.itemsInBasket.length === 1 && !this.userPrefContinueBrowsing) {
+				return true;
+			}
+			return false;
+		}
+	},
+	mounted() {
+		this.userPrefContinueBrowsing = store2('userPrefContinueBrowsing') === true; // read from localstorage
 	},
 	methods: {
 		handleAddToBasketEvent(payload) {
 			this.$emit('add-to-basket', payload);
+
 			if (payload.success) {
-				this.triggerAddToBasketInterstitial(payload.loanId);
+				if (this.canParticipateInExperiment) {
+					// If user can participate, make query for hasFreeCredits
+					// By gating this, it avoids unnecessarily calling this query.
+					this.apollo.query({
+						query: freeCreditQuery,
+					}).then(data => {
+						this.hasFreeCredits = _get(data, 'shop.basket.hasFreeCredits');
+
+						// Users with free credits go to legacy basket.
+						if (!this.hasFreeCredits) {
+							// User is eligible for experiment, track event
+							this.$kvTrackEvent(
+								'Lending',
+								'EXP-GROW-127-Jul2020',
+								this.addToBasketRedirectExperimentShown === true ? 'b' : 'a'
+							);
+
+							if (this.addToBasketRedirectExperimentShown) {
+								// User is part of shown group
+								this.$router.push({
+									path: '/basket',
+								});
+							} else {
+								// User is part of control group
+								this.triggerAddToBasketInterstitial(payload.loanId);
+							}
+						} else {
+							this.triggerAddToBasketInterstitial(payload.loanId);
+						}
+					}).catch(() => {
+						// In case something happens
+						this.triggerAddToBasketInterstitial(payload.loanId);
+					});
+				} else {
+					this.triggerAddToBasketInterstitial(payload.loanId);
+				}
 			}
+		}
+	},
+	created() {
+		// GROW-127 Add to basket redirect experiment
+		const addToBasketRedirectExperiment = this.apollo.readFragment({
+			id: 'Experiment:add_to_basket_redirect',
+			fragment: experimentVersionFragment,
+		}) || {};
+
+		if (addToBasketRedirectExperiment.version === 'control') {
+			this.addToBasketRedirectExperimentShown = false;
+		} else if (addToBasketRedirectExperiment.version === 'shown') {
+			this.addToBasketRedirectExperimentShown = true;
 		}
 	}
 };
