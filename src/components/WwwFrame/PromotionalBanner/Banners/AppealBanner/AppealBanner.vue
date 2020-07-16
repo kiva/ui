28576dcaf31f4d6654101b2d7a0f56e6404442c6
Joshua <joshuadstarkey@gmail.com>
2020-07-15 19:57:19 -0700

    lw-1972 content served from contentful, style tweaks in progress

--- src/components/WwwFrame/PromotionalBanner/Banners/AppealBanner/AppealBanner.vue
+++ src/components/WwwFrame/PromotionalBanner/Banners/AppealBanner/AppealBanner.vue
@@ -4,9 +4,11 @@
 			<div class="row"
 				@click="toggleAccordion"
 			>
-				<div class="appeal-header small-12 columns sitewide-header strong">
-					<h2>
-						<span>48 hours to donate, years of impact.</span>
+				<div class="columns small-12 medium-9 medium-offset-2 large-9 large-offset-2">
+					<div class="appeal-header sitewide-header">
+						<h2>
+							<span v-html="bannerHeadline" class="strong"></span>
+						</h2>
 						<kv-icon
 							@click="toggleAccordion"
 							:class="{ flipped: open }"
@@ -14,68 +16,63 @@
 							name="medium-chevron"
 							:from-sprite="true"
 						/>
-					</h2>
+						<kv-expandable easing="ease-in-out">
+							<div class="row appeal-content"
+								v-show="open"
+							>
+								<div class="medium-2 large-1 large-offset-1 columms">
+									<div
+										class="hide-for-small show-for-medium thermometer-holder"
+										:title="`${ percentTowardGoal }% raised`"
+									>
+										<appeal-thermometer
+											:percent-toward-goal="percentTowardGoal"
+										/>
+									</div>
+								</div>
+								<div class="small-12 medium-9 large-8 xlarge-7 columns sitewide-body">
+									<div class="appeal-copy">
+										<span v-html="bannerBody"></span>
+										<span class="strong" v-html="bannerMatchingText"></span>
+									</div>
+									<div class="show-for-small hide-for-medium thermometer-holder">
+										<appeal-thermometer
+											:percent-toward-goal="percentTowardGoal"
+										/>
+									</div>
+									<div class="button-wrapper">
+										<kv-button
+											class="mini custom-width"
+											@click.native.prevent.stop="updateDonationTo(20)"
+										>
+											$20
+										</kv-button>
+										<kv-button
+											class="mini custom-width"
+											@click.native.prevent.stop="updateDonationTo(35)"
+										>
+											$35
+										</kv-button>
+										<kv-button
+											class="mini custom-width"
+											@click.native.prevent.stop="updateDonationTo(50)"
+										>
+											$50
+										</kv-button>
+										<a
+											class="other-amount-link"
+											href="/donate/supportus"
+											@blur="validateInput"
+											v-kv-track-event="['promo', 'click', 'EOYBanner', 'other-button']"
+										>Other amount
+										</a>
+									</div>
+								</div>
+							</div>
+						</kv-expandable>
+					</div>
 				</div>
 			</div>
-
-			<kv-expandable easing="ease-in-out">
-				<div class="row appeal-content"
-					v-show="open"
-				>
-					<div class="small-12 medium-1 columms">
-						<div
-							class="hide-for-small show-for-medium thermometer-holder"
-							:title="`${ percentTowardGoal }% raised`"
-						>
-							<appeal-thermometer
-								:percent-toward-goal="percentTowardGoal"
-							/>
-						</div>
-					</div>
-					<div class="small-12 medium-10 columns sitewide-body">
-						<div class="appeal-copy">
-							<p>
-								Each loan posted on Kiva costs us more than $3 to facilitate so when
-								you donate - you help us cover costs, grow our impact, and develop
-								innovative programs. Your donation of any amount makes a difference!
-								<strong v-if="appealMatchEnabled">Your donations are matched while funds last!</strong>
-							</p>
-						</div>
-						<div class="show-for-small hide-for-medium thermometer-holder">
-							<appeal-thermometer
-								:percent-toward-goal="percentTowardGoal"
-							/>
-						</div>
-						<div class="button-wrapper">
-							<kv-button
-								class="smallest custom-width"
-								@click.native.prevent.stop="updateDonationTo(20)"
-							>
-								$20
-							</kv-button>
-							<kv-button
-								class="smallest custom-width"
-								@click.native.prevent.stop="updateDonationTo(35)"
-							>
-								$35
-							</kv-button>
-							<kv-button
-								class="smallest custom-width"
-								@click.native.prevent.stop="updateDonationTo(50)"
-							>
-								$50
-							</kv-button>
-							<a
-								class="other-amount-link"
-								href="/donate/supportus"
-								@blur="validateInput"
-								v-kv-track-event="['promo', 'click', 'EOYBanner', 'other-button']"
-							>Other amount
-							</a>
-						</div>
-					</div>
-				</div>
-			</kv-expandable>
 		</div>
 	</div>
 </template>
@@ -104,6 +101,10 @@ export default {
 		appealMatchEnabled: {
 			type: Boolean,
 			default: false,
+		},
+		appealBannerContent: {
+			type: Object,
+			default: () => {},
 		}
 	},
 	data() {
@@ -122,6 +123,33 @@ export default {
 			this.amountRaised = _get(data, 'general.kivaStats.latestDonationCampaign.amount_raised');
 			// eslint-disable-next-line max-len
 			this.targetAmount = _get(data, 'general.kivaStats.latestDonationCampaign.target_amount');
+		},
+	},
+	computed: {
+		isMatchedEnabled(props) {
+			console.log(props.appealMatchEnabled);
+			return true;
+		},
+		bannerHeadline(props) {
+			const appealHeadline = props.appealBannerContent.richText.content[0].content[0].value;
+			return appealHeadline;
+		},
+		bannerBody(props) {
+			// eslint-disable-next-line max-len
+			const appealBody = props.appealBannerContent.additionalContent[0].fields.richText.content[0].content[0].value;
+			return appealBody;
+		},
+		bannerMatchingText(props) {
+			let matchingText = props.appealBannerContent.dataObject.matchingTextReplacement;
+			// console.log('testing', props.appealBannerContent.dataObject.matchingText);
+			// console.log('testing', props.appealBannerContent.dataObject.matchingTextReplacement);
+
+			// NOT YET WORKING
+			if (props.appealMatchEnabled === true) {
+				// eslint-disable-next-line max-len
+				matchingText = props.appealBannerContent.dataObject.matchingText;
+			}
+			return matchingText;
 		},
 	},
 	mounted() {
@@ -185,7 +213,7 @@ export default {
 		validateInput() {
 			// format the value taken from the donation input
 			this.donationAmount = numeral(this.donationAmount).format('$0,0.00');
-		}
+		},
 	},
 };
 </script>
@@ -220,7 +248,10 @@ export default {
 	}
 
 	.appeal-copy {
-		margin-bottom: 1.25rem;
+		overflow: hidden;
+		line-height: 1.325rem;
+		margin-bottom: 1.5rem;
+		font-weight: 300;
 	}
 
 	h2 {
@@ -243,6 +274,14 @@ export default {
 			text-align: left;
 		}
 
+		.mini {
+			margin-bottom: 0;
+			font-size: 0.875rem;
+			height: 2rem;
+			line-height: 0.2rem;
+			padding: 1.125rem;
+		}
+
 		.custom-width {
 			margin-right: rem-calc(15);
 			margin-bottom: 0;
@@ -260,28 +299,28 @@ export default {
 	}
 
 	.other-amount-link {
-		margin: auto;
+		cursor: pointer;
+		font-size: 0.925rem;
+		width: 100%;
+		text-align: center;
 		line-height: 2.5;
-		display: block;
-		font-size: 1.25rem;
 		font-weight: 400;
 
-		@include breakpoint(medium) {
-			display: inline-block;
-			top: rem-calc(4);
-			position: relative;
-		}
+		// @include breakpoint(medium) {
+		// 	display: inline-block;
+		// 	top: rem-calc(4);
+		// 	position: relative;
+		// }
 	}
 
 	.toggle-arrow {
 		transition: transform 300ms ease;
 		display: block;
-		height: rem-calc(40);
-		width: rem-calc(40);
 		position: absolute;
 		top: 0.25rem;
-		right: 0.6rem;
-		margin: 0;
+		right: 15%;
+		height: 1.5625rem;
+		width: 1.5625rem;
 	}
 
 	.flipped {
