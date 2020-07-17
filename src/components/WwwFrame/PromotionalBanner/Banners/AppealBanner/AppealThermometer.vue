f47119414de4c8c5c92cd9f017910c44772c1194
Joshua <joshuadstarkey@gmail.com>
2020-07-16 21:01:16 -0700

    lw-1972 banner refinements

--- src/components/WwwFrame/PromotionalBanner/Banners/AppealBanner/AppealThermometer.vue
+++ src/components/WwwFrame/PromotionalBanner/Banners/AppealBanner/AppealThermometer.vue
@@ -1,71 +1,157 @@
 <template functional>
-	<div class="appeal-meter progress">
-		<span
-			class="meter"
-			:style="`width: ${ props.percentTowardGoal }%;`"
-		>
-		</span>
-		<span class="thermometer-bulb">
-			<span class="inner-bulb"></span>
-		</span>
-	</div>
+	<span class="thermometer-wrap">
+		<!-- grey meter background -->
+		<div class="thermometer meter-bg">
+			<span class="inner-meter"></span>
+		</div>
+		<span class="thermometer-bulb meter-bg"></span>
+		<!-- white meter middle-ground -->
+		<div class="thermometer meter-center">
+			<span class="inner-meter"></span>
+		</div>
+		<span class="thermometer-bulb meter-center"></span>
+		<!-- meter foreground -->
+		<div class="thermometer meter-fg">
+			<span
+				class="inner-meter"
+				:style="`width: ${ props.percentTowardGoal }%;`"
+			></span>
+		</div>
+		<span class="thermometer-bulb"></span>
+	</span>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import 'settings';
 
-.appeal-meter {
-	border: 3px solid black;
-	padding: rem-calc(3);
-	height: rem-calc(20);
-	width: 90%;
-	border-radius: rem-calc(8);
-	background-color: $platinum;
-	margin: rem-calc(30) rem-calc(20);
+.thermometer-wrap {
+	padding: 1rem;
 
 	@include breakpoint(medium) {
-		transform: rotate(270deg);
-		width: rem-calc(110);
-		margin: 0 auto;
-		top: rem-calc(55);
-		position: relative;
+		padding: 0;
+		position: absolute;
+		right: rem-calc(-85);
+		top: rem-calc(-51);
+		transition: top 500ms ease-in-out, opacity 500ms ease-in-out;
 	}
 
-	.meter {
-		border-radius: rem-calc(8);
-		background-color: $kiva-accent-red;
-		display: block;
-		height: 100%;
-		max-width: 100%;
-		width: 0;
-		transition: width 3500ms ease-in-out;
+	.thermometer {
+		position: relative;
+		border: none;
+		padding: 0;
+		margin-top: rem-calc(-47);
+		height: rem-calc(6);
+		margin-right: rem-calc(10);
+		margin-bottom: 0;
+		z-index: 10;
+
+		@include breakpoint(medium) {
+			transform: rotate(270deg);
+			position: relative;
+			width: rem-calc(150);
+			top: rem-calc(88);
+			left: rem-calc(-48);
+			max-width: rem-calc(120);
+		}
+
+		.inner-meter {
+			background-color: #D94830;
+			display: block;
+			height: 100%;
+			float: left;
+			border-radius: rem-calc(3);
+			transition: width 2500ms ease-in-out;
+			width: 0;
+			max-width: 100%;
+		}
+
+		&.meter-bg {
+			margin-top: rem-calc(-20);
+			left: 0;
+			height: rem-calc(22);
+			border-radius: rem-calc(22);
+			margin-right: 0;
+			z-index: 2;
+
+			@include breakpoint(medium) {
+				left: rem-calc(-48);
+				top: rem-calc(80);
+			}
+
+			.inner-meter {
+				background: $kiva-text-dark;
+				width: 100%;
+				float: none;
+				border-radius: rem-calc(22);
+			}
+		}
+
+		&.meter-center {
+			margin-top: rem-calc(-63);
+			margin-left: rem-calc(-5);
+			margin-right: rem-calc(4);
+			height: rem-calc(14);
+			z-index: 6;
+
+			@include breakpoint(medium) {
+				top: rem-calc(85);
+				left: rem-calc(-43);
+			}
+
+			.inner-meter {
+				width: 100%;
+				background: $kiva-bg-lightgray;
+				float: none;
+				border-radius: rem-calc(7);
+			}
+		}
 	}
 
 	.thermometer-bulb {
-		background-color: white;
+		background-color: #D94830;
 		position: relative;
 		display: inline-block;
 		border-radius: rem-calc(20);
-		height: rem-calc(30);
-		width: rem-calc(30);
+		height: rem-calc(20);
+		width: rem-calc(20);
 		left: rem-calc(-10);
-		top: -2.14rem;
-		// new
-		border: 3px solid black;
-		padding: rem-calc(3);
+		top: rem-calc(-13);
+		z-index: 9;
 
 		@include breakpoint(medium) {
-			left: rem-calc(-24);
-			top: -1.25rem;
+			left: rem-calc(2);
+			top: rem-calc(131);
 		}
 
-		.inner-bulb {
-			background-color: $kiva-accent-red;
-			border-radius: rem-calc(20);
-			position: fixed;
-			height: rem-calc(18);
-			width: rem-calc(18);
+		&.meter-bg {
+			background: #484848;
+			width: rem-calc(36);
+			height: rem-calc(36);
+			border-radius: rem-calc(36);
+			left: rem-calc(-18);
+			top: rem-calc(-29);
+			z-index: 1;
+
+			@include breakpoint(medium) {
+				left: rem-calc(-6);
+				top: rem-calc(115);
+			}
+		}
+
+		&.meter-center {
+			background: #FAFAFA;
+			width: rem-calc(28);
+			height: rem-calc(28);
+			top: rem-calc(-21);
+			left: rem-calc(-14);
+			z-index: 5;
+
+			@include breakpoint(medium) {
+				left: rem-calc(-2);
+				top: rem-calc(123);
+			}
 		}
 	}
 }
+
 </style>
