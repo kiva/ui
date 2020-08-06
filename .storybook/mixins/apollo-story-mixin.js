d10cccebacbd8bfff8277c9cd997a75ccfd3a8dc
Ryan Ludwig <ryanl@kiva.org>
2020-08-06 12:07:19 -0700

    Add federation to storybook apollo mixin

--- .storybook/mixins/apollo-story-mixin.js
+++ .storybook/mixins/apollo-story-mixin.js
@@ -1,13 +1,29 @@
 export default {
 	provide: {
+		federation: {
+			mutate() {
+				return Promise.resolve({});
+			},
+			watchQuery() {
+				return {
+					subscribe() {}
+				}
+			},
+			query() {
+				return Promise.resolve({});
+			},
+			readFragment() {
+				return {}
+			}
+		},
 		apollo: {
 			mutate() {
 				return Promise.resolve({});
 			},
 			watchQuery() {
 				return {
-			subscribe() {}
-			}
+					subscribe() {}
+				}
 			},
 			query() {
 				return Promise.resolve({});
