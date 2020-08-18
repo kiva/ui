477ae6f70234c4d2321177e1d349483881b0ee0e
Ryan Ludwig <ryanl@kiva.org>
2020-08-18 12:11:38 -0700

    Upgrade storybook to 6.0. Remove consolidated addons.

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,12 +3,10 @@ module.exports = {
 		'./stories/**/*.stories.(js|mdx)'
 	],
 	addons: [
-		'@storybook/addon-actions',
+		'@storybook/addon-essentials',
 		'@storybook/addon-knobs',
 		'@storybook/addon-links',
-		'@storybook/addon-docs',
 		'@storybook/addon-a11y',
-		'@storybook/addon-viewport',
 		'@storybook/addon-storysource'
 	]
 }
