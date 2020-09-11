5e23e4b26a18cac0f8ceeed99df642705437d3f3
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2020-09-11 10:21:28 -0700

    Create loading placeholder components

---
+++ src/components/Kv/KvLoadingParagraph.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="loading-paragraph">
+		<kv-loading-placeholder v-for="n in numLines" :key="n" />
+	</div>
+</template>
+
+<script>
+import KvLoadingPlaceholder from '@/components/Kv/KvLoadingPlaceholder';
+import getCacheKey from '@/util/getCacheKey';
+
+export default {
+	name: 'KvLoadingParagraph',
+	props: {
+		numLines: {
+			type: Number,
+			default: 4,
+		},
+	},
+	serverCacheKey: ({ numLines }) => getCacheKey(`KvLoadingParagraph-${numLines}`),
+	components: {
+		KvLoadingPlaceholder,
+	},
+};
+</script>
+
+<style lang="scss">
+@import 'settings';
+
+.loading-paragraph {
+	.loading-placeholder {
+		height: 1rem;
+		margin-bottom: 0.5rem;
+
+		&:last-of-type {
+			width: 70%;
+		}
+	}
+}
+</style>
