25cded8bb745dea23749ab3cb49ad8a855ce5b1c
Joshua <joshuadstarkey@gmail.com>
2020-09-17 17:07:56 -0700

    grow-251 assets, section content in place, formatting next

--- src/components/15Years/15YearsHowKivaWorks.vue
+++ src/components/15Years/15YearsHowKivaWorks.vue
@@ -1,11 +1,77 @@
 <template>
-	<div class="works section">
+	<div class="works section text-center">
 		<div class="row">
-			<div class="columns">
-				<h2>How Kiva Works</h2>
-				goes here
+			<div class="columns small-12">
+				<h2 class="impact-text">
+					How Kiva Works
+				</h2>
+				<p>
+					By lending as little as $25 on Kiva, you support<br>
+					the causes you care about and make real person impact.
+				</p>
 			</div>
 		</div>
+
+		<div class="row text-center">
+			<div class="small-12 large-4 columns">
+				<img
+					src="@/assets/images/15-years/how-kiva-works-choose.svg"
+					loading="lazy"
+				>
+				<h4 class="no-wrap">
+					Choose a borrower
+				</h4>
+				<div class="description-text">
+					<p>Browse by category and find<br>an entrepeneur to support.</p>
+				</div>
+			</div>
+			<div class="small-12 large-4 columns">
+				<img
+					src="@/assets/images/15-years/how-kiva-works-make.svg"
+					loading="lazy"
+				>
+				<h4>Make a loan</h4>
+				<div class="description-text">
+					<p>Help fund a loan with as little as $25.</p>
+				</div>
+			</div>
+			<div class="small-12 large-4 columns">
+				<img
+					src="@/assets/images/15-years/how-kiva-works-repaid.svg"
+					loading="lazy"
+				>
+				<h4>Get repaid</h4>
+				<div class="description-text">
+					<p>Kiva borrowers have a 96% repayment<br>rate historically.</p>
+				</div>
+			</div>
+			<div class="small-12 columns">
+				<img
+					src="@/assets/images/15-years/how-kiva-works-repeat.svg"
+					loading="lazy"
+				>
+				<h4>Repeat</h4>
+				<div class="description-text">
+					<p>Relend your money or withdraw your<br>funds.</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="columns">
+				<h2>
+					Join over 1.9 million fellow <br>changemakers
+				</h2>
+				<img
+					src="@/assets/images/15-years/change-maker-dots.svg"
+					loading="lazy"
+				>
+			</div>
+		</div>
+		<img
+			src="@/assets/images/15-years/footer-background.svg"
+			loading="lazy"
+		>
 	</div>
 </template>
 
@@ -21,5 +87,9 @@ export default {
 
 .works {
 	background: #fff;
+
+	.no-wrap {
+		white-space: nowrap;
+	}
 }
 </style>
