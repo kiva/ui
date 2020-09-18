29037c37d7eb1f2d21df7032d5826e77ec0d73ad
Joshua <joshuadstarkey@gmail.com>
2020-09-18 12:21:22 -0700

    grow-251 image size adjustments, css more structured

--- src/components/15Years/15YearsHowKivaWorks.vue
+++ src/components/15Years/15YearsHowKivaWorks.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="works section text-center">
 		<div class="row">
-			<div class="columns small-12">
-				<h2 class="impact-text">
+			<div class="works__heading-group small-12 columns">
+				<h2 class="works__headline impact-text ">
 					How Kiva Works
 				</h2>
 				<p>
@@ -13,45 +13,49 @@
 		</div>
 
 		<div class="row text-center">
-			<div class="small-12 large-4 columns">
+			<div class="works__step small-12 large-4 columns">
 				<img
+					class="works__step--img-choose"
 					src="@/assets/images/15-years/how-kiva-works-choose.svg"
 					loading="lazy"
 				>
-				<h4 class="no-wrap">
+				<h4 class="works__step--text">
 					Choose a borrower
 				</h4>
-				<div class="description-text">
+				<div class="works__step--text">
 					<p>Browse by category and find<br>an entrepeneur to support.</p>
 				</div>
 			</div>
-			<div class="small-12 large-4 columns">
+			<div class="works__step small-12 large-4 columns">
 				<img
+					class="works__step--img-make"
 					src="@/assets/images/15-years/how-kiva-works-make.svg"
 					loading="lazy"
 				>
 				<h4>Make a loan</h4>
-				<div class="description-text">
+				<div class="works__step--text">
 					<p>Help fund a loan with as little as $25.</p>
 				</div>
 			</div>
-			<div class="small-12 large-4 columns">
+			<div class="works__step small-12 large-4 columns">
 				<img
+					class="works__step--img-repaid"
 					src="@/assets/images/15-years/how-kiva-works-repaid.svg"
 					loading="lazy"
 				>
 				<h4>Get repaid</h4>
-				<div class="description-text">
+				<div class="works__step--text">
 					<p>Kiva borrowers have a 96% repayment<br>rate historically.</p>
 				</div>
 			</div>
-			<div class="small-12 columns">
+			<div class="works__step small-12 columns">
 				<img
+					class="works__step--img-repeat"
 					src="@/assets/images/15-years/how-kiva-works-repeat.svg"
 					loading="lazy"
 				>
 				<h4>Repeat</h4>
-				<div class="description-text">
+				<div class="works__step--text">
 					<p>Relend your money or withdraw your<br>funds.</p>
 				</div>
 			</div>
@@ -88,8 +92,34 @@ export default {
 .works {
 	background: #fff;
 
-	.no-wrap {
-		white-space: nowrap;
+	&__heading-group {
+		margin: 6.25rem 0;
+	}
+
+	&__headline {
+		margin-bottom: 2.5rem;
+	}
+
+	&__step {
+		&--img-choose {
+			width: 128px;
+		}
+
+		&--img-make {
+			width: 113px;
+		}
+
+		&--img-repaid {
+			width: 230px;
+		}
+
+		&--img-repeat {
+			width: 140px;
+		}
+
+		&--img--text {
+			white-space: nowrap;
+		}
 	}
 }
 </style>
