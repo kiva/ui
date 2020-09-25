07aa426d79613982b8595ea512f85079ada37fa4
Ryan Ludwig <ryanl@kiva.org>
2020-09-25 13:22:19 -0700

    Update profile data, add profile images.

--- src/components/15Years/15YearsIndividualsData.js
+++ src/components/15Years/15YearsIndividualsData.js
@@ -1,50 +1,247 @@
-// COPY LAST UPDATED BY DEV ON 2020/09/21
+// COPY LAST UPDATED BY DEV ON 2020/09/24
 
 /* eslint-disable max-len */
 export default [
 	{
 		name: 'Zaaqi',
 		title: 'Burger Magician',
-		img: '15-years-zaaqi',
+		img: 'need-content',
 		body: `<p>
-			Zaaqi Johnson had long dreamed of being his own boss. As a young entrepreneur with a passion for creating delicious, nutritious, high-quality food, imaging himself as an entrepreneur excited him. After spending years working for others in the food service industry, Zaaqi decided it was time to make his dream a reality.
-			</p><p>
-			Zaaqi recognized his talent for cooking many years ago. He worked in various food service positions before attending Monroe Community College’s Culinary Arts program in Rochester, New York. He was working at a local resort hotel where he was responsible for Sunday brunch, during which the restaurant typically served 150-200 people. Other than the occasional acknowledgement from friendly patrons, he had very little to show for the labor he was putting in while working for someone else.
-			</p><p>
-			This experience convinced Zaaqi that he possessed the skills and the stamina required to run his own restaurant. To start, he sold dinners to family and friends out of his mom’s kitchen, advertising through social media. He then used the money from those dinner sales to rent and renovate a restaurant space. His family and friends pitched in, too, both financially and physically. Together, they renovated and prepared the space soon to be Zoc’s.
-			</p><p>
-			In 2016, Zaaqi opened Zoc’s Gourmet Burgers and Shakes – a made-to-order casual dining restaurant – on the West Side of Rochester. His goal is to offer healthy food choices to his community, food that is prepared with the care and quality that urban residents have traditionally felt the need to travel to the suburbs to enjoy. In turn, he is committed to giving back to the community by using locally-sourced produce, meats, and baked goods from the neighborhood farmer’s market and family-owned bakery. Customers who choose to dine at Zoc's Gourmet can be sure they are eating food of the highest quality.
-			</p><p>
-			Zaaqi used his Kiva loan on marketing materials to help drive traffic to and grow his new restaurant. He designed a logo and branding, and printed fliers, mailers, and to-go food packaging. Zaaqi and his team were also able to use their Kiva funds to increase Zoc’s Gourmet’s social media presence through their primary website, zocsgourmet.com, as well as through GrubHub and Google+. Finally, Zaaqi used the remainder of his Kiva loan to improve the appearance and visibility of the Zoc’s Gourmet storefront, namely upgrading the business sign to enhance visibility from the street. These improvements were instrumental in helping Zoc’s Gourmet to grow as a business and become a staple in the West Rochester community.
-			</p><p>
-			Zaaqi has never been afraid of the hard work running his own restaurant requires. Rather, he is encouraged by the idea that his hard work contributes directly to the success of his business. And a success it is. The community has been incredibly supportive and reviews of Zoc’s are exceedingly positive. Congratulations, Zaaqi!
+		Need content
 		</p>
 		`,
 		videoId: null,
-		cta: 'Lend to an American entrepreneur',
-		link: '/lend-by-category/kiva-u-s'
+		cta1: 'Lend to an American entrepreneur',
+		cta2: 'Lend monthly to U.S. small businesses',
+		link1: '/lend-by-category/kiva-u-s',
+		link2: '/monthlygood?category=us_borrowers',
 	},
 	{
 		name: 'Johnny',
 		title: 'Master of the Hammer',
-		img: '15-years-johnny',
+		img: 'need-content',
 		body: null,
-		videoId: 389663203,
-		cta: 'Lend to a South American borrower',
-		link: '/lend/filter?countries=South%20America%20>%20Bolivia~South%20America%20>%20Brazil~South%20America%20>%20Colombia~South%20America%20>%20Ecuador~South%20America%20>%20Paraguay~South%20America%20>%20Peru&sortBy=popularity'
+		videoId: 174878843,
+		cta1: 'Lend to a South American borrower',
+		cta2: 'Lend monthly to U.S. small businesses',
+		link1: 'S. Am filtered loans', // TODO: find url.
+		link2: 'https://store.kiva.org',
 	},
 	{
 		name: 'Nilda',
 		title: 'Architect on Stilts',
-		img: '15-years-nilda',
+		img: 'need-content',
 		body: `<p>
-			Meet Nilda! As a married mother of six children, she works hard to support her family. Nilda lives in the Philippines, where she owns a nipa (a stilt house indigenous to the Philippines) roofing business and earns additional income from raising pigs.
-			</p></p>
-			Nilda borrowed 30,000 Philippine Pesos through long-time Kiva Field Partner Negros Women For Tomorrow Foundation (NWTF), her 12th loan from NWTF, to buy nipa leaves, bamboo sticks, and other materials to continue to grow her business. She hopes that her hard work, along with a helping hand from NWTF and Kiva, will help her achieve her dream to save enough money to pay for her children’s college tuition.
+		Meet Nilda! As a married mother of six children, she works hard to support her family. Nilda lives in the Philippines, where she owns a nipa – a stilt house indigenous to the Philippines – roofing business and earns additional income from raising pigs.
+		</p><p>
+		Nilda borrowed her 12th loan through long-time Kiva Field Partner NWTF to buy nipa leaves, bamboo sticks, and other materials to continue to grow her business. She hopes that her hard work, along with a helping hand from NWTF and Kiva, will help her achieve her dream to save enough money to pay for her children’s college tuition.
 		</p>
 		`,
 		videoId: null,
-		cta: 'Lend to a borrower like Nilda',
-		link: '/filter?countries=Asia%20>%20Cambodia~Asia%20>%20Indonesia~Asia%20>%20Kyrgyzstan~Asia%20>%20Philippines~Asia%20>%20Tajikistan~Asia%20>%20Vietnam&sortBy=popularity'
+		cta1: 'Lend to a borrower like Nilda',
+		cta2: 'Lend to a borrower like Nilda',
+		link1: 'Asia filtered loans', // TODO: find url.
+		link2: 'Asia filtered loans', // TODO: find url.
 	},
+	{
+		name: 'Lihn Do',
+		title: 'Community Campaigner',
+		img: 'lihn-do',
+		body: `<p>
+		Lihn Do's journey to becoming a Kiva lender began at a young age. "My parents came to Australia as refugees. Even though we didn't have a lot ourselves growing up, they always instilled in my brother and me that we could be doing something to help other people,” she said. Her family's background and values combined with her interest in the "idea that a dollar can go further than just what a dollar represents” sparked her interest in Kiva's model of crowdfunded microlending.
+		</p><p>
+		"I really love being a lender on Kiva because it makes me embrace and practice the values that I have.”
+		</p><p>
+		Lihn is based in Melbourne, Australia. However, before the COVID-19 pandemic, she traveled often for work. Her travels heightened her awareness to "the fact that when you're a tourist or in a specific location for work, you don't get to see everything that country has to offer. That includes the problems that exist there. So I tend to try to support [loans] in locations that I'm also working in or a place that I've read about, just to learn a bit more about the world.”
+		</p><p>
+		Lihn prefers to lend to female entrepreneurs and small business owners. She recognizes that women typically have more difficulty accessing, or entirely lack access to, traditional funding sources, like banks. She wants to help Kiva bridge this gap, even if in a small way.
+		</p><p>
+		Lihn's favorite aspect of lending on Kiva.org? The short-term, tangible impact that lending as little as $25 can have on people's lives. "There's such a thrill about being able to browse a website, and rather than doing online shopping or something for yourself, knowing that every time you click to add a loan to your basket, you're actually helping someone else in the world. It feels like such a privilege to be able to do that.”
+		</p><p>
+		She also enjoys sharing this passion for lending through Kiva with her community and often recruits new lenders. "One of the most impactful things that I've really enjoyed being able to do is telling other people about microfinance and expanding the number of people in my own community that have since joined Kiva.”
+		</p><p>
+		It's been particularly interesting for Lihn to have these conversations about Kiva and to see people realize that they, too, can have life-changing impact on people's lives without it requiring a large sum of money. "All of us can participate.”
+		</p><p>
+		In the future, Lihn is eager to see just how impactful Kiva can be, "Kiva turning 15 is pretty amazing. It proves all of the naysayers wrong that microfinance isn't something that works, isn't something that makes an impact, or isn't something that people would be interested in. I'm really excited to see how Kiva can continue to extend its impact.”
+		</p>
+		`,
+		videoId: null,
+		cta1: 'Lend like Linh',
+		cta2: 'Lend monthly to women',
+		link1: '/lend-by-category/women',
+		link2: '/monthlygood?category=women',
+	},
+	{
+		name: 'Danielle',
+		title: 'Father-Daughter Force',
+		img: 'danielle',
+		body: null,
+		videoId: 174878843,
+		cta1: 'Lend like Danielle',
+		cta2: 'Lend monthly to women',
+		link1: '/lend-by-category/women',
+		link2: '/monthlygood?category=women',
+	},
+	{
+		name: 'Howard',
+		title: 'Voice of Compassion',
+		img: 'need-content',
+		body: `
+		<p>
+		Howard Zugman was reading the newspaper on New Year’s Day in 2008 when he came upon an article sharing better ways to make a New Year’s resolution. For those tired of joining a gym only to stop going a couple of months later, the article instead suggested, among other options, lending money to the world’s most financially excluded individuals on Kiva.org. But Howard was skeptical.
+		</p><p>
+		Initially, Howard felt that the Kiva model was too good to be true. Why would someone lend money to a stranger, possibly one who lives thousands of miles away, without the guarantee of repayment? On the other hand, he felt that if the model did work, then what an unmatched opportunity to create impact.
+		</p><p>
+		To resolve his internal conflict and assess the credibility of lending through Kiva, Howard created a spreadsheet recording all loans fundraising on Kiva. By the time he was finished, he counted 4,000 loans of varying amounts from around the globe. This led him to conclude that Kiva was trustworthy, that “no one could be dumb enough to put all of this effort into a scam.” He decided to start lending.
+		</p><p>
+		Howard set his sights on two goals: he would be Kiva’s most prolific lender; and he would lend $1M by January 1, 2020. He quickly realized these were expensive aspirations. Where would he get the money?
+		</p><p>
+		Howard has been singing, for a small fee, at retirement homes for 32 years. He had been unsure of what to do with these modest earnings until he realized he could use them to help other people by lending on Kiva. With his Kiva superlender aspirations feeling more within reach, Howard began his lending journey.
+		</p><p>
+		"Kiva has become a very important part of my life. Every morning with my coffee I make my loans. If nothing else goes right that day, I know something good has happened."
+		</p><p>
+		As a Bay Area native, and with Kiva headquarters located in San Francisco, Howard considers himself fortunate to have had the opportunity to meet many Kivans over the years. After initially focusing his lending internationally, he realized that lending to U.S. borrowers located in the Bay Area meant that he could meet them, too. To date, Howard has met at least 70 of the U.S. borrowers he has supported. This sense of community among Kiva lenders, borrowers, and staff serves as a source of great joy and inspiration to Howard, “It’s been wonderful to be a part of Kiva. I’ve gotten out of it much more than I’ve put in.”
+		</p><p>
+		Although Howard fell just short of his initial lending aspirations, he has created life-changing impact for thousands of Kiva borrowers over the past 12 years. Kiva’s community of generous lenders, like Howard, is fundamental to building the financially inclusive world we envision.
+		</p><p>
+		"[Kiva] allows me to create the world that I want it to be, in a small way. That’s all because I happened to pick up the paper that day with my coffee." We’re so happy you did, Howard.
+		</p>
+		`,
+		videoId: null,
+		cta1: 'Lend like Howard',
+		cta2: 'Lend monthly',
+		link1: '/lend-by-category',
+		link2: '/monthlygood',
+	},
+	{
+		name: 'Julia',
+		title: 'High School Hero',
+		img: 'need-content',
+		body: null,
+		videoId: 174878843,
+		cta1: 'Lend like Julia',
+		cta2: 'Lend monthly to students',
+		link1: '/lend-by-category/education',
+		link2: '/monthlygood?category=education',
+	},
+	{
+		name: 'Drew + Allie',
+		title: 'Lending Power Couple',
+		img: 'drew-allie',
+		body: `<p>
+			Lorem ipsum cillum eiusmod culpa minim do adipisicing labore nisi in veniam excepteur amet eu. Aliquip veniam ut ea do nulla commodo. Eu in qui aute id ex culpa id eiusmod nostrud sunt. Proident laboris amet consequat aliquip nostrud do reprehenderit. Labore occaecat occaecat sint consectetur sunt. Veniam sint do reprehenderit proident ut veniam minim amet sint sit.
+		</p>
+		`,
+		videoId: null,
+		cta1: 'Lend like Drew and Alli',
+		cta2: 'Lend monthly',
+		link1: '/lend-by-category',
+		link2: '/monthlygood',
+	},
+	{
+		name: 'Apollo Ag',
+		title: 'Small Farm Funder',
+		img: 'need-content',
+		body: null,
+		videoId: 174878843,
+		cta1: 'Support groups like Apollo Agriculture',
+		cta2: 'Support groups like Apollo Agriculture',
+		link1: 'Africa filtered loans', // TODO: find url.
+		link2: 'Africa filtered loans', // TODO: find url.
+	},
+	{
+		name: 'WWBIC',
+		title: 'Heartland Supporters',
+		img: 'need-content',
+		body: null,
+		videoId: 174878843,
+		cta1: 'Lend to U.S. small businesses',
+		cta2: 'Lend monthly to American entrepreneurs',
+		link1: '/lend-by-category/kiva-u-s',
+		link2: '/monthlygood?category=us_borrowers',
+	},
+	{
+		name: 'David Kitusa',
+		title: 'The Local Changemaker',
+		img: 'need-content',
+		body: `<p>
+			Lorem ipsum cillum eiusmod culpa minim do adipisicing labore nisi in veniam excepteur amet eu. Aliquip veniam ut ea do nulla commodo. Eu in qui aute id ex culpa id eiusmod nostrud sunt. Proident laboris amet consequat aliquip nostrud do reprehenderit. Labore occaecat occaecat sint consectetur sunt. Veniam sint do reprehenderit proident ut veniam minim amet sint sit.
+		</p>
+		`,
+		videoId: null,
+		cta1: 'Lend to African borrowers',
+		cta2: 'Lend to African borrowers',
+		link1: 'Africa filtered loans', // TODO: find url.
+		link2: 'Africa filtered loans', // TODO: find url.
+	},
+	{
+		name: 'Rachel Chang',
+		title: 'Protocol Practitioner',
+		img: 'rachel',
+		body: `<p>
+			Lorem ipsum cillum eiusmod culpa minim do adipisicing labore nisi in veniam excepteur amet eu. Aliquip veniam ut ea do nulla commodo. Eu in qui aute id ex culpa id eiusmod nostrud sunt. Proident laboris amet consequat aliquip nostrud do reprehenderit. Labore occaecat occaecat sint consectetur sunt. Veniam sint do reprehenderit proident ut veniam minim amet sint sit.
+		</p>
+		`,
+		videoId: null,
+		cta1: 'Lend to African borrowers',
+		cta2: 'Lend to African borrowers',
+		link1: 'Africa filtered loans', // TODO: find url.
+		link2: 'Africa filtered loans', // TODO: find url.
+	},
+	{
+		name: 'Lev',
+		title: 'Refugee Advocate',
+		img: 'need-content',
+		body: `<p>
+			Lorem ipsum cillum eiusmod culpa minim do adipisicing labore nisi in veniam excepteur amet eu. Aliquip veniam ut ea do nulla commodo. Eu in qui aute id ex culpa id eiusmod nostrud sunt. Proident laboris amet consequat aliquip nostrud do reprehenderit. Labore occaecat occaecat sint consectetur sunt. Veniam sint do reprehenderit proident ut veniam minim amet sint sit.
+		</p>
+		`,
+		videoId: null,
+		cta1: 'Lend to refugees',
+		cta2: 'Lend monthly to refugees',
+		link1: '/lend-by-category/education',
+		link2: '/monthlygood?category=education',
+	},
+	{
+		name: 'Eduardo',
+		title: 'Kiva\'s Chronicler',
+		img: 'need-content',
+		body: `<p>
+		Growing up in Peru and now living in the United Kingdom, Eduardo has come full circle. He now explores his home country, and many others, through the Spanish loans that he translates as a volunteer with Kiva.
+		</p><p>
+		In the Kiva sphere, Eduardo works as a Volunteer Translator. He sees loans in their original language, in his case Spanish, and translates the text into English. Because Kiva borrowers are from all parts of the world, not all loan profiles come to us in English. More than 400 volunteers around the world, like Eduardo, translate and edit Kiva loans before they reach the site.
+		</p><p>
+		He’s been translating for Kiva for 3 years and this continuity with Kiva has allowed him to see some borrowers coming back to Kiva to request their second and third loans. For Eduardo, it’s rewarding to see these borrowers thriving in their endeavors and growing more successful.
+		</p><p>
+		“I want to see people succeed and I know that Kiva provides opportunities for people to do that.”
+		</p><p>
+		Kiva helps people move ahead in life, whether it’s to kick start their business ideas, finish their studies, or improve their living conditions. For Eduardo, this was a major draw for him to Kiva.
+		</p><p>
+		It struck him that many around the world are already so skilled; they just need an extra push from a Kiva loan and the collaboration of many lenders around the world to capitalize on their existing skills.
+		</p><p>
+		For Eduardo, and for many of us during this pandemic, aspects of life have changed drastically. However, his work for Kiva and what Kiva means to him is still the same.
+		</p><p>
+		Borrowers, now more than ever during an economy affected by COVID-19, depend on Kiva. Eduardo knows that Kiva continue to provide financial access and connect borrowers to a wide audience of lenders.
+		</p>
+		`,
+		videoId: null,
+		cta1: 'Lend to European borrowers',
+		cta2: 'Lend to European borrowers',
+		link1: 'Europe filtered loans', // TODO: find url.
+		link2: 'Europe filtered loans', // TODO: find url.
+	},
+	{
+		name: 'Busie',
+		title: 'Cross-Cultural Connector',
+		img: 'need-content',
+		body: `<p>
+			Lorem ipsum cillum eiusmod culpa minim do adipisicing labore nisi in veniam excepteur amet eu. Aliquip veniam ut ea do nulla commodo. Eu in qui aute id ex culpa id eiusmod nostrud sunt. Proident laboris amet consequat aliquip nostrud do reprehenderit. Labore occaecat occaecat sint consectetur sunt. Veniam sint do reprehenderit proident ut veniam minim amet sint sit.
+		</p>
+		`,
+		videoId: null,
+		cta1: 'Lend to African borrowers',
+		cta2: 'Lend to African borrowers',
+		link1: 'Africa filtered loans', // TODO: find url.
+		link2: 'Africa filtered loans', // TODO: find url.
+	}
 ];
