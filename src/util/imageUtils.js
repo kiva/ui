5d4109d30d95afb01998e151001ccabf3bbc729f
Ryan Ludwig <ryanl@kiva.org>
2020-09-25 17:23:36 -0700

    Check for height before resolving true

--- src/util/imageUtils.js
+++ src/util/imageUtils.js
@@ -6,7 +6,11 @@
 export function checkWebpSupport() {
 	return new Promise(resolve => {
 		const img = new Image();
-		img.onload = () => { resolve(true); };
+		img.onload = () => {
+			if (img.width > 0 && img.height > 0) {
+				resolve(true);
+			} else { resolve(false); }
+		};
 		img.onerror = () => { resolve(false); };
 		// eslint-disable-next-line max-len
 		img.src = 'data:image/webp;base64,UklGRkoAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAwAAAABBxAR/Q9ERP8DAABWUDggGAAAADABAJ0BKgEAAQADADQlpAADcAD++/1QAA=='; // webp with transparency check from Modernizr- https://github.com/Modernizr/Modernizr/blob/master/feature-detects/img/webp.js
@@ -21,7 +25,11 @@ export function checkWebpSupport() {
 export function checkAvifSupport() {
 	return new Promise(resolve => {
 		const img = new Image();
-		img.onload = () => { resolve(true); };
+		img.onload = () => {
+			if (img.width > 0 && img.height > 0) {
+				resolve(true);
+			} else { resolve(false); }
+		};
 		img.onerror = () => { resolve(false); };
 		// eslint-disable-next-line max-len
 		img.src = 'data:image/avif;base64,AAAAIGZ0eXBhdmlmAAAAAGF2aWZtaWYxbWlhZk1BMUIAAAEcbWV0YQAAAAAAAABIaGRscgAAAAAAAAAAcGljdAAAAAAAAAAAAAAAAGNhdmlmIC0gaHR0cHM6Ly9naXRodWIuY29tL2xpbmstdS9jYXZpZgAAAAAeaWxvYwAAAAAEQAABAAEAAAAAAUQAAQAAABcAAAAqaWluZgEAAAAAAAABAAAAGmluZmUCAAAAAAEAAGF2MDFJbWFnZQAAAAAOcGl0bQAAAAAAAQAAAHJpcHJwAAAAUmlwY28AAAAQcGFzcAAAAAEAAAABAAAAFGlzcGUAAAAAAAAAAQAAAAEAAAAQcGl4aQAAAAADCAgIAAAAFmF2MUOBAAwACggYAAYICGgIIAAAABhpcG1hAAAAAAAAAAEAAQUBAoMDhAAAAB9tZGF0CggYAAYICGgIIBoFHiAAAEQiBACwDoA='; // from Modernizr: https://github.com/Modernizr/Modernizr/blob/master/feature-detects/img/avif.js
