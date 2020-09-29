58cceec0dd3e18507b23f57f1e1168be24d44a9b
Adam Luptak <adam@rallyinteractive.com>
2020-09-29 14:24:02 -0600

    missing add files

---
+++ src/components/15Years/15YearsGlobePinCallout.js
@@ -0,0 +1,26 @@
+import {
+	Callout
+} from '@/lib/globekit/globekit.esm';
+
+class PinCallout extends Callout {
+	createElement() {
+		console.log(this.definition);
+		const div = document.createElement('div');
+		div.className = 'callout pin-callout';
+		div.dataset.code = this.definition.data.code;
+		return div;
+	}
+
+	setPosition(position) {
+		const nx = position.screen.x - ((this.size.left + this.size.right) / 2);
+		const ny = position.screen.y - ((this.size.left + this.size.right) / 2);
+		this.element.style.transform = `translate(${nx}px, ${ny}px)`;
+		this.element.style.zIndex = Math.round(10000 * position.world.similarityToCameraVector);
+		if (position.world.similarityToCameraVector < 0.7) {
+			const scale = Math.max((position.world.similarityToCameraVector - 0.4) / 0.3, 0);
+			this.element.style.transform += ` scale(${scale}, ${scale})`;
+		}
+	}
+}
+
+export default PinCallout;
