82da75c245b4eb984f756250087ec38880708baa
Adam Luptak <adam@rallyinteractive.com>
2020-09-30 16:51:48 -0600

    scroll to top behavior

--- src/components/15Years/15YearsGlobe.vue
+++ src/components/15Years/15YearsGlobe.vue
@@ -13,6 +13,7 @@ import geojson from '@/assets/data/components/15-years/geojson.json';
 import geoData from '@/assets/binary/geo/35-10.bin';
 import gkWasm from '@/assets/wasm/gkweb_bg.wasm';
 import textureKiva from '@/assets/images/15-years/texture-kiva.png';
+import { el } from 'date-fns/locale';
 
 export default {
 	name: 'FifteenYearsGlobe',
@@ -104,13 +105,21 @@ export default {
 			const results = this.datastore.getNearest(world.lat, world.lon, 500, 1);
 			if (results) {
 				const result = results[0][0];
-				const offset = window.innerWidth < 681 ? -20 : 0;
+				const isMobileLayout = window.innerWidth < 681;
+				const offset = isMobileLayout ? -20 : 0;
 				this.gkview.animateToLatLon(result.lat + offset, result.lon);
 				const callout = new CalloutDefinition(result.lat, result.lon, PinCallout, result.properties);
 				callout.altitude = 0.035;
 				this.calloutManager.replaceCallouts([...this.callouts, callout]);
 				this.$emit('selectcountry', result.properties);
 				this.currentCallout = callout;
+
+				if (isMobileLayout) {
+					console.log(window.scrollY, window.innerHeight);
+					if (window.scrollY > window.innerHeight * 0.45) {
+						this.$el.scrollIntoView({ behavior: 'smooth' });
+					}
+				}
 			} else {
 				this.calloutManager.replaceCallouts(this.callouts);
 				this.$emit('selectcountry', null);
@@ -166,7 +175,8 @@ export default {
 			}, 2000);
 
 			const { centroid } = country;
-			const offset = window.innerWidth < 681 ? -20 : 0;
+			const isMobileLayout = window.innerWidth < 681;
+			const offset = isMobileLayout ? -20 : 0;
 			this.gkview.animateToLatLon(centroid.lat + offset, centroid.lng);
 			const callout = new CalloutDefinition(centroid.lat, centroid.lng, PinCallout, country);
 			callout.altitude = 0.035;
