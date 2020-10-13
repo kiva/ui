fbba343958745e4628ef22b79530a95f70039f26
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2020-10-13 13:48:18 -0700

    GROW-309 Add CTA to encourage using globe and fix homepage partners section padding

---
+++ src/components/15Years/RadialIcon.vue
@@ -0,0 +1,43 @@
+<template functional>
+	<div class="radial-icon"></div>
+</template>
+
+<style lang="scss" scoped>
+@import 'settings';
+@import 'components/15-years/15-years';
+
+.radial-icon {
+	position: relative;
+	width: rem-calc(8);
+	height: rem-calc(8);
+	display: inline-block;
+	background-color: $twilight;
+	border-radius: 50%;
+
+	&::before {
+		content: '';
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		border-radius: 50%;
+		transform: translate(-50%, -50%);
+		border: rem-calc(3) solid $twilight;
+		animation: radial-grow 1s infinite;
+	}
+}
+
+@keyframes radial-grow {
+	0%,
+	25% {
+		width: 0%;
+		height: 0%;
+		opacity: 1;
+	}
+
+	100% {
+		width: 275%;
+		height: 275%;
+		opacity: 0;
+	}
+}
+</style>
