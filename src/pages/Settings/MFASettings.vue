317e7bc0a49199d088ff3d6829a2bf4abaa12d39
Joshua <joshuadstarkey@gmail.com>
2020-10-14 16:33:45 -0700

    grow-274 removing div as base element

--- src/pages/Settings/MFASettings.vue
+++ src/pages/Settings/MFASettings.vue
@@ -1,47 +1,45 @@
 <template>
-	<div>
-		<www-page>
-			<div class="page-wrap">
-				<div class="row">
-					<h2 class="column small-12 strong page-heading">
-						Security and login
-					</h2>
-					<div class="column small-12  medium-7">
-						<!-- Password section -->
-						<hr class="zero-top-margin">
-						<section>
-							<p class="medium-text heading">
-								Password
-							</p>
-						</section>
-						<hr>
+	<www-page>
+		<div class="page-wrap">
+			<div class="row">
+				<h2 class="column small-12 strong page-heading">
+					Security and login
+				</h2>
+				<div class="column small-12  medium-7">
+					<!-- Password section -->
+					<hr class="zero-top-margin">
+					<section>
+						<p class="medium-text heading">
+							Password
+						</p>
+					</section>
+					<hr>
 
-						<!-- MFA section -->
+					<!-- MFA section -->
 
-						<!-- TODO: Add feature flag once it exists.
-						This MFA settings area need to be hidden/shown based
-						on the MFA feature flag that is captured in GROW-276 -->
+					<!-- TODO: Add feature flag once it exists.
+					This MFA settings area need to be hidden/shown based
+					on the MFA feature flag that is captured in GROW-276 -->
 
-						<section>
-							<p class="medium-text heading">
-								2-step verification
-							</p>
-						</section>
-						<hr>
-					</div>
+					<section>
+						<p class="medium-text heading">
+							2-step verification
+						</p>
+					</section>
+					<hr>
+				</div>
 
-					<!-- FAQ section -->
-					<div class="column small-12 medium-5 faq-section">
-						<section>
-							<p class="medium-text heading">
-								FAQ
-							</p>
-						</section>
-					</div>
+				<!-- FAQ section -->
+				<div class="column small-12 medium-5 faq-section">
+					<section>
+						<p class="medium-text heading">
+							FAQ
+						</p>
+					</section>
 				</div>
 			</div>
-		</www-page>
-	</div>
+		</div>
+	</www-page>
 </template>
 
 <script>
