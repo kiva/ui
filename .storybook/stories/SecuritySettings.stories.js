bea3981bae70b9b81f50f681007a22cc18c2f462
Joshua <joshuadstarkey@gmail.com>
2020-10-22 09:40:22 -0700

    restructured how this is displayed in storybook

--- .storybook/stories/SecuritySettings.stories.js
+++ .storybook/stories/SecuritySettings.stories.js
@@ -1,21 +1,38 @@
+import Vue from 'vue'
+
+// import plugins
+import kivaPlugins from '@/plugins';
+Vue.use(kivaPlugins)
+
 import StoryRouter from 'storybook-vue-router';
 import apolloStoryMixin from '../mixins/apollo-story-mixin';
 import kvAuth0StoryMixin from '../mixins/kv-auth0-story-mixin';
 
 import SecuritySettingsPage from '@/pages/Settings/SecuritySettings';
+import TwoStepVerificationPage from '@/pages/Settings/TwoStepVerificationPage';
 
 export default {
-	title: 'Pages/settings/security',
+	title: 'Pages/Security Settings',
 	component: SecuritySettingsPage,
 	decorators: [StoryRouter()],
 };
 
-export const Default = () => ({
+export const SecurityAndLogin = () => ({
 	mixins: [apolloStoryMixin(), kvAuth0StoryMixin],
 	components: {
 		SecuritySettingsPage,
 	},
-	template:`
-	<security-settings-page />
+	template: `
+		<security-settings-page />
+	`,
+});
+
+export const TwoStepVerification = () => ({
+	mixins: [apolloStoryMixin(), kvAuth0StoryMixin],
+	components: {
+		TwoStepVerificationPage,
+	},
+	template: `
+		<two-step-verification-page />
 	`,
 });
