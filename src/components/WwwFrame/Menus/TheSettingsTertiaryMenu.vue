726c30699044b8ca00560904784894c6e005d842
Ryan Ludwig <ryanl@kiva.org>
2020-10-27 09:46:52 -0700

    Match links and order from old-stack settings tertiary nav

--- src/components/WwwFrame/Menus/TheSettingsTertiaryMenu.vue
+++ src/components/WwwFrame/Menus/TheSettingsTertiaryMenu.vue
@@ -6,7 +6,31 @@
 					to="/settings/account"
 					v-kv-track-event="['TertiaryNav','click-MyKiva-Settings-account']"
 				>
-					Account Settings
+					Account
+				</router-link>
+			</li>
+			<li>
+				<router-link
+					to="/settings/credit"
+					v-kv-track-event="['TertiaryNav','click-MyKiva-Settings-credit']"
+				>
+					Credit
+				</router-link>
+			</li>
+			<li>
+				<router-link
+					to="/settings/email"
+					v-kv-track-event="['TertiaryNav','click-MyKiva-Settings-email']"
+				>
+					Email
+				</router-link>
+			</li>
+			<li>
+				<router-link
+					to="/settings/data"
+					v-kv-track-event="['TertiaryNav','click-Settings-data']"
+				>
+					Data
 				</router-link>
 			</li>
 			<li>
@@ -19,26 +43,10 @@
 			</li>
 			<li>
 				<router-link
-					to="/settings/credit"
-					v-kv-track-event="['TertiaryNav','click-MyKiva-Settings-credit']"
+					to="/lend/saved-search"
+					v-kv-track-event="['TertiaryNav','click-MyKiva-Settings-saved-searches']"
 				>
-					Credit Settings
-				</router-link>
-			</li>
-			<li>
-				<router-link
-					to="/settings/email"
-					v-kv-track-event="['TertiaryNav','click-MyKiva-Settings-email']"
-				>
-					Email Preferences
-				</router-link>
-			</li>
-			<li>
-				<router-link
-					to="/settings/data"
-					v-kv-track-event="['TertiaryNav','click-Settings-data']"
-				>
-					Data Settings
+					Saved searches
 				</router-link>
 			</li>
 		</ul>
