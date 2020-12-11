16e9a717cd6574d96c6fbc7427f8754ba81c4a29
Ryan Ludwig <ryanl@kiva.org>
2020-12-11 11:10:58 -0800

    Initial pass at verification code input

---
+++ .storybook/stories/KvVerificationCodeInput.stories.js
@@ -0,0 +1,34 @@
+import KvVerificationCodeInput from '@/components/Kv/KvVerificationCodeInput';
+
+export default {
+	title: 'Kv/Form Elements/KvVerificationCodeInput',
+	component: KvVerificationCodeInput,
+};
+
+export const Default = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	components: {
+		KvVerificationCodeInput
+	},
+	args: {
+		maxlength: 4,
+		disabled: false
+	},
+	data() {
+		return {
+			verificationCode: ''
+		}
+	},
+	template: `
+		<div>
+			<label for="my_default_input">Enter your verification code</label>
+			<kv-verification-code-input
+				id="my_default_input"
+				:maxlength="maxlength"
+				:disabled="disabled"
+				v-model="verificationCode"
+			/>
+		</div>
+	`,
+});
+
---
+++ src/components/Kv/KvVerificationCodeInput.vue
@@ -0,0 +1,95 @@
+<template>
+	<div
+		class="kv-verification-code-input"
+		:style="cssVars"
+	>
+		<!--
+			How I imagine this to work with multiple inputs:
+			multiple <inputs hidden from screenreaders
+			1 input hidden from sighted with the autocomplete tag.
+			the values get updated in sync.
+			<input
+				class="hide-from-sr"
+				v-for="i in maxlength"
+				type="text"
+				inputmode="numeric"
+				pattern="[0-9]*"
+				:value="valuesArray[i]"
+				@input="updateValuesArray($event.target.value, i)"
+			>
+			<input
+				type="hidden"
+				:value="computedValueFromValuesArray"
+				autocomplete="one-time-code"
+			>
+		-->
+		<input
+			class="kv-verification-code-input__input"
+			type="text"
+			inputmode="numeric"
+			pattern="[0-9]*"
+			autocomplete="one-time-code"
+			:disabled="disabled"
+			:maxlength="maxlength"
+			:value="value"
+			v-on="inputListeners"
+			@input="$emit('input', $event.target.value)"
+		>
+	</div>
+</template>
+
+<script>
+// Some useful information on auto-completion and input modes here
+// https://www.twilio.com/blog/html-attributes-two-factor-authentication-autocomplete
+
+export default {
+	model: {
+		prop: 'value',
+		event: 'input'
+	},
+	props: {
+		maxlength: {
+			type: Number,
+			default: 6
+		},
+		value: {
+			type: String,
+			default: ''
+		},
+		disabled: {
+			type: Boolean,
+			default: false
+		}
+	},
+	computed: {
+		cssVars() {
+			return {
+				'--kv-verification-code-input-maxlength': this.maxlength
+			};
+		},
+		inputListeners() {
+			return {
+				// Pass through any listeners from the parent to the input element like blur, focus, etc. ...
+				// https://vuejs.org/v2/guide/components-custom-events.html#Binding-Native-Events-to-Components
+				...this.$listeners,
+				// ...except for the listener to the 'input' event which is emitted by this component
+				input: () => {},
+			};
+		},
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+@import 'settings';
+
+.kv-verification-code-input {
+	&__input {
+		font-size: 3rem; // All sizing is relative to this
+		font-variant-numeric: tabular-nums;
+		letter-spacing: 0.5em;
+		padding: 1em 0 1em 1em;
+		width: calc(var(--kv-verification-code-input-maxlength) * 1.1125em + 1.25em);
+	}
+}
+</style>
