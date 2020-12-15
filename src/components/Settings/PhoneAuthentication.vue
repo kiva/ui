300cce9c0490e196ed150ef03840b3ad0533acd1
Ryan Ludwig <ryanl@kiva.org>
2020-12-15 10:50:00 -0800

    Progress point for actually sending codes

--- src/components/Settings/PhoneAuthentication.vue
+++ src/components/Settings/PhoneAuthentication.vue
@@ -35,7 +35,7 @@
 					class="smallest expanded"
 					type="button"
 					:disabled="$v.phoneNumber.$invalid"
-					@click.native="sendVerificationCode('text')"
+					@click.native="sendVerificationCode('SMS')"
 				>
 					Text message
 				</kv-button>
@@ -43,7 +43,7 @@
 					class="smallest expanded"
 					type="button"
 					:disabled="$v.phoneNumber.$invalid"
-					@click.native="sendVerificationCode('call')"
+					@click.native="sendVerificationCode('voice')"
 				>
 					Phone call
 				</kv-button>
@@ -51,14 +51,14 @@
 		</section>
 
 		<section v-if="step === 1">
-			<h2 v-if="verificationType === 'text'">
+			<h2 v-if="verificationType === 'SMS'">
 				We just sent you a text message
 			</h2>
-			<h2 v-if="verificationType === 'call'">
+			<h2 v-if="verificationType === 'voice'">
 				You’ll receive a call shortly'
 			</h2>
 			<form
-				@submit="submitVerification"
+				@submit.prevent="confirmAuthenticatorEnrollment"
 			>
 				<p>Enter the code sent to {{ phoneNumber }}.</p>
 				<label for="verification_code">Enter your 6-digit code here:</label>
@@ -91,11 +91,46 @@
 import KvButton from '@/components/Kv/KvButton';
 import KvPhoneInput from '@/components/Kv/KvPhoneInput';
 import KvVerificationCodeInput from '@/components/Kv/KvVerificationCodeInput';
+import gql from 'graphql-tag';
 
 import { validationMixin } from 'vuelidate';
 import {
 	required, minLength, maxLength, numeric
 } from 'vuelidate/lib/validators';
+
+const enrollSMSAuthenticatorMutation = gql`
+mutation enrollSMSAuthenticator($mfa_token: String!, $phone_number: String!) {
+  my {
+		enrollSMSAuthenticator( mfa_token: $mfa_token, phone_number: $phone_number) {
+			binding_method,
+			oob_code
+		}
+	}
+}`;
+
+const enrollVoiceAuthenticatorMutation = gql`
+mutation enrollVoiceAuthenticator($mfa_token: String!, $phone_number: String!) {
+	my {
+		enrollVoiceAuthenticator( mfa_token: $mfa_token, phone_number: $phone_number) {
+			binding_method,
+			oob_code
+		}
+	}
+}`;
+
+const confirmSMSAuthenticatorEnrollmentMutation = gql`
+mutation confirmSMSAuthenticatorEnrollment($mfa_token: String!, $oob_code: String!, $binding_code: String!) {
+	my {
+		confirmSMSAuthenticatorEnrollment( mfa_token: $mfa_token, oob_code: $oob_code, binding_code: $binding_code)
+	}
+}`;
+
+const confirmVoiceAuthenticatorEnrollmentMutation = gql`
+mutation confirmVoiceAuthenticatorEnrollment($mfa_token: String!, $oob_code: String!, $binding_code: String!) {
+	my {
+		confirmVoiceAuthenticatorEnrollment( mfa_token: $mfa_token, oob_code: $oob_code, binding_code: $binding_code)
+	}
+}`;
 
 export default {
 	components: {
@@ -104,6 +139,7 @@ export default {
 		KvVerificationCodeInput,
 	},
 	mixins: [validationMixin],
+	inject: ['apollo', 'kvAuth0'],
 	validations: {
 		phoneNumber: {
 			required,
@@ -120,7 +156,9 @@ export default {
 		return {
 			phoneNumber: '',
 			isPhoneNumberValid: false,
-			verificationType: '', // text or call
+			verificationType: '', // SMS or voice
+			oobCode: '',
+			mfaToken: null,
 			userVerificationCode: '', // user entered number
 			step: 0,
 		};
@@ -134,25 +172,83 @@ export default {
 			this.userVerificationCode = ''; // clear out any existing codes.
 			this.verificationType = verificationType;
 
-			// TODO: Hit endpoint to call or text based on verification method
 			// TODO: Setup some pending/error indication in the UI
 
-			// if successful, move forward.
-			this.step = 1;
+			const mutation = this.verificationType === 'SMS'
+				? enrollSMSAuthenticatorMutation
+				: enrollVoiceAuthenticatorMutation;
+
+			if (this.kvAuth0.enabled) {
+				this.kvAuth0.checkSession()
+					.then(() => this.kvAuth0.getMfaManagementToken())
+					.then(token => {
+						this.mfaToken = token;
+						return this.apollo.mutate({
+							mutation,
+							variables: {
+								mfa_token: token,
+								phone_number: this.phoneNumber
+							}
+						});
+					}).then(result => {
+						console.dir(result);
+						if (result.errors) {
+							console.error(result.errors);
+							// TODO: Show error to user
+						} else {
+							this.oobCode = this.verificationType === 'SMS'
+								? result?.data?.my?.enrollSMSAuthenticator?.oob_code
+								: result?.data?.my?.enrollVoiceAuthenticator?.oob_code;
+							if (this.oobCode) {
+								this.step = 1;
+							}
+						}
+					});
+			}
 		},
-		submitVerification() {
+		confirmAuthenticatorEnrollment() {
 			console.log(this.userVerificationCode);
 			// TODO: Hit endpoint with userVerificationCode
 			// TODO: Setup some pending/error indication in the UI
 
-			// if successful, close the lightbox
-			this.$emit('verification-complete');
+			const mutation = this.verificationType === 'SMS'
+				? confirmSMSAuthenticatorEnrollmentMutation
+				: confirmVoiceAuthenticatorEnrollmentMutation;
+
+			if (this.kvAuth0.enabled) {
+				this.kvAuth0.checkSession()
+					.then(() => this.kvAuth0.getMfaManagementToken())
+					.then(token => {
+						this.mfaToken = token;
+						return this.apollo.mutate({
+							mutation,
+							variables: {
+								mfa_token: token,
+								oob_code: this.oobCode,
+								binding_code: this.userVerificationCode
+							}
+						});
+					}).then(result => {
+						console.dir(result);
+						console.dir(result);
+						if (result.errors) {
+							console.error(result.errors);
+							// TODO: Show error to user
+						} else {
+							// we're successful, move forward.
+							// if successful, close the lightbox
+							this.$emit('verification-complete');
+						}
+					});
+			}
 		},
 		reset() {
 			this.step = 0;
 			this.phoneNumber = '';
 			this.userVerificationCode = '';
-		}
+			this.oobCode = '';
+			this.mfaToken = null;
+		},
 	},
 };
 </script>
