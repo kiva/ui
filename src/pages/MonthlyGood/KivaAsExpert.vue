3b9f2ac925f8034e182bf0441b90a16b0e44a5ae
Eddie Ferrer <eddieferrer@user.noreply.github.com>
2021-01-13 13:40:41 -0800

    GROW-391 MG Landing Copy Change & Live KivaStats
    * Small copy change to KivaExpert section on mg landing page
    * Use kivaStats endpoint to power stats on MG Landing page

--- src/pages/MonthlyGood/KivaAsExpert.vue
+++ src/pages/MonthlyGood/KivaAsExpert.vue
@@ -3,23 +3,19 @@
 		<div class="row">
 			<div class="small-12 columns">
 				<h2 class="impact-text">
-					10 years, $1 billion in impact
+					{{ yearsSinceStartCalc }} years, ${{ amountFunded }} billion in impact
 				</h2>
 			</div>
 			<div class="small-12 large-7 columns">
 				<p>
 					<!-- eslint-disable-next-line max-len -->
-					With more than $1 billion in loans funded, Kiva is a leading global nonprofit creating opportunity for communities in need around the world. Your support will help us continue to push boundaries, from offering flexible loan terms that help farmers deal with unpredictable weather to providing capital to refugees in need of stability.
+					With nearly ${{ amountFunded }} billion in loans funded, Kiva is a leading global nonprofit creating opportunity for communities in need around the world. Your support will help us continue to push boundaries. Join the movement of <strong>{{ numLenders }} million lenders</strong> who’ve supported <strong>{{ numBorrowers }} million borrowers</strong>.
 				</p>
 				<kv-responsive-image
 					:images="billionImpactImages"
 					class="hide-for-large community-image"
-					alt="10 years and $1 billion in loans funded"
+					:alt="`${yearsSinceStartCalc} years, ${amountFunded} billion in impact`"
 				/>
-				<p class="join-the-movement">
-					<!-- eslint-disable-next-line max-len -->
-					Join the movement of over <strong>1.6 million lenders</strong> who’ve supported <strong>2.5 million borrowers</strong>.
-				</p>
 				<slot name="form"></slot>
 			</div>
 
@@ -27,7 +23,7 @@
 				<kv-responsive-image
 					:images="billionImpactImages"
 					class="show-for-large community-image"
-					alt="10 years and $1 billion in loans funded"
+					:alt="`${yearsSinceStartCalc} years, ${amountFunded} billion in impact`"
 				/>
 			</div>
 		</div>
@@ -35,22 +31,60 @@
 </template>
 
 <script>
+import numeral from 'numeral';
 import KvResponsiveImage from '@/components/Kv/KvResponsiveImage';
+import { differenceInYears } from 'date-fns';
+import homepageStatistics from '@/graphql/query/whyKivaData.graphql';
 
 const billionImpactImagesRequire = require.context('@/assets/images/10-years-billion-impact', true);
+const million = 1000000;
+const billion = 1000000000;
 
 export default {
+	components: {
+		KvResponsiveImage
+	},
 	data() {
 		return {
 			billionImpactImages: [
 				['small', billionImpactImagesRequire('./10-years-billion-impact_ghost.jpg')],
 				['small retina', billionImpactImagesRequire('./10-years-billion-impact_2x_ghost.jpg')],
-			]
+			],
+			// sensible defaults in the format returned by endpoint
+			amountFunded: 1.5 * billion,
+			numLenders: 1.9 * million,
+			numBorrowers: 3.8 * million,
+
 		};
 	},
-	components: {
-		KvResponsiveImage
+	inject: ['apollo'],
+	apollo: {
+		query: homepageStatistics,
+		preFetch: true,
+		result({ data }) {
+			const rawAmountFunded = data?.general?.kivaStats?.amountFunded;
+			const rawNumLenders = data?.general?.kivaStats?.numLenders;
+			const rawNumBorrowers = data?.general?.kivaStats?.numBorrowers;
+			// Only modify component values on valid values
+			if (!Number.isNaN(Number(rawAmountFunded))) {
+				this.amountFunded = numeral(rawAmountFunded).divide(billion).format('0.0');
+			}
+			if (!Number.isNaN(Number(rawNumLenders))) {
+				this.numLenders = numeral(rawNumLenders).divide(million).format('0.0');
+			}
+			if (!Number.isNaN(Number(rawNumBorrowers))) {
+				this.numBorrowers = numeral(rawNumBorrowers).divide(million).format('0.0');
+			}
+		},
 	},
+	computed: {
+		yearsSinceStartCalc() {
+			// Kiva was formally registered as a
+			// non-profit on October 12, 2005
+			const foundedDate = new Date(2005, 10, 12);
+			return differenceInYears(Date.now(), foundedDate);
+		},
+	}
 };
 
 </script>
@@ -78,8 +112,8 @@ export default {
 		margin: 0 auto;
 	}
 
-	.join-the-movement {
-		padding: 1.85rem 0;
+	::v-deep form {
+		margin-top: 2.5rem;
 	}
 }
 </style>
