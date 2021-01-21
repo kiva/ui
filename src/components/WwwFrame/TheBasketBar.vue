9b67e2e7d1c8466ae962eb78914d8771e6b727aa
Ryan Ludwig <ryanl@kiva.org>
2021-01-21 12:58:40 -0800

    Make basket button a floating circle/pill

--- src/components/WwwFrame/TheBasketBar.vue
+++ src/components/WwwFrame/TheBasketBar.vue
@@ -1,14 +1,27 @@
 <template>
-	<router-link to="/basket" v-if="count > 0" class="basket-bar hide-for-large">
-		Basket ({{ count }})
+	<router-link
+		v-if="count > 0"
+		:to="basketLink"
+		class="basket-bar hide-for-large"
+	>
+		<kv-icon
+			class="basket-bar__icon"
+			name="cart"
+			from-sprite="true"
+		/>
+		<span class="basket-bar__count">{{ count }}</span>
+		<span class="show-for-sr">items in your cart</span>
 	</router-link>
 </template>
 
 <script>
-import _get from 'lodash/get';
 import countQuery from '@/graphql/query/basketCount.graphql';
+import KvIcon from '@/components/Kv/KvIcon';
 
 export default {
+	components: {
+		KvIcon
+	},
 	inject: ['apollo'],
 	data() {
 		return {
@@ -19,34 +32,63 @@ export default {
 		query: countQuery,
 		preFetch: true,
 		result({ data }) {
-			this.count = _get(data, 'shop.nonTrivialItemCount');
+			this.count = data?.shop?.nonTrivialItemCount || 0;
 		}
 	},
+	props: {
+		corporate: {
+			type: Boolean,
+			default: false
+		},
+	},
+	computed: {
+		basketLink() {
+			return this.corporate ? this.addHashToRoute('show-basket') : '/basket';
+		}
+	},
+	methods: {
+		addHashToRoute(hash) {
+			const route = { ...this.$route };
+			route.hash = hash;
+			return route;
+		},
+	}
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import 'settings';
 
 .basket-bar {
-	display: block;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 	position: fixed;
-	bottom: 0;
-	width: 100%;
+	bottom: 1rem;
+	right: 1rem;
+	z-index: 1000;
+	min-width: rem-calc(60);
+	height: rem-calc(60);
+	padding: rem-calc(12);
+	border-radius: rem-calc(60);
 	box-shadow: 0 -1px 3px 0 rgba(51, 51, 51, 0.3);
-	transition: bottom 500ms ease;
-	text-align: center;
-	padding: rem-calc(18) rem-calc(36);
 	color: $white;
 	background-color: $kiva-accent-blue;
 	font-size: rem-calc(20);
 	font-weight: normal;
-	z-index: 1000;
 
-	&:hover {
+	&:hover,
+	&:focus {
 		color: $white;
 		background-color: $kiva-accent-darkblue;
 		text-decoration: none;
+	}
+
+	&__icon {
+		flex-shrink: 0;
+		fill: $white;
+		width: rem-calc(20);
+		height: rem-calc(20);
 	}
 }
 </style>
