0bc32530062305085ea9ccfeee94c173aef4fbd8
Joshua <joshuadstarkey@gmail.com>
2021-01-27 13:40:57 -0800

    Revert "grow-428 Had to reorder the ter-nav to be consistent with the ordering request in grow-428"
    This reverts commit 79f739a3e4a257e897b12b5acb28399b6ab79b00.

--- src/components/WwwFrame/Menus/TheSettingsTertiaryMenu.vue
+++ src/components/WwwFrame/Menus/TheSettingsTertiaryMenu.vue
@@ -9,14 +9,6 @@
 					Account
 				</router-link>
 			</li>
-			<li v-if="isMfaActive">
-				<router-link
-					to="/settings/security"
-					v-kv-track-event="['TertiaryNav','click-MyKiva-Settings-security']"
-				>
-					Security and login
-				</router-link>
-			</li>
 			<li>
 				<router-link
 					to="/settings/credit"
@@ -39,6 +31,14 @@
 					v-kv-track-event="['TertiaryNav','click-Settings-data']"
 				>
 					Data
+				</router-link>
+			</li>
+			<li v-if="isMfaActive">
+				<router-link
+					to="/settings/security"
+					v-kv-track-event="['TertiaryNav','click-MyKiva-Settings-security']"
+				>
+					Security and login
 				</router-link>
 			</li>
 			<li>
