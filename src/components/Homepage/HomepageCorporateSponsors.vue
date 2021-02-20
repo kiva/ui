78b1d8de4c5e9226d390234581aac2c065eec1e5
Joshua <joshuadstarkey@gmail.com>
2021-02-19 16:26:10 -0800

    grow-439 style refinements for section

--- src/components/Homepage/HomepageCorporateSponsors.vue
+++ src/components/Homepage/HomepageCorporateSponsors.vue
@@ -18,6 +18,7 @@
 				<img
 					:src="image.url"
 					:alt="image.description"
+					:class="'sponsors__li--' + image.description"
 					class="sponsors__li--logo"
 					loading="lazy"
 				>
@@ -65,18 +66,74 @@ export default {
 
 .sponsors {
 	&__section-img {
-		max-width: rem-calc(400);
+		width: 20rem;
+		margin-bottom: 1.5rem;
+
+		@include breakpoint(large) {
+			width: 25rem;
+		}
 	}
 
 	&__headline {
+		padding: rem-calc(10);
 
+		@include breakpoint(large) {
+			margin-bottom: rem-calc(50);
+		}
+	}
+
+	// Centering the last sponsor logo
+	&__li:last-child {
+		@include breakpoint(large) {
+			margin: 0 auto;
+		}
 	}
 
 	&__li {
-		margin: 0 auto;
-
+		// Applies to all sponsor logos
 		&--logo {
-			width: 100px;
+			margin: rem-calc(60) auto;
+		}
+
+		// styling individual sponsor logos
+		&--boa-logo {
+			width: rem-calc(306);
+		}
+
+		&--hitachi-logo {
+			width: rem-calc(136);
+		}
+
+		&--visa-foundation-logo {
+			width: rem-calc(92);
+		}
+
+		&--women-and-girls-empowerment-logo {
+			width: rem-calc(100);
+		}
+
+		&--principal-foundation-logo {
+			width: rem-calc(278);;
+		}
+
+		&--pepsi-foundation-logo {
+			width: rem-calc(182);
+		}
+
+		&--fossil-foundation-logo {
+			width: rem-calc(242);
+		}
+
+		&--it-cosmetics-logo {
+			width: rem-calc(146);
+		}
+
+		&--kate-spade-foundation-logo {
+			width: rem-calc(244);
+		}
+
+		&--xilinx-logo {
+			width: rem-calc(142);
 		}
 	}
 }
