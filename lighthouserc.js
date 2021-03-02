f4049b5592b58fee860d434f3e9706e44205d397
Ryan Ludwig <ryanl@kiva.org>
2021-03-02 15:31:49 -0800

    VUE-491 Push results to our LHCI server on Heroku (#2708)
    * Push results to our LHCI server on Heroku

--- lighthouserc.js
+++ lighthouserc.js
@@ -14,7 +14,9 @@ module.exports = {
 			]
 		},
 		upload: {
-			target: 'temporary-public-storage',
+			target: 'lhci',
+			serverBaseUrl: 'https://lighthouse-ci-kiva-dev.herokuapp.com/',
+			token: process.env.LHCI_DEV_BUILD_TOKEN,
 		},
 	},
 };
