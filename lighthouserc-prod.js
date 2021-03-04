6fee0d56fb57d7058deb1f039dc733fedd5b81cc
Ryan Ludwig <ryanl@kiva.org>
2021-03-04 10:01:18 -0800

    Make a separate action and lighthouse settings for running against prod

---
+++ lighthouserc-prod.js
@@ -0,0 +1,20 @@
+module.exports = {
+	ci: {
+		collect: {
+			settings: {
+				onlyCategories: ['accessibility', 'best-practices', 'performance', 'seo']
+			},
+			url: [
+				'https://www.kiva.org/',
+				'https://www.kiva.org/lend/filter',
+				'https://www.kiva.org/lend-by-category',
+				'https://www.kiva.org/lend-by-category/women'
+			]
+		},
+		upload: {
+			target: 'lhci',
+			serverBaseUrl: 'https://lighthouse-ci-kiva-prod.herokuapp.com/',
+			token: process.env.LHCI_PROD_BUILD_TOKEN,
+		},
+	},
+};
