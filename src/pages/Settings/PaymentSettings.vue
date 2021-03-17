1e3cab9ccea9e1f6026d1414d393753852158f23
Ryan Ludwig <ryanl@kiva.org>
2021-03-17 16:38:21 -0700

    change to mask

--- src/pages/Settings/PaymentSettings.vue
+++ src/pages/Settings/PaymentSettings.vue
@@ -24,7 +24,7 @@
 								class="payment-settings-default-form__cc-wrapper"
 							>
 								<kv-radio
-									class="payment-settings-default-form__radio fs-exclude"
+									class="payment-settings-default-form__radio fs-mask"
 									:id="`creditCard-${index}`"
 									:radio-value="paymentMethod.nonce"
 									v-model="selectedDefaultCardNonce"
