ad7018a6b9b1480d57a92c44a5d110d225140467
Eddie Ferrer <eddieferrer@user.noreply.github.com>
2021-03-22 10:20:45 -0700

    VUE-510 Update Husky and fix linting hook
    * Fixes CheckoutDropIn formatting
    * Update husky to v5
    * fix husky hooks

--- src/components/Checkout/CheckoutDropInPaymentWrapper.vue
+++ src/components/Checkout/CheckoutDropInPaymentWrapper.vue
@@ -27,15 +27,22 @@
 					name="termsAgreement"
 					class="checkbox"
 					v-model="termsAgreement"
-				>I have read and agree to the <a
-					:href="`https://${this.$appConfig.host}/legal/terms`"
-					target="_blank"
-					title="Open Terms of Use in a new window">Terms of Use</a> and <a
+				>
+					I have read and agree to the
+					<a
+						:href="`https://${this.$appConfig.host}/legal/terms`"
+						target="_blank"
+						title="Open Terms of Use in a new window"
+					>Terms of Use</a>
+					and
+					<a
 						:href="`https://${this.$appConfig.host}/legal/privacy`"
 						target="_blank"
-						title="Open Privacy Policy in a new window">Privacy Policy</a>.
+						title="Open Privacy Policy in a new window"
+					>Privacy Policy</a>.
 					<p v-if="$v.termsAgreement.$error" class="input-error">
-						You must agree to the Kiva Terms of service & Privacy policy.
+						You must agree to the Kiva Terms of service & Privacy
+						policy.
 					</p>
 				</kv-checkbox>
 				<kv-checkbox
@@ -43,7 +50,9 @@
 					class="checkbox"
 					name="emailUpdates"
 					:checked="true"
-				>Receive email updates from Kiva (including borrower updates and promos). You can unsubscribe anytime.
+				>
+					Receive email updates from Kiva (including borrower updates
+					and promos). You can unsubscribe anytime.
 				</kv-checkbox>
 			</div>
 			<div id="dropin-button">
@@ -59,7 +68,8 @@
 				</kv-button>
 			</div>
 			<p class="dropin-wrapper-attribution-text">
-				Thanks to PayPal, Kiva receives free payment processing for all loans.
+				Thanks to PayPal, Kiva receives free payment processing for all
+				loans.
 			</p>
 		</div>
 	</div>
@@ -85,22 +95,19 @@ export default {
 		KvButton,
 		KvIcon,
 		BraintreeDropInInterface,
-		KvCheckbox
+		KvCheckbox,
 	},
 	inject: ['apollo', 'cookieStore'],
-	mixins: [
-		checkoutUtils,
-		validationMixin
-	],
+	mixins: [checkoutUtils, validationMixin],
 	props: {
 		amount: {
 			type: String,
-			default: ''
+			default: '',
 		},
 		isGuestCheckout: {
 			type: Boolean,
-			default: false
-		}
+			default: false,
+		},
 	},
 	data() {
 		return {
@@ -113,9 +120,9 @@ export default {
 	validations: {
 		email: {
 			required,
-			email
+			email,
 		},
-		termsAgreement: { required: value => value === true }
+		termsAgreement: { required: value => value === true },
 	},
 	methods: {
 		submit() {
@@ -135,10 +142,15 @@ export default {
 					if (validationStatus === true) {
 						this.submitDropInPayment();
 					} else {
-						const errorMessage = _get(validationStatus, '[0].error');
+						const errorMessage = _get(
+							validationStatus,
+							'[0].error'
+						);
 						if (errorMessage === 'api.authenticationRequired') {
 							const loginHint = encodeURIComponent(
-								`login|${JSON.stringify({ msg: 're-auth-acc-exists' })}`
+								`login|${JSON.stringify({
+									msg: 're-auth-acc-exists',
+								})}`
 							);
 							window.location = `/ui-login?force=true&doneUrl=/checkout&login_hint=${loginHint}`;
 						} else {
@@ -155,8 +167,14 @@ export default {
 					const errorMessage = _get(error, 'errors[0].message');
 
 					Sentry.withScope(scope => {
-						scope.setTag('bt_stage_dropin', 'btSubmitValidationCatch');
-						scope.setTag('bt_basket_validation_error', errorMessage);
+						scope.setTag(
+							'bt_stage_dropin',
+							'btSubmitValidationCatch'
+						);
+						scope.setTag(
+							'bt_basket_validation_error',
+							errorMessage
+						);
 						Sentry.captureException(errorCode);
 					});
 				});
@@ -184,85 +202,115 @@ export default {
 
 					// Fire specific exception to Sentry/Raven
 					Sentry.withScope(scope => {
-						scope.setTag('bt_stage_dropin', 'btSubmitValidationCatch');
-						scope.setTag('bt_basket_validation_error', errorMessage);
+						scope.setTag(
+							'bt_stage_dropin',
+							'btSubmitValidationCatch'
+						);
+						scope.setTag(
+							'bt_basket_validation_error',
+							errorMessage
+						);
 						Sentry.captureException(errorCode);
 					});
 				});
 		},
 		submitDropInPayment() {
 			// request payment method
-			this.$refs.braintreeDropInInterface.btDropinInstance.requestPaymentMethod()
+			this.$refs.braintreeDropInInterface.btDropinInstance
+				.requestPaymentMethod()
 				.then(btSubmitResponse => {
 					const transactionNonce = btSubmitResponse?.nonce;
 					const deviceData = btSubmitResponse?.deviceData;
 					const paymentType = btSubmitResponse?.type;
 					if (transactionNonce) {
-						this.doBraintreeCheckout(transactionNonce, deviceData, paymentType);
+						this.doBraintreeCheckout(
+							transactionNonce,
+							deviceData,
+							paymentType
+						);
 					}
-				}).catch(btSubmitError => {
+				})
+				.catch(btSubmitError => {
 					console.error(btSubmitError);
 					// Fire specific exception to Sentry/Raven
 					Sentry.withScope(scope => {
-						scope.setTag('bt_stage_dropin', 'btRequestPaymentMethodCatch');
-						scope.setTag('bt_basket_validation_error', btSubmitError);
+						scope.setTag(
+							'bt_stage_dropin',
+							'btRequestPaymentMethodCatch'
+						);
+						scope.setTag(
+							'bt_basket_validation_error',
+							btSubmitError
+						);
 						Sentry.captureException(btSubmitError);
 					});
 				});
 		},
 		doBraintreeCheckout(nonce, deviceData, paymentType) {
 			// Apollo call to the query mutation
-			this.apollo.mutate({
-				mutation: braintreeDepositAndCheckout,
-				variables: {
-					amount: numeral(this.amount).format('0.00'),
-					nonce,
-					deviceData,
-					savePaymentMethod: false, // save payment methods handled by braintree drop in UI
-				}
-			}).then(kivaBraintreeResponse => {
-				// Check for errors in transaction
-				if (kivaBraintreeResponse.errors) {
-					this.$emit('updating-totals', false);
-					const errorCode = _get(kivaBraintreeResponse, 'errors[0].code');
-					const errorMessage = _get(kivaBraintreeResponse, 'errors[0].message');
-					const standardErrorCode = `(Braintree error: ${errorCode})`;
-					const standardError = `There was an error processing your payment.
+			this.apollo
+				.mutate({
+					mutation: braintreeDepositAndCheckout,
+					variables: {
+						amount: numeral(this.amount).format('0.00'),
+						nonce,
+						deviceData,
+						savePaymentMethod: false, // save payment methods handled by braintree drop in UI
+					},
+				})
+				.then(kivaBraintreeResponse => {
+					// Check for errors in transaction
+					if (kivaBraintreeResponse.errors) {
+						this.$emit('updating-totals', false);
+						const errorCode = _get(
+							kivaBraintreeResponse,
+							'errors[0].code'
+						);
+						const errorMessage = _get(
+							kivaBraintreeResponse,
+							'errors[0].message'
+						);
+						const standardErrorCode = `(Braintree error: ${errorCode})`;
+						const standardError = `There was an error processing your payment.
 					Please try again. ${standardErrorCode}`;
 
-					// Payment method failed, unselect attempted payment method
-					this.$refs.braintreeDropInInterface.btDropinInstance.clearSelectedPaymentMethod();
-					// Potential error message: 'Transaction failed. Please select a different payment method.';
+						// Payment method failed, unselect attempted payment method
+						this.$refs.braintreeDropInInterface.btDropinInstance.clearSelectedPaymentMethod();
+						// Potential error message: 'Transaction failed. Please select a different payment method.';
 
-					this.$showTipMsg(standardError, 'error');
+						this.$showTipMsg(standardError, 'error');
 
-					// Fire specific exception to Snowplow
-					this.$kvTrackEvent('basket', 'DropIn Payment Error', `${errorCode}: ${errorMessage}`);
+						// Fire specific exception to Snowplow
+						this.$kvTrackEvent(
+							'basket',
+							'DropIn Payment Error',
+							`${errorCode}: ${errorMessage}`
+						);
 
-					// exit
-					return kivaBraintreeResponse;
-				}
+						// exit
+						return kivaBraintreeResponse;
+					}
 
-				// Transaction is complete
-				const transactionId = _get(
-					kivaBraintreeResponse,
-					'data.shop.doNoncePaymentDepositAndCheckout'
-				);
-				// redirect to thanks with KIVA transaction id
-				if (transactionId) {
-					// fire BT Success event
-					this.$kvTrackEvent(
-						'basket',
-						`${paymentType} Braintree DropIn Payment`,
-						'Success',
-						transactionId,
-						transactionId
+					// Transaction is complete
+					const transactionId = _get(
+						kivaBraintreeResponse,
+						'data.shop.doNoncePaymentDepositAndCheckout'
 					);
-					// Complete transaction handles additional analytics + redirect
-					this.$emit('complete-transaction', transactionId);
-				}
-				return kivaBraintreeResponse;
-			});
+					// redirect to thanks with KIVA transaction id
+					if (transactionId) {
+						// fire BT Success event
+						this.$kvTrackEvent(
+							'basket',
+							`${paymentType} Braintree DropIn Payment`,
+							'Success',
+							transactionId,
+							transactionId
+						);
+						// Complete transaction handles additional analytics + redirect
+						this.$emit('complete-transaction', transactionId);
+					}
+					return kivaBraintreeResponse;
+				});
 		},
 	},
 };
@@ -287,7 +335,6 @@ export default {
 	}
 
 	.checkbox {
-
 		@include small-text();
 
 		font-weight: $global-weight-normal;
