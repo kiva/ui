f90fc6dc9ce411d8fc940789e6d2358d2f1a1551
Ryan Ludwig <ryanl@kiva.org>
2021-03-25 18:46:04 -0700

    Add a log util

--- server/util/memJsUtils.js
+++ server/util/memJsUtils.js
@@ -1,3 +1,5 @@
+const log = require('./log');
+
 module.exports = {
 	getFromCache: (key, cache) => {
 		return new Promise(resolve => {
@@ -9,11 +11,7 @@ module.exports = {
 						message: `MemJS Error Getting ${key}, Error: ${error}`
 					}));
 				}
-				console.info(JSON.stringify({
-					meta: {},
-					level: 'info',
-					message: `MemJS Success Getting ${key}`
-				}));
+				log(`MemJS Success Getting ${key}`);
 				resolve(data);
 			});
 		});
@@ -22,19 +20,11 @@ module.exports = {
 		return new Promise((resolve, reject) => {
 			cache.set(key, value, { expires }, (error, success) => {
 				if (error) {
-					console.error(JSON.stringify({
-						meta: {},
-						level: 'error',
-						message: `MemJS Error Setting Cache for ${key}, Error: ${error}`
-					}));
+					log(`MemJS Error Setting Cache for ${key}, Error: ${error}`, 'error');
 					reject();
 				}
 				if (success) {
-					console.info(JSON.stringify({
-						meta: {},
-						level: 'info',
-						message: `MemJS Success Setting Cache for ${key}, Success: ${success}`
-					}));
+					log(`MemJS Success Setting Cache for ${key}, Success: ${success}`);
 					resolve();
 				}
 			});
