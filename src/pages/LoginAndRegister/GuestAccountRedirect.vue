abbaee1f2e0a115b9d3648fe6839efadcd6c93fb
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2021-04-01 13:29:49 -0700

    GROW-555 Ensure query params are included in doneUrl

--- src/pages/LoginAndRegister/GuestAccountRedirect.vue
+++ src/pages/LoginAndRegister/GuestAccountRedirect.vue
@@ -22,20 +22,25 @@ export default {
 				}`,
 			}).then(({ data }) => {
 				// Redirect to /checkout if there are items in the basket, otherwise /portfolio
-				const finalUrl = data?.shop?.nonTrivialItemCount > 0 ? '/checkout' : '/portfolio';
+				const path = data?.shop?.nonTrivialItemCount > 0 ? '/checkout' : '/portfolio';
 				// Add claimed=1 to the url to show a confirmation tip message on the page
 				const query = { claimed: 1 };
-
-				let redirectPath = finalUrl;
+				const queryString = Object.keys(query)
+					.map(key => `${key}=${query[key]}`)
+					.join('&');
 
 				// Check to see if user is authenticated
 				if (!data?.my?.userAccount?.id) {
-					redirectPath = '/ui-login';
-					query.login_hint = 'login';
-					query.doneUrl = finalUrl;
+					return Promise.reject({
+						path: '/ui-login',
+						query: {
+							login_hint: 'login',
+							doneUrl: `${path}?${queryString}`,
+						},
+					});
 				}
 				return Promise.reject({
-					path: redirectPath,
+					path,
 					query,
 				});
 			});
