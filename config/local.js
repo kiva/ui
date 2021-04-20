5f7d000762723ec9964b39b8bf766b716424dd4e
go-bears <melissa.fabros@gmail.com>
2021-04-20 11:08:48 -0700

    point local server to dev graphql endpoint

--- config/local.js
+++ config/local.js
@@ -5,14 +5,20 @@ module.exports = merge(base, {
 	app: {
 		host: 'localhost',
 		publicPath: '/',
+<<<<<<< HEAD
 		photoPath: 'https://www-kiva-org.freetls.fastly.net/img/',
 		graphqlUri: 'https://marketplace-api.k1.kiva.org/graphql',
+||||||| parent of ba7262d4 (point local server to dev graphql endpoint)
+		graphqlUri: 'https://marketplace-api.k1.kiva.org/graphql',
+=======
+		graphqlUri: 'https://marketplace-api.dk1.kiva.org/graphql',
+>>>>>>> ba7262d4 (point local server to dev graphql endpoint)
 		auth0: {
 			enable: false,
 		},
 	},
 	server: {
-		graphqlUri: 'https://marketplace-api.k1.kiva.org/graphql',
+		graphqlUri: 'https://marketplace-api.dk1.kiva.org/graphql',
 		sessionUri: '',
 		memcachedEnabled: false,
 		memcachedServers: '',
