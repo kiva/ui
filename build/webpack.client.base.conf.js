6ec5a8f767326635aa9ac23e9dec4ac835b9ed97
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2021-05-03 16:16:22 -0700

    Remove mini-css-extract-plugin

--- build/webpack.client.base.conf.js
+++ build/webpack.client.base.conf.js
@@ -33,16 +33,10 @@ module.exports = merge(baseWebpackConfig, {
 			prefix: 'icon-',
 		}),
 		...(isProd ? [] : [new HardSourceWebpackPlugin.ExcludeModulePlugin([
-			{
-				// HardSource works with mini-css-extract-plugin but due to how
-				// mini-css emits assets, assets are not emitted on repeated builds with
-				// mini-css and hard-source together. Ignoring the mini-css loader
-				// modules, but not the other css loader modules, excludes the modules
-				// that mini-css needs rebuilt to output assets every time.
-				// The above is also true for .woff font files, jpgs/pngs referenced in
-				// css, and the SVG sprite created by svgXhr
-				test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
-			},
+			// Due to how some loaders emit assets, certain assets are not emitted
+			// on repeated builds with those loaders and hard-source together.
+			// This is true for .woff font files, jpgs/pngs referenced in
+			// css, and the SVG sprite created by svgXhr
 			{
 				test: /url-loader.*woff|url-loader.*png|url-loader.*jpg/,
 			},
