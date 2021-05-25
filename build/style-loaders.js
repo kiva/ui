a69ae9acfbbb71fda2ea93c27f88e0b6b0907544
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2021-05-24 17:58:23 -0700

    VUE-519 Update css dependencies to prep for tailwind install

--- build/style-loaders.js
+++ build/style-loaders.js
@@ -1,20 +1,21 @@
-// If document is defined, dart-sass will attempt to run as if it were in the browser. However, some
-// code editors expose document, so dart-sass will sometimes throw errors when this config is loaded
+// If document is defined, sass will attempt to run as if it were in the browser. However, some
+// code editors expose document, so sass will sometimes throw errors when this config is loaded
 // by editor plugins. We won't use webpack in a browser, so to resolve the issue we can just check
-// for document before attempting to load dart-sass.
+// for document before attempting to load sass.
 var isNode = typeof document === 'undefined';
-var sass = isNode ? require('dart-sass') : function() {};
+var sass = isNode ? require('sass') : function() {};
 
 module.exports = [
 	{ loader: 'css-loader' },
 	{
 		loader: 'postcss-loader',
 		options: {
-			ident: 'postcss',
-			plugins: (loader) => [
-				require('autoprefixer')(),
-				require('cssnano')()
-			]
+			postcssOptions: {
+				plugins: [
+					require('autoprefixer'),
+					require('cssnano')
+				]
+			}
 		}
 	},
 	{
