6ae22f3ef3bde8904a0a51ea466f159ebd3dcd29
Joshua <joshuadstarkey@gmail.com>
2021-06-01 15:20:05 -0700

    feat(style): borrower profile removing inline styles

--- src/pages/BorrowerProfile/BorrowerProfile.vue
+++ src/pages/BorrowerProfile/BorrowerProfile.vue
@@ -2,14 +2,10 @@
 	<www-page id="borrower-profile">
 		<div class="container">
 			<div class="grid grid-cols-12 gap-x-2">
-				<div class="col-span-12 md:col-start-2 md:col-span-10 lg:col-span-7"
-					style="background-color:lightgreen;"
-				>
+				<div class="col-span-12 md:col-start-2 md:col-span-10 lg:col-span-7">
 					Borrower details
 				</div>
-				<div class="col-span-12 md:col-start-6 md:col-span-6 lg:col-span-5"
-					style="background-color:lightblue;"
-				>
+				<div class="col-span-12 md:col-start-6 md:col-span-6 lg:col-span-5">
 					Powered by lenders info
 				</div>
 			</div>
