86b8b5859c06298c90f6f1a989ac157a164d30eb
Prince Mabandla <princem@kiva.org>
2021-06-03 15:42:16 -0700

    GROW-659 Accepting name as prop

--- src/components/BorrowerProfile/BorrowerName.vue
+++ src/components/BorrowerProfile/BorrowerName.vue
@@ -5,40 +5,13 @@
 </template>
 
 <script>
-import gql from 'graphql-tag';
 
 export default {
-	inject: ['apollo', 'cookieStore'],
-	data() {
-		return {
-			name: '',
-		};
-	},
-	apollo: {
-		query: gql`
-			query borrowerName($loanId: Int!) {
-				lend {
-					loan(id: $loanId) {
-						id
-						name
-					}
-				}
-			}
-		`,
-		preFetch: true,
-		preFetchVariables({ route }) {
-			return {
-				loanId: route?.params?.id ?? 0,
-			};
-		},
-		variables() {
-			return {
-				loanId: this.$route?.params?.id ?? 0,
-			};
-		},
-		result({ data }) {
-			this.name = data?.lend?.loan?.name ?? '';
-		},
-	},
+	props: {
+		name: {
+			type: String,
+			default: ''
+		}
+	}
 };
 </script>
