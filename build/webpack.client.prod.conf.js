2b5ed64406e3cdee338d5c7c315ab75c003f659e
Matt Stover <mcs@mcstover.com>
2021-06-03 11:10:51 -0700

    Remove commented out code

--- build/webpack.client.prod.conf.js
+++ build/webpack.client.prod.conf.js
@@ -2,15 +2,7 @@ process.env.NODE_ENV = 'production'
 
 const { merge } = require('webpack-merge');
 var baseWebpackConfig = require('./webpack.client.base.conf');
-// var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(baseWebpackConfig, {
-	mode: 'production',
-	plugins: [
-		// new BundleAnalyzerPlugin({
-		// 	analyzerMode: 'disabled',
-		// 	generateStatsFile: true,
-		// 	statsFilename: 'client-bundle-stats.json'
-		// })
-	]
+	mode: 'production'
 });
