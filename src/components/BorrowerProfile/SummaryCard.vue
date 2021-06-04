afc2665342de326a4311ccc7ec3c8cd85230a147
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2021-06-04 13:18:11 -0700

    GROW-658 Borrower avatar and SummaryCard

---
+++ src/components/BorrowerProfile/SummaryCard.vue
@@ -0,0 +1,119 @@
+<template>
+	<section class="flex flex-wrap bg-gray-100 p-2.5 md:bg-white md:p-3 md:rounded lg:p-4">
+		<borrower-avatar
+			class="flex-none w-8 h-8 mr-1.5 mb-1.5 md:w-9 md:h-9 md:mr-3 md:mb-3 lg:w-10 lg:h-10 lg:mr-4 lg:mb-4"
+			:alt="`Small, circular photograph of ${name}`"
+			:fallback="fallbackImage"
+			:sources="images"
+		/>
+		<div class="flex-auto">
+			<borrower-name :name="name" />
+			<!-- fundraising progress placeholder  -->
+		</div>
+		<loan-use
+			class="flex-none w-full"
+			:borrower-count="borrowerCount"
+			:loan-amount="loanAmount"
+			:name="name"
+			:status="status"
+			:use="use"
+		/>
+		<!-- location/sector placeholder class="flex-auto" -->
+	</section>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+import BorrowerAvatar from './BorrowerAvatar';
+import BorrowerName from './BorrowerName';
+import LoanUse from './LoanUse';
+
+export default {
+	inject: ['apollo', 'cookieStore'],
+	components: {
+		BorrowerAvatar,
+		BorrowerName,
+		LoanUse,
+	},
+	data() {
+		return {
+			borrowerCount: 0,
+			fallbackImage: '',
+			images: [],
+			loanAmount: '0',
+			name: '',
+			status: '',
+			use: '',
+		};
+	},
+	apollo: {
+		query: gql`
+			query borrowerAvatar($loanId: Int!) {
+				lend {
+					loan(id: $loanId) {
+						id
+						borrowerCount
+						image {
+							id
+							urlSm1x: url(customSize: "s64fz50")
+							urlSm2x: url(customSize: "s128fz50")
+							urlMd1x: url(customSize: "s72fz50")
+							urlMd2x: url(customSize: "s144fz50")
+							urlLg1x: url(customSize: "s80fz50")
+							urlLg2x: url(customSize: "s160fz50")
+						}
+						loanAmount
+						name
+						status
+						use
+					}
+				}
+			}
+		`,
+		preFetch: true,
+		preFetchVariables({ route }) {
+			return {
+				loanId: Number(route?.params?.id ?? 0),
+			};
+		},
+		variables() {
+			return {
+				loanId: Number(this.$route?.params?.id ?? 0),
+			};
+		},
+		result({ data }) {
+			this.borrowerCount = data?.lend?.loan?.borrowerCount ?? 0;
+			this.fallbackImage = data?.lend?.loan?.image?.urlSm1x ?? '';
+			this.loanAmount = data?.lend?.loan?.loanAmount ?? '0';
+			this.name = data?.lend?.loan?.name ?? '';
+			this.status = data?.lend?.loan?.status ?? '';
+			this.use = data?.lend?.loan?.use ?? '';
+
+			// Build images array
+			this.images = [
+				{
+					media: 'min-width: 1024px', // large
+					urls: {
+						'2x': data?.lend?.loan?.image?.urlLg2x ?? '',
+						'1x': data?.lend?.loan?.image?.urlLg1x ?? '',
+					},
+				},
+				{
+					media: 'min-width: 734px', // medium
+					urls: {
+						'2x': data?.lend?.loan?.image?.urlMd2x ?? '',
+						'1x': data?.lend?.loan?.image?.urlMd1x ?? '',
+					},
+				},
+				{
+					media: 'min-width: 0',
+					urls: {
+						'2x': data?.lend?.loan?.image?.urlSm2x ?? '',
+						'1x': data?.lend?.loan?.image?.urlSm1x ?? '',
+					},
+				},
+			];
+		},
+	},
+};
+</script>
