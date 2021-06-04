32e8006effb15616c536c58965f5e6b25dc1a63d
Joshua <joshuadstarkey@gmail.com>
2021-06-03 18:45:21 -0700

    build: Loan use component for borrower profile

---
+++ src/components/BorrowerProfile/LoanUse.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<span class="text-h2 text-gray-800">
+			{{ filteredLoanUse }}
+		</span>
+	</div>
+</template>
+
+<script>
+const loanUseFilter = require('../../plugins/loan-use-filter');
+
+export default {
+	props: {
+		loanAmount: {
+			type: String,
+			default: '',
+		},
+		use: {
+			type: String,
+			default: '',
+		},
+		status: {
+			type: String,
+			default: '',
+		},
+		name: {
+			type: String,
+			default: '',
+		},
+		borrowerCount: {
+			type: Number,
+			default: 1,
+		}
+	},
+	computed: {
+		filteredLoanUse() {
+			// eslint-disable-next-line max-len
+			return loanUseFilter(this.use, this.name, this.status, this.loanAmount, this.borrowerCount, 100);
+		},
+	}
+};
+
+</script>
