a6670c919df4efde3fb3aa4655ffe17f6405d56f
Prince Mabandla <princem@kiva.org>
2021-06-10 09:29:49 -0700

    GROW-634 loan description

--- src/components/BorrowerProfile/LoanDescription.vue
+++ src/components/BorrowerProfile/LoanDescription.vue
@@ -1,12 +1,77 @@
 <template>
-	<p class="tw-text-h2">
-		{{ loanDescriptionFiltered }}
-	</p>
+	<section>
+		<p 
+			class="tw-text-h2"
+		>
+			More about this loan
+		</p>
+		<div 
+			v-if="partnerName"
+		>
+			<div>
+				{{ moreInfoAboutLoan }}
+			</div>
+			<div 
+				v-if="loanAlertText"
+			>
+				<p  
+					class="tw-text-h3"
+				>
+					About {{ partnerName }}:
+				</p>
+				<p
+					v-for="(paragraph, index) in loanAlertTextParagraphs"
+					:key="index"
+					class="tw-text"
+				>
+					{{ paragraph }}
+				</p>
+			</div>
+			<div 
+				v-if="dualStatementNote"
+			>
+				<p 
+					class="tw-text-h3"
+				>
+					Important Note About This Loan
+				</p>
+				<p>
+					{{ dualStatementNote }}
+				</p>
+			</div>
+		</div>
+		<div 
+			v-if="!partnerName"
+		>
+			<p 
+				class="tw-text-h3"
+			>
+				Business Description
+			</p>
+			<p
+				v-for="(paragraph, index) in businessDescriptionParagraphs"
+				:key="index"
+				class="tw-text"
+			>
+				{{ paragraph }}
+			</p>
+			<p 
+				class="tw-text-h3"
+			>
+				What is the purpose of this loan?
+			</p>
+			<p
+				v-for="(paragraph, index) in purposeParagraphs"
+				:key="index"
+				class="tw-text"
+			>
+				{{ paragraph }}
+			</p>
+		</div>
+	</section>
 </template>
 
 <script>
-const loanDescriptionFilter = require('../../plugins/loan-description-filter');
-
 export default {
 	props: {
 		partnerName: {
@@ -17,28 +82,36 @@ export default {
 			type: String,
 			default: '',
 		},
-		moreAboutFieldPartner: {
-			type: String,
-			default: '',
-		},
 		dualStatementNote: {
 			type: String,
 			default: '',
 		},
-		directBusinessDescription: {
+		businessDescription: {//directBusinessDescription
 			type: String,
 			default: '',
 		},
-		directLoanPurpose: {
+		purpose: {//directLoanPurpose
 			type: String,
 			default: '',
-		}
+		},
+		loanAlertText: {//moreAboutFieldPartner
+			type: String,
+			default: '',
+		},
 	},
 	computed: {
-		loanDescriptionFiltered() {
-			// eslint-disable-next-line max-len
-			return loanUseFilter(this.use, this.name, this.status, this.loanAmount, this.borrowerCount);
+		loanAlertTextParagraphs() {
+			return this.toParagraphs(this.loanAlertText);
 		},
+		businessDescriptionParagraphs() {
+			return this.toParagraphs(this.businessDescription);
+		},
+		purposeParagraphs() {
+			return this.toParagraphs(this.purpose);
+		},
+		toParagraphs(text) {
+			return String(text).split(/(\r\n\r\n|\n\n|\n \n|<br>)/);
+		}
 	}
 };
 
