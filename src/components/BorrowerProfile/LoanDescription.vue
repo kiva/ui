7b1a26b4e553b153e01f87808c93ea851f25241b
Prince Mabandla <princem@kiva.org>
2021-06-09 19:41:16 -0700

    GROW-634 loan description

---
+++ src/components/BorrowerProfile/LoanDescription.vue
@@ -0,0 +1,45 @@
+<template>
+	<p class="tw-text-h2">
+		{{ loanDescriptionFiltered }}
+	</p>
+</template>
+
+<script>
+const loanDescriptionFilter = require('../../plugins/loan-description-filter');
+
+export default {
+	props: {
+		partnerName: {
+			type: String,
+			default: '',
+		},
+		moreInfoAboutLoan: {
+			type: String,
+			default: '',
+		},
+		moreAboutFieldPartner: {
+			type: String,
+			default: '',
+		},
+		dualStatementNote: {
+			type: String,
+			default: '',
+		},
+		directBusinessDescription: {
+			type: String,
+			default: '',
+		},
+		directLoanPurpose: {
+			type: String,
+			default: '',
+		}
+	},
+	computed: {
+		loanDescriptionFiltered() {
+			// eslint-disable-next-line max-len
+			return loanUseFilter(this.use, this.name, this.status, this.loanAmount, this.borrowerCount);
+		},
+	}
+};
+
+</script>
