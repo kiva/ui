585ec6736712959c4771125e1adc0de16d1a2134
Joshua <joshuadstarkey@gmail.com>
2021-06-10 10:13:59 -0700

    build: LendCta screen size styles mostly applied

--- src/components/BorrowerProfile/LendCta.vue
+++ src/components/BorrowerProfile/LendCta.vue
@@ -1,29 +1,76 @@
 <template>
-	<span>
-		<div class="rounded bg-white pl-4">
-			<p class="text-h3 pt-3 mb-3">{{ lgScreenheadline }}</p>
-			<kv-ui-select
-				id="LoanAmountDropdown"
-				class="pr-2.5 mb-3"
-				v-model="selectedOption"
-			>
-				<option
-					v-for="price in prices"
-					:key="price"
-					:value="price"
+	<span class="tw-z-1">
+		<!-- eslint-disable-next-line max-len -->
+		<div class="
+					tw-bg-brand
+					tw-fixed
+					tw-left-0
+					md:tw-relative
+					md:tw-left-unset
+					tw-bottom-0
+					tw-right-0
+					md:tw-rounded-b
+					md:tw-rounded-t-none
+					lg:tw-rounded-t
+					tw-bg-white
+					tw-border
+					tw-bt-1
+					tw-border-gray-300
+					md:tw-border-none
+					tw-pt-1
+					tw-pl-2.5
+					tw-pr-2.5
+					lg:tw-pl-4
+					lg:tw-pr-4"
+		>
+			<p class="tw-text-h3 tw-pt-3 lg:tw-mb-3 tw-hidden lg:tw-inline-block">
+				{{ lgScreenheadline }}
+			</p>
+			<span class="tw-flex">
+				<kv-ui-select
+					id="LoanAmountDropdown"
+					class="tw-pr-2.5 lg:tw-mb-3"
+					v-model="selectedOption"
 				>
-					${{ price }}
-				</option>
-			</kv-ui-select>
-			<kv-ui-button
-				@add-to-basket="$emit('add-to-basket', $event)"
-			>
-				{{ ctaButtonText }}
-			</kv-ui-button>
+					<option
+						v-for="price in prices"
+						:key="price"
+						:value="price"
+					>
+						${{ price }}
+					</option>
+				</kv-ui-select>
+				<kv-ui-button
+					class="tw-inline-flex tw-flex-1"
+					@add-to-basket="$emit('add-to-basket', $event)"
+				>
+					{{ ctaButtonText }}
+				</kv-ui-button>
+			</span>
 		</div>
-		<div class="rounded bg-white h-5 text-h4 justify-center mt-1 align-middle flex">
+		<!-- eslint-disable-next-line max-len -->
+		<div class="tw-bg-brand-300
+					tw-fixed
+					md:tw-static
+					tw-left-3
+					tw-right-3
+					tw-bottom-10
+					tw-rounded
+					tw-bg-white
+					tw-h-5
+					tw-text-h4
+					tw-justify-center
+					tw-mt-1
+					md:tw-mr-3
+					tw-flex
+					md:tw-w-3/5
+					lg:tw-w-full
+					md:tw-float-right
+					lg:tw-float-none
+					tw-p-1"
+		>
 			<kv-material-icon
-				class="h-2.5 pointer-events-none"
+				class="tw-h-2.5 tw-pointer-events-none"
 				:icon="mdiLightningBolt"
 			/>
 			powered by {{ numLenders }} lenders
@@ -58,20 +105,20 @@ export default {
 			default: '',
 		},
 		reservedAmount: {
-			type: Number,
-			default: 0,
+			type: String,
+			default: '',
 		},
 		lentPreviously: {
 			type: Boolean,
 			default: false,
 		},
 		amountInBasket: {
-			type: Number,
-			default: 0,
+			type: String,
+			default: '',
 		},
 		minNoteSize: {
-			type: Number,
-			default: 25,
+			type: String,
+			default: '',
 		},
 		status: {
 			type: String,
