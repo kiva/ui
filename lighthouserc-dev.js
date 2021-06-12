dd3e758be20e0d0c7f1dad5ee4458d64d6fe42f2
Matt Stover <mcs@mcstover.com>
2021-06-12 09:59:16 -0700

    Increase wait times and see if lbc can be reached

--- lighthouserc-dev.js
+++ lighthouserc-dev.js
@@ -2,19 +2,21 @@ module.exports = {
 	ci: {
 		collect: {
 			settings: {
-				onlyCategories: ['accessibility', 'best-practices', 'performance', 'seo']
+				onlyCategories: ['accessibility', 'best-practices', 'performance', 'seo'],
+				maxWaitForFcp: 10000,
+				maxWaitForLoad: 10000,
 			},
 			startServerCommand: 'npm start -- --config=local',
 			startServerReadyPattern: 'server started at',
 			url: [
 				'http://localhost:8888/',
 				'http://localhost:8888/lend/filter',
-				// 'http://localhost:8888/lend-by-category',
 				'http://localhost:8888/lend-by-category/women',
 				'http://localhost:8888/lend-by-category/arts',
 				'http://localhost:8888/ui-site-map',
 				'http://localhost:8888/cc/kiva-universal',
-				'http://localhost:8888/lp/support-refugees'
+				'http://localhost:8888/lp/support-refugees',
+				'http://localhost:8888/lend-by-category'
 			],
 			numberOfRuns: 5,
 		},
