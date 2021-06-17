f0772ee1effca31ada7e8037b8e42fb5e15d88d8
Prince Mabandla <princem@kiva.org>
2021-06-17 14:58:55 -0700

    GROW-636 More about this loan

--- src/components/BorrowerProfile/LoanDescription.vue
+++ src/components/BorrowerProfile/LoanDescription.vue
@@ -33,14 +33,23 @@
 
 			<section>
 				<!--
-				Previous Loan sections omitted. Needs separate ticket
+				"Previous Loan sections" omitted. Needs separate ticket
 				-->
 			</section>
 
 			<section v-if="storyTranslation">
-				<img v-if="reviewerImageLink" src="{{ reviewerImageLink }}" alt="Translator profile picture"/>
+				<img v-if="reviewerImageLink" src="{{ reviewerImageLink }}" alt="Translator profile picture">
 				<p class="tw-text-base">
-					Translated from {{ language }}<span v-if="reviewerName"> by {{ reviewerLink }} {{ reviewerName }}</span>.
+					Translated from {{ language }}
+					<span v-if="reviewerName">
+						by
+						<a
+							href="/work-with-us/internvolunteers"
+							title="Learn more about volunteering at Kiva"
+						>
+							Kiva volunteer
+						</a> {{ reviewerName }}
+					</span>.
 					<a>
 						View original language description.
 					</a>
@@ -60,91 +69,87 @@
 </template>
 
 <script>
-import _forEach from 'lodash/forEach';
 
 export default {
-		props: {
-			partnerName: { // LoanPartner.partnerName
-				type: String,
-				default: '',
-			},
-			borrowerOrGroupName: { // LoanBasic.name
-				type: String,
-				default: '',
-			},
-			anonymizationLevel: { // LoanBasic.anonymizationLevel
-				type: String,
-				default: '',
-			},
-			storyDescription: { // LoanBasic.description
-				type: String,
-				default: '',
-			},
-			descriptionInOriginalLanguage: { // LoanBasic.descriptionInOriginalLanguage
-				type: String,
-				default: '',
-			},
-			originalLanguage: { //LoanBasic.originalLanguage
-				type: Object,
-				default: () => {},
-			},
-			borrowerCount: { // LoanBasic.borrowerCount
-				type: String,
-				default: '',
-			},
-			borrowers: { // LoanBasic.borrowers
-				type: String,
-				default: '',
-			},
-			reviewer: { // LoanPartner.reviewer
-				type: Object,
-				default: () => {},
-			},
+	props: {
+		partnerName: { // LoanPartner.partnerName
+			type: String,
+			default: '',
 		},
-		computed: {
-			borrowersList() {
-				if(this.borrowerCount <= 1) {
-					return '';
-				}
-				let borrowersInThisGroup = "In this group: ";
-				_forEach(this.borrowers, borrower => {
-					borrowers_list += borrower.firstName;
-				});
-				return borrowersInThisGroup;
-			},
-			storyDescriptionParagraphs() {
-				return this.toParagraphs(this.storyDescription);
-			},
-			descriptionInOriginalLanguageParagraphs() {
-				return this.toParagraphs(this.descriptionInOriginalLanguage);
-			},
-			isAnonymizationLevelFull() {
-				return this.anonymizationLevel === 'full';
-			},
-			storyTranslation() {
-				return this.isPartnerLoan() && parseInt(this.originalLanguage?.id ?? 0, 10) !== 1;
-			},
-			language() {
-				return this.originalLanguage?.name ?? '';
-			},
-			isPartnerLoan() {
-				return this.partnerName !== '';
-			},
-			reviewerName() {
-				return this.reviewer?.bylineName ?? '';
-			},
-			reviewerLink() {
-				return '';
-			},
-			reviewerImageLink() {
-				return '';
-			},
+		borrowerOrGroupName: { // LoanBasic.name
+			type: String,
+			default: '',
 		},
-		methods: {
-			toParagraphs(text) {
-				return String(text).split(/(\r\n\r\n|\n\n|\n \n|<br>)/);
+		anonymizationLevel: { // LoanBasic.anonymizationLevel
+			type: String,
+			default: '',
+		},
+		storyDescription: { // LoanBasic.description
+			type: String,
+			default: '',
+		},
+		descriptionInOriginalLanguage: { // LoanBasic.descriptionInOriginalLanguage
+			type: String,
+			default: '',
+		},
+		originalLanguage: { // LoanBasic.originalLanguage
+			type: Object,
+			default: () => {},
+		},
+		borrowerCount: { // LoanBasic.borrowerCount
+			type: String,
+			default: '',
+		},
+		borrowers: { // LoanBasic.borrowers
+			type: String,
+			default: '',
+		},
+		reviewer: { // LoanPartner.reviewer
+			type: Object,
+			default: () => {},
+		},
+	},
+	computed: {
+		borrowersList() {
+			if (this.borrowerCount <= 1) {
+				return '';
 			}
+			let borrowersInThisGroup = 'In this group: ';
+			this.borrowers.forEach(borrower => {
+				borrowersInThisGroup += borrower.firstName;
+			});
+			return borrowersInThisGroup;
+		},
+		storyDescriptionParagraphs() {
+			return this.toParagraphs(this.storyDescription);
+		},
+		descriptionInOriginalLanguageParagraphs() {
+			return this.toParagraphs(this.descriptionInOriginalLanguage);
+		},
+		isAnonymizationLevelFull() {
+			return this.anonymizationLevel === 'full';
+		},
+		storyTranslation() {
+			return this.isPartnerLoan() && parseInt(this.originalLanguage?.id ?? 0, 10) !== 1;
+		},
+		language() {
+			return this.originalLanguage?.name ?? '';
+		},
+		isPartnerLoan() {
+			return this.partnerName !== '';
+		},
+		reviewerName() {
+			return this.reviewer?.bylineName ?? '';
+		},
+		reviewerImageLink() {
+			return this.reviewer?.image?.url ?? '';
+		},
+	},
+	methods: {
+		toParagraphs(text) {
+			return String(text).split(/(\r\n\r\n|\n\n|\n \n|<br>)/);
 		}
-	};
+	}
+};
 
 </script>
