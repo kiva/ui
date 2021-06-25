d2f968a2b9fb048b148abab6e5777748f40012e0
Dymond Allen <dymonda@interns.kiva.org>
2021-06-25 13:14:21 -0700

    Created the Lend Filter Alpha Page

---
+++ src/pages/Lend/FilterAlpha/LendFilterAlpha.vue
@@ -0,0 +1,35 @@
+<template>
+	<www-page
+		id="lend-filter-alpha"
+		:header-theme="headerTheme"
+		:main-class="'tw-bg-gray-50'"
+	>
+		<article class="tw-relative">
+			<div class="tw-relative" style="max-height: 460px;">
+				<div class="tw-absolute tw-top-0 tw-h-full tw-w-full tw-overflow-hidden">
+				</div>
+			</div>
+			<div class="lg:tw-absolute lg:tw-w-full lg:tw-h-full lg:tw-top-0 lg:tw-pt-8">
+			</div>
+		</article>
+		<!-- <aside>Similar loans</aside> -->
+	</www-page>
+</template>
+
+<script>
+import { lightHeader } from '@/util/siteThemes';
+
+import WwwPage from '@/components/WwwFrame/WwwPage';
+
+export default {
+	components: {
+		WwwPage,
+	},
+	data() {
+		return {
+			headerTheme: lightHeader,
+			loanId: Number(this.$route.params.id || 0),
+		};
+	},
+};
+</script>
