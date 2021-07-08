44c4809b5c9a51012ede2505d81e8552f72ff4c6
Ryan Ludwig <ryanl@kiva.org>
2021-07-08 09:54:52 -0700

    refactor: remove lodash _get

--- src/components/WwwFrame/TheTipMessage.vue
+++ src/components/WwwFrame/TheTipMessage.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script>
-import _get from 'lodash/get';
 import DOMPurify from 'dompurify';
 import tipMessageData from '@/graphql/query/tipMessage/tipMessageData.graphql';
 import KvTipMessage from '@/components/Kv/KvTipMessage';
@@ -18,13 +17,13 @@ export default {
 		preFetch: true,
 		result({ data }) {
 			if (this.$refs.tip) {
-				const showing = _get(data, 'tip.visible');
+				const showing = data?.tip?.visible ?? false;
 
 				if (showing) {
-					const message = _get(data, 'tip.message');
+					const message = data?.tip?.message ?? '';
 					const safeMessage = DOMPurify.sanitize(message, { ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'a'] });
-					const messageType = _get(data, 'tip.type');
-					const persist = _get(data, 'tip.persist');
+					const messageType = data?.tip?.type ?? '';
+					const persist = data?.tip?.persist ?? false;
 
 					this.$refs.tip.show(safeMessage, messageType, persist);
 				} else {
