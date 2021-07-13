421c8b44dc9c90e771d6b3ae7b5456115b8b6ea3
go-bears <melissa.fabros@gmail.com>
2021-07-12 20:34:26 -0700

    add fetch loans method to filter-alpha page
    add comments about limited loans

--- src/pages/Lend/FilterAlpha/LendFilterAlpha.vue
+++ src/pages/Lend/FilterAlpha/LendFilterAlpha.vue
@@ -18,18 +18,55 @@
 
 <script>
 import { lightHeader } from '@/util/siteThemes';
-
 import WwwPage from '@/components/WwwFrame/WwwPage';
+import flssLoanQuery from '@/graphql/query/flssLoansQuery.graphql';
 
 export default {
+	inject: ['apollo'],
 	components: {
 		WwwPage,
+	},
+	mounted() {
+		this.fetchLoans();
 	},
 	data() {
 		return {
 			headerTheme: lightHeader,
 			loanId: Number(this.$route.params.id || 0),
+			loanQueryFilters: () => {},
+			totalCount: 0,
+			loans: [],
+			zeroLoans: false,
 		};
+	},
+	methods: {
+		fetchLoans() {
+			this.apollo
+				.query({
+					query: flssLoanQuery,
+					variables: {
+						filterObject: this.loanQueryFilters,
+					},
+					fetchPolicy: 'network-only',
+				})
+				.then(({ data }) => {
+					const newLoans = data.fundraisingLoans?.values ?? [];
+					this.loans = newLoans;
+					// leaving console.log for sanity check
+					// flss query is defaulted to US loans currently to keep loans count
+					// manageable
+					console.log(newLoans);
+
+					const totalCount = data.fundraisingLoans.totalCount ?? 0;
+					this.totalCount = totalCount;
+					// leaving console.log for sanity check
+					console.log(totalCount);
+
+					if (this.totalCount === 0) {
+						this.zeroLoans = true;
+					}
+				});
+		},
 	},
 };
 </script>
