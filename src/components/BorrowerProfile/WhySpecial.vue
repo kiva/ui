e28319ebd55a4d5dd2d1c2d1e5fdfee15d9e1089
Matt Stover <mcs@mcstover.com>
2021-07-15 10:25:17 -0700

    Add placeholder content and introspection observer to load why special content.

--- src/components/BorrowerProfile/WhySpecial.vue
+++ src/components/BorrowerProfile/WhySpecial.vue
@@ -1,9 +1,14 @@
 <template>
 	<article>
+		<div v-if="loading" class="tw-w-full tw-my-5 md:tw-my-6 lg:tw-my-8">
+			<kv-loading-placeholder class="tw-w-full tw-mb-2 lg:tw-mb-3" :style="{height: '28px'}" />
+			<kv-loading-placeholder class="tw-mb-2" :style="{width: 60 + (Math.random() * 15) + '%', height: '28px'}" />
+		</div>
+
 		<h2 class="tw-sr-only">
 			Why this loan is special
 		</h2>
-		<p class="tw-text-h2 tw-my-5 md:tw-my-6 lg:tw-my-8">
+		<p v-if="!loading" class="tw-text-h2 tw-my-5 md:tw-my-6 lg:tw-my-8">
 			{{ fullWhySpecial }}
 		</p>
 	</article>
@@ -11,10 +16,16 @@
 
 <script>
 import gql from 'graphql-tag';
+import { createIntersectionObserver } from '@/util/observerUtils';
 import { formatWhySpecial } from '@/util/loanUtils';
+// TODO: replace the loading placeholder with component from kv-components when available.
+import KvLoadingPlaceholder from '@/components/Kv/KvLoadingPlaceholder';
 
 export default {
 	inject: ['apollo', 'cookieStore'],
+	components: {
+		KvLoadingPlaceholder,
+	},
 	props: {
 		loanId: {
 			type: Number,
@@ -23,6 +34,7 @@ export default {
 	},
 	data() {
 		return {
+			loading: true,
 			whySpecial: '',
 		};
 	},
@@ -48,6 +60,58 @@ export default {
 		result(result) {
 			this.whySpecial = result?.data?.lend?.loan?.whySpecial ?? '';
 		},
+	},
+	methods: {
+		createObserver() {
+			// Watch for this element being close to entering the viewport
+			this.observer = createIntersectionObserver({
+				targets: [this.$el],
+				rootMargin: '500px',
+				callback: entries => {
+					entries.forEach(entry => {
+						if (entry.target === this.$el && entry.intersectionRatio > 0) {
+							// This element is close to being in the viewport, so load the data.
+							// Because of the apollo cache it's safe to call this repeatedly.
+							this.loadData();
+						}
+					});
+				}
+			});
+			if (!this.observer) {
+				// Observer was not created, so call loadData right away as a fallback.
+				this.loadData();
+			}
+		},
+		destroyObserver() {
+			if (this.observer) {
+				this.observer.disconnect();
+			}
+		},
+		loadData() {
+			this.apollo.query({
+				query: gql`query whySpecial($loanId: Int!) {
+					lend {
+						loan(id: $loanId) {
+							id
+							whySpecial
+						}
+					}
+				}`,
+				variables: {
+					loanId: this.loanId
+				},
+			}).then(result => {
+				this.whySpecial = result?.data?.lend?.loan?.whySpecial ?? '';
+
+				this.loading = false;
+			});
+		},
+	},
+	mounted() {
+		this.createObserver();
+	},
+	beforeDestroy() {
+		this.destroyObserver();
 	},
 };
 </script>
