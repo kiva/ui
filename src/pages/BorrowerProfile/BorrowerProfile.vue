22978d6c544c68c57c5e5be44c4de65c6f2a4e07
Matt Stover <mcs@mcstover.com>
2021-07-15 17:28:19 -0700

    Apply z-index and position classes to ensure absolutely positioned elelements do not prevent interacting with other content.

--- src/pages/BorrowerProfile/BorrowerProfile.vue
+++ src/pages/BorrowerProfile/BorrowerProfile.vue
@@ -10,41 +10,41 @@
 					<hero-background />
 				</div>
 				<content-container class="md:tw-pt-6 lg:tw-pt-8">
-					<summary-card class="tw-relative lg:tw--mb-1.5" />
+					<summary-card class="tw-relative lg:tw--mb-1.5 tw-z-1" />
 				</content-container>
 			</div>
 			<div class="lg:tw-absolute lg:tw-w-full lg:tw-h-full lg:tw-top-0 lg:tw-pt-8">
 				<sidebar-container class="lg:tw-sticky lg:tw-top-12 lg:tw-mt-10 lg:tw-pb-8">
-					<lend-cta :loan-id="loanId" />
+					<lend-cta class="tw-z-2" :loan-id="loanId" />
 				</sidebar-container>
 			</div>
 			<content-container class="tw-mt-4 md:tw-mt-6 lg:tw-mt-8">
-				<loan-story class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8" :loan-id="loanId" />
+				<loan-story class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8 tw-relative" :loan-id="loanId" />
 			</content-container>
 			<div class="tw-bg-brand-50 tw-mb-5 md:tw-mb-6 lg:tw-mb-8">
 				<content-container>
-					<why-special :loan-id="loanId" />
+					<why-special class="tw-relative" :loan-id="loanId" />
 				</content-container>
 			</div>
 			<content-container>
-				<more-about-loan class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8" :loan-id="loanId" />
-				<borrower-country class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8" :loan-id="loanId" />
+				<more-about-loan class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8 tw-relative" :loan-id="loanId" />
+				<borrower-country class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8 tw-relative" :loan-id="loanId" />
 				<lenders-and-teams
 					key="lenders"
-					class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8"
+					class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8 tw-relative"
 					:loan-id="loanId"
 					display-type="lenders"
 				/>
 				<lenders-and-teams
 					key="teams"
-					class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8"
+					class="tw-mb-5 md:tw-mb-6 lg:tw-mb-8 tw-relative"
 					:loan-id="loanId"
 					display-type="teams"
 				/>
 			</content-container>
 			<div class="tw-bg-brand-50 tw-mb-5 md:tw-mb-6 lg:tw-mb-8">
 				<content-container>
-					<details-tabs :loan-id="loanId" name="bp-details" />
+					<details-tabs class="tw-relative" :loan-id="loanId" name="bp-details" />
 				</content-container>
 			</div>
 		</article>
