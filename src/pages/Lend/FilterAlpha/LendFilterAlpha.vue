a75193f18d7738d58b9113286361dd4a4f66a509
dymonda21 <85644244+dymonda21@users.noreply.github.com>
2021-07-20 11:49:35 -0700

    Created page layout for lend filter alpha (#3087)
    * Created page layout for lend filter alpha
    
    * Page layout for lend filter alpha
    
    * Created page layout for lend filter alpha
    
    * Created page layout for lend filter alpha page

--- src/pages/Lend/FilterAlpha/LendFilterAlpha.vue
+++ src/pages/Lend/FilterAlpha/LendFilterAlpha.vue
@@ -11,6 +11,75 @@
 			</div>
 			<div class="lg:tw-absolute lg:tw-w-full lg:tw-h-full lg:tw-top-0 lg:tw-pt-8">
 			</div>
+			<div>
+				<kv-page-container>
+					<h2 class="tw-mb-2">
+						Make a loan, change a life
+					</h2>
+					<h3 class="tw-mb-3">
+						Each Kiva loan helps people build a better future for themselves and their families.
+					</h3>
+					<kv-grid class="tw-grid-cols-2 md:tw-grid-cols-3">
+						<div class="tw-bg-gray-300 tw-text-left md:tw-text-center">
+							<p>Filters</p>
+						</div>
+
+						<div class="tw-col-span-2">
+							<div class="tw-bg-gray-300 tw-h-4">
+								Search Loans
+							</div>
+							<p>Loan Count</p>
+							<kv-grid class="tw-grid-rows-4">
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 1
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 2
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 3
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 4
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 5
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 6
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 7
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 8
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 9
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 10
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 11
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 12
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 13
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 14
+								</div>
+								<div class="tw-bg-gray-300 tw-h-4">
+									Loan Card 15
+								</div>
+							</kv-grid>
+						</div>
+					</kv-grid>
+				</kv-page-container>
+			</div>
 		</article>
 		<!-- <aside>Similar loans</aside> -->
 	</www-page>
@@ -20,11 +89,15 @@
 import { lightHeader } from '@/util/siteThemes';
 import WwwPage from '@/components/WwwFrame/WwwPage';
 import flssLoanQuery from '@/graphql/query/flssLoansQuery.graphql';
+import KvGrid from '~/@kiva/kv-components/vue/KvGrid';
+import KvPageContainer from '~/@kiva/kv-components/vue/KvPageContainer';
 
 export default {
 	inject: ['apollo'],
 	components: {
 		WwwPage,
+		KvGrid,
+		KvPageContainer,
 	},
 	mounted() {
 		this.fetchLoans();
