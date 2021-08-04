ca1786b351cdbcc9780bf90d3d59a327a75b60c7
go-bears <melissa.fabros@gmail.com>
2021-08-04 13:10:18 -0700

    initial commit for util unit test
    notes and code stubs from meeting with shua

---
+++ test/unit/flss/flssUtil.spec.js
@@ -0,0 +1,14 @@
+import * from '@/util/flssUtils';
+import { query } from 'express';
+
+
+// TODO
+// What to test?
+// 	Test that function calls the right endpoint
+// 	Test the function uses the right query
+
+// How to set up test?
+// 	Do I actually make a call to the public endpoint to make sure it returns data? or is
+// 	it okay just to check that i made the call the right endpoint.
+// 	How do I mock up a test for a call to an api endpoint?
+// 	How to use Jest?
---
+++ test/unit/specs/util/flssUtil.spec.js
@@ -0,0 +1,23 @@
+import * from '@/util/flssUtils';
+import { query } from 'express';
+
+
+// TODO
+// What to test?
+//
+// 	Test the function uses the right query, that it's using the right query
+
+// How to set up test?
+//
+// 	it okay just to check that i made the call the right endpoint.
+// make sure
+// 	How do I mock up a test for a call to an api endpoint?
+// 	How to use Jest?
+
+
+//Describe(flssutils)
+	// describe(fetchdata)
+	"it queries the currently fundraising loans"
+
+//	assignVersionSpy = jest.spyOn(expUtils, 'assignVersion');
+
