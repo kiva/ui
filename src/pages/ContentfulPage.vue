0e9b72fe04745554255417ef8e9dfd1c35ecc608
Eddie Ferrer <eddieferrer@user.noreply.github.com>
2021-09-08 11:22:28 -0700

    feat: Render contentful preview API results when preview query param is active for ContentfulPage component
    * Uses cookies to create an apollo link that automatically adds the preview variable to the apollo query when necessary
    VUE-671

--- src/pages/ContentfulPage.vue
+++ src/pages/ContentfulPage.vue
@@ -49,11 +49,11 @@ To use, simply create a route that defines contentfulPage in the meta data, e.g.
 },
 */
 
-import gql from 'graphql-tag';
 import { preFetchAll } from '@/util/apolloPreFetch';
 import { processPageContent } from '@/util/contentfulUtils';
 import logFormatter from '@/util/logFormatter';
 import * as siteThemes from '@/util/siteThemes';
+import contentfulEntries from '@/graphql/query/contentfulEntries.graphql';
 
 // Page frames
 const WwwPage = () => import('@/components/WwwFrame/WwwPage');
@@ -88,13 +88,6 @@ const MonthlyGoodSelectorWrapper = () => import('@/components/MonthlyGood/Monthl
 const KvFrequentlyAskedQuestions = () => import('@/components/Kv/KvFrequentlyAskedQuestions');
 
 const TestimonialCards = () => import('@/components/Contentful/TestimonialCards');
-
-// Query for getting contentful page data
-const pageQuery = gql`query contentfulPage($key: String) {
-	contentful {
-		entries(contentType: "page", contentKey: $key)
-	}
-}`;
 
 // Get the Contentful Page data from the data of an Apollo query result
 const getPageData = data => {
@@ -236,23 +229,26 @@ export default {
 		};
 	},
 	apollo: {
-		query: pageQuery,
+		query: contentfulEntries,
 		preFetchVariables({ route }) {
 			return {
-				key: route?.meta?.contentfulPage(route),
+				contentType: 'page',
+				contentKey: route?.meta?.contentfulPage(route),
 			};
 		},
 		variables() {
 			return {
-				key: this.$route?.meta?.contentfulPage(this.$route),
+				contentType: 'page',
+				contentKey: this.$route?.meta?.contentfulPage(this.$route),
 			};
 		},
 		preFetch(config, client, args) {
 			return client.query({
-				query: pageQuery,
+				query: contentfulEntries,
 				variables: {
-					key: args?.route?.meta?.contentfulPage(args?.route),
-				},
+					contentType: 'page',
+					contentKey: args?.route?.meta?.contentfulPage(args?.route),
+				}
 			}).then(({ data }) => {
 				// Get Contentful page data
 				const pageData = getPageData(data);
