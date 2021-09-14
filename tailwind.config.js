4b8e38f10dfceca97644edee5c3c92baba1e8864
Ryan Ludwig <ryanl@kiva.org>
2021-09-14 15:25:06 -0700

    fix: Fix Tailwind JIT from sometimes not adding new classes to the CSS file during dev

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,11 @@ const kvConfig = require('@kiva/kv-components/tailwind.config.js');
 module.exports = {
 	mode: 'jit',
 	presets: [kvConfig],
-	purge: {
-		content: [
-			'./node_modules/@kiva/kv-components/**/*.vue',
-			'./node_modules/@kiva/kv-components/utils/**/*.js',
-			'./server/**/*.html',
-			'./src/**/*.vue',
-			'./tailwind.purge.safelist.txt'
-		]
-	}
+	purge: [
+		'./node_modules/@kiva/kv-components/**/*.vue',
+		'./node_modules/@kiva/kv-components/utils/**/*.js',
+		'./server/**/*.html',
+		'./src/**/*.vue',
+		'./tailwind.purge.safelist.txt'
+	]
 };
