466846d8bddc40b0f5b5a5fb7b196ae6f7c2d348
Ryan Ludwig <ryanl@kiva.org>
2021-09-15 10:03:59 -0700

    refactor: roll back purge shape changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ const kvConfig = require('@kiva/kv-components/tailwind.config.js');
 module.exports = {
 	mode: 'jit',
 	presets: [kvConfig],
-	purge: [
-		'./node_modules/@kiva/kv-components/**/*.vue',
-		'./node_modules/@kiva/kv-components/utils/**/*.js',
-		'./server/**/*.html',
-		'./src/**/*.vue',
-		'./tailwind.purge.safelist.txt'
-	]
+	purge: {
+		content: [
+			'./node_modules/@kiva/kv-components/**/*.vue',
+			'./node_modules/@kiva/kv-components/utils/**/*.js',
+			'./server/**/*.html',
+			'./src/**/*.vue',
+			'./tailwind.purge.safelist.txt'
+		]
+	}
 };
