7ac2e70351bc757209b1cabcca1340109ba28204
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2021-09-23 08:50:17 -0700

    VUE-702 Run cypress without mock server or code coverage

--- test/e2e/plugins/index.js
+++ test/e2e/plugins/index.js
@@ -13,7 +13,7 @@
 
 module.exports = (on, config) => {
 	// eslint-disable-next-line global-require
-	require('@cypress/code-coverage/task')(on, config);
+	// require('@cypress/code-coverage/task')(on, config);
 
 	return config;
 };
--- test/e2e/specs/Example.spec.js
+++ test/e2e/specs/Example.spec.js
@@ -1,12 +1,4 @@
-import wwwPageMock from '../fixtures/wwwPageMock';
-
 describe('Example spec', () => {
-	beforeEach(() => {
-		cy.mock(wwwPageMock());
-		// Hide tracking cookie notice banner
-		cy.setCookie('kvgdpr', 'true');
-	});
-
 	it('Visits the site map', () => {
 		// Visit the site map
 		cy.visit('/ui-site-map');
