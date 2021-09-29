c3d26bf7174ff891e30c261fc064c3bdeb1db19a
Diane Wellman <wellman.diane@gmail.com>
2021-09-29 11:43:45 -0700

    COR-44  Applying theming to story cards, COR-8   Story Card Text Embellishment Classes
    X COR-44  Applying theming to story cards
    X COR-8   Story Card Text Embellishment Classes

--- src/components/Contentful/StoryCard.vue
+++ src/components/Contentful/StoryCard.vue
@@ -1,58 +1,79 @@
 <template>
-	<section
-		class="tw-h-full"
-		:class="themeClass"
-		v-if="hasBackgroundImage"
-	>
-		<kv-contentful-img
-			class="tw-h-full tw-object-cover"
-			:width="520"
-			fallback-format="jpg"
-			:contentful-src="backgroundImage.url"
-			:alt="backgroundImage.description"
-		/>
-		<p class="story-card__imageCard-title tw-text-h4">
-			{{ backgroundImage.title }}
-		</p>
-	</section>
+<<<<<<< HEAD
+	<kv-theme-provider :theme="theme">
+		<div
+			class="
+				overflow-hidden
+				tw-h-full
+			"
+			:class="themeClass"
+			v-if="hasBackgroundImage"
+		>
+			<kv-contentful-img
+				class="tw-h-full tw-object-cover"
+				:width="520"
+				fallback-format="jpg"
+				:contentful-src="backgroundImage.url"
+				:alt="backgroundImage.description"
+			/>
+			<p class="story-card__imageCard-title tw-text-h4">
+				{{ backgroundImage.title }}
+			</p>
+		</div>
 
-	<section
-		class="
-			story-card
-			tw-h-full
-			tw-bg-brand-200
-			tw-text-primary
-			tw-flex
-			tw-flex-col
-			tw-justify-between
-			tw-items-center
-			tw-p-6
-		"
-		:class="themeClass"
-		v-else
-	>
-		<p class="tw-text-center tw-text-h4">
-			{{ kickerHeadline }}
-		</p>
-		<dynamic-rich-text
-			class="story-card__content tw-text-center tw-h-full"
-			:html="cardContent"
-		/>
-		<dynamic-rich-text class="tw-text-center" :html="footer" />
-	</section>
+		<div
+			class="
+				story-card
+				tw-bg-primary
+				tw-flex
+				tw-flex-col
+				tw-h-full
+				tw-justify-between
+				tw-items-center
+				overflow-hidden
+				tw-text-primary
+				tw-px-4
+				tw-py-4
+				md:tw-py-8
+				md:tw-px-5
+				sm:tw-px-2
+				sm:tw-py-2
+			"
+			:class="themeClass"
+			v-else
+		>
+			<dynamic-rich-text
+				class="tw-text-center tw-text-h4 tw-text-action"
+				:html="cardTitle"
+			/>
+			<dynamic-rich-text
+				class="story-card__content tw-text-center tw-h-full"
+				:html="cardContent"
+			/>
+			<dynamic-rich-text class="tw-text-center" :html="footer" />
+		</div>
+	</kv-theme-provider>
 </template>
 
 <script>
 import { richTextRenderer } from '@/util/contentful/richTextRenderer';
 import DynamicRichText from '@/components/Contentful/DynamicRichText';
 import KvContentfulImg from '@/components/Kv/KvContentfulImg';
+import {
+	darkTheme,
+	darkGreenTheme,
+	mintTheme,
+	defaultTheme
+} from '@kiva/kv-tokens/configs/kivaColors';
+import KvThemeProvider from '~/@kiva/kv-components/vue/KvThemeProvider';
 /**
 * Story Card Component
 * */
 export default {
 	components: {
 		DynamicRichText,
-		KvContentfulImg
+		KvContentfulImg,
+		KvThemeProvider
 	},
 	props: {
 		/**
@@ -63,23 +84,39 @@ export default {
 			default: () => {},
 		},
 	},
+	data() {
+		return {
+			darkTheme,
+			darkGreenTheme,
+			mintTheme,
+			defaultTheme
+		};
+	},
 	computed: {
 		theme() {
-			return this.content?.theme ?? '';
+			const themeMapper = {
+				kivaCLassicLight: defaultTheme,
+				kivaClassicMint: mintTheme,
+				kivaClassicGreen: darkGreenTheme,
+				kivaClassicDark: darkTheme,
+				imageCard: darkTheme
+			}
+			return themeMapper[this.content?.theme] ?? defaultTheme;
 		},
 		themeClass() {
 			return `story-card__${this.content?.theme}`;
 		},
-		footer() {
-			const text = this.content?.footer ?? '';
+		cardTitle() {
+			const text = this.content?.cardTitle ?? '';
 			return text ? richTextRenderer(text) : '';
 		},
 		cardContent() {
 			const text = this.content?.cardContent ?? '';
 			return text ? richTextRenderer(text) : '';
 		},
-		kickerHeadline() {
-			return this.content?.kickerHeadline ?? '';
+		footer() {
+			const text = this.content?.footer ?? '';
+			return text ? richTextRenderer(text) : '';
 		},
 		backgroundImage() {
 			return {
@@ -94,21 +131,19 @@ export default {
 	}
 };
 </script>
+
 <style lang="scss" scoped>
 	@import 'settings';
 
 	.story-card,
 	.story-card__imageCard {
-		aspect-ratio: 4 / 5;
 		border-radius: 2.5rem;
-		overflow: hidden;
-		padding: 2.5rem 1.25rem;
+		min-height: 550px;
 		@include breakpoint(medium) {
-			padding: 2.5rem;
-			min-height: 550px;
+			min-height: 600px;
 		}
 		@include breakpoint(large) {
-			padding: 3rem;
+			min-height: 720px;
 		}
 	}
 
@@ -121,7 +156,6 @@ export default {
 	.story-card__imageCard >>> .kv-contentful-img::after,
 	.story-card__imageCard-title {
 		bottom: 0.5rem;
-		color: #fff;
 		content: "";
 		position: absolute;
 		right: 1rem;
@@ -153,34 +187,50 @@ export default {
 		justify-content: center;
 	}
 
-	// Theme overrides. TODO(dew): coord with Ryan to use kivaThemes
-	.story-card__kivaClassicLight {
-		background: #fff;
+	.story-card >>> .tw-prose h1 i, .story-card >>> .tw-prose h1 u,
+	.story-card >>> .tw-prose h2 i, .story-card >>> .tw-prose h2 u,
+	.story-card >>> .tw-prose h3 i, .story-card >>> .tw-prose h3 u {
+		position: relative;
+		display: inline-block;
+		font-style: normal;
 	}
 
-	.story-card__kivaClassicMint {
-		background: #95D4B3;
+	// Text formatting for headlines.
+	.story-card >>> .tw-prose h1 b,
+	.story-card >>> .tw-prose h2 b,
+	.story-card >>> .tw-prose h3 b {
+		color: rgba(var(--text-action));
 	}
 
-	.story-card__kivaClassicLight >>> .tw-prose h2,
-	.story-card__kivaClassicLight >>> .tw-prose h3,
-	.story-card__kivaClassicDark >>> .tw-prose h2,
-	.story-card__kivaClassicDark >>> .tw-prose h3 {
-		color: #212121;
+	.story-card >>> .tw-prose u {
+		text-decoration: none;
 	}
 
-	.story-card__kivaClassicGreen {
-		background: #2aa967;
+	.story-card >>> .tw-prose h1 u:after,
+	.story-card >>> .tw-prose h2 u:after,
+	.story-card >>> .tw-prose h3 u:after {
+		background-image: url("data:image/svg+xml;base64,PHN2ZyBwcmVzZXJ2ZUFzcGVjdFJhdGlvPSJub25lIiB2aWV3Qm94PSIwIDAgMTU4IDgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0ibTE1NC43NSA2Yy0zLjY2OS0uNDk5NDYtMTE0LjI2MjEtNi40OTI5NzktMTUyIDAiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzJhYTk2NyIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbWl0ZXJsaW1pdD0iMTAiIHN0cm9rZS13aWR0aD0iNCIvPjwvc3ZnPg==");
+		content: "";
+		display: block;
+		position: absolute;
+		bottom: -6px;
+		left: -4px;
+		width: calc(100% + 8px);
+		text-decoration: none;
+		height: 8px;
 	}
 
-	.story-card__kivaClassicDark {
-		background: #212121;
+	.story-card >>> .tw-prose h1 i::after,
+	.story-card >>> .tw-prose h2 i::after,
+	.story-card >>> .tw-prose h3 i::after {
+		background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiIHZpZXdCb3g9IjAgMCAzNzcgOTkiPgoJPHN0eWxlIHR5cGU9InRleHQvY3NzIj4KCQkuc3ZnX2xpbmV7ZmlsbDpub25lO3N0cm9rZTojMmFhOTY3O3N0cm9rZS13aWR0aDo0O3N0cm9rZS1saW5lY2FwOnJvdW5kO3N0cm9rZS1taXRlcmxpbWl0OjEwO3ZlY3Rvci1lZmZlY3Q6IG5vbi1zY2FsaW5nLXN0cm9rZTt9Cgk8L3N0eWxlPgoJPHBhdGggY2xhc3M9InN2Z19saW5lIiBkPSJNMzA4Ljg5NyAyNy43MTNDMzU2LjYwMSAzMy4zODA5IDQyMy4zODYgNTEuMjA4MyAzMDguODkzIDc3LjE3NUMxNjUuNzc2IDEwOS42MzMgLTMyLjI1OTYgOTYuNTc0IDkuODI2MzMgMzkuODExOEM0My40OTUxIC01LjU5Nzk3IDI1Ny4yNTEgMi40OTkyNiAzNTkuOTIgMTIuMjI0MSIvPgo8L3N2Zz4=");
+		content: "";
+		display: block;
+		position: absolute;
+		top: -10px;
+		left: -6px;
+		width: calc(100% + 24px);
+		height: calc(100% + 16px);
 	}
 
-	.story-card__kivaClassicGreen >>> .tw-prose h2,
-	.story-card__kivaClassicGreen >>> .tw-prose h3,
-	.story-card__kivaClassicDark >>> .tw-prose h2,
-	.story-card__kivaClassicDark >>> .tw-prose h3 {
-		color: #fff;
-	}
 </style>
