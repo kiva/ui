efb2e3caef0fbaad399e06e4e2be0cc62108dfe1
Ryan Ludwig <ryanl@kiva.org>
2021-10-07 15:46:01 -0700

    feat: Convert /borrow subpages to Kiva Classic typography

--- src/pages/Borrow/BorrowHowLong.vue
+++ src/pages/Borrow/BorrowHowLong.vue
@@ -1,49 +1,52 @@
 <template>
-	<div>
-		<div class="intro">
-			<intro-graphic class="intro__graphic" />
-			<p class="intro__subtitle">
-				Step 1: Pre-check
-			</p>
-			<h1 class="intro__title text-center small-12 columns">
-				How long and how much?
-			</h1>
-			<div class="intro__body small-12 large-8 columns text-center">
-				<p>
+	<div class="tw-text-center">
+		<intro-graphic
+			class="tw-w-full tw-mx-auto tw-mb-4"
+			style="max-width: 31rem;"
+		/>
+		<p class="tw-text-h4 tw-mb-1">
+			Step 1: Pre-check
+		</p>
+		<h1 class="tw-mb-4">
+			How long and how much?
+		</h1>
+		<kv-grid class="tw-grid-cols-12">
+			<div class="tw-col-span-12 md:tw-col-span-10 lg:tw-col-span-8 md:tw-col-start-2 lg:tw-col-start-3">
+				<p class="tw-mb-4">
 					We're often asked, "How much can I borrow and how long will it take to fund?"
 					It’s different for everyone, as loans can range from $1,000 - $15,000. We’ve seen
 					that borrowers who take action and reach out to their community receive
 					their loan as quickly as one week. The average time to complete the process is a few weeks.
 				</p>
-				<p>
+				<p class="tw-mb-4">
 					Start by deciding exactly how much you’ll need to borrow. With the right
 					preparation, you can get funded very quickly on our platform.
 				</p>
-				<p>
+				<p class="tw-mb-4">
 					Need help getting started?
 					<a href="https://www.kivaushub.org/borrower">Check out this useful guide</a>.
 				</p>
+				<kv-button :href="`/my/borrower/application?refid=${trusteeId}`">
+					I understand, let’s create an account
+				</kv-button>
 			</div>
-		</div>
-		<!-- eslint-disable max-len -->
-		<kv-button
-			:href="`/my/borrower/application?refid=${trusteeId}`"
-			class="cta-btn"
-		>
-			I understand, let’s create an account
-		</kv-button>
-		<!-- eslint-enable max-len -->
+		</kv-grid>
 	</div>
 </template>
 
 <script>
 import IntroGraphic from '@/assets/inline-svgs/borrow/howlong.svg';
-import KvButton from '@/components/Kv/KvButton';
+import KvButton from '~/@kiva/kv-components/vue/KvButton';
+import KvGrid from '~/@kiva/kv-components/vue/KvGrid';
 
 export default {
 	components: {
 		IntroGraphic,
 		KvButton,
+		KvGrid,
+	},
+	metaInfo: {
+		title: 'How Long | Borrow Pre-application',
 	},
 	computed: {
 		trusteeId() {
@@ -52,7 +55,3 @@ export default {
 	},
 };
 </script>
-
-<style lang="scss" scoped>
-
-</style>
--- src/pages/Borrow/BorrowIndex.vue
+++ src/pages/Borrow/BorrowIndex.vue
@@ -1,27 +1,22 @@
 <template>
 	<www-page>
-		<div class="row">
-			<div class="container">
-				<div class="small-12 columns">
-					<borrow-intro v-if="isIntro" />
-					<router-view v-else />
-				</div>
-			</div>
-		</div>
+		<kv-page-container class="tw-py-4 md:tw-py-6 lg:tw-py-8">
+			<borrow-intro v-if="isIntro" />
+			<router-view v-else />
+		</kv-page-container>
 	</www-page>
 </template>
 
 <script>
 import WwwPage from '@/components/WwwFrame/WwwPage';
 import BorrowIntro from '@/pages/Borrow/BorrowIntro';
+import KvPageContainer from '~/@kiva/kv-components/vue/KvPageContainer';
 
 export default {
 	components: {
 		BorrowIntro,
+		KvPageContainer,
 		WwwPage
-	},
-	metaInfo: {
-		title: 'Borrow Pre-application',
 	},
 	computed: {
 		isIntro() {
@@ -30,63 +25,3 @@ export default {
 	}
 };
 </script>
-
-<style lang="scss">
-// These styles are used on all 3 slides
-
-@import 'settings';
-
-.container {
-	margin: 4rem 0;
-
-	@include breakpoint(large) {
-		border-radius: rem-calc(8);
-		box-shadow: 0 0 0.5rem 0 rgba(0, 0, 0, 0.15); // TODO: make the same as MonthlyGoodSetupPageVariant
-		padding: 4rem 4rem;
-		margin: 4rem -4rem;
-		width: calc(100% + 8rem);
-	}
-}
-
-.checkout-steps {
-	max-width: 35rem;
-	margin: 0 auto 4rem auto;
-}
-
-.intro {
-	margin-bottom: 2rem;
-
-	&__graphic {
-		display: block;
-		width: 100%;
-		max-width: rem-calc(505);
-		margin: 0 auto 3rem;
-	}
-
-	&__title {
-		@include large-text();
-
-		margin-bottom: 1rem;
-	}
-
-	&__subtitle {
-		@include featured-text();
-
-		line-height: 1.2;
-		margin-bottom: 0.5rem;
-		font-weight: 700;
-		text-align: center;
-		color: $kiva-text-light;
-	}
-
-	&__body {
-		@include medium-text();
-
-		margin: 0 auto 2.5rem auto;
-	}
-}
-
-.cta-btn {
-	width: 100%;
-}
-</style>
