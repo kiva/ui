b99e6dc7ee4d36f8e016e3c6bdc0f09c5aa9082c
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2021-10-26 10:42:47 -0700

    feat(e2e-testing): add Testing Library for Cypress VUE-788

--- test/e2e/support/index.js
+++ test/e2e/support/index.js
@@ -13,6 +13,6 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 // import '@cypress/code-coverage/support';
-// import './commands';
+import './commands';
 
 // beforeEach(() => cy.lunar('reset'));
