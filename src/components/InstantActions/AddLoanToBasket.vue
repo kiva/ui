33b85cf5d2d74b6d993d1d7213be4b19e31180d5
Matt Stover <mcs@mcstover.com>
2021-10-27 11:15:18 -0700

    feat: Create initial infrastructure for Instant lending action. This paradigm will be used for the Instant Donation work too.

---
+++ src/components/InstantActions/AddLoanToBasket.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="instant-lending-processor">
+		{{ loanId }} | {{ lendAmount }}
+	</div>
+</template>
+
+<script>
+// import logFormatter from '@/util/logFormatter';
+
+export default {
+	inject: ['locale'],
+	metaInfo: {
+		title: 'Error'
+	},
+	props: {
+		loanId: {
+			type: Number,
+			default: 0
+		},
+		lendAmount: {
+			type: Number,
+			default: 25
+		}
+	},
+	data() {
+		return {
+			clientId: this.$route.query.client_id,
+			lenderLogin: this.$route.query.lender_login !== '0',
+		};
+	},
+	computed: {
+
+	},
+	created() {
+		// logFormatter(
+		// 	`Auth0 authentication error: ${this.errorCode}: ${this.errorDescription}`,
+		// 	'warn',
+		// 	{ ...this.$route.query }
+		// );
+	},
+};
+</script>
