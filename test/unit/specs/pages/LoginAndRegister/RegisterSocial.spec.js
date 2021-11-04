ed9e1210fbf25d5a35f7e01146144de6a242baed
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2021-11-03 20:29:10 -0700

    feat: require valid names during login/registration AUTH-305

---
+++ test/unit/specs/pages/LoginAndRegister/RegisterSocial.spec.js
@@ -0,0 +1,32 @@
+import RegisterSocial from '@/pages/LoginAndRegister/RegisterSocial';
+
+// Test that RegisterSocial.beforeRouteEnter redirects to the error page for route object `to`
+const testRedirectToError = to => {
+	const next = jest.fn();
+	RegisterSocial.beforeRouteEnter(to, {}, next);
+	expect(next.mock.calls.length).toBe(1);
+	expect(next.mock.calls[0][0]).toBe('/error');
+};
+
+// Test that RegisterSocial.beforeRouteEnter does not redirect for route object `to`
+const testNoRedirect = to => {
+	const next = jest.fn();
+	RegisterSocial.beforeRouteEnter(to, {}, next);
+	expect(next.mock.calls.length).toBe(1);
+	expect(next.mock.calls[0][0]).not.toBeDefined();
+};
+
+describe('RegisterSocial page', () => {
+	it('redirects to error page if query params are missing', () => {
+		testRedirectToError({});
+		testRedirectToError({ query: {} });
+		testRedirectToError({ query: { names: 1 } });
+		testRedirectToError({ query: { terms: 1 } });
+		testRedirectToError({ query: { names: 1, terms: 1 } });
+		testRedirectToError({ query: { state: 'abc' } });
+
+		testNoRedirect({ query: { state: 'abc', names: 1 } });
+		testNoRedirect({ query: { state: 'abc', terms: 1 } });
+		testNoRedirect({ query: { state: 'abc', names: 1, terms: 1 } });
+	});
+});
