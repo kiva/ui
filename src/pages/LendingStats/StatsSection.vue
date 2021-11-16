ae75b763ad7c212f3610ad3178d921618ba2673f
Ryan Ludwig <ryanl@kiva.org>
2021-11-15 17:22:05 -0800

    feat: kiva classic typography for lending-stats page

--- src/pages/LendingStats/StatsSection.vue
+++ src/pages/LendingStats/StatsSection.vue
@@ -1,12 +1,16 @@
 <template>
 	<section class="stats-section">
-		<h2>
+		<h2 class="tw-flex tw-gap-2 tw-mb-4">
 			<span>{{ title }}</span>
-			<span class="lent-count">{{ lentTo.length }}/{{ itemCount }}</span>
+			<span class="lent-count tw-text-base tw-bg-brand tw-text-white tw-py-0.5 tw-px-1 tw-self-center">
+				{{ lentTo.length }}/{{ itemCount }}
+			</span>
 		</h2>
-		<div class="row stats-list" ref="list">
+		<div class="row tw-overflow-hidden" ref="list">
 			<div class="columns small-6">
-				<h3>You've lent to</h3>
+				<h3 class="tw-pb-2">
+					You've lent to
+				</h3>
 				<item-list
 					:items="lentTo"
 					:item-key="itemKey"
@@ -16,7 +20,9 @@
 			</div>
 			<div class="columns small-6">
 				<template v-if="hasUnlent">
-					<h3>You haven't lent to</h3>
+					<h3 class="tw-pb-2">
+						You haven't lent to
+					</h3>
 					<item-list
 						:items="notLentTo"
 						:item-key="itemKey"
@@ -24,22 +30,31 @@
 						:param="queryParam"
 					/>
 				</template>
-				<div v-else class="completed">
-					<kv-icon name="confirmation" />
-					<span>Great job!</span>
+				<div v-else class="tw-flex tw-gap-2">
+					<kv-material-icon
+						class="tw-text-brand tw-w-4 tw-h-4 md:tw-w-6 md:tw-h-6"
+						:icon="mdiCheckCircle"
+					/>
+					<h3 class="tw-text-h2 tw-text-brand">
+						Great job!
+					</h3>
 				</div>
 			</div>
 		</div>
-		<div class="button-area">
+		<div class="button-area tw-w-full tw-flex tw-flex-col tw-flex-nowrap tw-items-center">
 			<button
 				v-if="canExpand"
 				@click="toggle"
-				class="show-more-button"
+				class="tw-text-link tw-font-medium tw-my-2"
 				:aria-pressed="showingMore ? 'true' : 'false'"
 			>
 				{{ showingMore ? 'Hide' : 'Show more' }}
 			</button>
-			<kv-button v-if="hasUnlent" :to="unlentUrlRoute" class="secondary lend-to-new-button">
+			<kv-button
+				v-if="hasUnlent"
+				:to="unlentUrlRoute"
+				variant="secondary"
+			>
 				Lend to a new {{ noun }}
 			</kv-button>
 		</div>
@@ -48,11 +63,11 @@
 
 <script>
 import _map from 'lodash/map';
-import numeral from 'numeral';
 import { expand, collapse } from '@/util/expander';
-import KvButton from '@/components/Kv/KvButton';
-import KvIcon from '@/components/Kv/KvIcon';
+import { mdiCheckCircle } from '@mdi/js';
 import ItemList from './ItemList';
+import KvButton from '~/@kiva/kv-components/vue/KvButton';
+import KvMaterialIcon from '~/@kiva/kv-components/vue/KvMaterialIcon';
 
 export default {
 	props: {
@@ -69,13 +84,14 @@ export default {
 	components: {
 		ItemList,
 		KvButton,
-		KvIcon,
+		KvMaterialIcon,
 	},
 	data() {
 		return {
 			numShowing: 12,
 			showingMore: false,
 			baseHeight: 0,
+			mdiCheckCircle,
 		};
 	},
 	computed: {
@@ -103,10 +119,10 @@ export default {
 	methods: {
 		measureBaseHeight() {
 			const title = this.$refs.list.querySelector('h3');
-			const titleHeight = numeral(window.getComputedStyle(title).getPropertyValue('height')).value();
+			const titleHeight = title.offsetHeight;
 
 			const item = this.$refs.list.querySelector('li');
-			const itemHeight = numeral(window.getComputedStyle(item).getPropertyValue('height')).value();
+			const itemHeight = item.offsetHeight;
 
 			const totalHeight = titleHeight + (Math.ceil(this.numShowing / 2) * itemHeight);
 			return `${totalHeight}px`;
@@ -139,84 +155,3 @@ export default {
 	}
 };
 </script>
-
-<style lang="scss">
-@import 'settings';
-
-.stats-section {
-	h2 {
-		color: $kiva-green;
-	}
-
-	.lent-count {
-		color: $white;
-		background-color: $kiva-darkgreen;
-		margin-left: 0.5rem;
-		padding: 0.5rem 1rem;
-		font-size: 1rem;
-		font-weight: normal;
-		vertical-align: middle;
-	}
-
-	.stats-list {
-		overflow: hidden;
-		margin-top: 1.5rem;
-
-		h3 {
-			padding-bottom: 0.5rem;
-
-			@include breakpoint(medium down) {
-				font-size: 1rem; // temporary font styling until we have canonical h3 styles
-			}
-		}
-	}
-
-	.completed {
-		display: flex;
-		align-items: center;
-		color: $kiva-green;
-		font-size: 1rem;
-
-		@include breakpoint(large) {
-			@include featured-text();
-		}
-
-		.icon {
-			fill: $kiva-green;
-			margin-right: 0.5rem;
-			width: 2rem;
-			height: 2rem;
-
-			@include breakpoint(large) {
-				width: 3rem;
-				height: 3rem;
-			}
-		}
-	}
-
-	.button-area {
-		width: 100%;
-		display: flex;
-		flex-flow: column nowrap;
-		align-items: center;
-	}
-
-	.show-more-button {
-		padding: 0.75rem;
-		font-weight: $global-weight-normal;
-		text-decoration: underline;
-
-		&:hover {
-			color: $kiva-textlink;
-		}
-
-		&:focus {
-			outline: none;
-		}
-	}
-
-	.lend-to-new-button {
-		margin-top: 1rem;
-	}
-}
-</style>
