016d09b6f348bd884e8700b8b8e6f99c114c63cb
Matt Stover <mcs@mcstover.com>
2021-11-19 12:01:26 -0800

    feat: remove experiment around homepage versions and redirects

--- src/pages/Homepage/Homepage.vue
+++ src/pages/Homepage/Homepage.vue
@@ -4,32 +4,13 @@
 
 <script>
 import gql from 'graphql-tag';
-
-import experimentVersionFragment from '@/graphql/fragments/experimentVersion.graphql';
-import experimentQuery from '@/graphql/query/experimentAssignment.graphql';
 import { preFetchAll } from '@/util/apolloPreFetch';
 
-const DefaultHomePage = () => import('@/pages/Homepage/DefaultHomepage');
 const ContentfulPage = () => import('@/pages/ContentfulPage');
 
-const homePageExperimentQuery = gql`query homepageFrame {
+const homePageLoginStatus = gql`query homepageFrame {
 	hasEverLoggedIn @client
-	general {
-		classicHomeExp: uiExperimentSetting(key: "home_classic") {
-			key
-			value
-		}
-	}
 }`;
-
-// Return an import function for the active homepage component
-const selectActiveHomepage = (legacyHomeExp = {}) => {
-	// Being in the 'x' variant of the classic home experiment forces using the legacy homepage
-	if (legacyHomeExp.version === 'x') {
-		return DefaultHomePage;
-	}
-	return ContentfulPage;
-};
 
 export default {
 	inject: ['apollo', 'cookieStore'],
@@ -43,28 +24,16 @@ export default {
 	},
 	data() {
 		return {
-			activeHomepage: null,
+			activeHomepage: ContentfulPage,
 		};
 	},
 	apollo: {
-		query: homePageExperimentQuery,
+		query: homePageLoginStatus,
 		preFetch(config, client, args) {
 			return client.query({
-				query: homePageExperimentQuery
-			}).then(() => client.query({
-				query: experimentQuery,
-				variables: { id: 'home_classic' },
-			})).then(expResult => {
-				// Redirect to /homepage-classic if experiment is active and route is not already matched
-				if (expResult?.data?.experiment?.version === 'b' && args?.route?.path !== '/homepage-classic') {
-					// cancel the promise, returning a route for redirect
-					return Promise.reject({
-						path: '/homepage-classic'
-					});
-				}
-				// Select which homepage to load
-				const componentImporter = selectActiveHomepage(expResult?.data?.experiment);
-				return componentImporter();
+				query: homePageLoginStatus
+			}).then(() => {
+				return ContentfulPage();
 			}).then(resolvedImport => {
 				// Call preFetch for the active homepage
 				const component = resolvedImport.default;
@@ -81,23 +50,6 @@ export default {
 					window.dataLayer.push({ event: 'activateUnbounceEvent' });
 				}
 			}
-
-			// Fetch classic homepage experiment data (CORE-76)
-			const classicHomeExp = this.apollo.readFragment({
-				id: 'Experiment:home_classic',
-				fragment: experimentVersionFragment,
-			}) || {};
-			// Fire Event for EXP-CORE-76
-			if (classicHomeExp.version && classicHomeExp.version !== 'unassigned') {
-				this.$kvTrackEvent(
-					'homepage',
-					'EXP-CORE-76',
-					classicHomeExp.version,
-				);
-			}
-
-			// Set active homepage
-			this.activeHomepage = selectActiveHomepage(classicHomeExp);
 		}
 	}
 };
