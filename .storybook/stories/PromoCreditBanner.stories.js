32bb494e07ee511013da43a0e0cd811a8959ca63
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2021-12-14 13:35:07 -0600

    feat: promocreditbanner component was created with its story

---
+++ .storybook/stories/PromoCreditBanner.stories.js
@@ -0,0 +1,69 @@
+import Vue from 'vue'
+import StoryRouter from 'storybook-vue-router';
+import PromoCreditBanner from '@/components/WwwFrame/PromotionalBanner/Banners/PromoCreditBanner.vue';
+
+// import plugins
+import kivaPlugins from '@/plugins';
+Vue.use(kivaPlugins);
+
+export default {
+	title: 'WwwFrame/Banners/PromoCreditBanner',
+	component: PromoCreditBanner,
+	decorators: [StoryRouter()],
+	args: {
+		basketState: {
+			shop: {
+				nonTrivialItemCount: 1,
+				basket: {
+					id: 12,
+					hasFreeCredits: true,
+					credits: {
+						values: {
+							id: 12,
+							available: true,
+							creditType: 'bonus_credit',
+							promoFund: {
+								id: 12,
+								displayName: 'test',
+							}
+						}
+					},
+					totals: {
+						bonusAvailableTotal: 124312.23,
+						freeTrialAvailableTotal: 2432.342,
+						redemptionCodeAvailableTotal: 24.241,
+						universalCodeAvailableTotal: 234
+					}
+
+				}
+			},
+			my: {
+				userAccount: {
+					id: 1017469,
+					balance: "0.00",
+					promoBalance: "10.00",
+				},
+				lender: {
+					image: {
+						id: 726677,
+						url: "https://www-dev-kiva-org.freetls.fastly.net/img/s140/726677.jpg",
+					},
+				},
+				isBorrower: false,
+				mostRecentBorrowedLoan: null,
+				trustee: null,
+			}
+		}
+	}
+
+};
+
+export const Default = (args, { argTypes }) => ({
+	props: Object.keys(argTypes),
+	components: {
+		PromoCreditBanner
+	},
+	template: `
+		<promo-credit-banner :basket-state="basketState" />
+	`,
+});
---
+++ src/components/WwwFrame/PromotionalBanner/Banners/PromoCreditBanner.vue
@@ -0,0 +1,177 @@
+<template>
+	<div
+		v-if="lendingRewardOffered"
+		class="tw-bg-brand tw-text-white md:tw-py-1.5 md:tw-px-0"
+		v-kv-track-event="['TopNav','click-Promo','Lending Reward Banner']"
+	>
+		Make a Kiva loan <br class="tw-inline">and receive a $25 free credit to lend again.
+	</div>
+	<div v-else class="bonus-banner-holder tw-bg-brand tw-text-center tw-py-1.5 tw-px-5">
+		<router-link
+			v-if="promoData && !promoData.pageId"
+			to="/lend/freeCreditEligible"
+			class="tw-text-white hover:tw-no-underline hover:tw-text-white"
+			v-kv-track-event="['TopNav','click-Promo','Bonus Banner']"
+		>
+			Select a borrower to lend your {{ promoData.bonusBalance | numeral('$0.00') }} free credit
+		</router-link>
+		<router-link
+			v-if="promoData && promoData.pageId"
+			:to="`/cc/${promoData.pageId}`"
+			class="tw-text-white hover:tw-no-underline hover:tw-text-white"
+			v-kv-track-event="['TopNav','click-Promo','MVP Bonus Banner']"
+		>
+			You have {{ promoData.available | numeral('$0.00') }} from {{ promoData.displayName }} to lend!
+		</router-link>
+	</div>
+</template>
+
+<script>
+import numeral from 'numeral';
+import gql from 'graphql-tag';
+import { indexIn } from '@/util/comparators';
+
+const promoCampaignInfo = gql`
+	query promoCampaign($basketId: String, $promoFundId: String) {
+		shop (basketId: $basketId) {
+			id
+			basket {
+				id
+				hasFreeCredits
+				credits {
+					values {
+						id
+						available
+						promoFund {
+							id
+						}
+					}
+				}
+			}
+			promoCampaign (promoFundId: $promoFundId) {
+				promoFund {
+					id
+					displayName
+				}
+				managedAccount {
+					id
+					managementType
+					pageId
+				}
+			}
+		}
+	}
+`;
+export default {
+	inject: ['apollo'],
+	props: {
+		basketState: {
+			type: Object,
+			default: () => {},
+		},
+	},
+	data() {
+		return {
+			bonusBalance: 0,
+			lendingRewardOffered: false,
+			promoCampaignData: null,
+		};
+	},
+	created() {
+		if (this.basketState && Object.entries(this.basketState).length) {
+			this.setPromoState(this.basketState);
+		}
+	},
+	mounted() {
+		// verify promoCampaign information
+		this.fetchManagedAccountCampaign();
+	},
+	computed: {
+		hasFreeCredits() {
+			return this.basketState?.shop?.basket?.hasFreeCredits ?? false;
+		},
+		priorityBasketCredit() {
+			// get credits list
+			const basketCredits = this.basketState?.shop?.basket?.credits?.values ?? [];
+			if (!basketCredits.length) return false;
+			// establish precedence for credit types
+			const sortBy = ['universal_code', 'redemption_code', 'bonus_credit', 'kiva_credit'];
+			// copy and sort the credits
+			const creditsArrayCopy = basketCredits.map(credit => credit);
+			creditsArrayCopy.sort(indexIn(sortBy, 'creditType'));
+			// return the 1st credit for presentation
+			return creditsArrayCopy.length ? creditsArrayCopy[0] : null;
+		},
+		creditAvailable() {
+			return this.priorityBasketCredit?.available ?? null;
+		},
+		promoFundId() {
+			return this.priorityBasketCredit?.promoFund?.id ?? null;
+		},
+		promoFundDisplayName() {
+			return this.priorityBasketCredit?.promoFund?.displayName ?? null;
+		},
+		managedAccountPageId() {
+			return this.promoCampaignData?.managedAccount?.pageId ?? null;
+		},
+		promoData() {
+			return {
+				bonusBalance: this.bonusBalance,
+				available: this.creditAvailable,
+				displayName: this.promoFundDisplayName,
+				pageId: this.managedAccountPageId,
+			};
+		}
+	},
+	watch: {
+		basketState: {
+			handler(basketState) {
+				if (basketState && Object.entries(basketState).length) {
+					this.setPromoState(basketState);
+				}
+			},
+			immediate: true,
+			deep: true,
+		}
+	},
+	methods: {
+		fetchManagedAccountCampaign() {
+			// TODO: Create an array of queries if multiple credits have an associated promo fund
+			// extended promotion information from managed account
+			if (this.promoFundId && this.hasFreeCredits && this.creditAvailable) {
+				this.apollo.query({
+					query: promoCampaignInfo,
+					variables: {
+						promoFundId: String(this.promoFundId)
+					}
+				})
+					.then(({ data }) => {
+						this.promoCampaignData = data?.shop?.promoCampaign ?? null;
+					});
+			}
+		},
+		setPromoState(promotionData) {
+			const promoBalance = numeral(promotionData.my?.userAccount?.promoBalance).value();
+			const bonusAvailableTotal = numeral(
+				promotionData.shop?.basket?.totals?.bonusAvailableTotal
+			).value();
+			const freeTrialAvailableTotal = numeral(
+				promotionData.shop?.basket?.totals?.freeTrialAvailableTotal
+			).value();
+			const redemptionCodeAvailableTotal = numeral(
+				promotionData.shop?.basket?.totals?.redemptionCodeAvailableTotal
+			).value();
+			const universalCodeAvailableTotal = numeral(
+				promotionData.shop?.basket?.totals?.universalCodeAvailableTotal
+			).value();
+			// prefer promoBalance from the user account if it's larger
+			const promoBalancePrecedence = promoBalance >= bonusAvailableTotal ? promoBalance : bonusAvailableTotal;
+			this.bonusBalance = promoBalancePrecedence
+				+ freeTrialAvailableTotal
+				+ redemptionCodeAvailableTotal
+				+ universalCodeAvailableTotal;
+			this.lendingRewardOffered = promotionData.shop?.lendingRewardOffered;
+		}
+	}
+};
+</script>
