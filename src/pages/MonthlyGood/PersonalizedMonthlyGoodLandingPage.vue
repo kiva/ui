0ad5b1a83ef1a49c23f7b086ed112e4ee0951ed5
Joshua <joshuadstarkey@gmail.com>
2022-02-07 17:52:35 -0700

    feat: core-404 personalized mg hero in place, only cleanup needed

--- src/pages/MonthlyGood/PersonalizedMonthlyGoodLandingPage.vue
+++ src/pages/MonthlyGood/PersonalizedMonthlyGoodLandingPage.vue
@@ -1,14 +1,79 @@
 <template>
 	<www-page>
-		TESTING
+		<kv-hero class="mg-hero bg-overlay">
+			<template #images>
+				<kv-contentful-img
+					:contentful-src="heroImage"
+					fallback-format="jpg"
+					:width="1440"
+					:alt="heroImageAlt"
+					:source-sizes="sourceSizes"
+					crop="&fit=fill&f=face"
+				/>
+			</template>
+			<template #overlayContent>
+				<form @submit.prevent.stop="submit" novalidate>
+					<div class="tw-bg-primary-inverse tw-bg-opacity-low md:tw-max-w-xs md:tw-ml-4 lg:tw-ml-10">
+						<div class="tw-p-1.5">
+							<h2 class="tw-text-primary-inverse " v-html="heroHeadline"></h2>
+							<p class="tw-text-subhead tw-text-primary-inverse" v-html="heroBody"></p>
+						</div>
+						<fieldset class="tw-mb-2 tw-flex">
+							<div class="tw-inline tw-max-w-xs tw-flex-1 tw-px-1">
+								<label
+									class="tw-sr-only"
+									:class="{ 'tw-text-danger': $v.$invalid }" :for="'amount-' + componentKey"
+								>
+									Amount
+								</label>
+								<kv-currency-input
+									:id="'amount-' + componentKey"
+									:value="amount"
+									@input="updateAmount"
+									class=""
+								/>
+								<ul class="validation-errors tw-text-danger" v-if="$v.$invalid">
+									<li v-if="!$v.amount.required">
+										Field is required
+									</li>
+									<li v-if="!$v.amount.minValue || !$v.amount.maxValue">
+										Enter an amount of $5-$8,500
+									</li>
+								</ul>
+							</div>
+							<kv-button
+								class="tw-flex-1 tw-mb-2 tw-inline tw-px-1"
+								type="submit"
+								:disabled="$v.$invalid"
+								v-kv-track-event="[
+									'MonthlyGood',
+									`click-start-form-${componentKey}`,
+									heroPrimaryCtaText
+								]"
+							>
+								{{ heroPrimaryCtaText }}
+							</kv-button>
+						</fieldset>
+					</div>
+				</form>
+			</template>
+		</kv-hero>
 	</www-page>
 </template>
 
 <script>
 import gql from 'graphql-tag';
+import { validationMixin } from 'vuelidate';
+import { required, minValue, maxValue } from 'vuelidate/lib/validators';
 import experimentVersionFragment from '@/graphql/fragments/experimentVersion.graphql';
 import experimentQuery from '@/graphql/query/experimentAssignment.graphql';
 import WwwPage from '@/components/WwwFrame/WwwPage';
+import KvHero from '@/components/Kv/KvHero';
+import KvContentfulImg from '@/components/Kv/KvContentfulImg';
+import { processPageContent } from '@/util/contentfulUtils';
+import KvCurrencyInput from '@/components/Kv/KvCurrencyInput';
+import { documentToHtmlString } from '~/@contentful/rich-text-html-renderer';
+import KvButton from '~/@kiva/kv-components/vue/KvButton';
 
 const pageQuery = gql`
 	query monthlyGoodPersonalizedLandingPage {
@@ -22,13 +87,57 @@ export default {
 	metaInfo: {
 		title: 'Personalized Monthly Good',
 	},
+	mixins: [
+		validationMixin,
+	],
 	components: {
 		WwwPage,
+		KvHero,
+		KvCurrencyInput,
+		KvButton,
+		KvContentfulImg,
+	},
+	validations: {
+		amount: {
+			required,
+			minValue: minValue(5),
+			maxValue: maxValue(8500)
+		},
 	},
 	props: {
 	},
 	data() {
 		return {
+			monthlyGoodAmount: 25,
+			amount: 25,
+			selectedGroup: this.category || 'default',
+			sourceSizes: [
+				{
+					width: 1920,
+					height: 650,
+					media: 'min-width: 1441px',
+				},
+				{
+					width: 1440,
+					height: 620,
+					media: 'min-width: 1025px',
+				},
+				{
+					width: 1024,
+					height: 441,
+					media: 'min-width: 681px',
+				},
+				{
+					width: 680,
+					height: 372,
+					media: 'min-width: 481px',
+				},
+				{
+					width: 480,
+					height: 540,
+					media: 'min-width: 0px',
+				},
+			],
 		};
 	},
 	inject: ['apollo', 'cookieStore'],
@@ -61,7 +170,70 @@ export default {
 			) {
 				this.$kvTrackEvent('MonthlyGood', 'EXP-CORE-399-Feb2022', 'b');
 			}
+
+			// Check for contentful content
+			const pageEntry = data.contentful?.entries?.items?.[0] ?? null;
+			this.pageData = pageEntry ? processPageContent(pageEntry) : null;
 		},
 	},
+	methods: {
+		// Emits values to parent component to allow the synchronization of 2 landing
+		// forms on the same page
+		updateSelected(value) {
+			this.$emit('update:selectedGroup', value);
+		},
+		// Emits values to parent component to allow the synchronization of 2 landing
+		// forms on the same page
+		updateAmount(value) {
+			this.$emit('update:amount', value);
+		},
+		submit() {
+			this.$router.push({
+				path: '/monthlygood/setup',
+				query: {
+					amount: this.amount,
+					category: this.selectedGroup,
+				}
+			});
+		}
+	},
+	computed: {
+		contentGroups() {
+			return this.pageData?.page?.pageLayout?.contentGroups ?? [];
+		},
+		componentKey() {
+			// This component can exist multiple times on the page or can be iterated over.
+			// If it has a key attribute, then it will be used in the input id to avoid
+			// duplicate inputs with the same id.
+			return this.$vnode.key || '';
+		},
+		heroContentGroup() {
+			return this.contentGroups?.find(({ key }) => {
+				return key ? key === 'personalized-mg-landing-hero' : false;
+			});
+		},
+		heroImage() {
+			return this.heroContentGroup?.media?.[0]?.file?.url ?? '';
+		},
+		heroImageAlt() {
+			return this.heroContentGroup?.media?.[0]?.description ?? '';
+		},
+		heroText() {
+			// This contentGroup doesnt have a type, so find by key
+			return this.heroContentGroup?.contents?.find(contentItem => {
+				return contentItem.key === 'personalized-mg-landing-hero-text';
+			});
+		},
+		heroBody() {
+			const text = this.heroText?.bodyCopy ?? '';
+			return documentToHtmlString(text).replace(/\n/g, '<br />');
+		},
+		heroHeadline() {
+			return this.heroText?.headline ?? "It's easy to do good.";
+		},
+		heroPrimaryCtaText() {
+			return this.heroText?.primaryCtaText ?? 'Start Monthly Good';
+		},
+	}
 };
 </script>
