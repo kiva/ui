081a4fbef3312c1b821ac1d75563c7cf2d8d5f1d
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2022-02-16 12:43:53 -0800

    feat: do not fire unbounce event if utm_medium is email
    * also stores this in session for future visits
    
    ACK-223

--- src/plugins/unbounce-event-mixin.js
+++ src/plugins/unbounce-event-mixin.js
@@ -4,9 +4,23 @@ import hasEverLoggedInQuery from '@/graphql/query/shared/hasEverLoggedIn.graphql
 export default {
 	inject: ['apollo', 'cookieStore'],
 	created() {
-		const unbounceEnabled = this.$route?.meta?.unbounce;
+		const unbounceRoute = this.$route?.meta?.unbounce ?? false;
+		const utmEmailMedium = this.$route?.query?.utm_medium === 'email';
 
-		// Route meta contains unbounce: true
+		// Set cookie to disabled unbounce for future visits
+		if (utmEmailMedium) {
+			this.cookieStore.set('kvutm_medium_email', true);
+		}
+
+		const utmEmailMediumCookie = this.cookieStore.get('kvutm_medium_email');
+
+		/** Only attempt to fire unbounce event for users who do
+		* not currently have utm_medium=email in query params,
+		* do not have utm cookie signifying a previous visit with
+		* utm_medium=email in query params, and are on a route
+		* with unbounce: true
+		*/
+		const unbounceEnabled = unbounceRoute && !utmEmailMedium && !utmEmailMediumCookie;
 		if (unbounceEnabled) {
 			try {
 				const data = this.apollo.readQuery({
