02e54503a92ecd7372e07dcc33a5878c83556049
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2022-05-04 12:03:23 -0500

    feat: tests were added to check both types of input in the loanamountbutton component

--- src/components/LoanCards/Buttons/LendAmountButton.vue
+++ src/components/LoanCards/Buttons/LendAmountButton.vue
@@ -31,7 +31,7 @@ export default {
 	},
 	computed: {
 		amountValue() {
-			return parseInt(parseFloat(this.amountLeft), 10);
+			return parseFloat(this.amountLeft).toFixed();
 		}
 	},
 	methods: {
