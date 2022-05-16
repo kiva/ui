a40276e694e7510559d818a6ccc3170534e6823a
Casey Dyer <dyersituations@gmail.com>
2022-05-16 15:05:30 -0700

    fix: added missing mocked function for auth0 that was causing error on load locally

--- src/util/KvAuth0.js
+++ src/util/KvAuth0.js
@@ -357,6 +357,7 @@ export const MockKvAuth0 = {
 	getKivaId: () => undefined,
 	getLastLogin: () => 0,
 	getMfaEnrollToken: () => Promise.resolve({}),
+	fakeAuthAllowed: () => undefined,
 	getFakeAuthCookieValue: () => undefined,
 	getFakeIdTokenPayload: () => undefined,
 	getSyncCookieValue: () => null,
