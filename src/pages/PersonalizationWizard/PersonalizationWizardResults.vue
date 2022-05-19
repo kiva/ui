e4d64f2296d53dbb6a683487de22950c6b975c40
EmilyG <emily.goroza@gmail.com>
2022-05-19 14:21:37 -0400

    feat: update 1

--- src/pages/PersonalizationWizard/PersonalizationWizardResults.vue
+++ src/pages/PersonalizationWizard/PersonalizationWizardResults.vue
@@ -1,7 +1,13 @@
 <template>
 	<www-page>
 		<template>
-			This is the personalization wizard results page
+			<kv-page-container>
+				<div class="tw-mt-4">
+					<h2 class="tw-text-h1 tw-mb-2">
+						We found the perfect loan for you!
+					</h2>
+				</div>
+			</kv-page-container>
 		</template>
 	</www-page>
 </template>
@@ -9,9 +15,13 @@
 <script>
 // import gql from 'graphql-tag';
 import WwwPage from '@/components/WwwFrame/WwwPage';
+import KvGrid from '~/@kiva/kv-components/vue/KvGrid';
+import KvPageContainer from '~/@kiva/kv-components/vue/KvPageContainer';
 
 export default {
 	components: {
+		KvGrid,
+		KvPageContainer,
 		WwwPage
 	},
 };
