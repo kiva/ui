c9f16337887f22c4eca57c566d6148ebd29d6678
EmilyG <emily.goroza@gmail.com>
2022-06-06 15:49:03 -0400

    fix: update styles

--- src/components/Checkout/UpsellModule.vue
+++ src/components/Checkout/UpsellModule.vue
@@ -1,46 +1,48 @@
 <template>
-	<div class="tw-bg-brand-100 tw-rounded tw-p-4 tw-flex tw-gap-4 tw-flex-col md:tw-flex-row">
-		<div>
-			<img :src="imageUrl" class="tw-rounded-full tw-w-16">
-		</div>
-		<div class="tw-grow tw-flex tw-flex-col tw-justify-center upsellLoanDetails">
-			<h4 class="tw-text-h4 tw-text-action tw-mb-0.5">
-				Support Another Borrower
-			</h4>
-			<h3 class="tw-text-h3 tw-mb-2">
-				Complete {{ loan.name }}'s loan for just ${{ amountLeft }}
-			</h3>
-			<div>
-				<fundraising-status-meter
-					:amount-left="amountLeft"
-					:percent-raised="percentRaised"
-					:is-funded="loan.status==='funded'"
-					:short-meter="true"
-					class="tw-mb-1"
+	<div class="tw-bg-brand-100 tw-rounded tw-p-4">
+		<div class="tw-w-full tw-text-right tw-h-3">
+			<button
+				@click="closeUpsellModule()"
+			>
+				<kv-material-icon
+					class="tw-w-3"
+					:icon="mdiClose"
 				/>
-				<p class="tw-text-h4 tw-m-0">
-					{{ amountLeft | numeral('$0,0[.]00') }} to go
-				</p>
-			</div>
+			</button>
 		</div>
-		<div class="tw-grow tw-flex tw-flex-wrap">
-			<div class="tw-w-full tw-text-right">
-				<button
-					@click="closeUpsellModule()"
-				>
-					<kv-material-icon
-						class="tw-w-3"
-						:icon="mdiClose"
-					/>
-				</button>
+		<div class="tw-flex tw-gap-1 md:tw-gap-4 tw-flex-col md:tw-flex-row tw-flex-wrap">
+			<div>
+				<img :src="imageUrl" class="tw-rounded-full sm:tw-w-6 tw-w-16">
 			</div>
-			<div class="tw-w-full tw-text-right">
-				<kv-button variant="link"
-					class="tw-w-44 lg:tw-w-full tw-mt-7"
-					@click="addToBasket(loanId, amountLeft)"
-				>
-					Add loan to basket
-				</kv-button>
+			<div class="tw-grow tw-flex tw-flex-col tw-justify-center upsellLoanDetails">
+				<h4 class="tw-text-h4 tw-text-action tw-mb-0.5">
+					Support Another Borrower
+				</h4>
+				<h3 class="tw-text-h3 tw-mb-2">
+					Complete {{ loan.name }}'s loan for just ${{ amountLeft }}
+				</h3>
+				<div>
+					<fundraising-status-meter
+						:amount-left="amountLeft"
+						:percent-raised="percentRaised"
+						:is-funded="loan.status==='funded'"
+						:short-meter="true"
+						class="tw-mb-1"
+					/>
+					<p class="tw-text-h4 tw-m-0">
+						{{ amountLeft | numeral('$0,0[.]00') }} to go
+					</p>
+				</div>
+			</div>
+			<div class="tw-grow tw-flex tw-flex-wrap">
+				<div class="tw-w-full tw-text-right tw-self-end">
+					<kv-button variant="link"
+						class="tw-w-full md:tw-w-44 tw-mt-7"
+						@click="addToBasket(loanId, amountLeft)"
+					>
+						Add loan to basket
+					</kv-button>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -106,12 +108,6 @@ export default {
 @media screen and (min-width: 1160px) {
 	.upsellLoanDetails {
 		min-width: 600px;
-	}
-}
-
-@media screen and (max-width: 400px) {
-	.upsellModule {
-		flex-direction: column;
 	}
 }
 
