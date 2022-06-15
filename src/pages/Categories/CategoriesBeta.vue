495dc1eb0b473b0a9900aa80277cce33aa41c2a4
Galen Otten <galeno@interns.kiva.org>
2022-06-15 16:11:51 -0400

    feat: first commit for the CategoriesBeta page with page frame, routing, and intro text

---
+++ src/pages/Categories/CategoriesBeta.vue
@@ -0,0 +1,47 @@
+<template>
+<www-page class="category-beta">
+
+		<div class="row">
+			<div class="small-12 columns tw-mt-3 tw-p-2">
+				<h1 class="tw-mb-2">
+					Make a loan, change a life
+				</h1>
+				<p class="tw-mb-4 tw-max-w-screen-lg">
+					Each Kiva loan helps people build a better
+					future for themselves and their families.
+				</p>
+			</div>
+		</div> 
+
+		<div class="row">
+		    <div class="small-12 columns">
+				<h2 class=" tw-mb-1">
+					Find loans by category
+				</h2>
+		    </div>
+        </div> 
+
+
+	</www-page>
+</template>
+
+<script>
+import WwwPage from '@/components/WwwFrame/WwwPage';
+
+
+
+export default {
+	name: 'CategoriesBeta',
+	components: {
+		WwwPage
+    },
+	data() {
+		return {
+		};
+	}
+	// computed: {
+	// },
+
+};
+
+</script>
---
+++ src/pages/Categories/MainCategoryTile.vue
@@ -0,0 +1,22 @@
+<template>
+	
+</template>
+
+<script>
+
+
+export default {
+	name: 'MainCategoryTile',
+	components: {
+		
+	},
+	// data() {
+	// 	return {
+	// 	};
+	// },
+	// computed: {
+	// },
+
+};
+
+</script>
