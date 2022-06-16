1a8205f8b1f424ac5f3ba1bb1022c832b264f251
Matt Stover <mcs@mcstover.com>
2022-06-16 16:23:02 -0600

    chore: add new cache nodes

--- config/k8sprod.js
+++ config/k8sprod.js
@@ -6,7 +6,7 @@ module.exports = merge(prod, {
 		publicPath: 'https://www-kiva-org.freetls.fastly.net/',
 	},
 	server: {
-		memcachedServers: 'awsprod-ui-memcached-cluster.xd6r4x.0001.usw2.cache.amazonaws.com:11211,awsprod-ui-memcached-cluster.xd6r4x.0002.usw2.cache.amazonaws.com:11211',
+		memcachedServers: 'awsprod-ui-memcached-cluster.xd6r4x.0001.usw2.cache.amazonaws.com:11211,awsprod-ui-memcached-cluster.xd6r4x.0002.usw2.cache.amazonaws.com:11211,awsprod-ui-memcached-cluster.xd6r4x.0003.usw2.cache.amazonaws.com:11211,awsprod-ui-memcached-cluster.xd6r4x.0004.usw2.cache.amazonaws.com:11211',
 		enableDDTrace: true,
 		disableCluster: true,
 	}
