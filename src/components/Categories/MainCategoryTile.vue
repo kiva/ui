17988972c1af8e8f705598ef42d80a285f2482d8
Galen <90521087+galen94@users.noreply.github.com>
2022-06-28 09:03:14 -0400

    Ack 323 category card storybook stories (#3947)
    * feat: first commit for the CategoriesBeta page with page frame, routing, and intro text
    
    * feat: first commit for the CategoriesBeta page
    with page frame, routing, and intro text
    
    * feat: first commit for CategoriesBeta page with page frame, intro text, and routing
    
    * feat: first commit for CategoriesBeta page with page frame, intro text, and routing
    
    * feat: first commit for CategoriesBeta page with page frame, intro text, and routing
    
    * feat: removing foundation CSS, rearranging pages/components files, draft of category tile-unfinished
    
    * feat: fixing indentation and spacing
    
    * feat: adding smaller tiles
    
    * fix: reset package-lock file
    
    * feat: minor changes
    
    * feat: fix intro section
    
    * feat: storybook category component, GraphQL API call
    
    * fix: making changes from the PR comments
    
    Co-authored-by: Galen Otten <galeno@interns.kiva.org>
    Co-authored-by: Eddie Ferrer <eddieferrer@users.noreply.github.com>

--- src/components/Categories/MainCategoryTile.vue
+++ src/components/Categories/MainCategoryTile.vue
@@ -1,59 +1,75 @@
 <template>
 	<div v-if="tileSize === 'large'" class="tw-mb-6">
-		<img class="tw-rounded tw-w-full"
-			:src="image"
-		>
-		<div class="tw-text-center tw-flex tw-justify-between">
-			<h3 class="tw-mt-3 tw-mb-3">
-				Lend to women
-			</h3>
-			<h4 class="tw-mt-3 tw-mb-3">
-				258 loans
-			</h4>
-		</div>
-		<div>
-			<span>
-				Help women around the world pursue their aspirations and gain
-				decision-making power in their homes and communities.
-			</span>
-		</div>
+		<kv-responsive-image class="category-image"
+			:images="retinaImageExists ?
+				[['small', image],
+					['small retina', retinaImage]]
+				:
+				[['small', image]]"
+			loading="lazy"
+			:alt="imageAlt"
+		/>
+		<large-tile-info
+			:category-name="categoryName"
+			:category-description="categoryDescription"
+			:number-loans="numberLoans"
+		/>
 	</div>
 	<div v-else-if="tileSize === 'medium'" class="tw-mb-6">
-		<img class="tw-rounded tw-w-full"
-			:src="image"
-		>
-		<h3 class="tw-mt-2 tw-mb-2">
-			Lend to women
-		</h3>
-		<div>
-			<span>
-				Help women around the world pursue their aspirations and gain
-				decision-making power in their homes and communities.
-			</span>
-		</div>
-		<h4 class="tw-mt-2 tw-mb-2 ">
-			258 loans
-		</h4>
-	</div>
-	<div v-else-if="tileSize === 'small'" class=" tw-mb-3">
-		<div class="tw-flex">
-			<div class="tw-mr-2">
-				<img class="tw-rounded"
-					:src="image"
-				>
-			</div>
+		<kv-responsive-image class="category-image"
+			:images="retinaImageExists ?
+				[['small', image],
+					['small retina', retinaImage]]
+				:
+				[['small', image]]"
+			loading="lazy"
+			:alt="imageAlt"
+		/>
+		<div class="tw-hidden md:tw-block">
+			<h3 class="tw-mt-2 tw-mb-2">
+				Lend to {{ categoryName }}
+			</h3>
 			<div>
-				<h3 class=" tw-mb-1">
-					Lend to women
+				<span class="tw-line-clamp-3">
+					{{ categoryDescription }}
+				</span>
+			</div>
+			<h4 class="tw-mt-1 tw-mb-2 ">
+				{{ numberLoans }} loans
+			</h4>
+		</div>
+		<div class="md:tw-hidden">
+			<large-tile-info
+				:category-name="categoryName"
+				:category-description="categoryDescription"
+				:number-loans="numberLoans"
+			/>
+		</div>
+	</div>
+	<div v-else-if="tileSize === 'small'" class=" tw-mb-6">
+		<div class="tw-flex">
+			<div class="tw-mr-2 tw-flex-none">
+				<kv-responsive-image class="category-image-small"
+					:images="retinaImageExists ?
+						[['small', image],
+							['small retina', retinaImage]]
+						:
+						[['small', image]]"
+					loading="lazy"
+					:alt="imageAlt"
+				/>
+			</div>
+			<div class="tw-grow">
+				<h3 class=" tw-mb-2">
+					Lend to {{ categoryName }}
 				</h3>
 				<div>
-					<span>
-						Help women around the world pursue their aspirations and gain
-						decision-making power in their homes and communities.
+					<span class="tw-line-clamp-2">
+						{{ categoryDescription }}
 					</span>
 				</div>
-				<h4 class="tw-mt-1 tw-mb-2 ">
-					258 loans
+				<h4 class="tw-mt-1 tw-mb-1 ">
+					{{ numberLoans }} loans
 				</h4>
 			</div>
 		</div>
@@ -61,6 +77,10 @@
 </template>
 
 <script>
+
+import KvResponsiveImage from '@/components/Kv/KvResponsiveImage';
+import LargeTileInfo from '@/components/Categories/LargeTileInfo';
+
 export default {
 	name: 'MainCategoryTile',
 	props: {
@@ -68,17 +88,69 @@ export default {
 			type: String,
 			default: 'small'
 		},
+		categoryName: {
+			type: String,
+			default: ''
+		},
+		categoryDescription: {
+			type: String,
+			default: ''
+		},
+		numberLoans: {
+			type: Number,
+			default: 0
+		},
+		image: {
+			type: String,
+			default: ''
+		},
+		retinaImage: {
+			type: String,
+			default: ''
+		},
+		imageAlt: {
+			type: String,
+			default: ''
+		}
 	},
 	components: {
+		KvResponsiveImage,
+		LargeTileInfo
 	},
 	data() {
 		return {
-			image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZmwTlZfck9YScpFfUPHQ6HVyJ3hU7SnN9Xg&usqp=CAU'
 		};
 	},
+	computed: {
+		retinaImageExists() {
+			if (this.retinaImage === '') {
+				return false;
+			}
+			return true;
+		},
+	}
 };
 </script>
 
 <style lang="scss" scoped>
-@import "settings";
+
+.category-image {
+	::v-deep {
+		img {
+			border-radius: 15px;
+		}
+	}
+}
+
+.category-image-small {
+	::v-deep {
+		img {
+			border-radius: 15px;
+			width: 152px;
+			height: 152px;
+			object-fit: cover;
+		}
+	}
+}
+
 </style>
