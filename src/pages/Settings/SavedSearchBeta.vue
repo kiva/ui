068d6da79fb5e1828f3872b737f02f2a7cb4a273
Tiara S. Coleman <tiarac@kiva.org>
2022-07-18 15:53:50 -0400

    feat: create SavedSearchItem component + use for entry list (new edits)

--- src/pages/Settings/SavedSearchBeta.vue
+++ src/pages/Settings/SavedSearchBeta.vue
@@ -1,33 +1,46 @@
 <template>
-	<www-page>
+	<www-page
+		:gray-background="true"
+	>
+		<template #secondary>
+			<the-my-kiva-secondary-menu />
+		</template>
 		<kv-page-container>
-			<kv-grid class="tw-pt-4 md:tw-pt-6 lg:tw-pt-8">
-				<h1 class="tw-mb-2">
-					Saved searches
-				</h1>
+			<kv-grid class="row-column">
+				<h2 class="tw-mb-2 tw-justify-start">
+					Saved Searches
+				</h2>
 			</kv-grid>
-			<kv-grid class="md:tw-pt-6 lg:tw-pt-8">
-				<div
-					v-for="(search, index) in savedSearches"
-					:key="index"
-				>
-					{{ search.name }}
-				</div>
-			</kv-grid>
+
+			<div>
+				<kv-grid class="tw-row-col tw-p-6 tw-m-1">
+					<save-search-item
+						v-for="(search, index) in savedSearches"
+						class="tw-grid tw-grid-col-1 tw-gap-4 tw-rounded tw-border-transparent
+						tw-max-w-lg tw-bg-primary"
+						:key="index"
+						:saved-search="search"
+					/>
+				</kv-grid>
+			</div>
 		</kv-page-container>
 	</www-page>
 </template>
 
 <script>
 import gql from 'graphql-tag';
+import SaveSearchItem from '@/components/Settings/SaveSearchItem';
 import WwwPage from '@/components/WwwFrame/WwwPage';
+import TheMyKivaSecondaryMenu from '@/components/WwwFrame/Menus/TheMyKivaSecondaryMenu';
 import KvGrid from '~/@kiva/kv-components/vue/KvGrid';
 import KvPageContainer from '~/@kiva/kv-components/vue/KvPageContainer';
 
 export default {
 	name: 'SavedSearchBeta',
 	components: {
+		SaveSearchItem,
 		WwwPage,
+		TheMyKivaSecondaryMenu,
 		KvGrid,
 		KvPageContainer,
 	},
@@ -35,7 +48,7 @@ export default {
 	data() {
 		return {
 			totalCount: 0,
-			savedSearches: () => []
+			savedSearches: () => [],
 		};
 	},
 	created() {
