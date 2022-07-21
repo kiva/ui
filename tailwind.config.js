f01c2fb8efb9692b25b81ad8d168a03e975af17d
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2022-07-21 09:08:57 -0600

    fix: better tailwind jit compiling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const kvConfig = require('@kiva/kv-components/tailwind.config.cjs');
 
 module.exports = {
+	prefix: 'tw-',
 	presets: [kvConfig],
 	content: [
 		'./node_modules/@kiva/kv-components/**/*.vue',
