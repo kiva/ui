e71732e872f4eeb25b84798d402e0fabe03fbb55
Christien Bedon <christian.bedon@contractors.kiva.org>
2022-09-08 12:22:19 -0500

    fix: style eslint

--- src/components/LoanCollections/HomeExp/LoanCategorySelectorHomeExp.vue
+++ src/components/LoanCollections/HomeExp/LoanCategorySelectorHomeExp.vue
@@ -81,8 +81,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-	@media(min-width: 734px) {
-
+	@media (min-width: 734px) {
 		.tabs-container {
 			width: 15.25rem;
 			@apply tw-flex md:tw-flex-col;
