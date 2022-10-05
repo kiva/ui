5cbd50dcf77c674671fd17ed525ba9b820416397
Matt Stover <mcs@mcstover.com>
2022-10-05 11:32:30 -0700

    fix: remove cc/kiva-universal from dev lighthouse

--- lighthouserc-dev.js
+++ lighthouserc-dev.js
@@ -12,8 +12,8 @@ module.exports = {
 				'http://localhost:8888/lend/filter',
 				'http://localhost:8888/lend-by-category/women',
 				'http://localhost:8888/ui-site-map',
-				'http://localhost:8888/cc/kiva-universal',
-				'http://localhost:8888/lp/support-refugees'
+				// 'http://localhost:8888/cc/kiva-universal',
+				'http://localhost:8888/lp/how-kiva-works'
 			],
 			numberOfRuns: 5,
 		},
