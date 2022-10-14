2c0281da8fcef9e77003cc25f320923a97de42d3
Christien Bedon <christian.bedon@contractors.kiva.org>
2022-10-13 19:35:07 -0500

    revert: loan search filters file

--- src/components/CorporateCampaign/LoanSearch/LoanSearchFilters.vue
+++ src/components/CorporateCampaign/LoanSearch/LoanSearchFilters.vue
@@ -369,7 +369,6 @@ export default {
 			const genderOptions = [
 				{ name: 'Women', key: 'female', __typename: 'GenderEnum' },
 				{ name: 'Men', key: 'male', __typename: 'GenderEnum' },
-				{ name: 'Nonbinary', key: 'nonbinary', __typename: 'GenderEnum' },
 			];
 			const selectedGenderRaw = genderOptions.filter(gender => {
 				if (this.appliedFilters && this.appliedFilters.gender) {
@@ -590,10 +589,11 @@ export default {
 		align-items: center;
 		justify-content: space-between;
 		flex-direction: column;
+		margin: 0 1rem 1rem;
 
 		@include breakpoint(medium) {
 			flex-direction: row;
-			margin: 0 0 1rem;
+			margin: 0 3.5rem 1rem;
 		}
 	}
 
