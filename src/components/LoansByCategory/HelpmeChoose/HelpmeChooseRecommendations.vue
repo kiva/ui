ef097e111e369706f5e1f6ef5e4336c6c3b1c2cd
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2022-10-24 15:12:46 -0500

    feat: borrower picker was created for help me choose experiment

--- src/components/LoansByCategory/HelpmeChoose/HelpmeChooseRecommendations.vue
+++ src/components/LoansByCategory/HelpmeChoose/HelpmeChooseRecommendations.vue
@@ -1,4 +1,22 @@
 <template>
-	<div>
+	<div class="tw-flex">
+		<!-- eslint-disable max-len -->
+		<helpme-choose-borrower-selector
+			image-url="https://res.cloudinary.com/kiva/w_960,h_600,c_fill,g_faces/a_ignore,fl_progressive,q_auto:best,f_auto/remote/ea3be8ae591d38de82c7a81b3657b22e.jpg"
+			name="BV Goudiry Group"
+			tag="Latinx/Hispanic-Owned Business"
+			:selected-loan="true"
+		/>
 	</div>
 </template>
+
+<script>
+import HelpmeChooseBorrowerSelector from './HelpmeChooseBorrowerSelector';
+
+export default {
+	name: 'HelpmeChooseRecommendations',
+	components: {
+		HelpmeChooseBorrowerSelector
+	}
+};
+</script>
