4b6400fa50e852b67b933416b6938adbb7ff86ad
Christian Bedon <christian.bedon@contractors.kiva.org>
2022-11-15 16:16:57 -0500

    refactor: step elements

--- src/components/Thanks/ShareStepper.vue
+++ src/components/Thanks/ShareStepper.vue
@@ -2,35 +2,14 @@
 	<div class="tw-my-2 tw-px-2">
 		<div style="width: 136px" class="tw-mx-auto tw-px-1">
 			<div class="tw-flex tw-items-center">
-				<div class="tw-flex tw-items-center tw-text-brand tw-relative">
-					<div
-						class="tw-bg-brand tw-h-3.5 tw-rounded-full tw-w-3.5"
-					>
-						<kv-icon
-							class="tw-fill-current tw-inline-flex tw-max-h-full tw-ml-1 tw-mt-1 tw-text-white tw-w-1.5"
-							name="checkmark"
-						/>
-					</div>
-					<p
-						class="tw-text-small tw-absolute tw-font-medium tw-mt-7 tw-text-center tw-uppercase tw--ml-0.5"
-					>
-						Lend
-					</p>
-				</div>
-				<div
-					class="tw-flex-auto tw-border-t-2 tw-border-brand"
-				></div>
-				<div class="tw-flex tw-items-center tw-text-brand tw-relative">
-					<div
-						class="tw-border-2 tw-border-brand tw-h-3.5 tw-rounded-full tw-w-3.5"
-					>
-					</div>
-					<p
-						class="tw-text-small tw-absolute tw-font-medium tw-mt-7 tw-text-center tw-uppercase tw--ml-1"
-					>
-						Share
-					</p>
-				</div>
+				<template v-for="step in steps">
+					<ShareStepperStep
+						:key="step.key"
+						:step="step.key"
+						:last-step="steps.length - 1"
+						:text="step.text"
+					/>
+				</template>
 			</div>
 		</div>
 		<p class="tw-text-center tw-mt-4 tw-text-h3">
@@ -40,16 +19,30 @@
 </template>
 
 <script>
-import KvIcon from '@/components/Kv/KvIcon';
+import ShareStepperStep from '@/components/Thanks/ShareStepperStep';
 
 export default {
 	name: 'ShareStepper',
-	components: { KvIcon },
+	components: { ShareStepperStep },
 	props: {
 		lenderName: {
 			type: String,
 			default: ''
 		}
+	},
+	data() {
+		return {
+			steps: [
+				{
+					key: 0,
+					text: 'Lend',
+				},
+				{
+					key: 1,
+					text: 'Share',
+				},
+			]
+		};
 	}
 };
 </script>
---
+++ src/components/Thanks/ShareStepperStep.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="tw-flex tw-items-center tw-text-brand tw-relative">
+		<div>
+			<div
+				class="tw-h-3.5 tw-rounded-full tw-w-3.5"
+				:class="[
+					{'tw-bg-brand': step !== lastStep},
+					{ 'tw-bg-transparent tw-border-2 tw-border-brand': step === lastStep }
+				]"
+			>
+				<kv-icon
+					v-if="step !== lastStep"
+					class="tw-fill-current tw-inline-flex tw-max-h-full tw-ml-1 tw-mt-1 tw-text-white tw-w-1.5"
+					name="checkmark"
+				/>
+			</div>
+			<p
+				:class="`tw-text-small tw-absolute tw-font-medium tw-mt-0.5 tw-text-center
+				tw-uppercase tw--ml-${0.5 * (step + 1)}`"
+			>
+				{{ text }}
+			</p>
+		</div>
+		<div v-if="step !== lastStep" class="tw-w-8 tw-border-t-2 tw-border-brand"></div>
+	</div>
+</template>
+<script>
+import KvIcon from '@/components/Kv/KvIcon';
+
+export default {
+	name: 'ShareStepperStep',
+	components: { KvIcon },
+	props: {
+		step: {
+			type: Number,
+			default: 0
+		},
+		lastStep: {
+			type: Number,
+			default: 1
+		},
+		iconName: {
+			type: String,
+			default: ''
+		},
+		text: {
+			type: String,
+			default: ''
+		}
+	}
+};
+</script>
