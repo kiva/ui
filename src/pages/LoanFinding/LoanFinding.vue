09abc4f47dbbbf39b757b3d8fb4e9fb6f0eafa3a
EmilyG <emily.goroza@gmail.com>
2022-11-28 15:35:13 -0500

    feat: new temp loan cards for MVP loan finding page

--- src/pages/LoanFinding/LoanFinding.vue
+++ src/pages/LoanFinding/LoanFinding.vue
@@ -1,5 +1,20 @@
 <template>
 	<div>
-		New loan finding page
+		New loan finding page with Kiva classic loan card
+		<kiva-classic-basic-loan-card
+			:loan-id="2474811"
+			:show-action-button="true"
+		/>
 	</div>
 </template>
+
+<script>
+import KivaClassicBasicLoanCard from '@/components/LoanCards/KivaClassicBasicLoanCard';
+
+export default {
+	name: 'LoanFinding',
+	components: {
+		KivaClassicBasicLoanCard
+	}
+};
+</script>
