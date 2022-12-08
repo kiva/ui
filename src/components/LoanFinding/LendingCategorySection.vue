edd90e40225a7ae19d8e2df858f0a63973b8862c
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2022-12-08 11:57:11 -0600

    feat: recommended loans section added to lending page

---
+++ src/components/LoanFinding/LendingCategorySection.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h2 class="tw-text-h2 tw-text-primary">
+			Recommended for you
+		</h2>
+		<p class="tw-text-subhead tw-text-primary">
+			Loans handpicked for you based on your lending history
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LendingCategorySection'
+};
+</script>
