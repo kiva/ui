84d25b8aaf4392588a4b5c9f7c48cb124e8d87ea
Matt Stover <mcs@mcstover.com>
2023-02-23 15:35:32 -0800

    fix: extract protectedRoutes and adjust checking mechanism and add new routes

---
+++ server/util/protectedRoutes.js
@@ -0,0 +1,19 @@
+module.exports = [
+	'auto-deposit',
+	'checkout',
+	'confirm-instant-donation',
+	'instant-donation-thanks',
+	'instant-lending-error',
+	'lend/saved-search',
+	'monthlygood/setup',
+	'monthlygood/thanks',
+	'join-team',
+	'portfolio',
+	'process-instant-lending',
+	'process-browser-auth',
+	'register',
+	'settings',
+	'start-verification',
+	'UI_REVISION',
+	'ui-site-map'
+];
