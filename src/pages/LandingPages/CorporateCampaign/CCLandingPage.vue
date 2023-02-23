7d946858a4a9c9ba5d3cbdc0a527cf69a77e72c3
Cam Parra <caeparra@gmail.com>
2023-02-22 17:58:23 -0700

    CIT-66 Removes add team pop up from checkout flow and adds a new checkbox for adding a team in the checkout box
    Signed-off-by: Cam Parra <caeparra@gmail.com>

--- src/pages/LandingPages/CorporateCampaign/CCLandingPage.vue
+++ src/pages/LandingPages/CorporateCampaign/CCLandingPage.vue
@@ -163,6 +163,7 @@
 					:show-donation="isMatchingCampaign || lendingRewardOffered"
 					:auto-redirect-to-thanks="false"
 					:promo-fund="promoFund"
+					:teamId="teamId"
 					@credit-removed="handleCreditRemoved"
 					@transaction-complete="transactionComplete"
 					@refresh-totals="refreshTotals"
