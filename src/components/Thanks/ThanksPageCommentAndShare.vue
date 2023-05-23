3575537398e7642e4baecd399dcef5bbe7c4d221
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2023-05-23 13:33:17 -0600

    fix: add success/error classes back

--- src/components/Thanks/ThanksPageCommentAndShare.vue
+++ src/components/Thanks/ThanksPageCommentAndShare.vue
@@ -424,6 +424,36 @@ $color-text: #212121;
 				transition: fill 0.25s ease-in;
 			}
 		}
+
+		&--success {
+			background-color: rgb(var(--bg-brand));
+			border-color: rgb(var(--bg-brand));
+		}
+
+		&--error {
+			background-color: rgb(var(--bg-danger));
+			border-color: rgb(var(--bg-danger));
+		}
+
+		&--success,
+		&--error {
+			color: #fff;
+			cursor: default;
+			transition:
+				background-color 0.25s ease-out,
+				border-color 0.25s ease-out,
+				color 0.25s ease-out;
+
+			&:hover {
+				color: #fff;
+				text-decoration: none;
+			}
+
+			.social__icon {
+				transition: fill 0.25s ease-out;
+				fill: #fff;
+			}
+		}
 	}
 }
 </style>
