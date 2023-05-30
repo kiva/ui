c389a76ebff79a0ce169fdc0f1faf413e4886f7d
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2023-05-30 09:54:47 -0600

    feat: populate lending stats badges section from contentful
    * Add a loader and a no badges message
    * Get all badge information from contentful
    
    ACK-614

--- src/pages/Portfolio/LendingStats/BadgesSection.vue
+++ src/pages/Portfolio/LendingStats/BadgesSection.vue
@@ -2,45 +2,48 @@
 	<section data-testid="lend-stat-badges" class="stats-section">
 		<h2 class="tw-flex tw-gap-2 tw-mb-4">
 			<span>My Badges</span>
-			<span class="lent-count tw-text-base tw-bg-brand tw-text-white tw-py-0.5 tw-px-1 tw-self-center">
-				{{ badgesObtained }}/{{ totalBadges }}
+			<span
+				v-if="!isLoading"
+				class="tw-text-base tw-bg-brand tw-text-white tw-py-0.5 tw-px-1 tw-self-center"
+			>
+				{{ badgesObtained }}/{{ totalPossibleBadges }}
 			</span>
+			<kv-loading-placeholder
+				v-else
+				class="tw-py-0.5 tw-px-1 tw-self-center"
+				:style="{width: '3rem', height: '2rem'}"
+			/>
 		</h2>
-		<div class="tw-flex tw-flex-wrap">
-			<template v-for="(achievement, index) in completedAchievements">
+		<!-- Loading Template -->
+		<div class="tw-flex" v-if="isLoading">
+			<kv-loading-placeholder
+				class="tw-h-10 tw-w-12 tw-flex-none tw-mx-auto tw-mr-2"
+				:style="{width: '6rem', height: '5rem'}"
+			/>
+			<div class="tw-w-full">
+				<kv-loading-placeholder
+					class="tw-mb-2"
+					:style="{width: '6.5rem', height: '1.5rem'}"
+				/>
+				<kv-loading-placeholder
+					:style="{width: '3.5rem', height: '1rem'}"
+				/>
+			</div>
+		</div>
+		<!-- Badges Template -->
+		<div class="tw-grid tw-grid-cols-1 md:tw-grid-cols-2 tw-gap-2" v-else>
+			<p v-if="completedAchievements.length === 0">
+				You haven't earned any badges yet.
+			</p>
+			<template v-for="(challenge, index) in challengeDataContentful" v-else>
 				<div
-					v-if="achievement.achievementId === EARTHDAY_23_CHALLENGE_KEY"
-					:key="index" class="tw-flex tw-mr-3 tw-mb-2"
+					:key="index" class="tw-flex tw-w-full"
 				>
-					<earth-day-challenge-badge class="tw-h-10 tw-w-12 tw-flex-none tw-mx-auto" />
+					<img :src="challenge.badgeSvg" class="tw-h-10 tw-w-12 tw-flex-none tw-mx-auto">
 					<div class="tw-w-full">
-						<span class="tw-font-medium">Earth Day Challenge</span>
+						<span class="tw-font-medium">{{ challenge.challengeName }}</span>
 						<p class="tw-text-secondary tw-text-small">
-							April 2023
-						</p>
-					</div>
-				</div>
-				<div
-					v-if="achievement.achievementId === IWD_CHALLENGE_KEY"
-					:key="index" class="tw-flex tw-mr-3 tw-mb-2"
-				>
-					<iwd-challenge-badge class="tw-h-10 tw-w-12 tw-flex-none tw-mx-auto" />
-					<div class="tw-w-full">
-						<span class="tw-font-medium">Women's Day Challenge</span>
-						<p class="tw-text-secondary tw-text-small">
-							March 2023
-						</p>
-					</div>
-				</div>
-				<div
-					v-if="achievement.achievementId === CLIMATE_CHALLENGE_KEY"
-					:key="index" class="tw-flex"
-				>
-					<eco-challenge-badge class="tw-h-10 tw-w-12 tw-flex-none tw-mx-auto" />
-					<div class="tw-w-full">
-						<span class="tw-font-medium">Eco-friendly October Challenge</span>
-						<p class="tw-text-secondary tw-text-small">
-							October 2022
+							{{ challenge.dateTagline }}
 						</p>
 					</div>
 				</div>
@@ -50,21 +53,16 @@
 </template>
 
 <script>
-import EcoChallengeBadge from '@/assets/icons/inline/achievements/eco-badge.svg';
-import IwdChallengeBadge from '@/assets/icons/inline/achievements/iwd-badge.svg';
-import EarthDayChallengeBadge from '@/assets/icons/inline/achievements/earthday-badge.svg';
-
-import {
-	EARTHDAY_23_CHALLENGE_KEY, IWD_CHALLENGE_KEY, CLIMATE_CHALLENGE_KEY
-} from '@/util/achievementUtils';
+import { gql } from '@apollo/client';
+import { formatMediaAssetArray } from '@/util/contentfulUtils';
+import KvLoadingPlaceholder from '~/@kiva/kv-components/vue/KvLoadingPlaceholder';
 
 export default {
 	name: 'BadgesSection',
+	inject: ['apollo'],
 	data() {
 		return {
-			EARTHDAY_23_CHALLENGE_KEY,
-			IWD_CHALLENGE_KEY,
-			CLIMATE_CHALLENGE_KEY
+			challengeDataContentful: [],
 		};
 	},
 	props: {
@@ -72,20 +70,61 @@ export default {
 			type: Array,
 			default: () => []
 		},
-		totalBadges: {
+		/* total number of possible badges */
+		totalPossibleBadges: {
 			type: Number,
 			default: 0
 		},
 	},
+	components: {
+		KvLoadingPlaceholder,
+	},
 	computed: {
 		badgesObtained() {
 			return this.completedAchievements.length ?? 0;
+		},
+		isLoading() {
+			return this.totalPossibleBadges === 0;
+		},
+		/* Challenge keys for completed challenges */
+		completedChallengeKeys() {
+			return this.completedAchievements.map(achievement => achievement.achievementId).join(',');
+		},
+	},
+	watch: {
+		// When this prop has loaded we can load the badges content from contentful
+		totalPossibleBadges(next) {
+			if (next !== 0) {
+				this.loadBadgesContent();
+			}
 		}
 	},
-	components: {
-		EcoChallengeBadge,
-		IwdChallengeBadge,
-		EarthDayChallengeBadge,
+	methods: {
+		loadBadgesContent() {
+			if (this.completedChallengeKeys !== '') {
+				const filterFields = `fields.key[in]=${this.completedChallengeKeys}`;
+				this.apollo.query({
+					query: gql`query contentfulDefinitions($customFields: String) {
+					contentful {
+						entries(contentType: "challenge", customFields: $customFields)
+					}
+				}`,
+					variables: {
+						customFields: filterFields
+					},
+				}).then(result => {
+					const contentfulData = result.data?.contentful?.entries?.items ?? null;
+					if (contentfulData) {
+						this.challengeDataContentful = contentfulData.map(item => {
+							return {
+								...item.fields,
+								badgeSvg: formatMediaAssetArray([item.fields.badgeImage])?.[0]?.file?.url,
+							};
+						});
+					}
+				});
+			}
+		}
 	},
 };
 </script>
--- src/util/achievementUtils.js
+++ src/util/achievementUtils.js
@@ -1,5 +1,6 @@
 import achievementMilestonesQuery from '@/graphql/query/achievementMilestones.graphql';
 
+// !TODO remove this when badge rework is complete
 export const EARTHDAY_23_CHALLENGE_KEY = 'earthday-23-challenge';
 export const IWD_CHALLENGE_KEY = 'iwd-challenge';
 export const CLIMATE_CHALLENGE_KEY = 'climate-challenge';
