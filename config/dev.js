95e2ec05846204fa3fa13e9d715d900f1d9b4e5b
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2023-07-13 18:22:56 -0700

    feat: option for requiring captcha on /register/social page MARS-446

--- config/dev.js
+++ config/dev.js
@@ -14,6 +14,7 @@ module.exports = merge(base, {
 		googleTagmanagerId: 'GTM-PXFRMT',
 		enableGA: true,
 		gaId: 'UA-11686022-3',
+		grecaptchaSitekey: '6LcXENcmAAAAAEC4ygspn1WTm4zP4gLexXDnWuXE',
 		enableHotjar: true,
 		hotjarId: '3071239',
 		enableSnowplow: true,
