8f6536044656915330d21e7129802c3d771b54a9
Christian Bedon <christian.bedon@contractors.kiva.org>
2023-09-12 15:26:20 -0500

    fix: replace inline style for tw class

--- src/pages/Portfolio/ImpactDashboard/YourDonations.vue
+++ src/pages/Portfolio/ImpactDashboard/YourDonations.vue
@@ -3,7 +3,7 @@
 		<h3 class="tw-mb-3 md:tw-mb-2">
 			Your donations to Kiva
 		</h3>
-		<kv-grid class="tw-grid-cols-12 tw-p-3 tw-rounded" :style="{ backgroundColor: '#F8F2E6' }">
+		<kv-grid class="tw-grid-cols-12 tw-p-3 tw-rounded tw-bg-marigold-1">
 			<div class="tw-col-span-12 md:tw-col-span-4 tw-text-center">
 				<kv-loading-placeholder v-if="loading" class="stat-placeholder" />
 				<dt v-show="!loading" class="stat-value">
