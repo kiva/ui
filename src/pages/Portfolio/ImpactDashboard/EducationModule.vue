87ec22ef9124fb51f1f042b05aef144d5ff42cb7
Christian Bedon <christian.bedon@contractors.kiva.org>
2023-09-25 10:50:25 -0500

    style: center education module icon

--- src/pages/Portfolio/ImpactDashboard/EducationModule.vue
+++ src/pages/Portfolio/ImpactDashboard/EducationModule.vue
@@ -1,14 +1,14 @@
 <template>
 	<async-portfolio-section @visible="whenVisible" data-testid="education-module">
 		<kv-grid class="tw-grid-cols-12 tw-items-center">
-			<div class="tw-col-span-12 lg:tw-col-span-6 tw-text-center md:tw-text-left">
-				<div class="tw-flex tw-rounded tw-h-12 tw-w-12 tw-bg-primary tw-p-1 tw-mr-2">
+			<div class="lg:tw-col-span-6 tw-col-span-12 md:tw-items-start tw-flex tw-flex-col tw-items-center">
+				<div class="tw-mr-0 md:tw-mr-2 tw-bg-primary tw-h-12 tw-p-1 tw-rounded tw-w-12">
 					<img class="tw-w-10 tw-h-10" alt="Leaf heart" :src="imageRequire(`./leaf_heart.svg`)">
 				</div>
-				<h2 class="tw-mb-0.5">
+				<h2 class="tw-mb-0.5 md:tw-text-left tw-text-center">
 					Donations power progress
 				</h2>
-				<p class="tw-text-subhead">
+				<p class="tw-text-subhead md:tw-text-left tw-text-center">
 					<!-- eslint-disable-next-line max-len -->
 					Every dollar donated to Kiva helps bring us one step closer to financial access for all. See what we’ve achieved with your support.
 				</p>
