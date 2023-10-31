b4a0ac1992abe517dbf591c75a7266de434f0301
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2023-10-31 12:00:36 -0600

    fix: boolean settings bug fixed for ftds in checkout and thanks pages

--- src/pages/Checkout/CheckoutPage.vue
+++ src/pages/Checkout/CheckoutPage.vue
@@ -282,6 +282,7 @@ import { gql } from '@apollo/client';
 import _get from 'lodash/get';
 import _filter from 'lodash/filter';
 import numeral from 'numeral';
+import { readBoolSetting } from '@/util/settingsUtils';
 import { preFetchAll } from '@/util/apolloPreFetch';
 import syncDate from '@/util/syncDate';
 import { myFTDQuery, formatTransactionData } from '@/util/checkoutUtils';
@@ -494,7 +495,8 @@ export default {
 			this.activeLoginDuration = parseInt(_get(data, 'general.activeLoginDuration.value'), 10) || 3600;
 
 			this.lenderTotalLoans = data?.my?.loans?.totalCount ?? 0;
-			this.isFtdMessageEnable = data?.general?.ftd_message_enable?.value ?? false;
+			// Enable FTDs message from settings
+			this.isFtdMessageEnable = readBoolSetting(data, 'general.ftd_message_enable.value');
 			this.ftdCreditAmount = data?.general?.ftd_amount?.value ?? '';
 			this.ftdValidDate = data?.general?.ftd_date?.value ?? '';
 		}
