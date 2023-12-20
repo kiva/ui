0068f5b8b2cbf58627ec560996710ea682404ddb
Matt Stover <mcs@mcstover.com>
2023-12-20 12:09:14 -0800

    fix: add trace + metric exporters

--- server/instrumentation.js
+++ server/instrumentation.js
@@ -1,19 +1,15 @@
 /* instrumentation.js */
 // Require dependencies
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
-const {
-	getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const {
-	PeriodicExportingMetricReader,
-	ConsoleMetricExporter,
-} = require('@opentelemetry/sdk-metrics');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto');
+const {	OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-proto');
+const {	getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const {	PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
 const sdk = new NodeSDK({
-	traceExporter: new ConsoleSpanExporter(),
+	traceExporter: new OTLPTraceExporter(),
 	metricReader: new PeriodicExportingMetricReader({
-		exporter: new ConsoleMetricExporter(),
+		exporter: new OTLPMetricExporter(),
 	}),
 	instrumentations: [getNodeAutoInstrumentations()],
 });
