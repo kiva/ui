ffc3bc029b7a956020f2bbef25dd34b483a948ab
Casey Dyer <dyersituations@gmail.com>
2024-01-08 15:38:49 -0800

    fix: hide lender avatars in mobile

--- src/components/Teams/TeamGoal.vue
+++ src/components/Teams/TeamGoal.vue
@@ -44,7 +44,7 @@
 
 			<div class="tw-flex tw-flex-row tw-justify-between tw-mt-1 tw-items-center tw-text-secondary">
 				<div class="tw-flex tw-items-center">
-					<div class="tw-shrink-0">
+					<div class="tw-hidden md:tw-flex tw-shrink-0">
 						<img
 							v-for="(lender, i) in participationLendersDisplayed"
 							:key="lender.id"
