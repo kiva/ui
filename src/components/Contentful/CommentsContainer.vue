398568b801609556b8e103004b8633c002558e90
Casey Dyer <dyersituations@gmail.com>
2024-01-29 12:44:39 -0800

    feat: get activity feed token

--- src/components/Contentful/CommentsContainer.vue
+++ src/components/Contentful/CommentsContainer.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import activityFeedTokenQuery from '@/graphql/query/activityFeedToken.graphql';
 import KvCommentsContainer from '~/@kiva/kv-components/vue/KvCommentsContainer';
 
 export default {
@@ -10,6 +11,7 @@ export default {
 	components: {
 		KvCommentsContainer
 	},
+	inject: ['apollo', 'cookieStore'],
 	props: {
 		/**
 		 * Contentful content object
@@ -19,10 +21,30 @@ export default {
 			default: () => ({}),
 		},
 	},
+	data() {
+		return {
+			activityFeedClientToken: undefined,
+		};
+	},
 	computed: {
 		activityId() {
 			return this.content?.dataObject?.activityId;
 		},
+	},
+	methods: {
+		getActivityFeedToken: async () => {
+			this.apollo.query({
+				query: activityFeedTokenQuery,
+				variables: {
+					visitorId: this.cookieStore.get('uiv'),
+				}
+			}).then(response => {
+				this.activityFeedClientToken = response?.data?.activityFeedClientToken?.token;
+			});
+		},
+	},
+	mounted() {
+		this.getActivityFeedToken();
 	},
 };
 </script>
