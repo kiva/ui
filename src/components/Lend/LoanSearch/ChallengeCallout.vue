43903b71f384fdcb5b5b17ad07ec56d0224676f9
Christian Bedon <christian.bedon@contractors.kiva.org>
2024-04-15 16:28:49 -0500

    fix: toast design

--- src/components/Lend/LoanSearch/ChallengeCallout.vue
+++ src/components/Lend/LoanSearch/ChallengeCallout.vue
@@ -1,76 +1,80 @@
 <template>
-	<div v-if="showAddedToCartMessage">
-		<kv-toast
-			ref="toastRef"
-			class="tw-fixed tw-top-9 md:tw-top-11 tw-left-0 tw-right-0 tw-z-banner toast-container"
-		>
-			<template #toastContent>
-				<div class="tw-flex tw-gap-1 tw-items-center">
-					<div class="tw-flex tw-shrink-0">
-						<kv-user-avatar
-							v-for="(p, i) in participants"
-							:key="p.id"
-							:lender-name="p.name"
-							:lender-image-url="p.image.url"
-							is-small
-							class="challenge-avatar md:tw-w-4 md:tw-h-4"
-							:class="{ 'tw--ml-1': i > 0 }"
-							:style="{ 'z-index': participants.length - i }"
-						/>
-					</div>
-					<div class="tw-flex tw-gap-0.5 tw-flex-wrap">
-						<span class="tw-whitespace-nowrap">Added to cart!</span>
-						<span>{{ participantsMessage }}</span>
-						<span
-							v-if="participants.length > 1 && borrowerName"
-							class="data-hj-suppress tw-whitespace-nowrap"
-						>{{ borrowerName }}.</span>
-						<a
-							href="/basket"
-							class="tw-flex"
-							v-kv-track-event="[
-								'basket',
-								'click',
-								'challenge-callout'
-							]"
-						>
-							<span class="tw-whitespace-nowrap">Head to checkout</span>
-							<kv-material-icon class="tw-w-3" :icon="mdiArrowTopRight" />
-						</a>
-					</div>
-				</div>
-			</template>
-		</kv-toast>
-	</div>
-	<div v-else-if="!hideMsg">
-		<kv-page-container class="container">
-			<kv-grid
-				class="tw-grid-cols-12"
+	<div>
+		<div>
+			<kv-toast
+				ref="toastRef"
+				class="tw-fixed tw-top-9 md:tw-top-11 tw-left-0 tw-right-0 tw-z-banner toast-container
+				tw-rounded-t-none md:tw-rounded-t"
+				@close="closeCallback"
 			>
-				<div class="tw-col-span-12 tw-w-full">
-					<div class="info tw-w-full tw-flex tw-items-center tw-justify-between">
-						<img
-							v-if="shareLenderImage && shareLenderName"
-							:alt="`${shareLenderName} image`"
-							:src="shareLenderImage"
-							class="md:tw-w-4 md:tw-h-4 tw-w-6 tw-h-6 tw-rounded-full data-hj-suppress"
-						>
-						<p class="tw-text-lg data-hj-suppress">
-							{{ headerCallout }} <a :href="teamLink">{{ teamName }}</a> hit their goal
-						</p>
-						<button
-							class="tw-flex"
-							@click="$emit('close')"
-						>
-							<kv-material-icon
-								class="tw-h-3 tw-w-3"
-								:icon="mdiClose"
+				<template #toastContent>
+					<div v-if="showAddedToCartMessage" class="tw-flex tw-gap-1 tw-items-center">
+						<div class="tw-flex tw-shrink-0">
+							<kv-user-avatar
+								v-for="(p, i) in participants"
+								:key="p.id"
+								:lender-name="p.name"
+								:lender-image-url="p.image.url"
+								is-small
+								class="challenge-avatar md:tw-w-4 md:tw-h-4"
+								:class="{ 'tw--ml-1': i > 0 }"
+								:style="{ 'z-index': participants.length - i }"
 							/>
-						</button>
+						</div>
+						<div class="tw-flex tw-gap-0.5 tw-flex-wrap">
+							<span class="tw-whitespace-nowrap">Added to cart!</span>
+							<span>{{ participantsMessage }}</span>
+							<span
+								v-if="participants.length > 1 && borrowerName"
+								class="data-hj-suppress tw-whitespace-nowrap"
+							>{{ borrowerName }}.</span>
+							<a
+								href="/basket"
+								class="tw-flex"
+								v-kv-track-event="[
+									'basket',
+									'click',
+									'challenge-callout'
+								]"
+							>
+								<span class="tw-whitespace-nowrap">Head to checkout</span>
+								<kv-material-icon class="tw-w-3" :icon="mdiArrowTopRight" />
+							</a>
+						</div>
 					</div>
-				</div>
-			</kv-grid>
-		</kv-page-container>
+				</template>
+			</kv-toast>
+		</div>
+		<div v-if="!hideMsg">
+			<kv-page-container class="container">
+				<kv-grid
+					class="tw-grid-cols-12"
+				>
+					<div class="tw-col-span-12 tw-w-full">
+						<div class="info tw-w-full tw-flex tw-items-center tw-justify-between">
+							<img
+								v-if="shareLenderImage && shareLenderName"
+								:alt="`${shareLenderName} image`"
+								:src="shareLenderImage"
+								class="md:tw-w-4 md:tw-h-4 tw-w-6 tw-h-6 tw-rounded-full data-hj-suppress"
+							>
+							<p class="tw-text-lg data-hj-suppress">
+								{{ headerCallout }} <a :href="teamLink">{{ teamName }}</a> hit their goal
+							</p>
+							<button
+								class="tw-flex"
+								@click="$emit('close')"
+							>
+								<kv-material-icon
+									class="tw-h-3 tw-w-3"
+									:icon="mdiClose"
+								/>
+							</button>
+						</div>
+					</div>
+				</kv-grid>
+			</kv-page-container>
+		</div>
 	</div>
 </template>
 
@@ -162,10 +166,18 @@ export default {
 	watch: {
 		showAddedToCartMessage(val) {
 			if (val) {
-				setTimeout(() => {
-					this.$refs.toastRef.show();
-				}, 500);
+				this.$refs.toastRef.show();
 			}
+		}
+	},
+	methods: {
+		closeCallback() {
+			this.$emit('close');
+		}
+	},
+	mounted() {
+		if (this.showAddedToCartMessage) {
+			this.$refs.toastRef.show();
 		}
 	}
 };
@@ -192,10 +204,6 @@ export default {
 
 .toast-container >>> div[data-test="tip-message"]{
 	@apply tw-mx-0;
-}
-
-.toast-container >>> div[data-test="tip-message"] > div:first-child {
-	@apply tw-px-0;
 }
 
 .toast-container >>> div.tw-shadow > :first-child {
