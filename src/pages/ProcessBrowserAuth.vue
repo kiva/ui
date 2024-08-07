0c8a220857dd84c6a02756f05d6aad3baf60b5e5
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2024-08-07 16:20:47 -0700

    fix: mfa api token auth requires using authorize call, not checkSession MP-479

--- src/pages/ProcessBrowserAuth.vue
+++ src/pages/ProcessBrowserAuth.vue
@@ -5,42 +5,21 @@
 <script>
 import store2 from 'store2';
 
-function checkHashSuccess(hash) {
-	if (hash.indexOf('error') > -1) {
-		return false;
-	}
-	if (hash.indexOf('access_token') === -1
-		&& hash.indexOf('id_token') === -1
-		&& hash.indexOf('refresh_token') === -1) {
-		return false;
-	}
-	return true;
-}
-
 export default {
 	name: 'ProcessBrowserAuth',
 	inject: ['kvAuth0'],
 	mounted() {
-		const hashKey = 'auth0.browser_hash';
+		const { state } = this.$route.query;
+		if (state) {
+			const auth0State = store2.session('auth0.state');
+			if (auth0State === state) {
+				const redirect = store2.session('auth0.redirect');
 
-		if (window.location.hash) {
-			const { hash } = window.location;
+				store2.session.remove('auth0.state');
+				store2.session.remove('auth0.redirect');
 
-			if (checkHashSuccess(hash)) {
-				// store hash for after post-auth redirect
-				store2.session(hashKey, hash);
-				// post-auth redirect
-				window.location = '/authenticate/ui?doneUrl=/process-browser-auth';
-			} else {
-				// some problem occured, so close the window and let normal error handling take over
-				this.kvAuth0.popupCallback({ hash });
+				this.$router.push(`${redirect}${window.location.hash}`);
 			}
-		} else {
-			// fetch & erase stored hash
-			const hash = store2.session(hashKey);
-			store2.session.remove(hashKey);
-			// final callback
-			this.kvAuth0.popupCallback({ hash });
 		}
 	},
 };
