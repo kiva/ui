c8ed11c31447079ce64074e22032f5f0bc6b1c0a
Christian Bedon <christian.bedon@contractors.kiva.org>
2024-08-13 16:22:43 -0500

    feat: set badge fullscreen on click and fade in elements

---
+++ .storybook/stories/AnimatedStars.stories.js
@@ -0,0 +1,19 @@
+import AnimatedStars from '@/components/Thanks/AnimatedStars.vue';
+
+export default {
+  title: 'Components/AnimatedStars',
+  component: AnimatedStars,
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { AnimatedStars },
+  template: '<div class="tw-bg-stone-1 tw-w-12 tw-h-12 tw-relative"><animated-stars v-bind="$props" /></div>',
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  // Define your default args here. For example:
+  // color: 'blue',
+  // size: 'large',
+};
