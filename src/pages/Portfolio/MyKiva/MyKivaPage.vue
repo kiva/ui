469e8e4d04c43ccabae765e6f63aa4ad71677074
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2024-09-18 11:39:20 -0400

    feat: my kiva navigation added

--- src/pages/Portfolio/MyKiva/MyKivaPage.vue
+++ src/pages/Portfolio/MyKiva/MyKivaPage.vue
@@ -1,14 +1,27 @@
 <template>
 	<www-page main-class="tw-bg-secondary">
+		<MyKivaNavigation
+			:visible="showNavigation"
+			:user-balance="userBalance"
+			@navigation-closed="showNavigation = false"
+		/>
 		<kv-page-container>
 			<!-- My Kiva Page Content -->
+			<div class="tw-min-h-screen">
+				<button @click="showNavigation = true;">
+					Show My Kiva Navigation
+				</button>
+			</div>
 		</kv-page-container>
 	</www-page>
 </template>
 
 <script>
+import logReadQueryError from '@/util/logReadQueryError';
+import userInfoQuery from '@/graphql/query/userInfo.graphql';
 import { trackExperimentVersion } from '@/util/experiment/experimentUtils';
 import WwwPage from '@/components/WwwFrame/WwwPage';
+import MyKivaNavigation from '@/components/MyKiva/MyKivaNavigation';
 import KvPageContainer from '~/@kiva/kv-components/vue/KvPageContainer';
 
 const MY_KIVA_EXP_KEY = 'my_kiva_page';
@@ -19,8 +32,30 @@ export default {
 	components: {
 		WwwPage,
 		KvPageContainer,
+		MyKivaNavigation,
 	},
-	mounted() {
+	data() {
+		return {
+			showNavigation: false,
+			userInfo: {},
+		};
+	},
+	computed: {
+		userBalance() {
+			return this.userInfo?.userAccount?.balance ?? '';
+		},
+	},
+	async mounted() {
+		try {
+			const { data } = await this.apollo.query({
+				query: userInfoQuery,
+			});
+
+			this.userInfo = data.my ?? {};
+		} catch (e) {
+			logReadQueryError(e, 'LenderSummary userIdQuery');
+		}
+
 		trackExperimentVersion(
 			this.apollo,
 			this.$kvTrackEvent,
