02ddb2ee625de8a383c9ff713f869e35bd07f60f
Roger Gutierrez <94026278+roger-in-kiva@users.noreply.github.com>
2024-09-24 11:04:20 -0600

    feat: my kiva navigation (#5521)
    * feat: my kiva navigation added
    
    * feat: my kiva navigation tweaks
    
    * fix: async mounted removed
    
    * fix: query updated
    
    * fix: pr comment tweak
    
    * fix: vue3 tweaks

--- src/pages/Portfolio/MyKiva/MyKivaPage.vue
+++ src/pages/Portfolio/MyKiva/MyKivaPage.vue
@@ -1,6 +1,13 @@
 <template>
 	<www-page main-class="tw-bg-secondary">
-		<MyKivaHero />
+		<MyKivaNavigation
+			:visible="showNavigation"
+			:user-balance="userBalance"
+			@navigation-closed="showNavigation = false"
+		/>
+		<MyKivaHero
+			@show-navigation="handleShowNavigation"
+		/>
 		<MyKivaProfile :lender="lender" />
 	</www-page>
 </template>
@@ -8,6 +15,7 @@
 <script>
 import { trackExperimentVersion } from '#src/util/experiment/experimentUtils';
 import WwwPage from '#src/components/WwwFrame/WwwPage';
+import MyKivaNavigation from '#src/components/MyKiva/MyKivaNavigation';
 import myKivaQuery from '#src/graphql/query/myKiva.graphql';
 import MyKivaHero from '#src/components/MyKiva/MyKivaHero';
 import MyKivaProfile from '#src/components/MyKiva/MyKivaProfile';
@@ -18,6 +26,7 @@ export default {
 	name: 'MyKivaPage',
 	inject: ['apollo', 'cookieStore'],
 	components: {
+		MyKivaNavigation,
 		WwwPage,
 		MyKivaHero,
 		MyKivaProfile
@@ -25,13 +34,33 @@ export default {
 	data() {
 		return {
 			lender: null,
+			showNavigation: false,
+			userInfo: {},
 		};
 	},
 	apollo: {
 		query: myKivaQuery,
 		preFetch: true,
 		result(result) {
+			this.userInfo = result.data?.my ?? {};
 			this.lender = result.data?.my?.lender ?? null;
+		},
+	},
+	computed: {
+		userBalance() {
+			return this.userInfo?.userAccount?.balance ?? '';
+		},
+		lenderName() {
+			return this.lender?.name ?? '';
+		},
+		lenderImageUrl() {
+			return this.lender?.image?.url ?? '';
+		},
+	},
+	methods: {
+		handleShowNavigation() {
+			this.showNavigation = true;
+			this.$kvTrackEvent('SecondaryNav top level', 'click', 'MyKiva-Settings-icon');
 		},
 	},
 	mounted() {
