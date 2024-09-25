374f44e5293d0513bff0b54aaffcac54a2d942b8
Casey Dyer <dyersituations@gmail.com>
2024-09-25 10:21:28 -0700

    fix: resolve issue with older carousel component

--- .storybook/stories/KivaClassicSingleCategoryCarousel.stories.js
+++ .storybook/stories/KivaClassicSingleCategoryCarousel.stories.js
@@ -5,36 +5,75 @@ import KvGrid from '@kiva/kv-components/vue/KvGrid';
 import KvPageContainer from '@kiva/kv-components/vue/KvPageContainer';
 
 const queryResult = {
-	"data": {
-		"lend": {
-			"loanChannelsById": [
+	data: {
+		lend: {
+			loanChannelsById: [
 				{
-					"id": 108,
-					"name": "Recommended by lenders",
-					"url": "https://www.dev.kiva.org/lend/recommended-by-lenders",
-					"description": "Although nothing is guaranteed, loans such as these are favored by experienced lenders because they are the most likely to yield a repayment in one month and to be entirely repaid within a year.",
-					"loans": {
-						"values": [
-							{
-								"id": 2413188
-							},
-							{
-								"id": 2411288
-							},
-							{
-								"id": 2406410
-							},
-							{
-								"id": 2406459
-							},
-							{
-								"id": 2406956
-							},
-							{
-								"id": 2408858
-							}
-						]
+					id: 108,
+					name: "Recommended by lenders",
+					url: "https://www.dev.kiva.org/lend/recommended-by-lenders",
+					description: "Although nothing is guaranteed, loans such as these are favored by experienced lenders because they are the most likely to yield a repayment in one month and to be entirely repaid within a year.",
+				}
+			],
+			loan: {
+				id: 1998250,
+				distributionModel: 'partner', // direct, partner, both
+				geocode: {
+					city: "Cranston",
+					state: "RI",
+					country: {
+						name: "Malawi",
+						isoCode: "MW"
 					}
+				},
+				image: {
+					hash: "d5ad26cd7acc24317edc1c04c6250074"
+				},
+				name: "Microloan Foundation Malawi",
+				sector: {
+					name: "Services"
+				},
+				whySpecial: "It helps Lending Partners withstand negative economic impacts of the COVID-19 pandemic.",
+				userProperties: {
+					lentTo: null
+				},
+				use: "this Lending Partner provide loans to women in rural Malawi during the COVID-19 crisis.",
+				status: "fundraising",
+				loanAmount: "250000.00",
+				borrowerCount: 1,
+				anonymizationLevel: "none",
+				fullLoanUse: "A loan of $250,000 helps this Lending Partner provide loans to women in rural Malawi during the COVID-19 crisis.",
+				fundraisingPercent: 0.75,
+				unreservedAmount: '600',
+				loanFundraisingInfo: {
+					fundedAmount: "218950.00",
+					reservedAmount: "0.00",
+					isExpiringSoon: false
+				},
+				plannedExpirationDate: "2020-09-10T19:30:13Z",
+				matchingText: "LISC",
+				matchRatio: 2,
+			}
+		},
+		fundraisingLoans: {
+			values: [
+				{
+					id: 2413188
+				},
+				{
+					id: 2411288
+				},
+				{
+					id: 2406410
+				},
+				{
+					id: 2406459
+				},
+				{
+					id: 2406956
+				},
+				{
+					id: 2408858
 				}
 			]
 		}
