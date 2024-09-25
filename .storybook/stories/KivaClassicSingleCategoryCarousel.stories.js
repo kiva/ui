da61990a579017f5752734373d661a012df0ec4d
Casey Dyer <dyersituations@gmail.com>
2024-09-25 13:00:07 -0700

    fix: cleaned up story mock data

--- .storybook/stories/KivaClassicSingleCategoryCarousel.stories.js
+++ .storybook/stories/KivaClassicSingleCategoryCarousel.stories.js
@@ -3,6 +3,7 @@ import apolloStoryMixin from '../mixins/apollo-story-mixin';
 import cookieStoreStoryMixin from '../mixins/cookie-store-story-mixin';
 import KvGrid from '@kiva/kv-components/vue/KvGrid';
 import KvPageContainer from '@kiva/kv-components/vue/KvPageContainer';
+import loanData from '../mock-data/loan-data-mock';
 
 const queryResult = {
 	data: {
@@ -15,45 +16,7 @@ const queryResult = {
 					description: "Although nothing is guaranteed, loans such as these are favored by experienced lenders because they are the most likely to yield a repayment in one month and to be entirely repaid within a year.",
 				}
 			],
-			loan: {
-				id: 1998250,
-				distributionModel: 'partner', // direct, partner, both
-				geocode: {
-					city: "Cranston",
-					state: "RI",
-					country: {
-						name: "Malawi",
-						isoCode: "MW"
-					}
-				},
-				image: {
-					hash: "d5ad26cd7acc24317edc1c04c6250074"
-				},
-				name: "Microloan Foundation Malawi",
-				sector: {
-					name: "Services"
-				},
-				whySpecial: "It helps Lending Partners withstand negative economic impacts of the COVID-19 pandemic.",
-				userProperties: {
-					lentTo: null
-				},
-				use: "this Lending Partner provide loans to women in rural Malawi during the COVID-19 crisis.",
-				status: "fundraising",
-				loanAmount: "250000.00",
-				borrowerCount: 1,
-				anonymizationLevel: "none",
-				fullLoanUse: "A loan of $250,000 helps this Lending Partner provide loans to women in rural Malawi during the COVID-19 crisis.",
-				fundraisingPercent: 0.75,
-				unreservedAmount: '600',
-				loanFundraisingInfo: {
-					fundedAmount: "218950.00",
-					reservedAmount: "0.00",
-					isExpiringSoon: false
-				},
-				plannedExpirationDate: "2020-09-10T19:30:13Z",
-				matchingText: "LISC",
-				matchRatio: 2,
-			}
+			loan: loanData[0],
 		},
 		fundraisingLoans: {
 			values: [
--- .storybook/stories/KivaMultiCategoryGrid.stories.js
+++ .storybook/stories/KivaMultiCategoryGrid.stories.js
@@ -3,49 +3,12 @@ import apolloStoryMixin from '../mixins/apollo-story-mixin';
 import cookieStoreStoryMixin from '../mixins/cookie-store-story-mixin';
 import LoanCategorySelectorHomeExp from '#src/components/LoanCollections/HomeExp/LoanCategorySelectorHomeExp';
 import KvGrid from '@kiva/kv-components/vue/KvGrid';
+import loanData from '../mock-data/loan-data-mock';
 
 const queryResult = {
 	data: {
 		lend: {
-			loan: {
-				id: 1998250,
-				distributionModel: 'partner', // direct, partner, both
-				geocode: {
-					city: "Cranston",
-					state: "RI",
-					country: {
-						name: "Malawi",
-						isoCode: "MW"
-					}
-				},
-				image: {
-					hash: "d5ad26cd7acc24317edc1c04c6250074"
-				},
-				name: "Microloan Foundation Malawi",
-				sector: {
-					name: "Services"
-				},
-				whySpecial: "It helps Lending Partners withstand negative economic impacts of the COVID-19 pandemic.",
-				userProperties: {
-					lentTo: null
-				},
-				use: "this Lending Partner provide loans to women in rural Malawi during the COVID-19 crisis.",
-				status: "fundraising",
-				loanAmount: "250000.00",
-				borrowerCount: 1,
-				anonymizationLevel: "none",
-				fullLoanUse: "A loan of $250,000 helps this Lending Partner provide loans to women in rural Malawi during the COVID-19 crisis.",
-				fundraisingPercent: 0.75,
-				unreservedAmount: '600',
-				loanFundraisingInfo: {
-					fundedAmount: "218950.00",
-					reservedAmount: "0.00",
-					isExpiringSoon: false
-				},
-				plannedExpirationDate: "2020-09-10T19:30:13Z",
-				matchingText: "LISC",
-				matchRatio: 2,
-			}
+			loan: loanData[0],
 		},
 	}
 };
