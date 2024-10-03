33c9f4f68ec2e5ffccd8cdfe2eb23d243eca4560
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2024-10-03 10:56:16 -0700

    docs: remove outdated comment

--- src/components/15Years/15YearsButton.vue
+++ src/components/15Years/15YearsButton.vue
@@ -46,8 +46,6 @@ export default {
 	methods: {
 		onClick(e) {
 			if (this.tag === 'button' && this.$attrs.type !== 'submit') {
-				// emit a vue event and prevent native event
-				// so we don't have to write @click in our templates
 				e.preventDefault();
 				this.$emit('click', e);
 			}
