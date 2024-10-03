4ee0470ec79494dd030b17cb99f959e33413077b
Christian Bedon <christian.bedon@contractors.kiva.org>
2024-10-03 11:27:29 -0500

    feat: use custom dash

--- src/components/MyKiva/BadgeContainer.vue
+++ src/components/MyKiva/BadgeContainer.vue
@@ -38,6 +38,6 @@ const title = computed(() => {
 <style lang="postcss" scoped>
 .in-progress {
   @apply tw-grayscale tw-rounded-full tw-w-fit tw-bg-secondary;
-  background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='gray' stroke-width='8' stroke-dasharray='23' stroke-dashoffset='0' stroke-linecap='round'/%3e%3c/svg%3e");
+  background-image: url('/src/assets/inline-svgs/my-kiva/dash.svg');
 }
 </style>
