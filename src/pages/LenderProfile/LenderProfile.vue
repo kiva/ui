88876585af8e1b8ab8aa18b816a89cf87dcec2c6
Christian Bedon <christian.bedon@contractors.kiva.org>
2024-10-17 16:04:38 -0500

    feat: add metadata content in lender profile for badge share

--- src/pages/LenderProfile/LenderProfile.vue
+++ src/pages/LenderProfile/LenderProfile.vue
@@ -18,12 +18,20 @@
 </template>
 
 <script>
+import { gql } from 'graphql-tag';
+
 import logReadQueryError from '#src/util/logReadQueryError';
 import WwwPage from '#src/components/WwwFrame/WwwPage';
 import lenderPublicProfileQuery from '#src/graphql/query/lenderPublicProfile.graphql';
 import LenderProfileWrapper from '#src/components/LenderProfile/LenderProfileWrapper';
 import NotFoundWrapper from '#src/components/NotFound/NotFoundWrapper';
 import KvPageContainer from '@kiva/kv-components/vue/KvPageContainer';
+
+const badgeQuery = gql`query contentfulBadgeImage ($badgeKey: String!) {
+	contentful {
+		entries(contentKey: $badgeKey, contentType: "challenge")
+	}
+}`;
 
 export default {
 	name: 'LenderProfile',
@@ -34,7 +42,7 @@ export default {
 		LenderProfileWrapper,
 		NotFoundWrapper,
 	},
-	metaInfo() {
+	head() {
 		return {
 			title: this.pageTitle,
 			meta: [
@@ -95,6 +103,9 @@ export default {
 			publicId: '',
 			allAchievements: [],
 			lenderIsPublic: false,
+			enableBadgeContent: false,
+			badgeImage: null,
+			badgeCategory: '',
 		};
 	},
 	apollo: {
@@ -102,11 +113,15 @@ export default {
 			const currentRoute = route.value ?? route ?? {};
 			const publicId = currentRoute.params?.publicId ?? '';
 
-			return client.query({
-				query: lenderPublicProfileQuery,
-				variables: { publicId }
-			});
-		},
+			const utmCampaign = currentRoute?.query?.utm_campaign ?? '';
+			const loadBadgeInfo = utmCampaign.includes('badge_') && utmCampaign.includes('social_share');
+			const badgeKey = utmCampaign.split('badge_')[1];
+
+			return Promise.all([
+				client.query({ query: lenderPublicProfileQuery, variables: { publicId } }),
+				loadBadgeInfo ? client.query({ query: badgeQuery, variables: { badgeKey } }) : null,
+			]);
+		}
 	},
 	computed: {
 		lenderName() {
@@ -119,22 +134,46 @@ export default {
 			return this.lenderInfo?.loanCount ?? 0;
 		},
 		pageTitle() {
+			if (this.enableBadgeContent) {
+				const checkoutLoans = this.$route.query?.badgeLoans ?? 0;
+				return `${this.lenderName} has supported ${checkoutLoans} to ${this.badgeCategory}`;
+			}
+
 			let title = `Lender > ${this.lenderName}`;
 			if (this.lenderWhereAbouts) title += ` from ${this.lenderWhereAbouts}`;
 			return title;
 		},
 		pageDescription() {
+			if (this.enableBadgeContent) {
+				return `Join ${this.lenderName} in celebrating this milestone`;
+			}
+
 			let description = `${this.lenderName}`;
 			if (this.lenderWhereAbouts) description += ` from ${this.lenderWhereAbouts}`;
 			description += ` has made ${this.loanCount} loan${this.loanCount === 1 ? '' : 's'} on Kiva.`;
 			return description;
 		},
 		seoImageUrl() {
-			return this.lenderInfo?.seoImage?.url ?? '';
+			return this.enableBadgeContent ? this.badgeImage : this.lenderInfo?.seoImage?.url ?? '';
 		},
 		completedAchievements() {
 			return this.allAchievements.filter(achievement => achievement.status === 'COMPLETE');
 		},
+	},
+	methods: {
+		loadBadgeInfo(badgeKey) {
+			const data = this.apollo.readQuery({
+				query: badgeQuery,
+				variables: { badgeKey }
+			});
+
+			const contentfulData = data?.contentful?.entries?.items ?? null;
+			if (contentfulData) {
+				this.enableBadgeContent = true;
+				this.badgeImage = contentfulData?.[0]?.fields?.badgeImage?.fields?.file?.url ?? null;
+				this.badgeCategory = contentfulData?.[0]?.fields?.challengeName ?? '';
+			}
+		}
 	},
 	created() {
 		this.publicId = this.$route?.params?.publicId ?? '';
@@ -151,6 +190,12 @@ export default {
 		this.lenderInfo = cachedLenderInfo?.community?.lender ?? {};
 		this.allAchievements = cachedLenderInfo?.userAchievementProgress?.achievementProgress ?? [];
 		this.lenderIsPublic = !!this.lenderInfo?.id;
+
+		const utmCampaign = this.$route?.query?.utm_campaign ?? '';
+		if (utmCampaign.includes('badge_') && utmCampaign.includes('social_share')) {
+			const badgeKey = utmCampaign.split('badge_')[1];
+			this.loadBadgeInfo(badgeKey);
+		}
 	}
 };
 </script>
