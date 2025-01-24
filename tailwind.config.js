00a32ddcf7a8e4f8fe57f945ea103a0d453802a8
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-01-23 17:04:42 -0800

    fix: include new kv-components dist folder in tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ import { tailwindConfig } from '@kiva/kv-tokens';
 export default {
 	presets: [tailwindConfig],
 	content: [
-		'./node_modules/@kiva/kv-components/**/*.vue',
-		'./node_modules/@kiva/kv-components/utils/**/*.js',
+		'./node_modules/@kiva/kv-components/dist/**/*.js',
 		'./server/**/*.html',
 		'./src/**/*.vue',
 		'./tailwind.purge.safelist.txt',
