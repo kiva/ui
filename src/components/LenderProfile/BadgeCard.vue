8e103c54b0eae3d0a1313df019db51b4d020ba57
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2025-01-28 13:01:21 -0600

    feat: badges section updated in public profile

---
+++ src/components/LenderProfile/BadgeCard.vue
@@ -0,0 +1,104 @@
+<template>
+	<div
+		class="tw-flex tw-flex-col tw-items-center"
+		style="max-width: 160px;"
+	>
+		<div>
+			<div
+				class="tw-relative hover:tw--translate-y-0.5 tw-ease-in-out tw-duration-300"
+				style="width: 120px; height: 170px;"
+			>
+				<div
+					v-if="badge.level > 3"
+					:class="[
+						'badge-card',
+						{ 'last-badge-card': !mouseOver, 'last-badge-card-hover': mouseOver }
+					]"
+				></div>
+				<div
+					v-if="badge.level > 2"
+					:class="[
+						'badge-card',
+						{ 'third-badge-card': !mouseOver, 'third-badge-card-hover': mouseOver }
+					]"
+				></div>
+				<div
+					v-if="badge.level > 1"
+					:class="[
+						'badge-card',
+						{ 'second-badge-card': !mouseOver, 'second-badge-card-hover': mouseOver }
+					]"
+				></div>
+				<div
+					class="badge-card tw-py-3.5 tw-px-1 tw-rotate-0 tw-cursor-pointer"
+					@mouseover="mouseOver = true"
+					@mouseleave="mouseOver = false"
+					@click="$emit('click', badge)"
+				>
+					<img
+						:src="getBadgeImgUrl(badge)"
+						class="tw-mx-auto tw-h-13"
+					>
+				</div>
+			</div>
+		</div>
+		<div class="tw-font-medium tw-text-center tw-pt-3.5">
+			{{ getBadgeTitle(badge) }}
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import useBadgeData from '#src/composables/useBadgeData';
+
+const { getTierBadgeDataByLevel } = useBadgeData();
+
+defineProps({
+	badge: {
+		type: Object,
+		default: () => ({})
+	},
+});
+
+defineEmits(['click']);
+
+const mouseOver = ref(false);
+
+const getBadgeTitle = badge => {
+	const levelData = getTierBadgeDataByLevel(badge, badge.level);
+	return levelData.tierName;
+};
+
+const getBadgeImgUrl = badge => {
+	if (badge.level === 0) {
+		return badge?.contentfulData?.[0]?.imageUrl ?? '';
+	}
+	const badgeData = badge?.contentfulData?.find(data => data.level === badge.level);
+	return badgeData?.imageUrl ?? '';
+};
+</script>
+
+<style lang="postcss" scoped>
+.badge-card {
+    @apply tw-bg-white tw-rounded tw-drop-shadow-lg tw-origin-bottom-right tw-absolute;
+    height: 160px;
+    width: 120px;
+}
+
+.second-badge-card {
+    transform: rotate(-5deg);
+}
+
+.third-badge-card, .second-badge-card-hover {
+    transform: rotate(-10deg);
+}
+
+.last-badge-card, .third-badge-card-hover {
+    transform: rotate(-15deg);
+}
+
+.last-badge-card-hover {
+    transform: rotate(-20deg);
+}
+</style>
--- src/components/LenderProfile/LenderBadges.vue
+++ src/components/LenderProfile/LenderBadges.vue
@@ -1,31 +1,39 @@
 <template>
 	<async-lender-section @visible="fetchUserAchievements">
 		<section v-if="completedBadges.length > 0">
-			<h2
-				v-if="!isLoading"
-				class="data-hj-suppress"
-			>
+			<h2 class="data-hj-suppress">
 				{{ badgesTitle }}
 			</h2>
-			<kv-loading-placeholder
-				v-else
-				style="height: 30px; width: 250px;"
-			/>
-			<badges-list
-				class="tw-my-4"
-				:completed-achievements="completedBadges"
-				:is-loading="isLoading"
-			/>
+			<!-- eslint-disable-next-line max-len -->
+			<div class="tw-w-full tw-inline-flex tw-flex-wrap tw-justify-center lg:tw-justify-start tw-gap-3 md:tw-gap-8 lg:tw-gap-3.5 tw-my-4">
+				<BadgeCard
+					v-for="badge in showedBadges"
+					:key="badge.id"
+					:badge="badge"
+					@click="handleBadgeClicked"
+				/>
+			</div>
 		</section>
+		<BadgeModal
+			v-if="selectedBadgeData"
+			:show="showBadgeModal"
+			:badge="selectedBadgeData"
+			:state="state"
+			:tier="tier"
+			:is-earned-section="true"
+			@badge-modal-closed="handleBadgeModalClosed"
+		/>
 	</async-lender-section>
 </template>
 
 <script setup>
-import BadgesList from '#src/pages/Portfolio/LendingStats/BadgesList';
-import { KvLoadingPlaceholder } from '@kiva/kv-components';
-import { computed, ref, inject } from 'vue';
+import { computed, inject, ref } from 'vue';
+import { defaultBadges } from '#src/util/achievementUtils';
 import useBadgeData from '#src/composables/useBadgeData';
+import BadgeModal from '#src/components/MyKiva/BadgeModal';
+import { STATE_EARNED } from '#src/composables/useBadgeModal';
 import AsyncLenderSection from './AsyncLenderSection';
+import BadgeCard from './BadgeCard';
 
 const props = defineProps({
 	lenderInfo: {
@@ -45,14 +53,57 @@ const {
 	fetchContentfulData,
 	completedBadges,
 } = useBadgeData(apollo);
-const isLoading = ref(true);
-const badgesTitle = computed(() => (props.lenderInfo?.name ? `${props.lenderInfo.name}'s badges` : 'Badges'));
+
+const showBadgeModal = ref(false);
+const selectedBadgeData = ref();
+const state = ref(STATE_EARNED);
+const tier = ref(null);
+
+const sortedTieredBadges = computed(() => {
+	const tieredBadges = [];
+	defaultBadges.forEach(element => {
+		const filteredBadges = completedBadges.value
+			.filter(badge => badge.id === element)
+			.sort((a, b) => b.level - a.level);
+
+		if (filteredBadges.length > 0) {
+			tieredBadges.push(filteredBadges[0]);
+		}
+	});
+	return tieredBadges;
+});
+
+const sortedEventBadges = computed(() => {
+	return completedBadges.value
+		.filter(b => b.achievementData?.tiers?.length === 0)
+		.sort((a, b) => new Date(a.earnedAtDate) - new Date(b.earnedAtDate));
+});
+
+const showedBadges = computed(() => {
+	return [
+		...sortedTieredBadges.value,
+		...sortedEventBadges.value
+	];
+});
+
+const badgesTitle = computed(() => (props.lenderInfo?.name ? `${props.lenderInfo.name}'s achievements` : 'Achievements')); // eslint-disable-line max-len
 
 const fetchUserAchievements = async () => {
 	await Promise.all([
 		fetchAchievementData(apollo, props.publicId),
 		fetchContentfulData(apollo),
 	]);
-	isLoading.value = false;
+};
+
+const handleBadgeClicked = badge => {
+	const selectedTier = badge.achievementData?.tiers?.find(tierEl => tierEl.level === badge.level) ?? null;
+	tier.value = selectedTier;
+	selectedBadgeData.value = badge;
+	showBadgeModal.value = true;
+};
+
+const handleBadgeModalClosed = () => {
+	selectedBadgeData.value = undefined;
+	showBadgeModal.value = false;
 };
 </script>
