eb5892d8e382d6b559ba81002ea698956e1ff558
Roger Gutierrez <94026278+roger-in-kiva@users.noreply.github.com>
2025-01-28 18:22:31 -0600

    feat: public profile badges update (#5767)

--- src/components/LenderProfile/LenderBadges.vue
+++ src/components/LenderProfile/LenderBadges.vue
@@ -1,31 +1,47 @@
 <template>
 	<async-lender-section @visible="fetchUserAchievements">
-		<section v-if="completedBadges.length > 0">
-			<h2
-				v-if="!isLoading"
-				class="data-hj-suppress"
+		<section
+			v-if="completedBadges.length > 0"
+			class="tw-bg-gray-100"
+		>
+			<div
+				class="tw-mx-auto tw-px-2.5 md:tw-px-4 lg:tw-px-8 tw-pt-4"
+				style="max-width: 1200px;"
 			>
-				{{ badgesTitle }}
-			</h2>
-			<kv-loading-placeholder
-				v-else
-				style="height: 30px; width: 250px;"
-			/>
-			<badges-list
-				class="tw-my-4"
-				:completed-achievements="completedBadges"
-				:is-loading="isLoading"
-			/>
+				<h2 class="data-hj-suppress">
+					{{ badgesTitle }}
+				</h2>
+				<!-- eslint-disable-next-line max-len -->
+				<div class="tw-w-full tw-inline-flex tw-flex-wrap tw-justify-center lg:tw-justify-start tw-gap-3 md:tw-gap-8 lg:tw-gap-3.5 tw-my-4">
+					<BadgeCard
+						v-for="badge in showedBadges"
+						:key="badge.id"
+						:badge="badge"
+						@click="handleBadgeClicked"
+					/>
+				</div>
+			</div>
 		</section>
+		<BadgeModal
+			v-if="selectedBadgeData"
+			:show="showBadgeModal"
+			:badge="selectedBadgeData"
+			:state="state"
+			:tier="tier"
+			:is-earned-section="true"
+			@badge-modal-closed="handleBadgeModalClosed"
+		/>
 	</async-lender-section>
 </template>
 
 <script setup>
-import BadgesList from '#src/pages/Portfolio/LendingStats/BadgesList';
-import { KvLoadingPlaceholder } from '@kiva/kv-components';
-import { computed, ref, inject } from 'vue';
+import { computed, inject, ref } from 'vue';
+import { defaultBadges } from '#src/util/achievementUtils';
 import useBadgeData from '#src/composables/useBadgeData';
+import BadgeModal from '#src/components/MyKiva/BadgeModal';
+import { STATE_EARNED } from '#src/composables/useBadgeModal';
 import AsyncLenderSection from './AsyncLenderSection';
+import BadgeCard from './BadgeCard';
 
 const props = defineProps({
 	lenderInfo: {
@@ -45,7 +61,39 @@ const {
 	fetchContentfulData,
 	completedBadges,
 } = useBadgeData(apollo);
-const isLoading = ref(true);
+
+const showBadgeModal = ref(false);
+const selectedBadgeData = ref();
+const state = ref(STATE_EARNED);
+const tier = ref(null);
+
+const sortedTieredBadges = computed(() => {
+	const tieredBadges = [];
+	defaultBadges.forEach(element => {
+		const filteredBadges = completedBadges.value
+			.filter(badge => badge.id === element)
+			.sort((a, b) => b.level - a.level);
+
+		if (filteredBadges.length > 0) {
+			tieredBadges.push(filteredBadges[0]);
+		}
+	});
+	return tieredBadges;
+});
+
+const sortedEventBadges = computed(() => {
+	return completedBadges.value
+		.filter(b => b.achievementData?.tiers?.length === 0)
+		.sort((a, b) => new Date(a.earnedAtDate) - new Date(b.earnedAtDate));
+});
+
+const showedBadges = computed(() => {
+	return [
+		...sortedTieredBadges.value,
+		...sortedEventBadges.value
+	];
+});
+
 const badgesTitle = computed(() => (
 	props.lenderInfo?.name
 		? `${props.lenderInfo.name}'s achievements`
@@ -57,6 +105,17 @@ const fetchUserAchievements = async () => {
 		fetchAchievementData(apollo, props.publicId),
 		fetchContentfulData(apollo),
 	]);
-	isLoading.value = false;
+};
+
+const handleBadgeClicked = badge => {
+	const selectedTier = badge.achievementData?.tiers?.find(tierEl => tierEl.level === badge.level) ?? null;
+	tier.value = selectedTier;
+	selectedBadgeData.value = badge;
+	showBadgeModal.value = true;
+};
+
+const handleBadgeModalClosed = () => {
+	selectedBadgeData.value = undefined;
+	showBadgeModal.value = false;
 };
 </script>
