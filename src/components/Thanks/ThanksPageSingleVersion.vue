e0f77cc23d216f737f336383b4aa81a8aa31841a
Christian Bedon <christian.bedon@contractors.kiva.org>
2025-02-03 16:12:10 -0500

    feat: modify opt-in module for unified ty page

--- src/components/Thanks/ThanksPageSingleVersion.vue
+++ src/components/Thanks/ThanksPageSingleVersion.vue
@@ -1,3 +1,63 @@
 <template>
-	<p>Content goes here</p>
+	<div
+		ref="tyUnifiedVersion"
+		class="tw-bg-eco-green-1 tw-p-3 md:tw-py-4 tw-flex tw-flex-col tw-gap-2.5"
+	>
+		<OptInModule
+			v-if="showOptInModule"
+			:loans="loans"
+			:is-guest="isGuest"
+			:number-of-badges="numberOfBadges"
+			:only-donations="onlyDonations"
+			class="print:tw-hidden"
+		/>
+	</div>
 </template>
+
+<script setup>
+import {
+	computed,
+} from 'vue';
+import OptInModule from './MyKiva/OptInModule';
+
+const props = defineProps({
+	isGuest: {
+		type: Boolean,
+		default: true,
+	},
+	isOptedIn: {
+		type: Boolean,
+		default: false,
+	},
+	loans: {
+		type: Array,
+		default: () => ([]),
+	},
+	receipt: {
+		type: Object,
+		default: () => ({}),
+	},
+	monthlyDonationAmount: {
+		type: String,
+		default: '',
+	},
+	/**
+	 * [{
+	 *   achievementId: string,
+	 * }]
+	 */
+	badgesAchieved: {
+		type: Array,
+		default: () => ([]),
+	},
+});
+
+const numberOfBadges = computed(() => (props.badgesAchieved.length || 1));
+
+const showOptInModule = computed(() => !props.isOptedIn);
+
+const onlyDonations = computed(() => (
+	(props.receipt && props.receipt?.totals?.itemTotal === props.receipt?.totals?.donationTotal)
+	|| props.monthlyDonationAmount?.length
+));
+</script>
