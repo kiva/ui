a15456b9014902a7b1d1e0a87303866b43d8c952
Roger Gutierrez <94026278+roger-in-kiva@users.noreply.github.com>
2025-02-03 15:29:47 -0600

    feat: commenting module added to unified ty page (#5771)
    * feat: commenting module added to unified ty page
    
    * fix: closing tag fixed
    
    * feat: catch added to tip message composable

--- src/components/Thanks/ThanksPageSingleVersion.vue
+++ src/components/Thanks/ThanksPageSingleVersion.vue
@@ -1,3 +1,37 @@
 <template>
-	<p>Content goes here</p>
+	<div class="tw-bg-eco-green-1 tw-p-3 md:tw-py-4 tw-flex tw-flex-col tw-gap-2.5">
+		<div class="content-box tw-mx-auto">
+			<div
+				v-if="!isGuest"
+			>
+				<LoanComment
+					v-for="loan in loans"
+					:key="loan.id"
+					:loan="loan"
+					class="tw-mb-2.5"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
+
+<script lang="ts" setup>
+import LoanComment from '#src/components/Thanks/SingleVersion/LoanComment';
+
+defineProps({
+	isGuest: {
+		type: Boolean,
+		default: true,
+	},
+	loans: {
+		type: Array,
+		default: () => ([]),
+	},
+});
+</script>
+
+<style lang="postcss" scoped>
+.content-box {
+	max-width: 620px;
+}
+</style>
