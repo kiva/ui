a2ba9378544b6a609a894d5d007a491886419776
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-03-13 17:02:55 -0700

    test: init app config for live loan fetch tests

--- test/unit/specs/server/live-loan-fetch.spec.js
+++ test/unit/specs/server/live-loan-fetch.spec.js
@@ -1,3 +1,4 @@
+const { initConfig } = require('../../../../server/util/config');
 const fetch = require('../../../../server/util/fetch');
 const fetchLoansByType = require('../../../../server/util/live-loan/live-loan-fetch');
 
@@ -41,7 +42,9 @@ describe('live-loan-fetch', () => {
 			expect(sortBy).toEqual(expectedSort);
 		}
 
-		beforeEach(() => {
+		beforeEach(async () => {
+			// Initialize app configuration
+			await initConfig();
 			// Suppress console warnings
 			jest.spyOn(console, 'warn').mockImplementation(jest.fn());
 		});
