37e0995a10c4871573fe11dd7f846878c045a5ae
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-03-14 09:31:01 -0700

    fix: only set user-agent header if configured

--- server/util/fetch.js
+++ server/util/fetch.js
@@ -3,13 +3,19 @@ import config from './config.js';
 
 export default function fetch(url, options) {
 	const onVm = url.indexOf('vm') > -1;
-	return minipassFetch(url, {
+
+	const opts = {
 		// fix request blocked b/c of self-signed certificate on dev-vm.
 		strictSSL: !onVm,
 		...options,
-		headers: {
+	};
+
+	if (config.server.userAgent) {
+		opts.headers = {
 			'User-Agent': config.server.userAgent,
-			...options?.headers,
-		}
-	});
+			...opts.headers,
+		};
+	}
+
+	return minipassFetch(url, opts);
 }
