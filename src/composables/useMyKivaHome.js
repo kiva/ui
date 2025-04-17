acdcdd8cdde6f977a6e8a45291809852518fc178
Christian Bedon <christian.bedon@contractors.kiva.org>
2025-04-17 18:04:29 -0500

    fix: missing mixin and composable

---
+++ src/composables/useMyKivaHome.js
@@ -0,0 +1,43 @@
+import {
+	ref,
+	onMounted,
+} from 'vue';
+import { getIsMyKivaEnabled, MY_KIVA_FOR_ALL_USERS_KEY } from '#src/util/myKivaUtils';
+import { readBoolSetting } from '#src/util/settingsUtils';
+import logFormatter from '#src/util/logFormatter';
+import myKivaForAllUsersQuery from '#src/graphql/query/shared/myKivaForAllUsers.graphql';
+
+export default function useMyKivaHome(apollo, $kvTrackEvent) {
+	const redirectToMyKivaHomepage = ref(false);
+	const myKivaFlagEnabled = ref(false);
+	const userData = ref(false);
+
+	const fetchUserData = () => {
+		apollo.readQuery({
+			query: myKivaForAllUsersQuery,
+		}).then(data => {
+			userData.value = data?.my ?? null;
+			myKivaFlagEnabled.value = readBoolSetting(data, MY_KIVA_FOR_ALL_USERS_KEY);
+		}).catch(e => {
+			logFormatter(e, 'Modal ATB User Data');
+		});
+	};
+
+	onMounted(() => {
+		fetchUserData();
+
+		console.log('hello from useMyKivaHome');
+
+		redirectToMyKivaHomepage.value = getIsMyKivaEnabled(
+			apollo,
+			$kvTrackEvent,
+			userData.value?.userPreferences,
+			userData.value?.my?.loans?.totalCount ?? 0,
+			myKivaFlagEnabled.value,
+		);
+	});
+
+	return {
+		redirectToMyKivaHomepage,
+	};
+}
---
+++ src/plugins/my-kiva-homepage-mixin.js
@@ -0,0 +1,27 @@
+import myKivaForAllUsersQuery from '#src/graphql/query/shared/myKivaForAllUsers.graphql';
+import { getIsMyKivaEnabled, MY_KIVA_FOR_ALL_USERS_KEY } from '#src/util/myKivaUtils';
+import { readBoolSetting } from '#src/util/settingsUtils';
+
+export default {
+	data() {
+		return {
+			redirectToMyKivaHomepage: false,
+		};
+	},
+	created() {
+		const myKivaData = this.apollo.readQuery({
+			query: myKivaForAllUsersQuery,
+		});
+
+		const userData = myKivaData?.my ?? null;
+		const myKivaFlagEnabled = readBoolSetting(myKivaData, MY_KIVA_FOR_ALL_USERS_KEY);
+
+		this.redirectToMyKivaHomepage = getIsMyKivaEnabled(
+			this.apollo,
+			this.$kvTrackEvent,
+			userData?.userPreferences,
+			userData?.my?.loans?.totalCount ?? 0,
+			myKivaFlagEnabled,
+		);
+	}
+};
