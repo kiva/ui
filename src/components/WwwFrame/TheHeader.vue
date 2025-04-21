8b1816bb49df82597cfb1f7d64800b1f1f898992
Roger Gutierrez <rogelio.gutierrez@contractors.kiva.org>
2025-04-21 15:42:29 -0600

    feat: sticky header for lg screens only

--- src/components/WwwFrame/TheHeader.vue
+++ src/components/WwwFrame/TheHeader.vue
@@ -2,7 +2,7 @@
 	<header
 		class="tw-transition-all tw-duration-1000 tw-ease-in-out"
 		:class="{
-			'md:tw-sticky tw-z-banner tw-w-full tw-top-0' : enableBasketExperiment,
+			'lg:tw-sticky tw-z-banner tw-w-full tw-top-0' : enableBasketExperiment,
 		}"
 	>
 		<nav
