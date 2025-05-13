16581ae5c6ecad379b80995911133a73c0e32d38
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-05-13 16:46:36 -0700

    refactor: remove duplicate values and adjust config inheritance to local <- k8s-local <- dynamic

--- config/selectConfig.js
+++ config/selectConfig.js
@@ -2,10 +2,10 @@ import fs from 'fs';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 
-export default async function selectConfig(env = 'index') {
+export default async function selectConfig(env = 'dynamic') {
 	const configPath = resolve(dirname(fileURLToPath(import.meta.url)), `./${env}.js`);
 	if (fs.existsSync(configPath)) {
 		return (await import(configPath)).default;
 	}
-	throw new Error(`Unknown environment '${env}'`);
+	throw new Error(`Unknown config file '${env}'`);
 }
