7eb4debc51fe917079d90ff7123065eff02c9da9
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-06-12 13:52:13 -0700

    fix: use loading placeholder for basket count

--- src/esiTags/globalData/basketCount.js
+++ src/esiTags/globalData/basketCount.js
@@ -1,15 +1,8 @@
 import { gql } from 'graphql-tag';
-import numeral from 'numeral';
 
 export const basketCountFragment = gql`fragment BasketCount on Shop {
 	id
 	nonTrivialItemCount
-	basket {
-		id
-		totals {
-			itemTotal
-		}
-	}
 }`;
 
 /**
@@ -21,11 +14,6 @@ export function basketCountData(data, cookieStore) {
 	// Set CSS variables for the basket count/total
 	const userData = {};
 	const basketCount = data?.shop?.nonTrivialItemCount ?? 0;
-	const basketItemTotal = numeral(data?.shop?.basket?.totals?.itemTotal ?? 0);
-	// Replicating logic from TheHeader.vue for showing total instead of count if hasLargeBasket is true (> $500)
-	const basketNumber = basketItemTotal.value() > 500 ? basketItemTotal.format('$0,0') : basketCount;
-	userData['basket-count'] = `"${basketCount}"`;
-	userData['basket-number'] = `"${basketNumber}"`;
 	if (!basketCount) {
 		userData['basket-count-display'] = 'none';
 	}
@@ -33,7 +21,6 @@ export function basketCountData(data, cookieStore) {
 	// Set CSS variables for the corporate basket count
 	const lcaLoanCount = cookieStore.get('lcaid') ? 1 : 0;
 	const corporateBasketCount = Math.max(basketCount - lcaLoanCount, 0);
-	userData['corporate-basket-count'] = `"${corporateBasketCount}"`;
 	if (!corporateBasketCount) {
 		userData['corporate-basket-count-display'] = 'none';
 	}
