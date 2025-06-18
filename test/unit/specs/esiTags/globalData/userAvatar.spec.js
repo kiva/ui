1f89833038bcf54bdde6f977ef910724ac05d150
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2025-06-17 17:43:02 -0700

    fix: add alt text for user avatar

--- test/unit/specs/esiTags/globalData/userAvatar.spec.js
+++ test/unit/specs/esiTags/globalData/userAvatar.spec.js
@@ -20,7 +20,7 @@ describe('userAvatarData', () => {
 			}
 		};
 		const result = userAvatarData(data);
-		expect(result['user-avatar']).toBe('url("https://www.kiva.org/img.jpg")');
+		expect(result['user-avatar']).toBe('url("https://www.kiva.org/img.jpg") / "My portfolio"');
 		expect(result['user-avatar-legacy-display']).toBe('none');
 		expect(result).not.toHaveProperty('user-avatar-display');
 	});
@@ -38,7 +38,7 @@ describe('userAvatarData', () => {
 			}
 		};
 		const result = userAvatarData(data);
-		expect(result['user-avatar']).toBe('url("https://www.kiva.org/726677.jpg")');
+		expect(result['user-avatar']).toBe('url("https://www.kiva.org/726677.jpg") / "My portfolio"');
 		expect(result['user-avatar-display']).toBe('none');
 		expect(result).not.toHaveProperty('user-avatar-legacy-display');
 	});
@@ -46,24 +46,24 @@ describe('userAvatarData', () => {
 	it('handles missing image url gracefully', () => {
 		const data = { my: { lender: { image: { id: 'img123' } } } };
 		const result = userAvatarData(data);
-		expect(result['user-avatar']).toBe('url("")');
+		expect(result['user-avatar']).toBe('url("") / "My portfolio"');
 	});
 
 	it('handles missing image object gracefully', () => {
 		const data = { my: { lender: {} } };
 		const result = userAvatarData(data);
-		expect(result['user-avatar']).toBe('url("")');
+		expect(result['user-avatar']).toBe('url("") / "My portfolio"');
 	});
 
 	it('handles missing lender gracefully', () => {
 		const data = { my: {} };
 		const result = userAvatarData(data);
-		expect(result['user-avatar']).toBe('url("")');
+		expect(result['user-avatar']).toBe('url("") / "My portfolio"');
 	});
 
 	it('handles missing my gracefully', () => {
 		const data = {};
 		const result = userAvatarData(data);
-		expect(result['user-avatar']).toBe('url("")');
+		expect(result['user-avatar']).toBe('url("") / "My portfolio"');
 	});
 });
