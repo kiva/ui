d5dbda5f62a275ed008c4dda0d651d32b399cc6a
Christian Bedon <christian.bedon@contractors.kiva.org>
2025-07-02 15:13:17 -0500

    refactor: loan next steps

--- src/components/Thanks/LoanNextSteps.vue
+++ src/components/Thanks/LoanNextSteps.vue
@@ -6,84 +6,95 @@
 				:key="index"
 				class="step-item"
 				:class="{ 'completed': index < currentStep,
-					'!tw-opacity-full !tw-animation-none': noAnimation }"
+					'!tw-opacity-full !tw-animation-none': noAnimation}"
 				:style="getDelayStyle(index)"
 			>
-				<div class="step" :class="{'!tw-border-2 !tw-border-action': index === currentStep}">
+				<div
+					v-if="index >= 0 && index < steps.length" class="line"
+					:class="getLineClasses(index)"
+				></div>
+				<div
+					class="step" :class="{'!tw-border-2 !tw-border-action': index === currentStep}"
+				>
 					<span v-if="index < currentStep" class="tw-flex">
 						<kv-material-icon class="tw-text-white tw-w-1.5 tw-h-1.5" :icon="mdiCheckBold" />
 					</span>
 				</div>
-				<div
-					v-if="index >= 0 && index < steps.length - 1" class="horizontal-line tw-hidden lg:tw-block"
-					:class="{ 'completed': index < currentStep, 'next': index === currentStep }"
-				></div>
 				<div class="step-text">
 					<span v-html="step.text"></span>
 				</div>
-				<div
-					v-if="index > 0 && index < steps.length" class="line lg:tw-hidden"
-					:class="{ 'completed': index < currentStep, 'next': index === currentStep }"
-				></div>
 			</li>
 		</ul>
 	</div>
 </template>
 
-<script>
+<script setup>
 import { mdiCheckBold } from '@mdi/js';
 import { KvMaterialIcon } from '@kiva/kv-components';
+import { computed, onMounted, inject } from 'vue';
+import useIsMobile from '#src/composables/useIsMobile';
 
-export default {
-	name: 'LoanNextSteps',
-	components: {
-		KvMaterialIcon
+const props = defineProps({
+	weeksToRepay: {
+		type: String,
+		default: ''
 	},
-	props: {
-		weeksToRepay: {
-			type: String,
-			default: ''
-		},
-		currentStep: {
-			type: Number,
-			default: 1
-		},
-		noAnimation: {
-			type: Boolean,
-			default: false
-		},
-		repaymentsStarted: {
-			type: Boolean,
-			default: false
-		},
-		loanId: {
-			type: Number,
-			default: null
-		},
+	currentStep: {
+		type: Number,
+		default: 1
 	},
-	data() {
-		return {
-			mdiCheckBold,
-			steps: [
-				{ text: 'Your contribution is received' },
-				{ text: 'Their loan is funded' },
-				{ text: 'They used the money to improve their life' },
-				// eslint-disable-next-line max-len
-				{ text: `${this.repaymentsStarted ? `First repayment in <br /> <span class="tw-text-action"> ${this.weeksToRepay} </span>` : 'They completed their repayments'}` }
-			]
-		};
+	noAnimation: {
+		type: Boolean,
+		default: false
 	},
-	methods: {
-		getDelayStyle(index) {
-			return {
-				animationDelay: `${index * 1}s`
-			};
-		}
+	repaymentsStarted: {
+		type: Boolean,
+		default: false
 	},
-	mounted() {
-		this.$kvTrackEvent('borrower-profile', 'view', 'next-steps', this.loanId);
+	loanId: {
+		type: Number,
+		default: null
 	}
+});
+
+const TABLET_BREAKPOINT = 769;
+
+defineEmits(['open-side-sheet']);
+const { isMobile } = useIsMobile(TABLET_BREAKPOINT);
+const $kvTrackEvent = inject('$kvTrackEvent');
+
+const steps = [
+	{ text: 'Your contribution is received' },
+	{ text: 'Their loan is funded' },
+	{ text: 'They used the money to improve their life' },
+	// eslint-disable-next-line max-len
+	{ text: `${props.repaymentsStarted ? `First repayment in <span class="tw-text-action"> ${props.weeksToRepay} </span>` : 'They completed their repayments'}` }
+];
+
+const getDelayStyle = index => {
+	return {
+		animationDelay: `${index * 1}s`,
+		zIndex: index + 1,
+	};
 };
+
+const isCompleted = computed(() => {
+	return steps.length === props.currentStep;
+});
+
+const getLineClasses = index => {
+	return {
+		'next-completed': isCompleted.value,
+		// eslint-disable-next-line max-len
+		next: !isCompleted.value && ((isMobile.value && index <= props.currentStep) || (!isMobile.value && index < props.currentStep)),
+		'tw-hidden': (isMobile.value && index === 0) || (!isMobile.value && index === steps.length - 1)
+	};
+};
+
+onMounted(() => {
+	$kvTrackEvent('borrower-profile', 'view', 'next-steps', props.loanId);
+});
+
 </script>
 
 <style lang="postcss" scoped>
@@ -103,7 +114,7 @@ export default {
 .step {
 	transition: background-color 1s ease;
 	@apply tw-bg-white tw-w-2.5 tw-h-2.5 tw-rounded-full tw-border-gray-200 tw-border-2
-		tw-flex tw-items-center tw-justify-center tw-font-medium tw-relative tw-z-2 tw-min-w-2.5;
+		tw-flex tw-items-center tw-justify-center tw-font-medium tw-relative tw-min-w-2.5;
 }
 
 .step-text {
@@ -114,14 +125,21 @@ export default {
 .line {
 	transition: background-color 1s ease;
 	animation: fadeIn 1s forwards;
-	@apply tw-absolute tw-w-0.5 tw-h-5 tw-bottom-4 tw-left-1 tw-opacity-0 tw-bg-gray-200 tw-z-1;
-}
+	bottom: 1.875rem;
 
-.horizontal-line {
-	transition: background-color 1s ease;
-	animation: fadeIn 1s forwards;
-	width: 138px;
-	@apply tw-absolute tw-w-full tw-h-0.5 tw-top-1 tw-left-8 tw-opacity-0 tw-bg-gray-200;
+	@apply tw-absolute tw-w-0.5 tw-h-5 tw-left-1 tw-opacity-0 tw-bg-gray-200;
+
+	@screen lg {
+		width: 100%;
+		left: 3.75rem;
+		bottom: 0;
+		top: 0.5rem;
+		height: 0.25rem;
+	}
+
+	@screen xl {
+		left: 4.5rem;
+	}
 }
 
 .completed .step {
@@ -129,11 +147,15 @@ export default {
 }
 
 .line.next {
-	background: linear-gradient(180deg, #276A43, #E0E0E0);
+	background: linear-gradient(180deg, #276A43 0%, #E0E0E0 100%);
+
+	@screen lg {
+		background: linear-gradient(90deg, #276A43 0%, #E0E0E0 100%);
+	}
 }
 
-.line.completed, .horizontal-line.completed {
-	background: linear-gradient(90deg, #276A43 0%, #E0E0E0 100%);
+.next-completed {
+	@apply !tw-bg-action;
 }
 
 .buttons button {
