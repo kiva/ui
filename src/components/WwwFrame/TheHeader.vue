3bfe1abe9b57f290e7db9e55ec45e819baf441ee
Christian Bedon <christian.bedon@contractors.kiva.org>
2025-08-26 15:20:47 -0500

    fix: lock page scrolling when menu is open in new header

--- src/components/WwwFrame/TheHeader.vue
+++ src/components/WwwFrame/TheHeader.vue
@@ -16,7 +16,9 @@
 			:user-id="userId"
 			:is-mobile="isMobile"
 			:lender-image-url="profilePic"
+			show-m-g-upsell-link
 			@load-lend-menu-data="loadMenu"
+			@update:menu-open="onMenuChange"
 		/>
 		<nav
 			v-else
@@ -560,7 +562,9 @@
 </template>
 
 <script>
-import { defineAsyncComponent } from 'vue';
+import {
+	defineAsyncComponent, watch
+} from 'vue';
 import {
 	hasLentBeforeCookie,
 	hasDepositBeforeCookie,
@@ -814,6 +818,13 @@ export default {
 			hasLentBefore,
 			hasDepositBefore,
 		});
+
+		watch(
+			() => this.$refs.newExpHeader?.menuOpen,
+			menuOpen => {
+				document.body.style.overflow = this.isMobile && menuOpen ? 'hidden' : '';
+			}
+		);
 
 		this.throttledDetermineIfMobile = _throttle(() => {
 			this.determineIfMobile();
