8a5f11a41301880553ef49b29857630a8dd06ad0
Roger Gutierrez <94026278+roger-in-kiva@users.noreply.github.com>
2025-10-23 15:35:39 -0600

    fix: side sheet ssr fixed (#6376)

--- src/pages/MyKiva/MyKivaPageContent.vue
+++ src/pages/MyKiva/MyKivaPageContent.vue
@@ -111,6 +111,7 @@
 		</section>
 		<BorrowerSideSheetWrapper
 			v-if="showBPSideSheet"
+			class="bp-sidesheet-wrapper"
 			:basket-items="basketItems"
 			:is-adding="isAdding"
 			:kv-track-function="$kvTrackEvent"
@@ -607,6 +608,11 @@ export default {
 			this.animatedSideSheet = isAnimated;
 		},
 	},
+	created() {
+		if (this.sidesheetLoan?.id) {
+			this.showLoanDetails({ id: Number(this.sidesheetLoan.id) }, true, false);
+		}
+	},
 	mounted() {
 		const urlHash = this.$router.currentRoute?.value?.hash;
 
@@ -614,10 +620,6 @@ export default {
 			const elementToScrollTo = document.querySelector(urlHash);
 			const topOfSectionToScrollTo = (elementToScrollTo?.offsetTop ?? 0) - 10 ?? 0;
 			this.smoothScrollTo({ yPosition: topOfSectionToScrollTo, millisecondsToAnimate: 750 });
-		}
-
-		if (this.sidesheetLoan?.id) {
-			this.showLoanDetails({ id: Number(this.sidesheetLoan.id) }, true, false);
 		}
 
 		this.$kvTrackEvent('portfolio', 'view', 'New My Kiva');
@@ -667,5 +669,18 @@ export default {
 
 #recommended-loans :deep(.kv-carousel > div:first-child) {
 	@apply !tw-gap-2;
+}
+
+:deep(.bp-sidesheet-wrapper > div) {
+	max-width: 100% !important;
+	min-height: 100vh;
+
+	@screen md {
+		max-width: 50% !important;
+	}
+
+	@screen xl {
+		max-width: 600px !important;
+	}
 }
 </style>
