c8c0825cf18b87d9b4e448f14d3f6b3c72b3a87b
Casey Dyer <dyersituations@gmail.com>
2025-11-13 18:03:53 -0800

    fix: missed one

--- src/components/LoanFinding/LendingCategorySection.vue
+++ src/components/LoanFinding/LendingCategorySection.vue
@@ -23,7 +23,7 @@
 				:multiple-slides-visible="true"
 				slides-to-scroll="visible"
 				:slide-max-width="singleSlideWidth"
-				:embla-options="{ loop: false, align: 'start', slidesToScroll: 1 }"
+				:embla-options="{ loop: false, align: 'start' }"
 				:controls-top-right="controlsTopRight"
 			>
 				<template v-for="(loan, index) in loans" #[`slide${index}`] :key="loanCardKey(index)">
