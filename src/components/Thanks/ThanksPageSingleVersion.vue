3e322684d1310b96b17b420b8f967192148a48ef
Alejandro Salazar <alejandro.s@berkeley.edu>
2026-01-08 18:57:22 -0500

    fix: add back else-if

--- src/components/Thanks/ThanksPageSingleVersion.vue
+++ src/components/Thanks/ThanksPageSingleVersion.vue
@@ -49,7 +49,7 @@
 				class="print:tw-hidden tw-mb-2.5"
 			/>
 			<JourneyGeneralPrompt
-				v-if="showJourneyModule"
+				v-else-if="showJourneyModule"
 				:loans="loans"
 				:is-guest="isGuest"
 				:is-opted-in="isOptedIn"
