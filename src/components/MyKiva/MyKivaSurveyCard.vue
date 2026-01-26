d82c1b47a7d151ae07babbd17141c072c3325c9c
Jaime Infante <chapinero@msn.com>
2026-01-26 18:31:03 -0500

    feat: mp-2432 update underline color

--- src/components/MyKiva/MyKivaSurveyCard.vue
+++ src/components/MyKiva/MyKivaSurveyCard.vue
@@ -58,6 +58,7 @@ onMounted(() => {
 <style lang="postcss" scoped>
 	.card {
 		background-image: url('/src/assets/images/my-kiva/survey-card-background.jpg');
+
 		--heading-underline-primary: url('/kvui/heading-underline.svg#F8CD69');
 	}
 </style>
