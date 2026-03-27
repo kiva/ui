cda34ba7889c734d656626de3ca48e3394b1e5b5
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2026-03-27 12:03:47 -0700

    test(borrower-profile): add stories for group loans, repeat borrower, and loan type tabs
    Cover regression scenarios from AD-7 planning that are better served
    by Storybook than E2E:
    - LoanStory: group loan (multi-borrower list), repeat borrower
      (previous loan link), single borrower
    - DetailsTabs: partner loan (Lending Partner tab), direct loan with
      trustee (Trustee tab), direct loan without trustee
    - Fixtures: groupLoan, repeatBorrowerLoan, directLoanWithTrustee
    
    AD-7

--- .storybook/stories/BorrowerProfile/DetailsTabs.stories.js
+++ .storybook/stories/BorrowerProfile/DetailsTabs.stories.js
@@ -2,23 +2,39 @@ import DetailsTabs from '#src/components/BorrowerProfile/DetailsTabs';
 
 import apolloStoryMixin from '../../mixins/apollo-story-mixin';
 import cookieStoreStoryMixin from '../../mixins/cookie-store-story-mixin';
-import { fundraisingPartnerLoan, createQueryResult } from './mockLoanFixtures';
+import {
+	createQueryResult,
+	fundraisingPartnerLoan,
+	fundraisingDirectLoan,
+	directLoanWithTrustee,
+} from './mockLoanFixtures';
+
+function detailsTabsStory(loan) {
+	return () => ({
+		components: { DetailsTabs },
+		mixins: [
+			apolloStoryMixin({ queryResult: createQueryResult(loan) }),
+			cookieStoreStoryMixin(),
+		],
+		template: `
+			<details-tabs
+				:loan-id="${loan.id}"
+				name="${loan.name}"
+			/>
+		`,
+	});
+}
 
 export default {
 	title: 'Components/BorrowerProfile/DetailsTabs',
 	component: DetailsTabs,
 };
 
-export const Default = () => ({
-	components: { DetailsTabs },
-	mixins: [
-		apolloStoryMixin({ queryResult: createQueryResult(fundraisingPartnerLoan) }),
-		cookieStoreStoryMixin(),
-	],
-	template: `
-		<details-tabs
-			:loan-id="${fundraisingPartnerLoan.id}"
-			name="${fundraisingPartnerLoan.name}"
-		/>
-	`,
-});
+export const PartnerLoan = detailsTabsStory(fundraisingPartnerLoan);
+PartnerLoan.storyName = 'Partner Loan (Lending Partner Tab)';
+
+export const DirectLoanWithTrustee = detailsTabsStory(directLoanWithTrustee);
+DirectLoanWithTrustee.storyName = 'Direct Loan (Trustee Tab)';
+
+export const DirectLoanNoTrustee = detailsTabsStory(fundraisingDirectLoan);
+DirectLoanNoTrustee.storyName = 'Direct Loan (No Trustee)';
