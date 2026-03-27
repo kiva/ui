82df24d8359ba511b5192543a55b5ed393d97af3
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2026-03-27 12:03:47 -0700

    test(borrower-profile): add stories for group loans, repeat borrower, and loan type tabs
    Cover regression scenarios from AD-7 planning that are better served
    by Storybook than E2E:
    - LoanStory: group loan (multi-borrower list), repeat borrower
      (previous loan link), single borrower
    - DetailsTabs: partner loan (Lending Partner tab), direct loan with
      trustee (Trustee tab), direct loan without trustee
    - Fixtures: groupLoan, repeatBorrowerLoan, directLoanWithTrustee
    
    AD-7

---
+++ .storybook/stories/BorrowerProfile/LoanStory.stories.js
@@ -0,0 +1,45 @@
+import LoanStory from '#src/components/BorrowerProfile/LoanStory';
+
+import apolloStoryMixin from '../../mixins/apollo-story-mixin';
+import cookieStoreStoryMixin from '../../mixins/cookie-store-story-mixin';
+import kvAuth0StoryMixin from '../../mixins/kv-auth0-story-mixin';
+import {
+	createQueryResult,
+	fundraisingPartnerLoan,
+	groupLoan,
+	repeatBorrowerLoan,
+	createMockLoan,
+} from './mockLoanFixtures';
+
+function loanStoryStory(loan) {
+	return () => ({
+		components: { LoanStory },
+		mixins: [
+			apolloStoryMixin({ queryResult: createQueryResult(loan) }),
+			cookieStoreStoryMixin(),
+			kvAuth0StoryMixin,
+		],
+		setup() {
+			return { loan };
+		},
+		template: `<loan-story :loan="loan" />`,
+	});
+}
+
+export default {
+	title: 'Components/BorrowerProfile/LoanStory',
+	component: LoanStory,
+};
+
+export const SingleBorrower = loanStoryStory(fundraisingPartnerLoan);
+
+export const GroupLoan = loanStoryStory(groupLoan);
+GroupLoan.storyName = 'Group Loan (Multiple Borrowers)';
+
+export const RepeatBorrower = loanStoryStory(repeatBorrowerLoan);
+RepeatBorrower.storyName = 'Repeat Borrower (Previous Loan Link)';
+
+export const NoPreviousLoan = loanStoryStory(
+	createMockLoan({ previousLoanId: null }),
+);
+NoPreviousLoan.storyName = 'No Previous Loan';
