d03ad75f918901928b3872b57dd36e641479c385
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2026-06-09 05:55:41 -0600

    refactor: composable pattern

--- src/composables/useMultiMatching.js
+++ src/composables/useMultiMatching.js
@@ -3,13 +3,10 @@ import multiMatchingQuery from '#src/graphql/query/multiMatchingEnabled.graphql'
 import { readBoolSetting } from '#src/util/settingsUtils';
 import logReadQueryError from '#src/util/logReadQueryError';
 
-const enableMultiMatching = ref(false);
-let fetched = false;
-
 export default function useMultiMatching() {
 	const apollo = inject('apollo');
-	if (apollo && !fetched) {
-		fetched = true;
+	const enableMultiMatching = ref(false);
+	if (apollo) {
 		apollo.query({ query: multiMatchingQuery }).then(({ data }) => {
 			enableMultiMatching.value = readBoolSetting(data, 'general.multiMatchingEnabled.value') ?? false;
 		}).catch(e => logReadQueryError(e, 'useMultiMatching multiMatchingEnabled'));
