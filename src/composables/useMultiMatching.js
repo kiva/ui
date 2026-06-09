455a6f97128c69d464b9ad88280c1d7d2b1b1e0c
Eddie Ferrer <eddieferrer@users.noreply.github.com>
2026-06-09 05:25:34 -0600

    fix: refactor composable

--- src/composables/useMultiMatching.js
+++ src/composables/useMultiMatching.js
@@ -1,14 +1,18 @@
 import { ref, inject } from 'vue';
 import multiMatchingQuery from '#src/graphql/query/multiMatchingEnabled.graphql';
 import { readBoolSetting } from '#src/util/settingsUtils';
+import logReadQueryError from '#src/util/logReadQueryError';
+
+const enableMultiMatching = ref(false);
+let fetched = false;
 
 export default function useMultiMatching() {
 	const apollo = inject('apollo');
-	const enableMultiMatching = ref(false);
-	if (apollo) {
+	if (apollo && !fetched) {
+		fetched = true;
 		apollo.query({ query: multiMatchingQuery }).then(({ data }) => {
 			enableMultiMatching.value = readBoolSetting(data, 'general.multiMatchingEnabled.value') ?? false;
-		}).catch(() => {});
+		}).catch(e => logReadQueryError(e, 'useMultiMatching multiMatchingEnabled'));
 	}
 	return { enableMultiMatching };
 }
