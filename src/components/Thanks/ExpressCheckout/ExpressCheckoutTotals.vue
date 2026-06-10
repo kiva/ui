f87dc83340f46be8ddbfe1b7532c78a3bde0bade
crisT <cristhiant@kiva.org>
2026-06-10 10:08:30 -0500

    feat: [MP-2747] - add promotions rows, fix switch and start MP-2900

--- src/components/Thanks/ExpressCheckout/ExpressCheckoutTotals.vue
+++ src/components/Thanks/ExpressCheckout/ExpressCheckoutTotals.vue
@@ -14,6 +14,46 @@
 			{{ formattedItemTotal }}
 		</p>
 
+		<template v-if="hasRedemptionCode">
+			<p>
+				{{ formattedRedemptionApplied }} Promotion
+			</p>
+			<div class="tw-flex tw-items-center tw-justify-end">
+				<span>-{{ formattedRedemptionApplied }}</span>
+				<!-- <button
+					:disabled="removingPromotion"
+					class="tw-ml-1 tw-h-2.5 tw-flex"
+					data-testid="express-checkout-remove-promotion"
+					@click="removePromotion"
+				>
+					<KvMaterialIcon
+						class="tw-w-2.5 tw-text-secondary"
+						:icon="mdiClose"
+					/>
+				</button> -->
+			</div>
+		</template>
+
+		<template v-if="hasBonusCredit">
+			<p>
+				{{ formattedBonusApplied }} Kiva Promotion
+			</p>
+			<div class="tw-flex tw-items-center tw-justify-end">
+				<span>-{{ formattedBonusApplied }}</span>
+				<!-- <button
+					:disabled="removingKivaPromotion"
+					class="tw-ml-1 tw-h-2.5 tw-flex"
+					data-testid="express-checkout-remove-kiva-promotion"
+					@click="removeKivaPromotion"
+				>
+					<KvMaterialIcon
+						class="tw-w-2.5 tw-text-secondary"
+						:icon="mdiClose"
+					/>
+				</button> -->
+			</div>
+		</template>
+
 		<template v-if="isCreditApplied || isCreditAvailable">
 			<KvSwitch
 				v-model="creditAppliedModel"
@@ -21,10 +61,6 @@
 				class="tw-flex"
 			>
 				Apply Kiva Credit
-				<KvLoadingSpinner
-					v-show="changingCredit"
-					class="tw-inline"
-				/>
 			</KvSwitch>
 			<p class="tw-text-right">
 				{{ isCreditApplied ? `-${formattedAppliedCredit}` : '' }}
@@ -46,7 +82,7 @@ import {
 } from 'vue';
 import numeral from 'numeral';
 import { applyKivaCredit, removeKivaCredit, watchBasketTotals } from '@kiva/kv-shop';
-import { KvLoadingSpinner, KvSwitch } from '@kiva/kv-components';
+import { KvSwitch } from '@kiva/kv-components';
 import useTipMessage from '#src/composables/useTipMessage';
 import logFormatter from '#src/util/logFormatter';
 
@@ -68,6 +104,8 @@ const availableCredit = ref('0.00');
 const totalDue = ref('0.00');
 const changingCredit = ref(false);
 const creditAppliedModel = ref(false);
+const redemptionCodeAppliedTotal = ref('0.00');
+const bonusAppliedTotal = ref('0.00');
 let totalsSubscription = null;
 
 const loanLabel = computed(() => {
@@ -81,9 +119,13 @@ const formatMoney = value => numeral(value).format('$0,0.00');
 const formattedItemTotal = computed(() => formatMoney(itemTotal.value));
 const formattedAppliedCredit = computed(() => formatMoney(appliedCredit.value));
 const formattedTotalDue = computed(() => formatMoney(totalDue.value));
+const formattedRedemptionApplied = computed(() => formatMoney(redemptionCodeAppliedTotal.value));
+const formattedBonusApplied = computed(() => formatMoney(bonusAppliedTotal.value));
 
 const isCreditAvailable = computed(() => (numeral(availableCredit.value).value() ?? 0) > 0);
 const isCreditApplied = computed(() => (numeral(appliedCredit.value).value() ?? 0) > 0);
+const hasRedemptionCode = computed(() => (numeral(redemptionCodeAppliedTotal.value).value() ?? 0) > 0);
+const hasBonusCredit = computed(() => (numeral(bonusAppliedTotal.value).value() ?? 0) > 0);
 
 const toggleCredit = async apply => {
 	changingCredit.value = true;
@@ -119,6 +161,8 @@ onMounted(() => {
 			appliedCredit.value = data?.shop?.basket?.totals?.kivaCreditAppliedTotal || '0.00';
 			availableCredit.value = data?.shop?.basket?.totals?.kivaCreditToReapply || '0.00';
 			totalDue.value = data?.shop?.basket?.totals?.creditAmountNeeded || '0.00';
+			redemptionCodeAppliedTotal.value = data?.shop?.basket?.totals?.redemptionCodeAppliedTotal || '0.00';
+			bonusAppliedTotal.value = data?.shop?.basket?.totals?.bonusAppliedTotal || '0.00';
 			creditAppliedModel.value = isCreditApplied.value;
 		},
 		error: e => {
