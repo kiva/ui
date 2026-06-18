77a25608d73c554a5c445deba7e6f0454385a796
Casey Dyer <dyersituations@gmail.com>
2026-06-18 10:02:46 -0700

    fix: add click events to new cta

--- src/components/Portfolio/LoansFirstLoanCta.vue
+++ src/components/Portfolio/LoansFirstLoanCta.vue
@@ -5,10 +5,22 @@
 		</h2>
 		<p class="tw-text-base">
 			Make your first loan today and change a life! Start by lending to a
-			<a href="/lend-by-category/women" class="tw-text-action tw-underline">woman</a>,
-			<a href="/lend-by-category/single-parents" class="tw-text-action tw-underline">single parent</a>,
+			<a
+				href="/lend-by-category/women"
+				class="tw-text-action tw-underline"
+				v-kv-track-event="['portfolio', 'click', 'first-loan-cta-women']"
+			>woman</a>,
+			<a
+				href="/lend-by-category/single-parents"
+				class="tw-text-action tw-underline"
+				v-kv-track-event="['portfolio', 'click', 'first-loan-cta-single-parents']"
+			>single parent</a>,
 			or
-			<a href="/lend-by-category" class="tw-text-action tw-underline">another category</a>.
+			<a
+				href="/lend-by-category"
+				class="tw-text-action tw-underline"
+				v-kv-track-event="['portfolio', 'click', 'first-loan-cta-another-category']"
+			>another category</a>.
 		</p>
 	</div>
 </template>
