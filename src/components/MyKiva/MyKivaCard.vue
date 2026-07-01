1ed5dadfadc03a50afd2826dfa838c8229a8f3e9
crisT <cristhiant@kiva.org>
2026-07-01 17:55:03 -0500

    feat: [MP-2924] - mykivacard persist font

--- src/components/MyKiva/MyKivaCard.vue
+++ src/components/MyKiva/MyKivaCard.vue
@@ -80,6 +80,7 @@
 			<div class="tw-flex tw-flex-col tw-justify-end tw-shrink-0 tw-gap-0.5">
 				<div class="text-content tw-text-primary-inverse">
 					<h2
+						class="tw-text-title"
 						:class="titleClass"
 					>
 						{{ title }}
