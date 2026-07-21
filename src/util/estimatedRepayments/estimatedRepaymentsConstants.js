3cd94a06fb90c66960f889ac93f2d1f0f44bea8b
Casey Dyer <dyersituations@gmail.com>
2026-07-20 18:52:19 -0700

    fix: move constant into component

--- src/util/estimatedRepayments/estimatedRepaymentsConstants.js
+++ src/util/estimatedRepayments/estimatedRepaymentsConstants.js
@@ -40,26 +40,5 @@ export const REPAYMENT_SERIES = [
 	},
 ];
 
-// The legacy page's static disclaimer, ported verbatim. Rendered via v-html (all
-// content is static/trusted here), so inline markup like <em> is allowed.
-export const DISCLAIMER_PARAGRAPHS = [
-	'*Scheduled repayment estimates are calculated based on the <em>original</em> repayment schedule for '
-		+ 'each of your loans. Please note, these estimates don\'t account for delinquencies, defaults, '
-		+ 'currency loss, or for when borrowers pay more than was scheduled and the principal has been '
-		+ 'paid down. Click the amounts in the detailed view to see the advanced repayment details for '
-		+ 'each loan.',
-	'Each month\'s repayment estimation is rounded down to the nearest penny for each loan, and '
-		+ 'therefore actual repayment amounts received may differ from what is estimated. The due date '
-		+ 'for repayments is on the 1st of the month. Many of Kiva\'s Lending Partners begin paying '
-		+ 'lenders earlier, on the settlement date which is the 17th of the month <em>prior</em> to the due date. '
-		+ 'However, repayments may continue post to lender accounts anytime between the 17th and the 1st '
-		+ 'before they\'re considered delinquent. For example: Often a majority of the amount estimated '
-		+ 'for December 1st will be returned to the lender\'s account on November 17th, but some may post '
-		+ 'later than the 17th. The "Due" column shows the actual due dates, not the '
-		+ 'dates when repayments post to your account.',
-	'These figures also don\'t imply that repayments are in any way guaranteed, as lending through '
-		+ 'Kiva involves risk of principal loss.',
-];
-
 export const SETTLEMENT_NOTE = '(Typically repaid by the "Settlement Date," which is the 17th of '
 	+ 'the month prior to the "Due Date")';
