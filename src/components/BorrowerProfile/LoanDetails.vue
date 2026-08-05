573165b32982669ef5a16e37679a753ffd78a12a
Joshua Kiwiet-Pantaleoni <joshuak@kiva.org>
2026-08-04 17:10:16 -0700

    feat(LoanDetails): link dual-statement loans to the repayment schedule
    Restores the "(Additional information)" link under Loan length, which opens
    the repayment schedule lightbox for loans carrying a dual statement note.
    
    AD-416

--- src/components/BorrowerProfile/LoanDetails.vue
+++ src/components/BorrowerProfile/LoanDetails.vue
@@ -18,6 +18,15 @@
 					linkText: 'Loan length'
 				})"
 			/>
+			<div v-if="isDualStatement" class="tw-mb-2.5">
+				<button
+					class="tw-underline tw-text-link"
+					data-testid="bp-loan-detail-dual-statement-info"
+					@click="openRepaymentSchedule"
+				>
+					(Additional information)
+				</button>
+			</div>
 			<description-list-item
 				data-testid="bp-loan-detail-repayment-schedule"
 				:term="'Repayment schedule'"
@@ -82,6 +91,7 @@
 		</dl>
 		<repayment-schedule
 			v-if="displayRepaymentSchedule"
+			ref="repaymentSchedule"
 			:loan-id="loanId"
 			:status="status"
 		/>
@@ -96,6 +106,7 @@ import DescriptionListItem from '#src/components/BorrowerProfile/DescriptionList
 import DescriptionListLoading from '#src/components/BorrowerProfile/DescriptionListLoading';
 import RepaymentSchedule from '#src/components/BorrowerProfile/RepaymentSchedule';
 import { capitalize } from '#src/util/stringParserUtils';
+import { isDualStatementLoan } from '#src/util/repaymentSchedule';
 
 const loanDetailsQuery = gql`query borrowerProfileLoanDetails($loanId: Int!) {
 	lend {
@@ -117,6 +128,7 @@ const loanDetailsQuery = gql`query borrowerProfileLoanDetails($loanId: Int!) {
 				lossLiabilityCurrencyExchange
 			}
 			... on LoanPartner {
+				dualStatementNote
 				partner {
 					id
 					name
@@ -168,8 +180,14 @@ export default {
 			refundedDate: '',
 			defaultedDate: '',
 			endedDate: '',
+			dualStatementNote: '',
 			loading: true,
 		};
+	},
+	methods: {
+		openRepaymentSchedule() {
+			this.$refs.repaymentSchedule?.openLightbox();
+		},
 	},
 	apollo: {
 		lazy: true,
@@ -194,12 +212,16 @@ export default {
 			this.refundedDate = loan?.refundedDate ?? '';
 			this.defaultedDate = loan?.defaultedDate ?? '';
 			this.endedDate = loan?.endedDate ?? '';
+			this.dualStatementNote = loan?.dualStatementNote ?? '';
 			this.loading = false;
 		},
 	},
 	computed: {
 		isPartnerLoan() {
 			return this.partnerName !== '';
+		},
+		isDualStatement() {
+			return isDualStatementLoan(this.dualStatementNote) && this.displayRepaymentSchedule;
 		},
 		loanLength() {
 			return this.isPartnerLoan ? this.loanLenderRepaymentTerm : this.loanTermLenderRepaymentTerm;
