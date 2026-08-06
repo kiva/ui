2c8bcbb0dfb399f951a2183fa4c5dde3907daf57
Christian Bedon <christian.bedon@contractors.kiva.org>
2026-08-06 13:47:16 -0500

    fix: remove unnecesary param to show the modal

--- src/pages/Portfolio/ImpactDashboard/ImpactDashboardPage.vue
+++ src/pages/Portfolio/ImpactDashboard/ImpactDashboardPage.vue
@@ -55,7 +55,7 @@ import TheMyKivaSecondaryMenu from '#src/components/WwwFrame/Menus/TheMyKivaSeco
 import ThePortfolioTertiaryMenu from '#src/components/WwwFrame/Menus/ThePortfolioTertiaryMenu';
 import { gql } from 'graphql-tag';
 import { readBoolSetting } from '#src/util/settingsUtils';
-import { GOAL_STATUS, GOALS_CURRENT_YEAR, hasGoalInReviewStarted } from '#src/composables/useGoalData';
+import { GOAL_STATUS, GOALS_CURRENT_YEAR } from '#src/composables/useGoalData';
 import portfolioQuery from '#src/graphql/query/portfolioQuery.graphql';
 import badgeGoalMixin from '#src/plugins/badge-goal-mixin';
 import { hasLoanFunFactFootnote } from '#src/util/myKivaUtils';
@@ -179,8 +179,7 @@ export default {
 
 		this.showTeamChallenge = teamsChallengeEnable && this.allowedTeams.length > 0;
 
-		this.goalInReviewEnable = (readBoolSetting(portfolioQueryData, 'general.goal_in_review_enable.value') ?? false)
-			&& hasGoalInReviewStarted(this.$route);
+		this.goalInReviewEnable = readBoolSetting(portfolioQueryData, 'general.goal_in_review_enable.value') ?? false;
 
 		this.userPreferences = portfolioQueryData?.my?.userPreferences ?? null;
 
